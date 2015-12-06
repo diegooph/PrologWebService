b3a6c9ff3963af4ebc9622ffe3d4997b53de5f9d
Luiz Felipe <luiz.felipe_95@hotmail.com>
2015-12-05 23:21:51 -0200

    VeiculoDaoImpl terimando e novo jar logica adicionado.

---
+++ src/main/java/br/com/empresa/oprojeto/webservice/dao/VeiculoDaoImpl.java
@@ -0,0 +1,47 @@
+package br.com.empresa.oprojeto.webservice.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.empresa.oprojeto.models.Veiculo;
+import br.com.empresa.oprojeto.webservice.dao.interfaces.VeiculoDao;
+
+public class VeiculoDaoImpl extends ConnectionFactory implements VeiculoDao {
+
+	@Override
+	public List<Veiculo> getVeiculosAtivosByUnidade(Long codUnidade) 
+			throws SQLException {
+		List<Veiculo> veiculos = new ArrayList<>();
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rSet = null;
+		try {
+			conn = getConnection();
+			// TODO: se a nova tabela unidade_veiculo for feita, isso terá que
+			// mudar. Precisará usar Join.
+			stmt = conn.prepareStatement("SELECT * FROM VEICULO WHERE "
+					+ "COD_UNIDADE = ? AND STATUS_ATIVO = TRUE");
+			rSet = stmt.executeQuery();
+			while (rSet.next()) {
+				Veiculo veiculo = createVeiculo(rSet);
+				veiculos.add(veiculo);
+			}
+		} finally {
+			closeConnection(conn, stmt, rSet);
+		}
+		return veiculos;
+	}
+	
+	private Veiculo createVeiculo(ResultSet rSet) throws SQLException {
+		Veiculo veiculo = new Veiculo();
+		veiculo.setPlaca(rSet.getString("PLACA"));
+		veiculo.setModelo(rSet.getString("MODELO"));
+		veiculo.setAtivo(rSet.getBoolean("STATUS_ATIVO"));
+		return veiculo;
+	}
+
+}
