5eefd1d47297aa62ae84148818d3f8410a8046e0
Jean Zart <jeanzart@gmail.com>
2015-12-07 13:34:51 -0200

    Create

--- src/main/java/br/com/empresa/oprojeto/webservice/dao/IndicadorDaoImpl.java
+++ src/main/java/br/com/empresa/oprojeto/webservice/dao/IndicadorDaoImpl.java
@@ -8,12 +8,20 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import br.com.empresa.oprojeto.models.Colaborador;
 import br.com.empresa.oprojeto.models.indicadores.DevolucaoCaixa;
 import br.com.empresa.oprojeto.models.indicadores.Indicador;
 import br.com.empresa.oprojeto.webservice.dao.interfaces.IndicadorDao;
 
 public class IndicadorDaoImpl extends ConnectionFactory implements IndicadorDao {
+	
+	private static final int COD_DEVCX = 1;
+	private static final int COD_DEVNF = 2;
+	private static final int COD_JORNADA = 3;
+	private static final int COD_TEMPO_INTERNO = 4;
+	private static final int COD_TEMPO_EM_ROTA = 5;
+	private static final int COD_TEMPO_LARGADA = 6;
+	private static final int COD_TRACKING = 7;
+	
 
 	@Override
 	public List<Indicador> getJornadaByPeriodo(long cpf, Date dataInicial, 
@@ -45,12 +53,12 @@ public class IndicadorDaoImpl extends ConnectionFactory implements IndicadorDao 
 			stmt = conn.prepareStatement("SELECT * FROM COLABORADOR");
 			rSet = stmt.executeQuery();
 			while (rSet.next()) {
-				DevolucaoCaixa c = createDevCx(rSet);
+				Indicador c = createDevCx(rSet);
 				lIndicador.add(c);
 			}
 		}
 
-
+finally{}
 
 
 
@@ -69,13 +77,16 @@ public class IndicadorDaoImpl extends ConnectionFactory implements IndicadorDao 
 		return null;
 	}
 	
-	private DevolucaoCaixa createDevCx (ResultSet rSet) throws SQLException{
-		DevolucaoCaixa dev = new DevolucaoCaixa();
-		
-		
-		
+	private Indicador createDevCx (ResultSet rSet) throws SQLException{
+		Indicador dev = new DevolucaoCaixa();
+			
 		
 		return dev;		
+	}
+	
+	private void getMeta (Indicador i, int cod_meta){
+		
+		
 	}
 	
 	
