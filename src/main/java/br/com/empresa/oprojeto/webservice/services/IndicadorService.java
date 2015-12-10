4e376915df5a808aa474aa774244d916a453e75a
Luiz Felipe <luiz.felipe_95@hotmail.com>
2015-12-10 18:14:01 -0200

    Indicador Resource e Service criados

--- src/main/java/br/com/empresa/oprojeto/webservice/services/IndicadorService.java
+++ src/main/java/br/com/empresa/oprojeto/webservice/services/IndicadorService.java
@@ -1,5 +1,21 @@
 package br.com.empresa.oprojeto.webservice.services;
 
-public class IndicadorService {
+import java.sql.SQLException;
+import java.time.LocalDate;
 
+import br.com.empresa.oprojeto.models.indicador.IndicadorHolder;
+import br.com.empresa.oprojeto.webservice.dao.IndicadorDaoImpl;
+
+public class IndicadorService {
+	private IndicadorDaoImpl dao = new IndicadorDaoImpl();
+	
+	public IndicadorHolder getIndicadoresByPeriodo(LocalDate dataInicial, 
+			LocalDate dataFinal, long cpf) {
+		try {
+			return dao.getIndicadoresByPeriodo(dataInicial, dataFinal, cpf);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
