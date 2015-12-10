cb671b169855cc4d94908b744c686c13075254e9
Jean Zart <jeanzart@gmail.com>
2015-12-10 19:28:27 -0200

    sfsf

--- src/main/java/br/com/empresa/oprojeto/webservice/rest/IndicadorResource.java
+++ src/main/java/br/com/empresa/oprojeto/webservice/rest/IndicadorResource.java
@@ -1,5 +1,29 @@
 package br.com.empresa.oprojeto.webservice.rest;
 
-public class IndicadorResource {
+import java.time.LocalDate;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import br.com.empresa.oprojeto.models.indicador.IndicadorHolder;
+import br.com.empresa.oprojeto.webservice.services.IndicadorService;
+
+@Path("/indicadores")
+@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
+@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+public class IndicadorResource {
+	private IndicadorService service = new IndicadorService();
+	
+	@POST
+	@Path("/byPeriodo")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public IndicadorHolder getIndicadoresByPeriodo(@FormParam("dataInicial") 
+	LocalDate dataInicial, @FormParam("dataFinal") LocalDate dataFinal, 
+	@FormParam("cpf") long cpf) {
+		return service.getIndicadoresByPeriodo(dataInicial, dataFinal, cpf);
+	}
 }
