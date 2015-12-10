9820fbc667632281689d3c87f982e73adedba1d7
Jean Zart <jeanzart@gmail.com>
2015-12-10 17:24:25 -0200

    commit

--- src/main/java/br/com/empresa/oprojeto/webservice/dao/IndicadorDaoImpl.java
+++ src/main/java/br/com/empresa/oprojeto/webservice/dao/IndicadorDaoImpl.java
@@ -6,6 +6,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,8 +16,14 @@ import br.com.empresa.oprojeto.models.indicador.DevolucaoNfHolder;
 import br.com.empresa.oprojeto.models.indicador.IndicadorHolder;
 import br.com.empresa.oprojeto.models.indicador.ItemDevolucaoCx;
 import br.com.empresa.oprojeto.models.indicador.ItemDevolucaoNf;
+import br.com.empresa.oprojeto.models.indicador.ItemJornadaLiquida;
 import br.com.empresa.oprojeto.models.indicador.ItemTempoInterno;
+import br.com.empresa.oprojeto.models.indicador.ItemTempoLargada;
+import br.com.empresa.oprojeto.models.indicador.ItemTempoRota;
+import br.com.empresa.oprojeto.models.indicador.JornadaLiquidaHolder;
 import br.com.empresa.oprojeto.models.indicador.TempoInternoHolder;
+import br.com.empresa.oprojeto.models.indicador.TempoLargadaHolder;
+import br.com.empresa.oprojeto.models.indicador.TempoRotaHolder;
 import br.com.empresa.oprojeto.models.util.DateUtil;
 import br.com.empresa.oprojeto.models.util.TimeUtils;
 import br.com.empresa.oprojeto.webservice.dao.interfaces.IndicadorDao;
@@ -38,8 +46,16 @@ public class IndicadorDaoImpl extends DataBaseConnection implements IndicadorDao
 			+ "? AND DATA BETWEEN ? AND ? ORDER BY M.DATA";
 
 	private IndicadorHolder indicadorHolder = new IndicadorHolder();
-
+	//TODO: IMPLEMENTAR METODO PARA METAS.
 	private LocalTime metaTempoInternoHoras = LocalTime.now();
+	private LocalTime metaTempoRotaHoras = LocalTime.now();
+	private LocalTime metaTempoLargadaHoras = LocalTime.now();
+	private LocalTime metaJornadaLiquidaHoras = LocalTime.now();
+	private double metaTempoLargadaMapas = 0;
+	private double metaTempoInternoMapas = 0;
+	private double metaTempoRotaMapas = 0;
+	private double metaDevCx = 0;
+	private double metaDevNf = 0;
 
 	@Override
 	public IndicadorHolder getIndicadoresByPeriodo(LocalDate dataInicial, LocalDate dataFinal, long cpf)
@@ -60,6 +76,9 @@ public class IndicadorDaoImpl extends DataBaseConnection implements IndicadorDao
 			createDevCx(rSet);
 			createDevNf(rSet);
 			createTempoInterno(rSet);
+			createTempoRota(rSet);
+			createTempoLargada(rSet);
+			createJornadaLiquida(rSet);
 
 		}
 		finally {
@@ -99,9 +118,10 @@ public class IndicadorDaoImpl extends DataBaseConnection implements IndicadorDao
 		devCaixa.setDevolvidasTotal(devolvidasTotal);
 		devCaixa.setEntreguesTotal(entreguesTotal);
 		devCaixa.setResultadoTotal(devolvidasTotal / carregadasTotal);
+		devCaixa.setMeta(metaDevCx);
 
 		indicadorHolder.setDevCaixa(devCaixa);
-
+		rSet.first();
 	}
 
 	public void createDevNf (ResultSet rSet) throws SQLException{
@@ -132,8 +152,10 @@ public class IndicadorDaoImpl extends DataBaseConnection implements IndicadorDao
 		devNf.setDevolvidasTotal(devolvidasTotal);
 		devNf.setEntreguesTotal(entreguesTotal);
 		devNf.setResultadoTotal(devolvidasTotal / carregadasTotal);
+		devNf.setMeta(metaDevNf);
 
 		indicadorHolder.setDevNf(devNf);
+		rSet.first();
 
 	}
 
@@ -144,7 +166,6 @@ public class IndicadorDaoImpl extends DataBaseConnection implements IndicadorDao
 		int totalMapas = 0;
 		int mapasOk = 0;
 		int mapasNok = 0;
-		double resultado = 0;
 		LocalTime hrSaida;
 		LocalTime tempoInterno;
 
@@ -163,20 +184,124 @@ public class IndicadorDaoImpl extends DataBaseConnection implements IndicadorDao
 			}else{mapasNok = mapasNok + 1;}
 		}
 		tempoInternoHolder.setListItemTempoInterno(listTempoInterno);
+		tempoInternoHolder.setResultado(tempoInternoHolder.getMapasOk() / tempoInternoHolder.getTotalMapas());
+		tempoInternoHolder.setMeta(metaTempoInternoMapas);
+		rSet.first();
+	}
+
+	public void createTempoRota(ResultSet rSet) throws SQLException{
+
+		TempoRotaHolder tempoRotaHolder = new TempoRotaHolder();
+		List<ItemTempoRota> listTempoRota = new ArrayList<>();
+		int totalMapas = 0;
+		int mapasOk = 0;
+		int mapasNok = 0;
+
+		while(rSet.next()){
+			ItemTempoRota itemTempoRota = new ItemTempoRota();
+			itemTempoRota.setData(DateUtil.toLocalDate(rSet.getDate("DATA")));
+			itemTempoRota.setHrEntrada(TimeUtils.toLocalTime((rSet.getTime("HRENTR"))));
+			itemTempoRota.setHrSaida(TimeUtils.toLocalTime(rSet.getTimestamp("HRSAIDA")));
+			// saber o tempo que o caminhão ficou na rua, por isso hora de entrada(volta da rota) = hora de saída( saída para rota)
+			itemTempoRota.setResultado(TimeUtils.differenceBetween(itemTempoRota.getHrEntrada(), itemTempoRota.getHrSaida()));
+			listTempoRota.add(itemTempoRota);
+			totalMapas = totalMapas + 1;
+			if(bateuMeta(metaTempoRotaHoras, itemTempoRota.getResultado())){
+				mapasOk = mapasOk + 1;
+			}else{mapasNok = mapasNok + 1;}
+		}
+		tempoRotaHolder.setListTempoRota(listTempoRota);
+		tempoRotaHolder.setResultado(tempoRotaHolder.getMapasOk() / tempoRotaHolder.getTotalMapas());
+		tempoRotaHolder.setMeta(metaTempoRotaMapas);
+		rSet.first();
+		
+	}
+
+	public void createTempoLargada(ResultSet rSet) throws SQLException{
+
+		TempoLargadaHolder tempoLargadaHolder = new TempoLargadaHolder();
+		List<ItemTempoLargada> listTempoLargada = new ArrayList<>();
+		int totalMapas = 0;
+		int mapasOk = 0;
+		int mapasNok = 0;
+
+		while(rSet.next()){
+			ItemTempoLargada itemTempoLargada = new ItemTempoLargada();
+			itemTempoLargada.setData(DateUtil.toLocalDate(rSet.getDate("DATA")));
+			itemTempoLargada.setHrMatinal(TimeUtils.toLocalTime((rSet.getTime("HRMATINAL"))));
+			itemTempoLargada.setHrSaida(TimeUtils.toLocalTime(rSet.getTimestamp("HRSAIDA")));
+			itemTempoLargada.setResultado(calculaTempoLargada(itemTempoLargada.getHrSaida(), itemTempoLargada.getHrMatinal()));
+			listTempoLargada.add(itemTempoLargada);
+			totalMapas = totalMapas + 1;
+			if(bateuMeta(metaTempoLargadaHoras, itemTempoLargada.getResultado())){
+				mapasOk = mapasOk + 1;
+			}else{mapasNok = mapasNok + 1;}
+		}
+		tempoLargadaHolder.setListTempoLargada(listTempoLargada);
+		tempoLargadaHolder.setResultado(tempoLargadaHolder.getMapasOk() / tempoLargadaHolder.getTotalMapas());
+		tempoLargadaHolder.setMeta(metaTempoLargadaMapas);
+		rSet.first();
+	}		
 
 
+	public void createJornadaLiquida(ResultSet rSet) throws SQLException{
+
+		JornadaLiquidaHolder jornadaLiquidaHolder = new JornadaLiquidaHolder();
+		List<ItemJornadaLiquida> listJornadaLiquida = new ArrayList<>();
+		LocalTime matinal;
+		LocalTime rota;
+		LocalTime tempoInterno;
+		int totalMapas = 0;
+		int mapasOk = 0;
+		int mapasNok = 0;
+
+		while (rSet.next()){
+			
+			tempoInterno = TimeUtils.toLocalTime(rSet.getTime("TEMPOINTERNO"));
+			rota = TimeUtils.differenceBetween(TimeUtils.toLocalTime(rSet.getTimestamp("HRENTR")),
+					TimeUtils.toLocalTime(rSet.getTimestamp("HRSAI")));
+			matinal = calculaTempoLargada(TimeUtils.toLocalTime(rSet.getTimestamp("HRSAI")),
+					TimeUtils.toLocalTime(rSet.getTime("HRMATINAL")));
+			
+			ItemJornadaLiquida itemJornadaLiquida = new ItemJornadaLiquida();
+			itemJornadaLiquida.setData(DateUtil.toLocalDate(rSet.getDate("DATA")));
+			itemJornadaLiquida.setTempoInterno(tempoInterno);
+			itemJornadaLiquida.setTempoRota(rota);
+			itemJornadaLiquida.setTempoLargada(matinal);
+			itemJornadaLiquida.setResultado(TimeUtils.somaHoras(TimeUtils.somaHoras(matinal, rota),tempoInterno));
+			totalMapas = totalMapas + 1;
+			if(bateuMeta(metaJornadaLiquidaHoras, itemJornadaLiquida.getResultado())){
+				mapasOk = mapasOk + 1;
+			}else{ mapasNok = mapasNok + 1;}
+		}
+		
+
+
+
+		rSet.first();
+
+	}
+	
+	private boolean bateuMeta (LocalTime meta, LocalTime resultado){
+		//TODO: IMPLEMENTAR!
+		//if(resultado<=meta){
+		//return true;
+		//} else {
+		return false;
 	}
 
 
+	private LocalTime calculaTempoLargada (LocalTime hrSaida, LocalTime hrMatinal){
 
-private boolean bateuMeta (LocalTime meta, LocalTime resultado){
-	//TODO: IMPLEMENTAR!
-	//if(resultado<meta){
-	//return true;
-	//} else {
-	return false;
+		LocalTime matinal = LocalTime.of(00, 30, 00);
+
+		if(hrMatinal.isAfter(hrSaida)){
+			return matinal;
+		} else {
+			return hrSaida.minus(hrMatinal.getLong(ChronoField.MILLI_OF_DAY),ChronoUnit.MILLIS);
+		}
+	}
 
 
-}
 }
 
