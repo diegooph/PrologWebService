6038fd27e6a09e0c7da16eb15e7451d0f4aa7f47
Jean Zart <jeanzart@gmail.com>
2015-12-14 17:20:42 -0200

    Produtividade Service e Resource

---
+++ src/main/java/br/com/empresa/oprojeto/webservice/rest/ProdutividadeResource.java
@@ -0,0 +1,37 @@
+package br.com.empresa.oprojeto.webservice.rest;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import br.com.empresa.oprojeto.models.produtividade.ItemProdutividade;
+import br.com.empresa.oprojeto.models.util.DateUtils;
+import br.com.empresa.oprojeto.webservice.services.ProdutividadeService;
+
+@Path("/produtividade")
+@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
+@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+public class ProdutividadeResource{
+	private ProdutividadeService service = new ProdutividadeService();
+
+	@POST
+	@Path("/byPeriodo")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public List<ItemProdutividade> getProdutividadeByPeriodo(@FormParam("dataInicial")
+	Date dataInicial, @FormParam("dataFinal") Date dataFinal,
+	@FormParam("cpf") long cpf){
+		return service.getProdutividadeByPeriodo(DateUtils.toLocalDate(dataInicial),
+				DateUtils.toLocalDate(dataFinal), cpf);
+
+	}
+
+}
+
+
+
