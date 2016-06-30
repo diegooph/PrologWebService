9c5ce5c2f6459b55901f87f349968c85e4ddfd4e
Jean Zart <jeanzart@gmail.com>
2016-06-30 18:31:51 -0300

    Agora sim

--- src/main/java/br/com/zalf/prolog/webservice/pneu/relatorios/RelatorioResource.java
+++ src/main/java/br/com/zalf/prolog/webservice/pneu/relatorios/RelatorioResource.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -22,9 +21,9 @@ public class RelatorioResource {
 	
 	@GET
 	@Secured
-	@Path("/resumoSulcos/{codUnidades}")
+	@Path("/resumoSulcos/")
 	public List<Faixa> getQtPneusByFaixaSulco(
-			@PathParam("codUnidades") List<String> codUnidades,
+			@QueryParam("codUnidades") List<String> codUnidades,
 			@QueryParam("status") List<String> status){
 		return service.getQtPneusByFaixaSulco(codUnidades, status);
 	}
