78ae774c2e13728eff49acab24f52830d45aadeb
luizfp <luiz.felipe_95@hotmail.com>
2016-09-06 01:49:13 -0300

    refactor: correção import não encontrado

--- src/test/java/test/FotaTest.java
+++ src/test/java/test/FotaTest.java
@@ -2,12 +2,12 @@ package test;
 
 import org.junit.Test;
 
-import br.com.zalf.prolog.webservice.frota.servico.ServicoService;
+//import br.com.zalf.prolog.webservice.frota.servico.ServicoService;
 import junit.framework.TestCase;
 
 public class FotaTest extends TestCase{
 	
-	private ServicoService service = new ServicoService();
+//	private ServicoService service = new ServicoService();
 	
 	Long cpf = 12345678987L;
 	String token = "b820oonp8l2pm1qo2s0skike1f";
