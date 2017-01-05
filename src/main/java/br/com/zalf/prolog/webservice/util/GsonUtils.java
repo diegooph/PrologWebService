53ab15ec2ba3881a2b12effb9a899570d3d540b3
Jean Zart <jeanzart@gmail.com>
2017-01-05 13:41:45 -0200

    feat(GsonUtils): cadastrado seralializer para as Alternativas

--- src/main/java/br/com/zalf/prolog/webservice/util/GsonUtils.java
+++ src/main/java/br/com/zalf/prolog/webservice/util/GsonUtils.java
@@ -3,10 +3,13 @@ package br.com.zalf.prolog.webservice.util;
 import br.com.zalf.prolog.commons.network.AbstractResponse;
 import br.com.zalf.prolog.commons.network.Response;
 import br.com.zalf.prolog.commons.network.ResponseWithCod;
+import br.com.zalf.prolog.commons.questoes.Alternativa;
 import br.com.zalf.prolog.frota.pneu.servico.Calibragem;
 import br.com.zalf.prolog.frota.pneu.servico.Inspecao;
 import br.com.zalf.prolog.frota.pneu.servico.Movimentacao;
 import br.com.zalf.prolog.frota.pneu.servico.Servico;
+import br.com.zalf.prolog.gente.quiz.AlternativaEscolhaQuiz;
+import br.com.zalf.prolog.gente.quiz.AlternativaOrdenamentoQuiz;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -22,18 +25,24 @@ public class GsonUtils {
 			.enableComplexMapKeySerialization();
 
     static {
-        RuntimeTypeAdapterFactory<Servico> adapterServico = RuntimeTypeAdapterFactory
-                .of(Servico.class)
-                .registerSubtype(Calibragem.class)
-                .registerSubtype(Movimentacao.class)
-                .registerSubtype(Inspecao.class);
+		RuntimeTypeAdapterFactory<Servico> adapterServico = RuntimeTypeAdapterFactory
+				.of(Servico.class)
+				.registerSubtype(Calibragem.class)
+				.registerSubtype(Movimentacao.class)
+				.registerSubtype(Inspecao.class);
 
-        RuntimeTypeAdapterFactory<AbstractResponse> adapterResponse = RuntimeTypeAdapterFactory
+		RuntimeTypeAdapterFactory<Alternativa> adapterAlternativa = RuntimeTypeAdapterFactory
+				.of(Alternativa.class)
+				.registerSubtype(AlternativaEscolhaQuiz.class)
+				.registerSubtype(AlternativaOrdenamentoQuiz.class);
+
+		RuntimeTypeAdapterFactory<AbstractResponse> adapterResponse = RuntimeTypeAdapterFactory
                 .of(AbstractResponse.class)
                 .registerSubtype(Response.class)
                 .registerSubtype(ResponseWithCod.class);
 
         sBuilder.registerTypeAdapterFactory(adapterServico);
+		sBuilder.registerTypeAdapterFactory(adapterAlternativa);
         sBuilder.registerTypeAdapterFactory(adapterResponse);
     }
 
