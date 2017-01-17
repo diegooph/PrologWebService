a73802a57c708eb8c3a640624b98792b79ba256e
Jean Zart <jeanzart@gmail.com>
2017-01-16 23:24:08 -0200

    fix(ContrachequeService): substitui vírgula por ponto antes do parse para Double no campo valor

--- src/main/java/br/com/zalf/prolog/webservice/gente/contracheque/ContrachequeService.java
+++ src/main/java/br/com/zalf/prolog/webservice/gente/contracheque/ContrachequeService.java
@@ -73,7 +73,7 @@ public class ContrachequeService {
                     }else if(tabela.get(i).get(j+1).trim().isEmpty()){
                         return Response.Error("Campo valor não pode estar em branco, linha: " + (i+1) + ", coluna: " + (j+2));
                     }
-                    Double valor = Double.parseDouble(tabela.get(i).get(j+1));
+                    Double valor = Double.parseDouble(tabela.get(i).get(j+1).replace(",", "."));
                     if(valor != 0) {
                         itens.add(createItemImportContracheque(Long.parseLong(tabela.get(i).get(0).replaceAll("[^\\d]", "")), codigos.get(j), descricoes.get(j), subDescricoes.get(j),
                                 valor));
