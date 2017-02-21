aa3e9ea1e21f3e7d9acc3be3e07d691f44658e20
Luiz Felipe <luiz.felipe_95@hotmail.com>
2017-02-21 15:31:14 -0300

    refactor(Relatórios Pneus): insere permissão necessária para acessar o relatório

--- src/main/java/br/com/zalf/prolog/webservice/pneu/relatorios/RelatorioResource.java
+++ src/main/java/br/com/zalf/prolog/webservice/pneu/relatorios/RelatorioResource.java
@@ -4,6 +4,7 @@ import br.com.zalf.prolog.commons.Report;
 import br.com.zalf.prolog.frota.pneu.relatorio.Aderencia;
 import br.com.zalf.prolog.frota.pneu.relatorio.Faixa;
 import br.com.zalf.prolog.frota.pneu.relatorio.ResumoServicos;
+import br.com.zalf.prolog.permissao.pilares.Frota;
 import br.com.zalf.prolog.webservice.interceptors.auth.Secured;
 
 import javax.ws.rs.*;
@@ -13,42 +14,39 @@ import java.sql.SQLException;
 import java.util.List;
 
 @Path("/pneus/relatorios")
+@Secured(permissions = Frota.Pneu.RELATORIOS)
 @Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
 @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
 public class RelatorioResource {
-	
+
 	private RelatorioService service = new RelatorioService();
-	
+
 	@GET
-	@Secured
 	@Path("/resumoSulcos")
 	public List<Faixa> getQtPneusByFaixaSulco(
 			@QueryParam("codUnidades") List<String> codUnidades,
 			@QueryParam("status") List<String> status){
 		return service.getQtPneusByFaixaSulco(codUnidades, status);
 	}
-	
+
 	@GET
-	@Secured
 	@Path("/resumoPressao")
 	public List<Faixa> getQtPneusByFaixaPressao(
 			@QueryParam("codUnidades") List<String> codUnidades,
 			@QueryParam("status") List<String> status){
 		return service.getQtPneusByFaixaPressao(codUnidades, status);
 	}
-	
+
 	@GET
-	@Secured
 	@Path("/aderencia/{codUnidade}/{ano}/{mes}")
 	public List<Aderencia> getAderenciaByUnidade(
 			@PathParam("ano") int ano,
 			@PathParam("mes") int mes,
-			@PathParam("codUnidade") Long codUnidade){	
+			@PathParam("codUnidade") Long codUnidade){
 		return service.getAderenciaByUnidade(ano, mes, codUnidade);
 	}
 
 	@GET
-	@Secured
 	@Path("resumoServicos/{ano}/{mes}")
 	public List<ResumoServicos> getResumoServicosByUnidades(
 			@PathParam("ano") int ano,
@@ -58,67 +56,53 @@ public class RelatorioResource {
 	}
 
 	@GET
-	@Secured
 	@Path("/previsao-trocas/{codUnidade}/csv")
 	@Produces("application/csv")
 	public StreamingOutput getPrevisaoTrocaCsv(@PathParam("codUnidade") Long codUnidade,
-                                                @QueryParam("dataInicial") long dataInicial,
-                                                @QueryParam("dataFinal") long dataFinal){
+											   @QueryParam("dataInicial") long dataInicial,
+											   @QueryParam("dataFinal") long dataFinal){
 		return outputStream -> service.getPrevisaoTrocaCsv(codUnidade, dataInicial, dataFinal, outputStream);
 	}
 
 	@GET
-	@Secured
 	@Path("/previsao-trocas/{codUnidade}/report")
 	public Report getPrevisaoTrocaReport(@PathParam("codUnidade") Long codUnidade,
-                                          @QueryParam("dataInicial") long dataInicial,
-                                          @QueryParam("dataFinal") long dataFinal) throws SQLException{
+										 @QueryParam("dataInicial") long dataInicial,
+										 @QueryParam("dataFinal") long dataFinal) throws SQLException{
 		return service.getPrevisaoTrocaReport(codUnidade, dataInicial, dataFinal);
 	}
 
 	@GET
-	@Secured
 	@Path("/previsao-trocas/consolidados/{codUnidade}/csv")
 	@Produces("application/csv")
 	public StreamingOutput getPrevisaoTrocaConsolidadoCsv(@PathParam("codUnidade") Long codUnidade,
-												@QueryParam("dataInicial") long dataInicial,
-												@QueryParam("dataFinal") long dataFinal){
+														  @QueryParam("dataInicial") long dataInicial,
+														  @QueryParam("dataFinal") long dataFinal){
 		return outputStream -> service.getPrevisaoTrocaConsolidadoCsv(codUnidade, dataInicial, dataFinal, outputStream);
 	}
 
 	@GET
-	@Secured
 	@Path("/previsao-trocas/consolidados/{codUnidade}/report")
 	public Report getPrevisaoTrocaConsolidadoReport(@PathParam("codUnidade") Long codUnidade,
-										  @QueryParam("dataInicial") long dataInicial,
-										  @QueryParam("dataFinal") long dataFinal) throws SQLException{
+													@QueryParam("dataInicial") long dataInicial,
+													@QueryParam("dataFinal") long dataFinal) throws SQLException{
 		return service.getPrevisaoTrocaConsolidadoReport(codUnidade, dataInicial, dataFinal);
 	}
 
 	@GET
-	@Secured
 	@Path("/aderencias/placas/{codUnidade}/csv")
 	@Produces("application/csv")
 	public StreamingOutput getAderenciaPlacasCsv(@PathParam("codUnidade") Long codUnidade,
-														   @QueryParam("dataInicial") long dataInicial,
-														   @QueryParam("dataFinal") long dataFinal){
+												 @QueryParam("dataInicial") long dataInicial,
+												 @QueryParam("dataFinal") long dataFinal){
 		return outputStream -> service.getAerenciaPlacasCsv(codUnidade, dataInicial, dataFinal, outputStream);
 	}
 
 	@GET
-	@Secured
 	@Path("/aderencia/placas/{codUnidade}/report")
 	public Report getAderenciaPlacasReport(@PathParam("codUnidade") Long codUnidade,
-													 @QueryParam("dataInicial") long dataInicial,
-													 @QueryParam("dataFinal") long dataFinal) throws SQLException{
+										   @QueryParam("dataInicial") long dataInicial,
+										   @QueryParam("dataFinal") long dataFinal) throws SQLException{
 		return service.getAderenciaPlacasReport(codUnidade, dataInicial, dataFinal);
 	}
-
-
-	
 }
-	
-	
-
-
-
