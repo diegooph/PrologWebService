156cbd07ab59b6b7656412f738683db852f5dab4
Luiz Felipe <luiz.felipe_95@hotmail.com>
2017-02-25 22:33:33 -0300

    style(OrdemServicoResource): corrige formatção do código

--- src/main/java/br/com/zalf/prolog/webservice/frota/ordemServico/OrdemServicoResource.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/ordemServico/OrdemServicoResource.java
@@ -25,15 +25,14 @@ public class OrdemServicoResource {
 
     @GET
     @Android
-    @Secured(permissions = { Pilares.Frota.OrdemServico.Checklist.VISUALIZAR, Pilares.Frota.OrdemServico.Checklist.CONSERTAR_ITEM })
+    @Secured(permissions = {Pilares.Frota.OrdemServico.Checklist.VISUALIZAR, Pilares.Frota.OrdemServico.Checklist.CONSERTAR_ITEM})
     @Path("/{codUnidade}/{tipoVeiculo}/{placa}/{status}")
     public List<OrdemServico> getOs(@PathParam("placa") String placa,
                                     @PathParam("status") String status,
                                     @PathParam("codUnidade") Long codUnidade,
                                     @PathParam("tipoVeiculo") String tipoVeiculo,
                                     @QueryParam("limit") Integer limit,
-                                    @QueryParam("offset") Long offset){
-
+                                    @QueryParam("offset") Long offset) {
         return service.getOs(placa, status, codUnidade, tipoVeiculo, limit, offset);
     }
 
@@ -41,37 +40,37 @@ public class OrdemServicoResource {
     @Android
     @Secured(permissions = Pilares.Frota.OrdemServico.Checklist.CONSERTAR_ITEM)
     @Path("/consertaItem/{codUnidade}/{placa}")
-    public Response consertaItem (@PathParam("codUnidade")Long codUnidade, ItemOrdemServico item,
-    @PathParam("placa") String placa){
-        if (service.consertaItem(codUnidade, item, placa)){
+    public Response consertaItem(@PathParam("codUnidade") Long codUnidade, ItemOrdemServico item,
+                                  @PathParam("placa") String placa) {
+        if (service.consertaItem(codUnidade, item, placa)) {
             return Response.Ok("Serviço consertado com sucesso");
-        }else{
+        } else {
             return Response.Error("Erro ao consertar o item");
         }
     }
 
     @GET
     @Android
-    @Secured(permissions = { Pilares.Frota.OrdemServico.Checklist.VISUALIZAR, Pilares.Frota.OrdemServico.Checklist.CONSERTAR_ITEM })
+    @Secured(permissions = {Pilares.Frota.OrdemServico.Checklist.VISUALIZAR, Pilares.Frota.OrdemServico.Checklist.CONSERTAR_ITEM})
     @Path("/manutencao/{placa}/{status}/{prioridade}")
     public List<ItemOrdemServico> getItensOsManutencaoHolder(@PathParam("placa") String placa,
                                                              @QueryParam("limit") int limit,
                                                              @QueryParam("offset") long offset,
                                                              @PathParam("status") String status,
-                                                             @PathParam("prioridade") String prioridade){
+                                                             @PathParam("prioridade") String prioridade) {
         return service.getItensOsManutencaoHolder(placa, status, limit, offset, prioridade);
     }
 
     @GET
     @Android
-    @Secured(permissions = { Pilares.Frota.OrdemServico.Checklist.VISUALIZAR, Pilares.Frota.OrdemServico.Checklist.CONSERTAR_ITEM })
+    @Secured(permissions = {Pilares.Frota.OrdemServico.Checklist.VISUALIZAR, Pilares.Frota.OrdemServico.Checklist.CONSERTAR_ITEM})
     @Path("/manutencao/{codUnidade}/{tipoVeiculo}/{placa}/{status}")
     public List<ManutencaoHolder> getResumoManutencaoHolder(@PathParam("placa") String placa,
                                                             @PathParam("tipoVeiculo") String codTipo,
                                                             @PathParam("codUnidade") Long codUnidade,
                                                             @QueryParam("limit") int limit,
                                                             @QueryParam("offset") long offset,
-                                                            @PathParam("status") String status){
+                                                            @PathParam("status") String status) {
         return service.getResumoManutencaoHolder(placa, codTipo, codUnidade, limit, offset, status);
     }
 }
