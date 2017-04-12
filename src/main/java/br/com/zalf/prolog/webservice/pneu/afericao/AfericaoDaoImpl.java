37e57f26b8c9c7bd6e3b90ce4daa19f93ac52be1
Jean Zart <jeanzart@gmail.com>
2017-04-12 12:41:42 -0300

    refactor(AfericaoDaoImpl): altera a forma de ordenar os pneus buscados de um veículo

--- src/main/java/br/com/zalf/prolog/webservice/pneu/afericao/AfericaoDaoImpl.java
+++ src/main/java/br/com/zalf/prolog/webservice/pneu/afericao/AfericaoDaoImpl.java
@@ -254,7 +254,6 @@ public class AfericaoDaoImpl extends DatabaseConnection implements AfericaoDao {
 
     @Override
     public Afericao getByCod(Long codAfericao, Long codUnidade) throws SQLException {
-
         Connection conn = null;
         ResultSet rSet = null;
         PreparedStatement stmt = null;
@@ -267,20 +266,19 @@ public class AfericaoDaoImpl extends DatabaseConnection implements AfericaoDao {
             conn = getConnection();
             stmt = conn.prepareStatement("SELECT A.KM_VEICULO, A.CODIGO as COD_AFERICAO, A.DATA_HORA, A.PLACA_VEICULO, A.KM_VEICULO, A.TEMPO_REALIZACAO, C.CPF, C.NOME, AV.COD_AFERICAO, AV.ALTURA_SULCO_CENTRAL, AV.ALTURA_SULCO_EXTERNO, AV.ALTURA_SULCO_INTERNO, \n" +
                     "AV.PSI::INT AS PRESSAO_ATUAL, AV.POSICAO, P.CODIGO, MP.CODIGO AS COD_MARCA, MP.NOME AS MARCA, MO.CODIGO AS COD_MODELO, MO.NOME AS MODELO,\n" +
-                    "DP.ALTURA, DP.LARGURA, DP.ARO, DP.CODIGO AS COD_DIMENSAO, P.PRESSAO_RECOMENDADA, P.ALTURA_SULCOS_NOVOS, P.STATUS, P.VIDA_ATUAL, P.VIDA_TOTAL, " +
+                    "DP.ALTURA, DP.LARGURA, DP.ARO, DP.CODIGO AS COD_DIMENSAO, P.PRESSAO_RECOMENDADA, P.ALTURA_SULCOS_NOVOS, P.STATUS, P.VIDA_ATUAL, P.VIDA_TOTAL,\n" +
                     "MB.codigo AS COD_MODELO_BANDA, MB.nome AS NOME_MODELO_BANDA, MAB.codigo AS COD_MARCA_BANDA, MAB.nome AS NOME_MARCA_BANDA\n" +
                     "FROM AFERICAO A JOIN AFERICAO_VALORES AV ON A.CODIGO = AV.COD_AFERICAO\n" +
+                    "JOIN pneu_ordem po on av.posicao = po.posicao_prolog\n" +
                     "JOIN PNEU P ON P.CODIGO = AV.COD_PNEU AND P.COD_UNIDADE = AV.COD_UNIDADE\n" +
                     "JOIN MODELO_PNEU MO ON MO.CODIGO = P.COD_MODELO JOIN MARCA_PNEU MP ON MP.CODIGO = MO.COD_MARCA\n" +
                     "JOIN DIMENSAO_PNEU DP ON DP.CODIGO = P.COD_DIMENSAO\n" +
                     "JOIN UNIDADE U ON U.CODIGO = P.cod_unidade\n" +
                     "LEFT JOIN modelo_banda MB ON MB.codigo = P.cod_modelo_banda AND MB.cod_empresa = U.cod_empresa\n" +
-                    "  LEFT JOIN marca_banda MAB ON MAB.codigo = MB.cod_marca AND MAB.cod_empresa = MB.cod_empresa\n" +
+                    "LEFT JOIN marca_banda MAB ON MAB.codigo = MB.cod_marca AND MAB.cod_empresa = MB.cod_empresa\n" +
                     "JOIN COLABORADOR C ON C.CPF = A.CPF_AFERIDOR\n" +
                     "WHERE AV.COD_AFERICAO = ? AND AV.COD_UNIDADE = ?\n" +
-                    "ORDER BY Substring(AV.posicao::text FROM 2 for 1) ASC,\n" +
-                    "substring(AV.posicao::text FROM 1 for 1) ASC,\n" +
-                    "substring(AV.posicao::text FROM 3 for 1) DESC ");
+                    "ORDER BY po.ordem_exibicao ASC");
             stmt.setLong(1, codAfericao);
             stmt.setLong(2, codUnidade);
             rSet = stmt.executeQuery();
@@ -296,7 +294,7 @@ public class AfericaoDaoImpl extends DatabaseConnection implements AfericaoDao {
                     pneu.setPosicao(rSet.getInt("POSICAO"));
                     pneus.add(pneu);
                 }
-                veiculo.setListPneus(pneuDao.ordenaLista(pneus));
+                veiculo.setListPneus(pneus);
                 afericao.setVeiculo(veiculo);
             }
         } finally {
