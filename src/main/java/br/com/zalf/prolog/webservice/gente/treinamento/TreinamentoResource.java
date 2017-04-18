b81b62b29c9a2af10c775cc069d467de387130e6
Jean Zart <jeanzart@gmail.com>
2017-04-18 09:30:38 -0300

    style(Treinamento): identa o código

--- src/main/java/br/com/zalf/prolog/webservice/gente/treinamento/TreinamentoResource.java
+++ src/main/java/br/com/zalf/prolog/webservice/gente/treinamento/TreinamentoResource.java
@@ -23,88 +23,88 @@ import java.util.List;
 @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
 public class TreinamentoResource {
 
-	private TreinamentoService service = new TreinamentoService();
+    private TreinamentoService service = new TreinamentoService();
 
-	@POST
-	@Path("/upload")
-	@Secured(permissions = {Pilares.Gente.Treinamentos.CRIAR, Pilares.Gente.Treinamentos.ALTERAR})
-	@Consumes({MediaType.MULTIPART_FORM_DATA})
-	public AbstractResponse uploadTreinamento(
-			@FormDataParam("file") InputStream fileInputStream,
-			@FormDataParam("file") FormDataContentDisposition fileDetail,
-			@FormDataParam("treinamento") FormDataBodyPart jsonPart) {
+    @POST
+    @Path("/upload")
+    @Secured(permissions = {Pilares.Gente.Treinamentos.CRIAR, Pilares.Gente.Treinamentos.ALTERAR})
+    @Consumes({MediaType.MULTIPART_FORM_DATA})
+    public AbstractResponse uploadTreinamento(
+            @FormDataParam("file") InputStream fileInputStream,
+            @FormDataParam("file") FormDataContentDisposition fileDetail,
+            @FormDataParam("treinamento") FormDataBodyPart jsonPart) {
 
-		if (!fileDetail.getFileName().toLowerCase().endsWith(".pdf"))
-			return Response.Error("ERRO! Arquivo não está no formato PDF!");
+        if (!fileDetail.getFileName().toLowerCase().endsWith(".pdf"))
+            return Response.Error("ERRO! Arquivo não está no formato PDF!");
 
-		jsonPart.setMediaType(MediaType.APPLICATION_JSON_TYPE);
-		Treinamento treinamento = jsonPart.getValueAs(Treinamento.class);
-		if (treinamento == null) {
-			return Response.Error("ERRO! Treinamento veio nulo");
-		} else {
-			UploadTreinamento upload = new UploadTreinamento();
-			if (upload.doIt(treinamento, fileInputStream)) {
-				Long codTreinamento = service.insert(treinamento);
-				if (codTreinamento != null) {
-					return ResponseWithCod.Ok("Treinamento inserido com sucesso", codTreinamento);
-				} else {
-					return Response.Error("Erro ao inserir treinamento");
-				}
-			} else {
-				return Response.Error("Erro ao reailizar upload do arquivo");
-			}
-		}
-	}
+        jsonPart.setMediaType(MediaType.APPLICATION_JSON_TYPE);
+        Treinamento treinamento = jsonPart.getValueAs(Treinamento.class);
+        if (treinamento == null) {
+            return Response.Error("ERRO! Treinamento veio nulo");
+        } else {
+            UploadTreinamento upload = new UploadTreinamento();
+            if (upload.doIt(treinamento, fileInputStream)) {
+                Long codTreinamento = service.insert(treinamento);
+                if (codTreinamento != null) {
+                    return ResponseWithCod.Ok("Treinamento inserido com sucesso", codTreinamento);
+                } else {
+                    return Response.Error("Erro ao inserir treinamento");
+                }
+            } else {
+                return Response.Error("Erro ao reailizar upload do arquivo");
+            }
+        }
+    }
 
-	@POST
-	@Secured(permissions = Pilares.Gente.Treinamentos.VISUALIZAR_PROPRIOS)
-	@Path("/visualizados/{codTreinamento}/{cpf}")
-	public Response marcarTreinamentoComoVisto(@PathParam("codTreinamento") Long codTreinamento,
-											   @PathParam("cpf") Long cpf) {
-		if (service.marcarTreinamentoComoVisto(codTreinamento, cpf)) {
-			return Response.Ok("Treinamento marcado com sucesso");
-		} else {
-			return Response.Error("Erro ao marcar treinamento");
-		}
-	}
+    @POST
+    @Secured(permissions = Pilares.Gente.Treinamentos.VISUALIZAR_PROPRIOS)
+    @Path("/visualizados/{codTreinamento}/{cpf}")
+    public Response marcarTreinamentoComoVisto(@PathParam("codTreinamento") Long codTreinamento,
+                                               @PathParam("cpf") Long cpf) {
+        if (service.marcarTreinamentoComoVisto(codTreinamento, cpf)) {
+            return Response.Ok("Treinamento marcado com sucesso");
+        } else {
+            return Response.Error("Erro ao marcar treinamento");
+        }
+    }
 
-	@GET
-	@Secured(permissions = Pilares.Gente.Treinamentos.VISUALIZAR_PROPRIOS)
-	@Android
-	@Path("/vistosColaborador/{cpf}")
-	public List<Treinamento> getVistosByColaborador(@PathParam("cpf") Long cpf) {
-		return service.getVistosByColaborador(cpf);
-	}
+    @GET
+    @Secured(permissions = Pilares.Gente.Treinamentos.VISUALIZAR_PROPRIOS)
+    @Android
+    @Path("/vistosColaborador/{cpf}")
+    public List<Treinamento> getVistosByColaborador(@PathParam("cpf") Long cpf) {
+        return service.getVistosByColaborador(cpf);
+    }
 
-	@GET
-	@Secured(permissions = Pilares.Gente.Treinamentos.VISUALIZAR_PROPRIOS)
-	@Android
-	@Path("/naoVistosColaborador/{cpf}")
-	public List<Treinamento> getNaoVistosByColaborador(@PathParam("cpf") Long cpf) {
-		return service.getNaoVistosByColaborador(cpf);
-	}
+    @GET
+    @Secured(permissions = Pilares.Gente.Treinamentos.VISUALIZAR_PROPRIOS)
+    @Android
+    @Path("/naoVistosColaborador/{cpf}")
+    public List<Treinamento> getNaoVistosByColaborador(@PathParam("cpf") Long cpf) {
+        return service.getNaoVistosByColaborador(cpf);
+    }
 
-	@GET
-	@Secured(permissions = {Pilares.Gente.Treinamentos.VISUALIZAR_PROPRIOS, Pilares.Gente.Treinamentos.ALTERAR,
-			Pilares.Gente.Treinamentos.CRIAR})
-	@Path("/{codUnidade}/{codFuncao}")
-	public List<Treinamento> getAll(
-			@PathParam("codUnidade") Long codUnidade,
-			@PathParam("codFuncao") String codFuncao,
-			@QueryParam("dataInicial") long dataInicial,
-			@QueryParam("dataFinal") long dataFinal,
-			@QueryParam("limit") long limit,
-			@QueryParam("offset") long offset) {
-		return service.getAll(DateUtils.toLocalDate(new java.sql.Date(dataInicial)),
-				DateUtils.toLocalDate(new java.sql.Date(dataFinal)), codFuncao, codUnidade, limit, offset);
-	}
+    @GET
+    @Secured(permissions = {Pilares.Gente.Treinamentos.VISUALIZAR_PROPRIOS, Pilares.Gente.Treinamentos.ALTERAR,
+            Pilares.Gente.Treinamentos.CRIAR})
+    @Path("/{codUnidade}/{codFuncao}")
+    public List<Treinamento> getAll(
+            @PathParam("codUnidade") Long codUnidade,
+            @PathParam("codFuncao") String codFuncao,
+            @QueryParam("dataInicial") long dataInicial,
+            @QueryParam("dataFinal") long dataFinal,
+            @QueryParam("limit") long limit,
+            @QueryParam("offset") long offset) {
+        return service.getAll(DateUtils.toLocalDate(new java.sql.Date(dataInicial)),
+                DateUtils.toLocalDate(new java.sql.Date(dataFinal)), codFuncao, codUnidade, limit, offset);
+    }
 
-	@GET
-	@Secured
-	@Path("/visualizacoes/{codUnidade}/{codTreinamento}")
-	public List<TreinamentoColaborador> getVisualizacoesByTreinamento(
-			@PathParam("codUnidade") Long codTreinamento,
-			@PathParam("codTreinamento") Long codUnidade){
-		return service.getVisualizacoesByTreinamento(codTreinamento, codUnidade);
-	}
+    @GET
+    @Secured
+    @Path("/visualizacoes/{codUnidade}/{codTreinamento}")
+    public List<TreinamentoColaborador> getVisualizacoesByTreinamento(
+            @PathParam("codUnidade") Long codTreinamento,
+            @PathParam("codTreinamento") Long codUnidade) {
+        return service.getVisualizacoesByTreinamento(codTreinamento, codUnidade);
+    }
 }
