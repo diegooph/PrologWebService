574fc612045ca1396311b61859b7887460ad0e47
Luiz Felipe <luiz.felipe_95@hotmail.com>
2017-05-09 21:04:20 -0300

    fix(upload treinamento): corrige método de envio de arquivos

--- src/main/java/br/com/zalf/prolog/webservice/gente/treinamento/UploadTreinamentoHelper.java
+++ src/main/java/br/com/zalf/prolog/webservice/gente/treinamento/UploadTreinamentoHelper.java
@@ -1,6 +1,7 @@
 package br.com.zalf.prolog.webservice.gente.treinamento;
 
 import br.com.zalf.prolog.gente.treinamento.Treinamento;
+import br.com.zalf.prolog.webservice.util.L;
 import br.com.zalf.prolog.webservice.util.S3FileSender;
 import org.apache.commons.io.IOUtils;
 
@@ -17,7 +18,9 @@ import java.util.List;
 public class UploadTreinamentoHelper {
     private static final String AWS_ACCESS_KEY_ID = "AKIAI6KFIYRHPVSFDFUA";
     private static final String AWS_SECRET_KEY = "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";
-    private static final String BUCKET_NAME = "treinamentos";
+    private static final String BUCKET_NAME_PDF = "treinamentos-prolog/pdf";
+    private static final String BUCKET_NAME_IMAGES = "treinamentos-prolog/images";
+    private static final String TAG = UploadTreinamentoHelper.class.getSimpleName();
     private PDFTransformer transformer;
 
     public UploadTreinamentoHelper(PDFTransformer transformer) {
@@ -33,12 +36,14 @@ public class UploadTreinamentoHelper {
 
         final S3FileSender fileSender = new S3FileSender(AWS_ACCESS_KEY_ID, AWS_SECRET_KEY);
         for (File imagem : imagens) {
-            fileSender.sendFile(BUCKET_NAME, imagem.getName(), imagem);
-            urls.add(fileSender.generateFileUrl(BUCKET_NAME, imagem.getName()));
+            fileSender.sendFile(BUCKET_NAME_IMAGES, imagem.getName(), imagem);
+            final String imageName = fileSender.generateFileUrl(BUCKET_NAME_IMAGES, imagem.getName());
+            urls.add(imageName);
+            L.d(TAG, "Imagem enviada: " + imageName);
         }
         treinamento.setUrlsImagensArquivo(urls);
-        fileSender.sendFile(BUCKET_NAME, pdfName, createFile(inputStream, pdfName));
-        treinamento.setUrlArquivo(fileSender.generateFileUrl(BUCKET_NAME, pdfName));
+        fileSender.sendFile(BUCKET_NAME_PDF, pdfName, createFile(inputStream, pdfName));
+        treinamento.setUrlArquivo(fileSender.generateFileUrl(BUCKET_NAME_PDF, pdfName));
         return treinamento;
     }
 
