8acfca014f04d338a432c5cf4da03589de9f179b
Luiz Felipe <luiz.felipe_95@hotmail.com>
2017-05-10 10:44:48 -0300

    refactor: prepara para release

--- src/main/java/br/com/zalf/prolog/webservice/BuildConfig.java
+++ src/main/java/br/com/zalf/prolog/webservice/BuildConfig.java
@@ -1,6 +1,6 @@
 package br.com.zalf.prolog.webservice;
 
 public class BuildConfig {
-	 public static final boolean DEBUG = true;
-	 public static final int VERSION_CODE = 12;
+	 public static final boolean DEBUG = false;
+	 public static final int VERSION_CODE = 13;
 }
