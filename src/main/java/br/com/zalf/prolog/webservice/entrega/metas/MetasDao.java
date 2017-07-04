18b654483454314b6dcfec9d979efa8fcbd52c55
didi <dioge.vanzella@gmail.com>
2017-07-04 15:42:40 -0300

    refactor(MetasDao): insere comentários na classe

--- src/main/java/br/com/zalf/prolog/webservice/entrega/metas/MetasDao.java
+++ src/main/java/br/com/zalf/prolog/webservice/entrega/metas/MetasDao.java
@@ -7,8 +7,23 @@ import java.sql.SQLException;
  */
 public interface MetasDao {
 
+	/**
+	 * Retorna as metas da unidade.
+	 *
+	 * @param codUnidade código da unidade
+	 * @return um objeto {@link Metas} contendo os valores de cada meta daquela unidade
+	 * @throws SQLException caso a operação não pôde ser concluída
+	 */
 	Metas getByCodUnidade(Long codUnidade) throws SQLException;
 
+	/**
+	 * Altera as metas de uma unidade.
+	 *
+	 * @param metas      objeto contendo as novas metas
+	 * @param codUnidade código da unidade que irá atualizar as metas
+	 * @return verdadeiro caso operação realizada com sucesso, falso caso contrário
+	 * @throws SQLException caso a operação não pôde ser concluída
+	 */
 	boolean update(Metas metas, Long codUnidade) throws SQLException;
 
 }
