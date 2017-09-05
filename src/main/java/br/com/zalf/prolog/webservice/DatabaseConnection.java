74841ea8ac34faf5a85d4ac62dcc756f1b0c8604
Jean Zart <jeanzart@gmail.com>
2017-09-05 18:05:51 -0300

    Merge branch 'dev' of /Users/webservice/Documents/intellij/PrologWebService with conflicts.

--- src/main/java/br/com/zalf/prolog/webservice/DatabaseConnection.java
+++ src/main/java/br/com/zalf/prolog/webservice/DatabaseConnection.java
@@ -36,7 +36,7 @@ public class DatabaseConnection {
 //			+ "prolog-db-instance.csg59phgb0xp.sa-east-1.rds.amazonaws.com:"
 //			+ "5432/prolog_database_testes_janeiro";
 //	private static final String TESTE_URL = "jdbc:postgresql://192.168.15.11:5432/postgresMaio";
-	private static final String TESTE_URL = "jdbc:postgresql://localhost:5433/postgresSetembro";
+	private static final String TESTE_URL = "jdbc:postgresql://localhost:5432/postgresAgosto";
 	private static final String TESTE_USUARIO = "postgres";
 	private static final String TESTE_SENHA = "postgres";
 
