138775e4a51a7d56a9b2861d4e27d728c81b7f3c
Luiz Felipe <luiz.felipe_95@hotmail.com>
2017-10-06 13:55:43 -0300

    refactor: retorna para ambiente de desenvolvimento

--- src/main/java/br/com/zalf/prolog/webservice/integracao/avacorpavilan/AvaCorpAvilanConstants.java
+++ src/main/java/br/com/zalf/prolog/webservice/integracao/avacorpavilan/AvaCorpAvilanConstants.java
@@ -1,19 +1,29 @@
 package br.com.zalf.prolog.webservice.integracao.avacorpavilan;
 
+import br.com.zalf.prolog.webservice.BuildConfig;
+
 /**
  * Created on 9/28/17
  *
  * @author Luiz Felipe (https://github.com/luizfp)
  */
+@SuppressWarnings("ConstantConditions")
 public class AvaCorpAvilanConstants {
 
     private AvaCorpAvilanConstants() {
 
     }
 
-//    public static final String BASE_URL = "http://189.11.175.146/IntegracaoPrologTestes/";
-    public static final String BASE_URL = "http://189.11.175.146/IntegracaoProlog/";
+    public static final String BASE_URL;
+    public static final String NAMESPACE;
 
-//    public static final String NAMESPACE = "http://www.avacorp.com.br/integracaoprologtestes";
-    public static final String NAMESPACE = "http://www.avacorp.com.br/integracaoprolog";
+    static {
+        final String BASE_URL_TESTES = "http://189.11.175.146/IntegracaoPrologTestes/";
+        final String BASE_URL_PROD = "http://189.11.175.146/IntegracaoProlog/";
+        BASE_URL = BuildConfig.DEBUG ? BASE_URL_PROD : BASE_URL_TESTES;
+
+        final String NAMESPACE_TESTES = "http://www.avacorp.com.br/integracaoprologtestes";
+        final String NAMESPACE_PROD = "http://www.avacorp.com.br/integracaoprolog";
+        NAMESPACE = BuildConfig.DEBUG ? NAMESPACE_TESTES : NAMESPACE_PROD;
+    }
 }
