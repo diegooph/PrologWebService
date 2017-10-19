ab77b689f6a2728121b3ea8c80e150144ece9609
Luiz Felipe <luiz.felipe_95@hotmail.com>
2017-10-19 18:31:31 -0200

    feat(treinamento): implementa busca dos treinamentos apenas por código da unidade

--- src/main/java/br/com/zalf/prolog/webservice/gente/treinamento/TreinamentoResource.java
+++ src/main/java/br/com/zalf/prolog/webservice/gente/treinamento/TreinamentoResource.java
@@ -101,15 +101,27 @@ public class TreinamentoResource {
     @Secured(permissions = {Pilares.Gente.Treinamentos.VISUALIZAR_PROPRIOS, Pilares.Gente.Treinamentos.ALTERAR,
             Pilares.Gente.Treinamentos.CRIAR})
     @Path("/{codUnidade}/{codFuncao}")
-    public List<Treinamento> getAll(
-            @PathParam("codUnidade") Long codUnidade,
-            @PathParam("codFuncao") String codFuncao,
-            @QueryParam("dataInicial") long dataInicial,
-            @QueryParam("dataFinal") long dataFinal,
-            @QueryParam("limit") long limit,
-            @QueryParam("offset") long offset) {
+    public List<Treinamento> getAll(@PathParam("codUnidade") Long codUnidade,
+                                    @PathParam("codFuncao") String codFuncao,
+                                    @QueryParam("dataInicial") long dataInicial,
+                                    @QueryParam("dataFinal") long dataFinal,
+                                    @QueryParam("limit") long limit,
+                                    @QueryParam("offset") long offset) {
         return service.getAll(DateUtils.toLocalDate(new java.sql.Date(dataInicial)),
                 DateUtils.toLocalDate(new java.sql.Date(dataFinal)), codFuncao, codUnidade, limit, offset);
+    }
+
+    @GET
+    @Secured(permissions = {Pilares.Gente.Treinamentos.VISUALIZAR_PROPRIOS, Pilares.Gente.Treinamentos.ALTERAR,
+            Pilares.Gente.Treinamentos.CRIAR})
+    @Path("/{codUnidade}")
+    public List<Treinamento> getAll(@PathParam("codUnidade") Long codUnidade,
+                                    @QueryParam("dataInicial") long dataInicial,
+                                    @QueryParam("dataFinal") long dataFinal,
+                                    @QueryParam("limit") long limit,
+                                    @QueryParam("offset") long offset) {
+        return service.getAll(DateUtils.toLocalDate(new java.sql.Date(dataInicial)),
+                DateUtils.toLocalDate(new java.sql.Date(dataFinal)), null, codUnidade, limit, offset);
     }
 
     @PUT
