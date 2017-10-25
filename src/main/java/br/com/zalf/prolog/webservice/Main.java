ffd0fb89a1c1b1c53f6cbfb37c0dece70f22406d
Luiz Felipe <luiz.felipe_95@hotmail.com>
2017-10-25 13:25:58 -0200

    refactor(Main): remove classe teste

--- src/main/java/br/com/zalf/prolog/webservice/Main.java
+++ src/main/java/br/com/zalf/prolog/webservice/Main.java
@@ -1,13 +1,11 @@
 package br.com.zalf.prolog.webservice;
 
-import br.com.zalf.prolog.webservice.integracao.avacorpavilan.SincronizadorChecklistsAvilan;
-
 import java.sql.SQLException;
 
 
 public class Main {
 
 	public static void main(String[] args) throws SQLException {
-		new SincronizadorChecklistsAvilan().sync();
+
 	}
 }
