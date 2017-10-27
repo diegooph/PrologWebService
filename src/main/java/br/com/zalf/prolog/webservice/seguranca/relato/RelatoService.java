807e90388f7fc7ea29f870b48477c81ed29d3063
didi <dioge.vanzella@gmail.com>
2017-10-27 16:58:39 -0200

    refactor(RelatoService): altera retorno dos métodos para lançar uma exceção se não encontrar dados no banco

--- src/main/java/br/com/zalf/prolog/webservice/seguranca/relato/RelatoService.java
+++ src/main/java/br/com/zalf/prolog/webservice/seguranca/relato/RelatoService.java
@@ -4,7 +4,6 @@ import br.com.zalf.prolog.webservice.seguranca.relato.model.Relato;
 
 import java.sql.SQLException;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,87 +11,87 @@ import java.util.List;
  */
 public class RelatoService {
 
-	private RelatoDao dao = new RelatoDaoImpl();
-	
-	public boolean insert(Relato relato) {
-		try {
-			return dao.insert(relato);
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
-	
-	public boolean delete(Long codRelato) {
-		try {
-			return dao.delete(codRelato);
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
-	
-	public Relato getByCod(Long codigo) {
-		try {
-			return dao.getByCod(codigo);
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-	
-	public List<Relato> getAll(Long codUnidade, int limit, long offset, double latitude, double longitude, boolean isOrderByDate, String status) {
-		try {
-			return dao.getAll(codUnidade, limit, offset, latitude, longitude, isOrderByDate, status);
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return new ArrayList<Relato>();
-		}
-	}
-	
-	public List<Relato> getRealizadosByColaborador(Long cpf, int limit, long offset, double latitude, double longitude, boolean isOrderByDate, String status, String campoFiltro) {
-		try {
-			return dao.getRealizadosByColaborador(cpf, limit, offset, latitude, longitude, isOrderByDate, status, campoFiltro);
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return new ArrayList<Relato>();
-		}
-	}
-	
-	public boolean classificaRelato(Relato relato){
-		try{
-			return dao.classificaRelato(relato);
-		}catch(SQLException e){
-			e.printStackTrace();
-			return false;
-		}
-	}
-	
-	public boolean fechaRelato(Relato relato){
-		try{
-			return dao.fechaRelato(relato);
-		}catch(SQLException e){
-			e.printStackTrace();
-			return false;
-		}
-	}
+    private RelatoDao dao = new RelatoDaoImpl();
 
-	public List<Relato> getAllByUnidade(LocalDate dataInicial, LocalDate dataFinal, String equipe,
-			Long codUnidade,long limit, long offset, String status) {
-		try {
-			return dao.getAllByUnidade(dataInicial, dataFinal, equipe, codUnidade, limit, offset, status);
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return new ArrayList<Relato>();
-		}
-	}
-	
-	public List<Relato> getAllExcetoColaborador(Long cpf, int limit, long offset, double latitude, double longitude, boolean isOrderByDate, String status) {
-		try {
-			return dao.getAllExcetoColaborador(cpf, limit, offset, latitude, longitude, isOrderByDate, status);
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return new ArrayList<Relato>();
-		}
-	}
+    public boolean insert(Relato relato) {
+        try {
+            return dao.insert(relato);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean delete(Long codRelato) {
+        try {
+            return dao.delete(codRelato);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public Relato getByCod(Long codigo) {
+        try {
+            return dao.getByCod(codigo);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public List<Relato> getAll(Long codUnidade, int limit, long offset, double latitude, double longitude, boolean isOrderByDate, String status) {
+        try {
+            return dao.getAll(codUnidade, limit, offset, latitude, longitude, isOrderByDate, status);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Erro ao buscar os relatos para unidade: " + codUnidade + " e status: " + status);
+        }
+    }
+
+    public List<Relato> getRealizadosByColaborador(Long cpf, int limit, long offset, double latitude, double longitude, boolean isOrderByDate, String status, String campoFiltro) {
+        try {
+            return dao.getRealizadosByColaborador(cpf, limit, offset, latitude, longitude, isOrderByDate, status, campoFiltro);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Erro ao buscar os relatos realizados pelo colaborador: " + cpf + " e status: " + status);
+        }
+    }
+
+    public boolean classificaRelato(Relato relato) {
+        try {
+            return dao.classificaRelato(relato);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean fechaRelato(Relato relato) {
+        try {
+            return dao.fechaRelato(relato);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public List<Relato> getAllByUnidade(LocalDate dataInicial, LocalDate dataFinal, String equipe,
+                                        Long codUnidade, long limit, long offset, String status) {
+        try {
+            return dao.getAllByUnidade(dataInicial, dataFinal, equipe, codUnidade, limit, offset, status);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Erro ao buscar os relatos unidade: " + codUnidade + " e status: " + status);
+        }
+    }
+
+    public List<Relato> getAllExcetoColaborador(Long cpf, int limit, long offset, double latitude, double longitude, boolean isOrderByDate, String status) {
+        try {
+            return dao.getAllExcetoColaborador(cpf, limit, offset, latitude, longitude, isOrderByDate, status);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Erro ao buscar os relatos exceto colaborador: " + cpf + " e status: " + status);
+        }
+    }
 }
