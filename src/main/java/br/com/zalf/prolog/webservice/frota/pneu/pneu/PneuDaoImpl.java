1487136575137e31ed74ab7a8e44af1ab621d201
Luiz Felipe <luiz.felipe_95@hotmail.com>
2017-11-27 15:09:39 -0200

    fix(PneuDaoImpl): corrige update dos sulcos do pneu

--- src/main/java/br/com/zalf/prolog/webservice/frota/pneu/pneu/PneuDaoImpl.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/pneu/PneuDaoImpl.java
@@ -522,12 +522,12 @@ public class PneuDaoImpl extends DatabaseConnection implements PneuDao {
             stmt = conn.prepareStatement("UPDATE PNEU SET ALTURA_SULCO_INTERNO = ?, ALTURA_SULCO_EXTERNO = ?, "
                     + "ALTURA_SULCO_CENTRAL_INTERNO = ?, ALTURA_SULCO_CENTRAL_EXTERNO = ? "
                     + "WHERE CODIGO = ? AND COD_UNIDADE = ?;");
-            stmt.setDouble(2, pneu.getSulcosAtuais().getInterno());
-            stmt.setDouble(3, pneu.getSulcosAtuais().getExterno());
-            stmt.setDouble(4, pneu.getSulcosAtuais().getCentralInterno());
-            stmt.setDouble(5, pneu.getSulcosAtuais().getCentralExterno());
-            stmt.setString(6, pneu.getCodigo());
-            stmt.setLong(7, codUnidade);
+            stmt.setDouble(1, pneu.getSulcosAtuais().getInterno());
+            stmt.setDouble(2, pneu.getSulcosAtuais().getExterno());
+            stmt.setDouble(3, pneu.getSulcosAtuais().getCentralInterno());
+            stmt.setDouble(4, pneu.getSulcosAtuais().getCentralExterno());
+            stmt.setString(5, pneu.getCodigo());
+            stmt.setLong(6, codUnidade);
             final int count = stmt.executeUpdate();
             if (count == 0) {
                 throw new SQLException("Erro ao atualizar os dados do Pneu");
