25ced36b6a20acf0c6ade43ba2d82674261a8840
Luiz Felipe <luiz.felipe_95@hotmail.com>
2018-01-16 09:06:27 -0200

    refactor(FaleConoscoRelatorioService): loga exceção caso aconteça algum erro

--- src/main/java/br/com/zalf/prolog/webservice/gente/faleConosco/relatorios/FaleConoscoRelatorioService.java
+++ src/main/java/br/com/zalf/prolog/webservice/gente/faleConosco/relatorios/FaleConoscoRelatorioService.java
@@ -2,6 +2,7 @@ package br.com.zalf.prolog.webservice.gente.faleConosco.relatorios;
 
 import br.com.zalf.prolog.webservice.Injection;
 import br.com.zalf.prolog.webservice.commons.report.Report;
+import br.com.zalf.prolog.webservice.commons.util.Log;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -12,13 +13,17 @@ import java.util.Date;
  * Created by Zart on 02/05/17.
  */
 public class FaleConoscoRelatorioService {
+    private static final String TAG = FaleConoscoRelatorioService.class.getSimpleName();
     private final FaleConoscoRelatorioDao dao = Injection.provideFaleConoscoRelatorioDao();
 
     public void getResumoCsv(Long codUnidade, OutputStream outputStream, Date dataInicial, Date dataFinal) {
         try {
             dao.getResumoCsv(codUnidade, outputStream, dataInicial, dataFinal);
         } catch (SQLException | IOException e) {
-            e.printStackTrace();
+            Log.e(TAG, String.format("Erro ao buscar relatório de resumo dos " +
+                    "fale conosco da unidade: %d\n" +
+                    "Período: %s à %s", codUnidade, dataInicial.toString(), dataFinal.toString()), e);
+            throw new RuntimeException(e);
         }
     }
 
@@ -26,9 +31,10 @@ public class FaleConoscoRelatorioService {
         try {
             return dao.getResumoReport(codUnidade, dataInicial, dataFinal);
         } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
+            Log.e(TAG, String.format("Erro ao buscar relatório de resumo dos " +
+                    "fale conosco da unidade: %d\n" +
+                    "Período: %s à %s", codUnidade, dataInicial.toString(), dataFinal.toString()), e);
+            throw new RuntimeException(e);
         }
     }
-
 }
