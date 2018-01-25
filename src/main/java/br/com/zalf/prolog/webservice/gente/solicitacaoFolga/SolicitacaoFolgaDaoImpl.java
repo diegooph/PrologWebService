7b14bbcd461bcee40822751fa91ba13ae47e5772
Luiz Felipe <luiz.felipe_95@hotmail.com>
2018-01-25 09:09:19 -0200

    refactor(SolicitacaoFolgaDaoImpl): reorganiza código

--- src/main/java/br/com/zalf/prolog/webservice/gente/solicitacaoFolga/SolicitacaoFolgaDaoImpl.java
+++ src/main/java/br/com/zalf/prolog/webservice/gente/solicitacaoFolga/SolicitacaoFolgaDaoImpl.java
@@ -15,206 +15,195 @@ import java.util.List;
 
 public class SolicitacaoFolgaDaoImpl extends DatabaseConnection implements SolicitacaoFolgaDao {
 
-	@Override
-	public AbstractResponse insert(SolicitacaoFolga s) throws SQLException {
-		Connection conn = null;
-		PreparedStatement stmt = null;
-		ResultSet rSet = null;
-		try {
-			// verifica se a folga esta sendo solicitada com 48h de antecedência (2 dias)
-			if (ChronoUnit.DAYS.between(LocalDate.now(), DateUtils.toLocalDate(s.getDataFolga())) < 2) {
-				return Response.error("Erro ao inserir a solicitação de folga");
-			}
-			conn = getConnection();
-			stmt = conn.prepareStatement("INSERT INTO SOLICITACAO_FOLGA ( "
-					+ "CPF_COLABORADOR, DATA_SOLICITACAO, DATA_FOLGA, "
-					+ "MOTIVO_FOLGA, STATUS, PERIODO) VALUES (?, ?, ?, ?, ?, ?) RETURNING CODIGO");
-			stmt.setLong(1, s.getColaborador().getCpf());
-			stmt.setDate(2, new Date(System.currentTimeMillis()));
-			stmt.setDate(3, DateUtils.toSqlDate(s.getDataFolga()));
-			stmt.setString(4, s.getMotivoFolga());
-			stmt.setString(5, SolicitacaoFolga.STATUS_PENDENTE);
-			stmt.setString(6, s.getPeriodo());
-			rSet = stmt.executeQuery();
-			if(rSet.next()){
-				return ResponseWithCod.ok("Solicitação inserida com sucesso", rSet.getLong("CODIGO"));
-			}else{
-				return Response.error("Erro ao inserir a solicitação de folga");
-			}
-		}
-		finally {
-			closeConnection(conn, stmt, rSet);
-		}		
-	}
+    @Override
+    public AbstractResponse insert(SolicitacaoFolga s) throws SQLException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rSet = null;
+        try {
+            // verifica se a folga esta sendo solicitada com 48h de antecedência (2 dias).
+            if (ChronoUnit.DAYS.between(LocalDate.now(), DateUtils.toLocalDate(s.getDataFolga())) < 2) {
+                return Response.error("Erro ao inserir a solicitação de folga");
+            }
+            conn = getConnection();
+            stmt = conn.prepareStatement("INSERT INTO SOLICITACAO_FOLGA ( "
+                    + "CPF_COLABORADOR, DATA_SOLICITACAO, DATA_FOLGA, "
+                    + "MOTIVO_FOLGA, STATUS, PERIODO) VALUES (?, ?, ?, ?, ?, ?) RETURNING CODIGO");
+            stmt.setLong(1, s.getColaborador().getCpf());
+            stmt.setDate(2, new Date(System.currentTimeMillis()));
+            stmt.setDate(3, DateUtils.toSqlDate(s.getDataFolga()));
+            stmt.setString(4, s.getMotivoFolga());
+            stmt.setString(5, SolicitacaoFolga.STATUS_PENDENTE);
+            stmt.setString(6, s.getPeriodo());
+            rSet = stmt.executeQuery();
+            if (rSet.next()) {
+                return ResponseWithCod.ok("Solicitação inserida com sucesso", rSet.getLong("CODIGO"));
+            } else {
+                return Response.error("Erro ao inserir a solicitação de folga");
+            }
+        } finally {
+            closeConnection(conn, stmt, rSet);
+        }
+    }
 
-	@Override
-	public boolean update(SolicitacaoFolga solicitacaoFolga) throws SQLException {
-		Connection conn = null;
-		PreparedStatement stmt = null;
-		try {
-			conn = getConnection();
-			stmt = conn.prepareStatement("UPDATE SOLICITACAO_FOLGA SET "
-					+ " CPF_COLABORADOR=? , "
-					+ " CPF_FEEDBACK=? , "
-					+ " DATA_SOLICITACAO=? , "
-					+ " DATA_FOLGA=? , "
-					+ " DATA_FEEDBACK=? , "
-					+ " MOTIVO_FOLGA=? , "
-					+ " JUSTIFICATIVA_FEEDBACK=? , "
-					+ " STATUS=? , "
-					+ " PERIODO=? "
-					+ "WHERE CODIGO=?");
+    @Override
+    public boolean update(SolicitacaoFolga solicitacaoFolga) throws SQLException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        try {
+            conn = getConnection();
+            stmt = conn.prepareStatement("UPDATE SOLICITACAO_FOLGA SET "
+                    + " CPF_COLABORADOR=? , "
+                    + " CPF_FEEDBACK=? , "
+                    + " DATA_SOLICITACAO=? , "
+                    + " DATA_FOLGA=? , "
+                    + " DATA_FEEDBACK=? , "
+                    + " MOTIVO_FOLGA=? , "
+                    + " JUSTIFICATIVA_FEEDBACK=? , "
+                    + " STATUS=? , "
+                    + " PERIODO=? "
+                    + "WHERE CODIGO=?");
 
-			stmt.setLong(1, solicitacaoFolga.getColaborador().getCpf());
+            stmt.setLong(1, solicitacaoFolga.getColaborador().getCpf());
 
-			if(solicitacaoFolga.getColaboradorFeedback() != null){
-				stmt.setLong(2, solicitacaoFolga.getColaboradorFeedback().getCpf());
-			}else{
-				stmt.setNull(2, java.sql.Types.BIGINT);
-			}
-			if(solicitacaoFolga.getDataSolicitacao() != null){
-				stmt.setDate(3, DateUtils.toSqlDate(solicitacaoFolga.getDataSolicitacao()));
-			}else{
-				stmt.setDate(3, null);
-			}
-			if(solicitacaoFolga.getDataFolga() != null){
-				stmt.setDate(4, DateUtils.toSqlDate(solicitacaoFolga.getDataFolga()));
-			}else{
-				stmt.setDate(4, null);
-			}
-			if(solicitacaoFolga.getDataFeedback() != null){
-				stmt.setDate(5, DateUtils.toSqlDate(solicitacaoFolga.getDataFeedback()));
-			}else{
-				stmt.setDate(5, new Date(System.currentTimeMillis()));
-			}
-			stmt.setString(6, solicitacaoFolga.getMotivoFolga());
-			stmt.setString(7, solicitacaoFolga.getJustificativaFeedback());
-			stmt.setString(8, solicitacaoFolga.getStatus());
-			stmt.setString(9, solicitacaoFolga.getPeriodo());
-			stmt.setLong(10, solicitacaoFolga.getCodigo());
-			int count = stmt.executeUpdate();
-			if(count > 0){
-				return true;
-			}else{
-				return false;
-			}
-		}
-		finally {
-			closeConnection(conn, stmt, null);
-		}
-	}
+            if (solicitacaoFolga.getColaboradorFeedback() != null) {
+                stmt.setLong(2, solicitacaoFolga.getColaboradorFeedback().getCpf());
+            } else {
+                stmt.setNull(2, java.sql.Types.BIGINT);
+            }
+            if (solicitacaoFolga.getDataSolicitacao() != null) {
+                stmt.setDate(3, DateUtils.toSqlDate(solicitacaoFolga.getDataSolicitacao()));
+            } else {
+                stmt.setDate(3, null);
+            }
+            if (solicitacaoFolga.getDataFolga() != null) {
+                stmt.setDate(4, DateUtils.toSqlDate(solicitacaoFolga.getDataFolga()));
+            } else {
+                stmt.setDate(4, null);
+            }
+            if (solicitacaoFolga.getDataFeedback() != null) {
+                stmt.setDate(5, DateUtils.toSqlDate(solicitacaoFolga.getDataFeedback()));
+            } else {
+                stmt.setDate(5, new Date(System.currentTimeMillis()));
+            }
+            stmt.setString(6, solicitacaoFolga.getMotivoFolga());
+            stmt.setString(7, solicitacaoFolga.getJustificativaFeedback());
+            stmt.setString(8, solicitacaoFolga.getStatus());
+            stmt.setString(9, solicitacaoFolga.getPeriodo());
+            stmt.setLong(10, solicitacaoFolga.getCodigo());
+            final int count = stmt.executeUpdate();
+            return count > 0;
+        } finally {
+            closeConnection(conn, stmt, null);
+        }
+    }
 
-	@Override
-	public boolean delete(Long codigo) throws SQLException {
-		Connection conn = null;
-		PreparedStatement stmt = null;
+    @Override
+    public boolean delete(Long codigo) throws SQLException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        try {
+            conn = getConnection();
+            stmt = conn.prepareStatement("DELETE FROM SOLICITACAO_FOLGA WHERE CODIGO = ? AND STATUS = 'PENDENTE'");
+            stmt.setLong(1, codigo);
+            final int count = stmt.executeUpdate();
+            if (count > 0) {
+                return true;
+            }
+        } finally {
+            closeConnection(conn, stmt, null);
+        }
+        return false;
+    }
 
-		try {
-			conn = getConnection();
-			stmt = conn.prepareStatement("DELETE FROM SOLICITACAO_FOLGA WHERE CODIGO = ? AND STATUS = 'PENDENTE'");
-			stmt.setLong(1, codigo);
-			int count = stmt.executeUpdate();
-			if(count > 0){
-				return true;
-			}
-		}	
-		finally{
-			closeConnection(conn, stmt, null);
-		}
-		return false;
-	}
+    @Override
+    public List<SolicitacaoFolga> getAll(LocalDate dataInicial, LocalDate dataFinal,
+                                         Long codUnidade, String codEquipe, String status, String cpfColaborador) throws SQLException {
 
-	@Override
-	public List<SolicitacaoFolga> getAll(LocalDate dataInicial, LocalDate dataFinal, 
-			Long codUnidade, String codEquipe, String status, String cpfColaborador) throws SQLException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rSet = null;
+        final List<SolicitacaoFolga> list = new ArrayList<>();
+        try {
+            conn = getConnection();
+            String query = "SELECT SF.*, C.NOME AS NOME_SOLICITANTE, C_FEEDBACK.NOME AS NOME_FEEDBACK FROM "
+                    + "SOLICITACAO_FOLGA SF "
+                    + "JOIN COLABORADOR C ON C.CPF = SF.CPF_COLABORADOR "
+                    + "LEFT JOIN COLABORADOR C_FEEDBACK ON C_FEEDBACK.CPF = SF.CPF_FEEDBACK "
+                    + "JOIN EQUIPE E ON E.CODIGO = C.COD_EQUIPE "
+                    + "WHERE SF.DATA_FOLGA BETWEEN ? AND ? "
+                    + "AND C.COD_UNIDADE = ? "
+                    + "AND E.CODIGO::TEXT LIKE ? "
+                    + "AND SF.STATUS LIKE ? "
+                    + "AND SF.CPF_COLABORADOR::TEXT LIKE ?"
+                    + "ORDER BY SF.DATA_SOLICITACAO";
 
-		Connection conn = null;
-		PreparedStatement stmt = null;
-		ResultSet rSet = null;
-		List<SolicitacaoFolga> list = new ArrayList<>();
-		try {
-			conn = getConnection();
-			String query = "SELECT SF.*, C.NOME AS NOME_SOLICITANTE, C_FEEDBACK.NOME AS NOME_FEEDBACK FROM "
-					+ "SOLICITACAO_FOLGA SF "
-					+ "JOIN COLABORADOR C ON C.CPF = SF.CPF_COLABORADOR "
-					+ "LEFT JOIN COLABORADOR C_FEEDBACK ON C_FEEDBACK.CPF = SF.CPF_FEEDBACK "
-					+ "JOIN EQUIPE E ON E.CODIGO = C.COD_EQUIPE "
-					+ "WHERE SF.DATA_FOLGA BETWEEN ? AND ? "
-					+ "AND C.COD_UNIDADE = ? "
-					+ "AND E.CODIGO::TEXT LIKE ? "
-					+ "AND SF.STATUS LIKE ? "
-					+ "AND SF.CPF_COLABORADOR::TEXT LIKE ?"
-					+ "ORDER BY SF.DATA_SOLICITACAO";
+            stmt = conn.prepareStatement(query);
+            stmt.setDate(1, DateUtils.toSqlDate(dataInicial));
+            stmt.setDate(2, DateUtils.toSqlDate(dataFinal));
+            stmt.setLong(3, codUnidade);
+            stmt.setString(4, codEquipe);
+            stmt.setString(5, status);
+            stmt.setString(6, cpfColaborador);
+            rSet = stmt.executeQuery();
+            while (rSet.next()) {
+                list.add(createSolicitacaoFolga(rSet));
+            }
+        } finally {
+            closeConnection(conn, stmt, rSet);
+        }
+        return list;
+    }
 
-			stmt = conn.prepareStatement(query);
-			stmt.setDate(1, DateUtils.toSqlDate(dataInicial));
-			stmt.setDate(2, DateUtils.toSqlDate(dataFinal));
-			stmt.setLong(3, codUnidade);
-			stmt.setString(4, codEquipe);
-			stmt.setString(5, status);
-			stmt.setString(6, cpfColaborador);
-			rSet = stmt.executeQuery();
-			while(rSet.next()){
-				list.add(createSolicitacaoFolga(rSet));
-			}				
-		}
-		finally {
-			closeConnection(conn, stmt, rSet);
-		}
-		return list;
-	}
+    @Override
+    public List<SolicitacaoFolga> getByColaborador(Long cpf) throws SQLException {
+        List<SolicitacaoFolga> list = new ArrayList<>();
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rSet = null;
+        try {
+            conn = getConnection();
+            stmt = conn.prepareStatement("SELECT SF.CODIGO, SF.CPF_COLABORADOR, " +
+                    "SF.CPF_FEEDBACK, SF.DATA_FEEDBACK, SF.DATA_FOLGA, " +
+                    "SF.DATA_SOLICITACAO, SF.MOTIVO_FOLGA, " +
+                    "SF.JUSTIFICATIVA_FEEDBACK, SF.PERIODO, SF.STATUS, C.NOME AS NOME_SOLICITANTE, " +
+                    "C_FEEDBACK.NOME AS NOME_FEEDBACK " +
+                    "FROM SOLICITACAO_FOLGA SF JOIN COLABORADOR C ON " +
+                    "SF.CPF_COLABORADOR = C.CPF LEFT JOIN COLABORADOR C_FEEDBACK ON " +
+                    "SF.CPF_FEEDBACK = C_FEEDBACK.CPF WHERE " +
+                    "SF.CPF_COLABORADOR = ?;");
+            stmt.setLong(1, cpf);
+            rSet = stmt.executeQuery();
+            while (rSet.next()) {
+                list.add(createSolicitacaoFolga(rSet));
+            }
+        } finally {
+            closeConnection(conn, stmt, rSet);
+        }
+        return list;
+    }
 
-	@Override
-	public List<SolicitacaoFolga> getByColaborador(Long cpf) throws SQLException {
-		List<SolicitacaoFolga> list  = new ArrayList<>();
-		Connection conn = null;
-		PreparedStatement stmt = null;
-		ResultSet rSet = null;
-		try {
-			conn = getConnection();
-			stmt = conn.prepareStatement("SELECT SF.CODIGO, SF.CPF_COLABORADOR, " +
-					"SF.CPF_FEEDBACK, SF.DATA_FEEDBACK, SF.DATA_FOLGA, " +
-					"SF.DATA_SOLICITACAO, SF.MOTIVO_FOLGA, " +
-					"SF.JUSTIFICATIVA_FEEDBACK, SF.PERIODO, SF.STATUS, C.NOME AS NOME_SOLICITANTE, " +
-					"C_FEEDBACK.NOME AS NOME_FEEDBACK " +
-					"FROM SOLICITACAO_FOLGA SF JOIN COLABORADOR C ON " +
-					"SF.CPF_COLABORADOR = C.CPF LEFT JOIN COLABORADOR C_FEEDBACK ON " +
-					"SF.CPF_FEEDBACK = C_FEEDBACK.CPF WHERE " +
-					"SF.CPF_COLABORADOR = ?;");
-			stmt.setLong(1, cpf);
-			rSet = stmt.executeQuery();
-			while (rSet.next()) {
-				SolicitacaoFolga solicitacaoFolga = createSolicitacaoFolga(rSet);
-				list.add(solicitacaoFolga);
-			}
-		} finally {
-			closeConnection(conn, stmt, rSet);
-		}
-		return list;
-	}
+    private SolicitacaoFolga createSolicitacaoFolga(ResultSet rSet) throws SQLException {
+        final SolicitacaoFolga solicitacaoFolga = new SolicitacaoFolga();
+        solicitacaoFolga.setCodigo(rSet.getLong("CODIGO"));
 
-	private SolicitacaoFolga createSolicitacaoFolga(ResultSet rSet) throws SQLException {
-		SolicitacaoFolga solicitacaoFolga = new SolicitacaoFolga();
-		solicitacaoFolga.setCodigo(rSet.getLong("CODIGO"));
+        final Colaborador colaborador = new Colaborador();
+        colaborador.setCpf(rSet.getLong("CPF_COLABORADOR"));
+        colaborador.setNome(rSet.getString("NOME_SOLICITANTE"));
+        solicitacaoFolga.setColaborador(colaborador);
 
-		Colaborador colaborador = new Colaborador();
-		colaborador.setCpf(rSet.getLong("CPF_COLABORADOR"));
-		colaborador.setNome(rSet.getString("NOME_SOLICITANTE"));
-		solicitacaoFolga.setColaborador(colaborador);
+        final Colaborador colaboradorFeedback = new Colaborador();
+        colaboradorFeedback.setCpf(rSet.getLong("CPF_FEEDBACK"));
+        colaboradorFeedback.setNome(rSet.getString("NOME_FEEDBACK"));
+        solicitacaoFolga.setColaboradorFeedback(colaboradorFeedback);
 
-		Colaborador colaboradorFeedback = new Colaborador();
-		colaboradorFeedback.setCpf(rSet.getLong("CPF_FEEDBACK"));
-		colaboradorFeedback.setNome(rSet.getString("NOME_FEEDBACK"));
-		solicitacaoFolga.setColaboradorFeedback(colaboradorFeedback);
-
-		solicitacaoFolga.setDataFeedback(rSet.getDate("DATA_FEEDBACK"));
-		solicitacaoFolga.setDataFolga(rSet.getDate("DATA_FOLGA"));
-		solicitacaoFolga.setDataSolicitacao(rSet.getDate("DATA_SOLICITACAO"));
-		solicitacaoFolga.setMotivoFolga(rSet.getString("MOTIVO_FOLGA"));
-		solicitacaoFolga.setJustificativaFeedback(rSet.getString("JUSTIFICATIVA_FEEDBACK"));
-		solicitacaoFolga.setPeriodo(rSet.getString("PERIODO"));
-		solicitacaoFolga.setStatus(rSet.getString("STATUS"));
-		return solicitacaoFolga;
-	}
-
+        solicitacaoFolga.setDataFeedback(rSet.getDate("DATA_FEEDBACK"));
+        solicitacaoFolga.setDataFolga(rSet.getDate("DATA_FOLGA"));
+        solicitacaoFolga.setDataSolicitacao(rSet.getDate("DATA_SOLICITACAO"));
+        solicitacaoFolga.setMotivoFolga(rSet.getString("MOTIVO_FOLGA"));
+        solicitacaoFolga.setJustificativaFeedback(rSet.getString("JUSTIFICATIVA_FEEDBACK"));
+        solicitacaoFolga.setPeriodo(rSet.getString("PERIODO"));
+        solicitacaoFolga.setStatus(rSet.getString("STATUS"));
+        return solicitacaoFolga;
+    }
 }
