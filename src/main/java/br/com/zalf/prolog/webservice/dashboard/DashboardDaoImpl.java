1e51c3384e09bba80199a902115037969be5cc4d
Luiz Felipe <luiz.felipe_95@hotmail.com>
2018-02-22 19:03:15 -0300

    fix(dashboards): corrige query de busca dos componentes de um colaborador

--- src/main/java/br/com/zalf/prolog/webservice/dashboard/DashboardDaoImpl.java
+++ src/main/java/br/com/zalf/prolog/webservice/dashboard/DashboardDaoImpl.java
@@ -108,7 +108,7 @@ public class DashboardDaoImpl extends DatabaseConnection implements DashboardDao
                     "  JOIN PUBLIC.CARGO_FUNCAO_PROLOG_V11 CFP ON C.COD_FUNCAO = CFP.COD_FUNCAO_COLABORADOR AND C.COD_UNIDADE = CFP.COD_UNIDADE " +
                     "  JOIN PUBLIC.DASHBOARD_COMPONENTE_FUNCAO_PROLOG DCFP ON CFP.COD_FUNCAO_PROLOG = DCFP.COD_FUNCAO_PROLOG AND DC.CODIGO = DCFP.COD_COMPONENTE " +
                     "  JOIN PUBLIC.DASHBOARD_COMPONENTE_TIPO DCT ON DC.COD_TIPO_COMPONENTE = DCT.CODIGO " +
-                    "  GROUP BY 1,2,3;");
+                    "  ORDER BY 3;");
             stmt.setString(1, userToken);
             rSet = stmt.executeQuery();
             final List<DashboardPilarComponents> componentsPilar = new ArrayList<>();
