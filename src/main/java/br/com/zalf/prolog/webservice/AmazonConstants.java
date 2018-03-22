b8f9d84ba0f709f2d5c8c428d4a2ed721320d23f
didi <dioge.vanzella@gmail.com>
2018-03-22 11:22:07 -0300

    refactor(AmazonConstants): insere bucket para onde as imagens de checklists devem ir

--- src/main/java/br/com/zalf/prolog/webservice/AmazonConstants.java
+++ src/main/java/br/com/zalf/prolog/webservice/AmazonConstants.java
@@ -8,12 +8,16 @@ public class AmazonConstants {
     public static final String AWS_SECRET_KEY = "8GVMek8o28VEssST5yM0RHipZYW6gz8wO/buKLig";
 
     private static final String BUCKET_TEST_TREINAMENTO = "prolog-teste/treinamento";
+    private static final String BUCKET_TEST_CHECKLIST = "prolog-teste/checklist";
     public static final String BUCKET_NAME_PDF_TREINAMENTOS = BuildConfig.DEBUG
             ? BUCKET_TEST_TREINAMENTO
             : "treinamentos-prolog/pdf";
     public static final String BUCKET_NAME_IMAGES_TREINAMENTOS = BuildConfig.DEBUG
             ? BUCKET_TEST_TREINAMENTO
             : "treinamentos-prolog/images";
+    public static final String BUCKET_CHECKLIST_GALERIA_IMAGENS = BuildConfig.DEBUG
+            ? BUCKET_TEST_CHECKLIST
+            : "checklist-imagens";
 
     private AmazonConstants() {
         throw new IllegalStateException(AmazonConstants.class.getSimpleName() + " cannot be instantiated!");
