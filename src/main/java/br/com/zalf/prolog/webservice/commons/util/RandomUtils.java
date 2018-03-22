e510c34d9f148b1245a88d9f95bcd0f2e8aae4f6
didi <dioge.vanzella@gmail.com>
2018-03-22 11:21:23 -0300

    feat(RandomUtils): cria classe utilitaria para gerar "coisas" randomicas

---
+++ src/main/java/br/com/zalf/prolog/webservice/commons/util/RandomUtils.java
@@ -0,0 +1,28 @@
+package br.com.zalf.prolog.webservice.commons.util;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Random;
+
+/**
+ * Created on 22/03/18.
+ *
+ * @author Diogenes Vanzela (https://github.com/diogenesvanzella)
+ */
+public class RandomUtils {
+    private static final String DATA = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private final static Random RANDOM = new Random();
+
+    private RandomUtils() {
+        throw new IllegalStateException(RandomUtils.class.getSimpleName() + " cannot be instantiated!");
+    }
+
+    @NotNull
+    public static String randomAlphanumeric(final int length) {
+        final StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            sb.append(DATA.charAt(RANDOM.nextInt(DATA.length())));
+        }
+        return sb.toString();
+    }
+}
