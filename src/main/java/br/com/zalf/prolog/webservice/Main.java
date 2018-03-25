0ee3be68bd08829ca7858263e51572eb683f43a4
Luiz Felipe <luiz.felipe_95@hotmail.com>
2018-03-25 16:39:47 -0300

    refactor(Main): limpa classe main

--- src/main/java/br/com/zalf/prolog/webservice/Main.java
+++ src/main/java/br/com/zalf/prolog/webservice/Main.java
@@ -1,27 +1,8 @@
 package br.com.zalf.prolog.webservice;
 
-import java.sql.SQLException;
-import java.util.Random;
-
-
 public class Main {
 
-	public static final String DATA = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	public static Random RANDOM = new Random();
+    public static void main(String[] args) {
 
-	public static String randomString(int len) {
-		StringBuilder sb = new StringBuilder(len);
-
-		for (int i = 0; i < len; i++) {
-			sb.append(DATA.charAt(RANDOM.nextInt(DATA.length())));
-		}
-
-		return sb.toString();
-	}
-
-	public static void main(String[] args) throws SQLException {
-
-
-		System.out.println(randomString(10));
-	}
+    }
 }
