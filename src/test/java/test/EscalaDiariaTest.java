0f878c2be86e845c1719143b8976fc48d3e415a6
didi <dioge.vanzella@gmail.com>
2018-04-23 17:30:46 -0300

    refactor(Test): cria base class para os testes, ele fará o init do banco

--- src/test/java/test/EscalaDiariaTest.java
+++ src/test/java/test/EscalaDiariaTest.java
@@ -1,16 +1,12 @@
 package test;
 
 import br.com.zalf.prolog.webservice.Injection;
-import br.com.zalf.prolog.webservice.database.DatabaseConnection;
-import br.com.zalf.prolog.webservice.database.DatabaseManager;
 import br.com.zalf.prolog.webservice.imports.escala_diaria.EscalaDiaria;
 import br.com.zalf.prolog.webservice.imports.escala_diaria.EscalaDiariaDao;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.sql.SQLException;
-import java.time.LocalDate;
 import java.util.List;
 
 /**
@@ -18,15 +14,11 @@ import java.util.List;
  *
  * @author Diogenes Vanzela (https://github.com/diogenesvanzella)
  */
-public class EscalaDiariaTest extends DatabaseConnection {
+public class EscalaDiariaTest extends BaseTest {
 
-    private static final long COD_UNIDADE = 5;
-    private static final LocalDate DATA_INICIAL = LocalDate.parse("2018-03-18");
-    private static final LocalDate DATA_FINAL = LocalDate.parse("2018-04-19");
-
-    @Before
+    @Override
     public void initialize() {
-        DatabaseManager.init();
+        // do nothing
     }
 
     @Test
