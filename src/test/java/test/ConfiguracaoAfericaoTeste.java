0fc5beb8e8640cbd9ee0535b04f082f738dcfc41
didi <dioge.vanzella@gmail.com>
2018-05-08 18:48:41 -0300

    feat(Test): cria classe de Teste para os métodos de configurações de aferição

---
+++ src/test/java/test/ConfiguracaoAfericaoTeste.java
@@ -0,0 +1,96 @@
+package test;
+
+import br.com.zalf.prolog.webservice.Injection;
+import br.com.zalf.prolog.webservice.commons.gson.GsonUtils;
+import br.com.zalf.prolog.webservice.frota.pneu.afericao.configuracao.ConfiguracaoAfericaoDao;
+import br.com.zalf.prolog.webservice.frota.pneu.afericao.model.ConfiguracaoTipoVeiculoAfericao;
+import br.com.zalf.prolog.webservice.frota.veiculo.model.TipoVeiculo;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * Created on 08/05/18.
+ *
+ * @author Diogenes Vanzela (https://github.com/diogenesvanzella)
+ */
+public class ConfiguracaoAfericaoTeste extends BaseTest {
+    @Override
+    public void initialize() {
+        // Do nothing
+    }
+
+    @Test
+    public void testInsertConfiguracoesTipoAfericao() throws SQLException {
+        final ConfiguracaoAfericaoDao dao = Injection.provideConfiguracaoAfericaoDao();
+        final ConfiguracaoTipoVeiculoAfericao config = new ConfiguracaoTipoVeiculoAfericao();
+        config.setCodUnidade(5L);
+        final TipoVeiculo tipoVeiculo = new TipoVeiculo();
+        tipoVeiculo.setCodigo(13L);
+        config.setTipoVeiculo(tipoVeiculo);
+        config.setPodeAferirSulco(true);
+        config.setPodeAferirPressao(true);
+        config.setPodeAferirSulcoPressao(false);
+        config.setPodeAferirEstepe(true);
+
+        dao.insertOrUpdateConfiguracao(5L, config);
+
+        List<ConfiguracaoTipoVeiculoAfericao> configuracoes = dao.getConfiguracoesTipoAfericaoVeiculo(5L);
+
+        System.out.println(GsonUtils.getGson().toJson(configuracoes));
+        Assert.assertFalse(configuracoes.isEmpty());
+    }
+
+    @Test
+    public void testGetConfiguracoesTipoAfericao() throws SQLException {
+        final ConfiguracaoAfericaoDao dao = Injection.provideConfiguracaoAfericaoDao();
+        List<ConfiguracaoTipoVeiculoAfericao> configuracoes = dao.getConfiguracoesTipoAfericaoVeiculo(5L);
+
+        System.out.println(GsonUtils.getGson().toJson(configuracoes));
+        Assert.assertFalse(configuracoes.isEmpty());
+    }
+
+    @Test
+    public void testUpdateAndGetConfiguracoesTipoAfericao() throws SQLException {
+        final ConfiguracaoAfericaoDao dao = Injection.provideConfiguracaoAfericaoDao();
+        final ConfiguracaoTipoVeiculoAfericao config = new ConfiguracaoTipoVeiculoAfericao();
+        config.setCodUnidade(5L);
+        final TipoVeiculo tipoVeiculo = new TipoVeiculo();
+        tipoVeiculo.setCodigo(63L);
+        config.setTipoVeiculo(tipoVeiculo);
+        config.setPodeAferirSulco(false);
+        config.setPodeAferirPressao(false);
+        config.setPodeAferirSulcoPressao(true);
+        config.setPodeAferirEstepe(false);
+
+        dao.insertOrUpdateConfiguracao(5L, config);
+
+        List<ConfiguracaoTipoVeiculoAfericao> configuracoes = dao.getConfiguracoesTipoAfericaoVeiculo(5L);
+
+        System.out.println(GsonUtils.getGson().toJson(configuracoes));
+        Assert.assertFalse(configuracoes.isEmpty());
+    }
+
+    @Test
+    public void testUpdateSameConfiguracoesTipoAfericao() throws SQLException {
+        final ConfiguracaoAfericaoDao dao = Injection.provideConfiguracaoAfericaoDao();
+        final ConfiguracaoTipoVeiculoAfericao config = new ConfiguracaoTipoVeiculoAfericao();
+        config.setCodUnidade(5L);
+        final TipoVeiculo tipoVeiculo = new TipoVeiculo();
+        tipoVeiculo.setCodigo(63L);
+        config.setTipoVeiculo(tipoVeiculo);
+        config.setPodeAferirSulco(false);
+        config.setPodeAferirPressao(false);
+        config.setPodeAferirSulcoPressao(false);
+        config.setPodeAferirEstepe(false);
+
+        dao.insertOrUpdateConfiguracao(5L, config);
+
+        List<ConfiguracaoTipoVeiculoAfericao> configuracoes = dao.getConfiguracoesTipoAfericaoVeiculo(5L);
+
+        System.out.println(GsonUtils.getGson().toJson(configuracoes));
+        Assert.assertFalse(configuracoes.isEmpty());
+    }
+}
