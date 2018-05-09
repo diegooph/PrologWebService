530b89f6772cbe33491c887abb3e84d010891902
thaisksf <thaisksf@gmail.com>
2018-05-09 13:26:29 -0300

    refactor(errorhandling): altera construtor exception genérica

--- src/main/java/br/com/zalf/prolog/webservice/errorhandling/exception/GenericException.java
+++ src/main/java/br/com/zalf/prolog/webservice/errorhandling/exception/GenericException.java
@@ -1,10 +1,28 @@
 package br.com.zalf.prolog.webservice.errorhandling.exception;
 
 
+import org.jetbrains.annotations.NotNull;
+
+import javax.ws.rs.core.Response;
+
 public class GenericException extends ProLogException {
 
-    public GenericException (int httpStatusCode, String message, String developerMessage) {
+    public GenericException(@NotNull final String message,
+                            @NotNull final String developerMessage,
+                            @NotNull final Exception parentException) {
+            super(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(),
+                    ProLogErrorCodes.GENERIC.errorCode(),
+                    message,
+                    developerMessage,
+                    parentException);
+    }
 
+    public GenericException(@NotNull final String message,
+                            @NotNull final String developerMessage) {
+        super(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(),
+                ProLogErrorCodes.GENERIC.errorCode(),
+                message,
+                developerMessage);
     }
 
 }
