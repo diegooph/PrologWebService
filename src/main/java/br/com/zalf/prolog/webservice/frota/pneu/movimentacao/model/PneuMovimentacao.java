587d55895c5579cedcd9c051f35c05e1a23e61eb
didi <dioge.vanzella@gmail.com>
2018-05-26 15:19:40 -0300

    refactor(PneuMovimentacao): cria objeto pneu específico para ser utilizado nos casos das movimentações

---
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/movimentacao/model/PneuMovimentacao.java
@@ -0,0 +1,20 @@
+package br.com.zalf.prolog.webservice.frota.pneu.movimentacao.model;
+
+import br.com.zalf.prolog.webservice.frota.pneu.pneu.model.Pneu;
+
+/**
+ * Created on 25/05/18.
+ *
+ * @author Diogenes Vanzela (https://github.com/diogenesvanzella)
+ */
+public class PneuMovimentacao extends Pneu {
+
+    public PneuMovimentacao() {
+        super();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
---
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/movimentacao/model/PneuMovimentacaoAnalise.java
@@ -0,0 +1,42 @@
+package br.com.zalf.prolog.webservice.frota.pneu.movimentacao.model;
+
+import br.com.zalf.prolog.webservice.frota.pneu.recapadoras.Recapadora;
+
+/**
+ * Created on 25/05/18.
+ *
+ * @author Diogenes Vanzela (https://github.com/diogenesvanzella)
+ */
+public class PneuMovimentacaoAnalise extends PneuMovimentacao {
+    private Recapadora recapadora;
+    private String codColeta;
+
+    public PneuMovimentacaoAnalise() {
+    }
+
+    public PneuMovimentacaoAnalise(final Recapadora recapadora, final String codColeta) {
+        this.recapadora = recapadora;
+        this.codColeta = codColeta;
+    }
+
+    public Recapadora getRecapadora() {
+        return recapadora;
+    }
+
+    public void setRecapadora(final Recapadora recapadora) {
+        this.recapadora = recapadora;
+    }
+
+    public String getCodColeta() {
+        return codColeta;
+    }
+
+    public void setCodColeta(final String codColeta) {
+        this.codColeta = codColeta;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
