431711ee5e486cbbed211b916ca9521d690787aa
didi <dioge.vanzella@gmail.com>
2018-06-21 19:06:05 -0300

    refactor(ServicoRelatorio): altera paths dos relatórios de servico

--- src/main/java/br/com/zalf/prolog/webservice/frota/pneu/servico/relatorio/ServicoRelatorioService.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/servico/relatorio/ServicoRelatorioService.java
@@ -9,79 +9,80 @@ import org.jetbrains.annotations.NotNull;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.sql.SQLException;
+import java.util.List;
 
 public final class ServicoRelatorioService {
-    private static final String TAG =  ServicoRelatorioService.class.getSimpleName();
+    private static final String TAG = ServicoRelatorioService.class.getSimpleName();
     private final ServicoRelatorioDao dao = Injection.provideServicoRelatorioDao();
 
-    public Report getEstratificacaoServicosFechadosReport(@NotNull final Long codUnidade,
+    public Report getEstratificacaoServicosFechadosReport(@NotNull final List<Long> codUnidades,
                                                           @NotNull final String dataInicial,
                                                           @NotNull final String dataFinal) {
         try {
             return dao.getEstratificacaoServicosFechadosReport(
-                    codUnidade,
+                    codUnidades,
                     ProLogDateParser.validateAndParse(dataInicial),
                     ProLogDateParser.validateAndParse(dataFinal));
         } catch (SQLException e) {
             Log.e(TAG, String.format("Erro ao buscar o relatório que estratifica os serviços fechados (REPORT). \n" +
-                    "Unidade: %d \n" +
+                    "Unidades: %s \n" +
                     "Data Inicial: %s \n" +
-                    "Data Final: %s", codUnidade, dataInicial, dataFinal), e);
+                    "Data Final: %s", codUnidades, dataInicial, dataFinal), e);
             return null;
         }
     }
 
     public void getEstratificacaoServicosFechadosCsv(@NotNull final OutputStream outputStream,
-                                                     @NotNull final Long codUnidade,
+                                                     @NotNull final List<Long> codUnidades,
                                                      @NotNull final String dataInicial,
                                                      @NotNull final String dataFinal) throws RuntimeException {
         try {
             dao.getEstratificacaoServicosFechadosCsv(
                     outputStream,
-                    codUnidade,
+                    codUnidades,
                     ProLogDateParser.validateAndParse(dataInicial),
                     ProLogDateParser.validateAndParse(dataFinal));
         } catch (SQLException | IOException e) {
             Log.e(TAG, String.format("Erro ao buscar o relatório que estratifica os serviços fechados (CSV). \n" +
-                    "Unidade: %d \n" +
+                    "Unidades: %s \n" +
                     "Data Inicial: %s \n" +
-                    "Data Final: %s", codUnidade, dataInicial, dataFinal), e);
+                    "Data Final: %s", codUnidades, dataInicial, dataFinal), e);
             throw new RuntimeException();
         }
     }
 
-    public Report getEstratificacaoServicosAbertosReport(@NotNull final Long codUnidade,
-                                                          @NotNull final String dataInicial,
-                                                          @NotNull final String dataFinal) {
+    public Report getEstratificacaoServicosAbertosReport(@NotNull final List<Long> codUnidades,
+                                                         @NotNull final String dataInicial,
+                                                         @NotNull final String dataFinal) {
         try {
             return dao.getEstratificacaoServicosAbertosReport(
-                    codUnidade,
+                    codUnidades,
                     ProLogDateParser.validateAndParse(dataInicial),
                     ProLogDateParser.validateAndParse(dataFinal));
         } catch (SQLException e) {
             Log.e(TAG, String.format("Erro ao buscar o relatório que estratifica os serviços abertos (REPORT). \n" +
-                    "Unidade: %d \n" +
+                    "Unidades: %s \n" +
                     "Data Inicial: %s \n" +
-                    "Data Final: %s", codUnidade, dataInicial, dataFinal), e);
+                    "Data Final: %s", codUnidades, dataInicial, dataFinal), e);
             return null;
         }
     }
 
     public void getEstratificacaoServicosAbertosCsv(@NotNull final OutputStream outputStream,
-                                                     @NotNull final Long codUnidade,
-                                                     @NotNull final String dataInicial,
-                                                     @NotNull final String dataFinal) throws RuntimeException {
+                                                    @NotNull final List<Long> codUnidades,
+                                                    @NotNull final String dataInicial,
+                                                    @NotNull final String dataFinal) throws RuntimeException {
         try {
             dao.getEstratificacaoServicosAbertosCsv(
                     outputStream,
-                    codUnidade,
+                    codUnidades,
                     ProLogDateParser.validateAndParse(dataInicial),
                     ProLogDateParser.validateAndParse(dataFinal));
         } catch (SQLException | IOException e) {
             Log.e(TAG, String.format("Erro ao buscar o relatório que estratifica os serviços abertos (CSV). \n" +
-                    "Unidade: %d \n" +
+                    "Unidades: %s \n" +
                     "Data Inicial: %s \n" +
-                    "Data Final: %s", codUnidade, dataInicial, dataFinal), e);
+                    "Data Final: %s", codUnidades, dataInicial, dataFinal), e);
             throw new RuntimeException();
         }
     }
