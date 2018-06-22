d65ac1975589aa436563e7847f5ddbf2df061755
Luiz Felipe <luiz.felipe_95@hotmail.com>
2018-06-22 11:40:21 -0300

    feat: cria método para inserir novas mensagens através de formulário de contato no app

---
+++ src/main/java/br/com/zalf/prolog/webservice/contato/EntreEmContatoResource.java
@@ -0,0 +1,30 @@
+package br.com.zalf.prolog.webservice.contato;
+
+import br.com.zalf.prolog.webservice.commons.network.AbstractResponse;
+import br.com.zalf.prolog.webservice.errorhandling.exception.ProLogException;
+import org.jetbrains.annotations.NotNull;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created on 21/06/2018
+ *
+ * @author Luiz Felipe (https://github.com/luizfp)
+ */
+@Path("/comercial/mensagens")
+@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
+@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+public final class EntreEmContatoResource {
+    @NotNull
+    private final EntreEmContatoService service = new EntreEmContatoService();
+
+    @POST
+    @Path("/")
+    public AbstractResponse insert(@NotNull final MensagemContato contato) throws ProLogException {
+        return service.insertNovoContato(contato);
+    }
+}
