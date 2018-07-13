6d0027abe6ee646afbb412d6523316b33aa59c76
Luiz Felipe <luiz.felipe_95@hotmail.com>
2018-07-13 20:42:27 -0300

    refactor: bloqueia tudo referente a pneus para a versão 55 do app e abaixo

--- src/main/java/br/com/zalf/prolog/webservice/frota/pneu/movimentacao/MovimentacaoResource.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/movimentacao/MovimentacaoResource.java
@@ -29,7 +29,7 @@ import java.util.List;
 @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
 @AppVersionCodeHandler(
         implementation = DefaultAppVersionCodeHandler.class,
-        targetVersionCode = 51,
+        targetVersionCode = 55,
         versionCodeHandlerMode = VersionCodeHandlerMode.BLOCK_THIS_VERSION_AND_BELOW,
         actionIfVersionNotPresent = VersionNotPresentAction.BLOCK_ANYWAY)
 public class MovimentacaoResource {
