d3294e2ef3a828bb0d68219df019f2a4aa1686b8
Luiz Felipe <luiz.felipe_95@hotmail.com>
2018-07-30 13:39:52 -0300

    refactor: altera mensagem de erro do relatório de folha de ponto

--- src/main/java/br/com/zalf/prolog/webservice/gente/controleintervalo/relatorios/ControleIntervaloRelatorioService.java
+++ src/main/java/br/com/zalf/prolog/webservice/gente/controleintervalo/relatorios/ControleIntervaloRelatorioService.java
@@ -144,6 +144,7 @@ public class ControleIntervaloRelatorioService {
         }
     }
 
+    @NotNull
     public List<FolhaPontoRelatorio> getFolhaPontoRelatorio(@NotNull final Long codUnidade,
                                                             @NotNull final String codTipoIntervalo,
                                                             @NotNull final String cpf,
@@ -164,7 +165,7 @@ public class ControleIntervaloRelatorioService {
                     "dataInicial: %s \n" +
                     "dataFinal: %s", codUnidade, codTipoIntervalo, cpf, dataInicial, dataFinal);
             Log.e(TAG, errorMessage, e);
-            throw exceptionHandler.map(e, errorMessage);
+            throw exceptionHandler.map(e, "Erro ao gerar relatório, tente novamente");
         }
     }
 
