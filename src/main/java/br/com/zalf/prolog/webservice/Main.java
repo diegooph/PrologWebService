11ffad81b30b81096f2351aae1d92f7d9b735e5a
didi <dioge.vanzella@gmail.com>
2018-08-28 11:28:57 -0300

    refactor(teste): teste commit na PL-904

--- src/main/java/br/com/zalf/prolog/webservice/Main.java
+++ src/main/java/br/com/zalf/prolog/webservice/Main.java
@@ -3,6 +3,6 @@ package br.com.zalf.prolog.webservice;
 public class Main {
 
     public static void main(String[] args) {
-        String teste = "teste PL nos commits";
+        String teste = "teste PL nos commits mas no subject";
     }
 }
