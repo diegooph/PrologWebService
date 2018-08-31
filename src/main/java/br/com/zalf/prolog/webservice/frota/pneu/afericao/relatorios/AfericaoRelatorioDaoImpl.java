d7d2032c5cf881fd6ad0bc4abfe2de65df71cd5e
thaisksf <thaisksf@gmail.com>
2018-08-31 14:50:10 -0300

    refactor(AfericaoRelatorio): avança no desenvolvimento do relatório de todas as aferições realizadas.
    PL-1112

--- src/main/java/br/com/zalf/prolog/webservice/frota/pneu/afericao/relatorios/AfericaoRelatorioDaoImpl.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/afericao/relatorios/AfericaoRelatorioDaoImpl.java
@@ -1,8 +1,18 @@
 package br.com.zalf.prolog.webservice.frota.pneu.afericao.relatorios;
 
+import br.com.zalf.prolog.webservice.commons.report.CsvWriter;
+import br.com.zalf.prolog.webservice.commons.report.Report;
+import br.com.zalf.prolog.webservice.commons.report.ReportTransformer;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.OutputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.time.LocalDate;
+
+import static br.com.zalf.prolog.webservice.database.DatabaseConnection.closeConnection;
+import static br.com.zalf.prolog.webservice.database.DatabaseConnection.getConnection;
 
 /**
  * Created on 30/08/18.
@@ -11,11 +21,57 @@ import java.io.OutputStream;
  */
 public class AfericaoRelatorioDaoImpl implements AfericaoRelatorioDao {
 
-    @Override
-    public void getDadosGeraisProduditivdadeCsv(@NotNull final OutputStream out,
-                                                @NotNull final Long codUnidade) throws Throwable {
 
+    @Override
+    public void getDadosGeraisAfericaoCsv(@NotNull final OutputStream out,
+                                           @NotNull final Long codUnidade,
+                                           @NotNull final LocalDate dataInicial,
+                                           @NotNull final LocalDate dataFinal) throws Throwable {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rSet = null;
+        try {
+            conn = getConnection();
+            stmt = getDadosGeraisAfericaoStmt(conn, codUnidade, dataInicial, dataFinal);
+            rSet = stmt.executeQuery();
+            new CsvWriter
+                    .Builder(out)
+                    .withResultSet(rSet)
+                    .build()
+                    .write();
+        }finally {
+            closeConnection(conn, stmt, rSet);
+        }
     }
 
+    @NotNull
+    @Override
+    public Report getDadosGeraisAfericaoReport(@NotNull final Long codUnidade,
+                                                @NotNull final LocalDate dataInicial,
+                                                @NotNull final LocalDate dataFinal) throws Throwable {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rSet = null;
+        try{
+            conn = getConnection();
+            stmt = getDadosGeraisAfericaoStmt(conn, codUnidade, dataInicial, dataFinal);
+            rSet = stmt.executeQuery();
+            return ReportTransformer.createReport(rSet);
+        } finally {
+            closeConnection(conn, stmt, rSet);
+        }
+    }
 
+    @NotNull
+    private PreparedStatement getDadosGeraisAfericaoStmt(@NotNull final Connection conn,
+                                                          @NotNull final Long codUnidade,
+                                                          @NotNull final LocalDate dataInicial,
+                                                          @NotNull final LocalDate dataFinal) throws Throwable {
+        final PreparedStatement stmt =
+                conn.prepareStatement("SELECT * FROM ");
+        stmt.setLong(1, codUnidade);
+        stmt.setObject(2, dataInicial);
+        stmt.setObject(3, dataFinal);
+        return stmt;
+    }
 }
