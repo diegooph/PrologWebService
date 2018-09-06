dfc06c986d2b0b3f7737661d1d70099ccb0c13c7
didi <dioge.vanzella@gmail.com>
2018-09-06 14:57:00 -0300

    refactor(JustificativaAjuste): insere novo atributo para saber se está ativo ou não
    PL-979

--- src/main/java/br/com/zalf/prolog/webservice/gente/controleintervalo/justificativa_ajuste/JustificativaAjuste.java
+++ src/main/java/br/com/zalf/prolog/webservice/gente/controleintervalo/justificativa_ajuste/JustificativaAjuste.java
@@ -1,16 +1,18 @@
 package br.com.zalf.prolog.webservice.gente.controleintervalo.justificativa_ajuste;
 
+import org.jetbrains.annotations.NotNull;
+
 /**
  * Created on 04/09/18.
  *
  * @author Diogenes Vanzela (https://github.com/diogenesvanzella)
  */
 public class JustificativaAjuste {
-
     private Long codJustificativaAjuste;
     private Long codEmpresa;
     private String nomeJustificativaAjuste;
     private boolean obrigatorioObservacao;
+    private boolean isAtiva;
 
     public JustificativaAjuste() {
     }
@@ -47,6 +49,14 @@ public class JustificativaAjuste {
         this.obrigatorioObservacao = obrigatorioObservacao;
     }
 
+    public boolean isAtiva() {
+        return isAtiva;
+    }
+
+    public void setAtiva(final boolean ativa) {
+        isAtiva = ativa;
+    }
+
     @Override
     public String toString() {
         return "JustificativaAjuste{" +
@@ -54,6 +64,7 @@ public class JustificativaAjuste {
                 ", codEmpresa=" + codEmpresa +
                 ", nomeJustificativaAjuste='" + nomeJustificativaAjuste + '\'' +
                 ", obrigatorioObservacao=" + obrigatorioObservacao +
+                ", isAtiva=" + isAtiva +
                 '}';
     }
 }
