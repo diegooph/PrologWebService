eccf38a87bb39b2f66e41401484f87b831127740
Luiz Felipe <luiz.felipe_95@hotmail.com>
2018-09-13 10:50:45 -0300

    refactor(Main): remove código de teste

--- src/main/java/br/com/zalf/prolog/webservice/Main.java
+++ src/main/java/br/com/zalf/prolog/webservice/Main.java
@@ -1,20 +1,8 @@
 package br.com.zalf.prolog.webservice;
 
-import br.com.zalf.prolog.webservice.commons.gson.GsonUtils;
-import br.com.zalf.prolog.webservice.gente.controleintervalo.model.MarcacaoAjusteAdicao;
-import br.com.zalf.prolog.webservice.gente.controleintervalo.model.MarcacaoAjusteAdicaoInicioFim;
-import br.com.zalf.prolog.webservice.gente.controleintervalo.model.MarcacaoAjusteAtivacaoInativacao;
-import br.com.zalf.prolog.webservice.gente.controleintervalo.model.MarcacaoAjusteEdicao;
-
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(GsonUtils.getGson().toJson(MarcacaoAjusteAdicao.createDummy()));
-        System.out.println("\n\n\n");
-        System.out.println(GsonUtils.getGson().toJson(MarcacaoAjusteAdicaoInicioFim.createDummy()));
-        System.out.println("\n\n\n");
-        System.out.println(GsonUtils.getGson().toJson(MarcacaoAjusteAtivacaoInativacao.createDummy()));
-        System.out.println("\n\n\n");
-        System.out.println(GsonUtils.getGson().toJson(MarcacaoAjusteEdicao.createDummy()));
+
     }
 }
