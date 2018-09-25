40e6680405493cab1fa855b00ae8a70ebbf910f7
didi <dioge.vanzella@gmail.com>
2018-09-25 18:02:08 -0300

    refactor(JustificativaAjuste): implementa método de adição de justificativa

--- src/main/java/br/com/zalf/prolog/webservice/gente/controleintervalo/ajustes/justificativa/JustificativaAjusteResource.java
+++ src/main/java/br/com/zalf/prolog/webservice/gente/controleintervalo/ajustes/justificativa/JustificativaAjusteResource.java
@@ -1,11 +1,10 @@
 package br.com.zalf.prolog.webservice.gente.controleintervalo.ajustes.justificativa;
 
+import br.com.zalf.prolog.webservice.commons.network.AbstractResponse;
 import br.com.zalf.prolog.webservice.commons.util.Platform;
 import br.com.zalf.prolog.webservice.commons.util.UsedBy;
-import br.com.zalf.prolog.webservice.gente.controleintervalo.model.IntervaloMarcacao;
-import br.com.zalf.prolog.webservice.interceptors.auth.AuthType;
+import br.com.zalf.prolog.webservice.errorhandling.exception.ProLogException;
 import br.com.zalf.prolog.webservice.interceptors.auth.Secured;
-import br.com.zalf.prolog.webservice.permissao.pilares.Pilares;
 import org.jetbrains.annotations.NotNull;
 
 import javax.ws.rs.*;
@@ -24,13 +23,13 @@ public final class JustificativaAjusteResource {
     @NotNull
     final JustificativaAjusteService service = new JustificativaAjusteService();
 
-    @GET
+    @POST
     @UsedBy(platforms = Platform.ANDROID)
-    @Secured(authTypes = {AuthType.BEARER, AuthType.BASIC}, permissions = Pilares.Gente.Intervalo.MARCAR_INTERVALO)
-    @Path("/abertos/{codUnidade}/{cpf}/{codTipoIntervalo}")
-    public IntervaloMarcacao getIntervaloAberto(@PathParam("codUnidade") Long codUnidade,
-                                                @PathParam("cpf") Long cpf,
-                                                @PathParam("codTipoIntervalo") Long codTipoInvervalo) throws Exception {
-        return null;
+    @Secured()
+    @Path("/adicionar")
+    public AbstractResponse adicionarMarcacaoAjuste(
+            @HeaderParam("Authorization") String userToken,
+            @NotNull final JustificativaAjuste justificativaAjuste) throws ProLogException {
+        return service.insertJustificativaAjuste(userToken, justificativaAjuste);
     }
 }
--- src/main/java/br/com/zalf/prolog/webservice/gente/controleintervalo/ajustes/justificativa/JustificativaAjusteService.java
+++ src/main/java/br/com/zalf/prolog/webservice/gente/controleintervalo/ajustes/justificativa/JustificativaAjusteService.java
@@ -1,6 +1,12 @@
 package br.com.zalf.prolog.webservice.gente.controleintervalo.ajustes.justificativa;
 
 import br.com.zalf.prolog.webservice.Injection;
+import br.com.zalf.prolog.webservice.commons.network.AbstractResponse;
+import br.com.zalf.prolog.webservice.commons.network.ResponseWithCod;
+import br.com.zalf.prolog.webservice.commons.util.Log;
+import br.com.zalf.prolog.webservice.commons.util.TokenCleaner;
+import br.com.zalf.prolog.webservice.errorhandling.exception.ProLogException;
+import br.com.zalf.prolog.webservice.errorhandling.exception.ProLogExceptionHandler;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -8,8 +14,22 @@ import org.jetbrains.annotations.NotNull;
  *
  * @author Diogenes Vanzela (https://github.com/diogenesvanzella)
  */
-public final class JustificativaAjusteService {
+final class JustificativaAjusteService {
 
+    private static final String TAG = JustificativaAjusteService.class.getSimpleName();
     @NotNull
-    final JustificativaAjusteDao dao = Injection.provideJustificativaAjusteDao();
+    private final JustificativaAjusteDao dao = Injection.provideJustificativaAjusteDao();
+    @NotNull
+    private final ProLogExceptionHandler exceptionHandler = Injection.provideProLogExceptionHandler();
+
+    AbstractResponse insertJustificativaAjuste(@NotNull final String userToken,
+                                               @NotNull final JustificativaAjuste justificativaAjuste) throws ProLogException {
+        try {
+            return ResponseWithCod.ok("Justificativa inserida com sucesso",
+                    dao.insertJustificativaAjuste(TokenCleaner.getOnlyToken(userToken), justificativaAjuste));
+        } catch (Throwable e) {
+            Log.e(TAG, "Erro ao inserir a Justificativa", e);
+            throw exceptionHandler.map(e, "Erro ao inserir a Justificativa, tente novamente");
+        }
+    }
 }
