14496947ec679eb4f844db4ec9dc74ef0c1325cc
Luiz Felipe <luiz.felipe_95@hotmail.com>
2018-10-23 14:44:11 -0300

    refactor(aferição): altera path configurações de alerta de sulcos
    PL-1386

--- src/main/java/br/com/zalf/prolog/webservice/frota/pneu/afericao/configuracao/ConfiguracaoAfericaoResource.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/afericao/configuracao/ConfiguracaoAfericaoResource.java
@@ -5,6 +5,7 @@ import br.com.zalf.prolog.webservice.errorhandling.exception.ProLogException;
 import br.com.zalf.prolog.webservice.frota.pneu.afericao.configuracao.model.ConfiguracaoAlertaColetaSulco;
 import br.com.zalf.prolog.webservice.frota.pneu.afericao.configuracao.model.ConfiguracaoTipoVeiculoAferivel;
 import br.com.zalf.prolog.webservice.interceptors.auth.Secured;
+import br.com.zalf.prolog.webservice.interceptors.log.DebugLog;
 import br.com.zalf.prolog.webservice.permissao.pilares.Pilares;
 import org.jetbrains.annotations.NotNull;
 
@@ -18,6 +19,7 @@ import java.util.List;
  * @author Diogenes Vanzela (https://github.com/diogenesvanzella)
  */
 @Path("/afericoes/configuracoes/")
+@DebugLog
 @Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
 @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
 public class ConfiguracaoAfericaoResource {
@@ -42,14 +44,14 @@ public class ConfiguracaoAfericaoResource {
 
     @PUT
     @Secured(permissions = Pilares.Frota.Afericao.ConfiguracaoAfericao.CONFIGURAR)
-    @Path("/alertas-sulco")
+    @Path("/alertas-sulcos")
     public Response updateTiposVeiculos(List<ConfiguracaoAlertaColetaSulco> configuracoes) throws ProLogException {
         return service.updateConfiguracaoAlertaColetaSulco(configuracoes);
     }
 
     @GET
     @Secured(permissions = Pilares.Frota.Afericao.ConfiguracaoAfericao.CONFIGURAR)
-    @Path("/alertas-sulco")
+    @Path("/alertas-sulcos")
     public List<ConfiguracaoAlertaColetaSulco> getConfiguracoesAlertaColetaSulco(
             @QueryParam("codColaborador") Long codColaborador) throws ProLogException {
         return service.getConfiguracoesAlertaColetaSulco(codColaborador);
