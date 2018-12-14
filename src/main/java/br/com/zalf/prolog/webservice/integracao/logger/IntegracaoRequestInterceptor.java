daa3f04ac4a37fb20da82d9ed0c93f5272f825c3
didi <dioge.vanzella@gmail.com>
2018-12-14 14:44:09 -0200

    feat(LogRequisicao): cria estrutura para salvar o log de acesso aos métodos integrados
    PL-1554

---
+++ src/main/java/br/com/zalf/prolog/webservice/integracao/logger/IntegracaoRequestInterceptor.java
@@ -0,0 +1,140 @@
+package br.com.zalf.prolog.webservice.integracao.logger;
+
+import br.com.zalf.prolog.webservice.Injection;
+import br.com.zalf.prolog.webservice.commons.util.ProLogCustomHeaders;
+import org.apache.commons.io.IOUtils;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.HttpMethod;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ResourceInfo;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+/**
+ * Created on 13/12/18.
+ *
+ * @author Diogenes Vanzela (https://github.com/diogenesvanzella)
+ */
+@LogIntegracaoRequest
+@Provider
+public final class IntegracaoRequestInterceptor implements ContainerRequestFilter {
+    @Context
+    private ResourceInfo resourceInfo;
+
+    @Context
+    private HttpServletRequest servletRequest;
+
+    @Override
+    public void filter(final ContainerRequestContext requestContext) throws IOException {
+        if (requestContext == null)
+            return;
+
+        try {
+            if (isJson(requestContext)) {
+                final String tokenRequisicao = getTokenRequisicao(requestContext);
+                final LogRequisicao logRequisicao = createLogRequisicao(requestContext);
+                if (!logRequisicao.isEmpty()) {
+                    Injection.provideLogDao().insertRequestLog(tokenRequisicao, logRequisicao);
+                }
+            }
+        } catch (Throwable throwable) {
+            // TODO - :shushing_face: Não aconteceu nada!
+        }
+    }
+
+    @NotNull
+    private String getTokenRequisicao(@NotNull final ContainerRequestContext requestContext) {
+        final String tokenHeader = requestContext.getHeaderString(ProLogCustomHeaders.HEADER_TOKEN_INTEGRACAO);
+        if (tokenHeader == null) {
+            throw new IllegalStateException("Não é possível ler o token da requisição");
+        }
+        return tokenHeader.substring(ProLogCustomHeaders.HEADER_TOKEN_INTEGRACAO.length()).trim();
+    }
+
+    @NotNull
+    private LogRequisicao createLogRequisicao(
+            @NotNull final ContainerRequestContext requestContext) throws IOException {
+        final LogRequisicao logRequisicao = new LogRequisicao();
+        logRequisicao.setClassResource(getFullClassDescription());
+        logRequisicao.setMethodResource(getFullMethodDescription());
+        logRequisicao.setHttpMethod(requestContext.getMethod());
+        logRequisicao.setUrlAcesso(getFullUrlAcesso(requestContext));
+        logRequisicao.setHeaders(getFullHeaders(requestContext));
+        logRequisicao.setParameters(getFullParameters(requestContext));
+        logRequisicao.setBodyRequest(readBody(requestContext));
+        logRequisicao.setDataHoraRequisicao(LocalDateTime.now());
+        return logRequisicao;
+    }
+
+    @Nullable
+    private String getFullParameters(@NotNull final ContainerRequestContext requestContext) {
+        if (requestContext.getUriInfo() == null
+                || (requestContext.getUriInfo().getPathParameters() == null
+                && requestContext.getUriInfo().getQueryParameters() == null))
+            return null;
+
+        final String pathParameters = requestContext.getUriInfo().getPathParameters().toString();
+        final String queryParameters = requestContext.getUriInfo().getQueryParameters().toString();
+        final String pathParams = pathParameters.isEmpty() || pathParameters.equals("{}") ? "" : pathParameters;
+        final String queryParams = queryParameters.isEmpty() || queryParameters.equals("{}") ? "" : queryParameters;
+        return pathParams + queryParams;
+    }
+
+    @Nullable
+    private String getFullHeaders(@NotNull final ContainerRequestContext requestContext) {
+        if (requestContext.getHeaders() == null)
+            return null;
+
+        return requestContext.getHeaders().toString();
+    }
+
+    @Nullable
+    private String getFullUrlAcesso(@NotNull final ContainerRequestContext requestContext) {
+        if (requestContext.getUriInfo() == null || requestContext.getUriInfo().getAbsolutePath() == null)
+            return null;
+
+        return requestContext.getUriInfo().getAbsolutePath().toString();
+    }
+
+    @Nullable
+    private String getFullClassDescription() {
+        if (resourceInfo == null || resourceInfo.getResourceClass() == null)
+            return null;
+
+        return resourceInfo.getResourceClass().getName();
+    }
+
+    @Nullable
+    private String getFullMethodDescription() {
+        if (resourceInfo == null || resourceInfo.getResourceMethod() == null)
+            return null;
+        final String returnType = resourceInfo.getResourceMethod().getGenericReturnType().getTypeName();
+        final String signature = resourceInfo.getResourceMethod().getName();
+        final String parameters = Arrays.toString(resourceInfo.getResourceMethod().getGenericParameterTypes());
+        return returnType.concat(" ").concat(signature).concat("(").concat(parameters).concat(");");
+    }
+
+    @NotNull
+    private String readBody(@NotNull final ContainerRequestContext requestContext) throws IOException {
+        final String body = IOUtils.toString(requestContext.getEntityStream(), StandardCharsets.UTF_8);
+        final InputStream in = IOUtils.toInputStream(body, StandardCharsets.UTF_8);
+        requestContext.setEntityStream(in);
+        return body;
+    }
+
+    private boolean isJson(@NotNull final ContainerRequestContext requestContext) {
+        if (requestContext.getMethod().equals(HttpMethod.GET)) {
+            return true;
+        }
+        return requestContext.getMediaType().toString().contains("application/json");
+    }
+}
--- src/main/java/br/com/zalf/prolog/webservice/log/LogDaoImpl.java
+++ src/main/java/br/com/zalf/prolog/webservice/log/LogDaoImpl.java
@@ -1,10 +1,15 @@
 package br.com.zalf.prolog.webservice.log;
 
+import br.com.zalf.prolog.webservice.commons.util.SqlType;
 import br.com.zalf.prolog.webservice.database.DatabaseConnection;
+import br.com.zalf.prolog.webservice.integracao.logger.LogRequisicao;
+import org.jetbrains.annotations.NotNull;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+
+import static br.com.zalf.prolog.webservice.commons.util.StatementUtils.bindValueOrNull;
 
 public class LogDaoImpl extends DatabaseConnection implements LogDao {
 
@@ -13,7 +18,7 @@ public class LogDaoImpl extends DatabaseConnection implements LogDao {
 	}
 
 	@Override
-	public boolean insert(String log, String identificador) throws SQLException {
+	public boolean insert(@NotNull final String log, @NotNull final String identificador) throws SQLException {
 		Connection conn = null;
 		PreparedStatement stmt = null;
 		try {
@@ -21,10 +26,46 @@ public class LogDaoImpl extends DatabaseConnection implements LogDao {
 			stmt = conn.prepareStatement("INSERT INTO LOG_JSON(JSON, IDENTIFICADOR) VALUES (?,?)");
 			stmt.setString(1, log);
 			stmt.setString(2, identificador);
-			int count = stmt.executeUpdate();
-			return count > 0;
+			return stmt.executeUpdate() > 0;
 		} finally {
-			closeConnection(conn, stmt, null);
+			close(conn, stmt);
+		}
+	}
+
+	@Override
+	public void insertRequestLog(@NotNull final String tokenRequisicao,
+								 @NotNull final LogRequisicao logRequisicao) throws Throwable {
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		try {
+			conn = getConnection();
+			stmt = conn.prepareStatement("INSERT INTO INTEGRACAO.LOG_REQUISICAO " +
+					"(COD_EMPRESA, " +
+					" CLASS_RESOURCE, " +
+					" METHOD_RESOURCE, " +
+					" HTTP_METHOD, " +
+					" URL_ACESSO, " +
+					" HEADERS, " +
+					" PARAMETERS, " +
+					" BODY_REQUEST, " +
+					" DATA_HORA_REQUISICAO) " +
+					"VALUES ((SELECT TI.COD_EMPRESA " +
+					"         FROM INTEGRACAO.TOKEN_INTEGRACAO TI " +
+					"         WHERE TI.TOKEN_INTEGRACAO = ?), ?, ?, ?, ?, ?, ?, ?, ?);");
+			stmt.setString(1, tokenRequisicao);
+			bindValueOrNull(stmt,2, logRequisicao.getClassResource(), SqlType.TEXT);
+			bindValueOrNull(stmt,3, logRequisicao.getMethodResource(), SqlType.TEXT);
+			bindValueOrNull(stmt,4, logRequisicao.getHttpMethod(), SqlType.TEXT);
+			bindValueOrNull(stmt,5, logRequisicao.getUrlAcesso(), SqlType.TEXT);
+			bindValueOrNull(stmt,6, logRequisicao.getHeaders(), SqlType.TEXT);
+			bindValueOrNull(stmt,7, logRequisicao.getParameters(), SqlType.TEXT);
+			bindValueOrNull(stmt,8, logRequisicao.getBodyRequest(), SqlType.TEXT);
+			stmt.setObject(9, logRequisicao.getDataHoraRequisicao());
+			if (stmt.executeUpdate() <= 0) {
+				throw new SQLException("Não foi possível salvar o Log");
+			}
+		} finally {
+			close(conn, stmt);
 		}
 	}
 
