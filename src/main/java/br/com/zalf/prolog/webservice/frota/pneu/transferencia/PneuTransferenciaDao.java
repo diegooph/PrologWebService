5aa8551dd99e55e9903992d696caacfce724b9ca
thaisksf <thaisksf@gmail.com>
2018-12-21 12:06:03 -0200

    refactor(PneuTransferencia): documentação
    PL-1535

--- src/main/java/br/com/zalf/prolog/webservice/frota/pneu/transferencia/PneuTransferenciaDao.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/transferencia/PneuTransferenciaDao.java
@@ -15,15 +15,39 @@ import java.util.List;
  */
 public interface PneuTransferenciaDao {
 
+    /**
+     * Insere a realização de uma transferência.
+     *
+     * @param pneuTransferenciaRealizacao   A {@link PneuTransferenciaRealizacao} contendo os dados da transferência.
+     * @param pneuTransferenciaDao Código da {@link PneuTransferenciaDao}.
+     * @throws Throwable Se ocorrer erro no banco.
+     */
     void insertTransferencia(@NotNull final PneuTransferenciaRealizacao pneuTransferenciaRealizacao,
                              @NotNull final PneuTransferenciaDao pneuTransferenciaDao) throws Throwable;
 
+    /**
+     * Método para buscar uma lista de transferências realizadas
+     *
+     * @param codUnidadesOrigem Lista de códigos das unidades de origem aplicado na busca.
+     * @param codUnidadesDestino Lista de códigos das unidades de origem destino na busca.
+     * @param dataInicial Data inicial da busca.
+     * @param dataFinal Data final da busca.
+     * @return Uma lista de transferências que foram realizadas com base no filtro aplicado.
+     * @throws Throwable Se algum erro ocorrer.
+     */
     @NotNull
     List<PneuTransferenciaListagem> getListagem(@NotNull final List<Long> codUnidadesOrigem,
                                                 @NotNull final List<Long> codUnidadesDestino,
                                                 @NotNull final LocalDate dataInicial,
                                                 @NotNull final LocalDate dataFinal) throws Throwable;
 
+    /**
+     * Método para as informações de uma transferência realizada
+     *
+     * @param codTransferencia código de uma transferência realizada na busca.
+     * @return Informações sobre a transferência.
+     * @throws Throwable Se algum erro ocorrer.
+     */
     @NotNull
     PneuTransferenciaProcessoVisualizacao getVisualizacao(@NotNull final Long codTransferencia) throws Throwable;
 }
