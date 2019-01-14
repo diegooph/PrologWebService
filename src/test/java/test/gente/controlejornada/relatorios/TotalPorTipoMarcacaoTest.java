7c2e6d0833e607aad0d424bf74f0d8e4df27195d
Luiz Felipe <luiz.felipe_95@hotmail.com>
2019-01-14 17:51:36 -0200

    feat(controle-jornada): cria teste para validar relatório de totais por tipo de marcação
    PL-1617

---
+++ src/test/java/test/gente/controlejornada/relatorios/TotalPorTipoMarcacaoTest.java
@@ -0,0 +1,158 @@
+package test.gente.controlejornada.relatorios;
+
+import br.com.zalf.prolog.webservice.commons.util.ProLogDateParser;
+import br.com.zalf.prolog.webservice.database.DatabaseConnection;
+import br.com.zalf.prolog.webservice.gente.controlejornada.relatorios.ControleJornadaRelatorioService;
+import br.com.zalf.prolog.webservice.gente.controlejornada.relatorios.FolhaPontoRelatorio;
+import br.com.zalf.prolog.webservice.gente.controlejornada.relatorios.FolhaPontoTipoIntervalo;
+import org.jetbrains.annotations.NotNull;
+import org.junit.Test;
+import test.BaseTest;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Types;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created on 14/01/19
+ *
+ * @author Luiz Felipe (https://github.com/luizfp)
+ */
+public final class TotalPorTipoMarcacaoTest extends BaseTest {
+    private static final Long COD_UNIDADE = 5L;
+    private static final String TODOS_TIPOS_INTERVALOS = "%";
+    private static final String DATA_INICIAL = "2018-01-01";
+    private static final String DATA_FIM = "2019-01-01";
+    private ControleJornadaRelatorioService service;
+    private Connection connection;
+
+    @Override
+    public void initialize() {
+        service = new ControleJornadaRelatorioService();
+        connection = DatabaseConnection.getConnection();
+    }
+
+    @Override
+    public void destroy() {
+        DatabaseConnection.close(connection);
+
+    }
+
+    @Test
+    public void testTotalPorTipoMarcacaoRelatorio() throws Throwable {
+        final Map<String, TotalPorTipoMarcacao> totaisTiposColab = getTotalTempoByTipoIntervaloStmt(
+                connection,
+                COD_UNIDADE,
+                TODOS_TIPOS_INTERVALOS,
+                ProLogDateParser.toLocalDate(DATA_INICIAL),
+                ProLogDateParser.toLocalDate(DATA_FIM));
+
+        final List<FolhaPontoRelatorio> relatorios = service.getFolhaPontoRelatorio(
+                COD_UNIDADE,
+                TODOS_TIPOS_INTERVALOS,
+                "%",
+                DATA_INICIAL,
+                DATA_FIM);
+
+        for (final FolhaPontoRelatorio pontoRelatorio : relatorios) {
+            for (final FolhaPontoTipoIntervalo tipoMarcacao : pontoRelatorio.getTiposIntervalosMarcados()) {
+                final String mapKey = createMapKey(
+                        pontoRelatorio.getColaborador().getCpf(),
+                        tipoMarcacao.getCodigo());
+                final TotalPorTipoMarcacao totalPorTipoMarcacao = totaisTiposColab.remove(mapKey);
+
+                assertNotNull("totalPorTipoMarcacao não encontrado para a chave: " + mapKey, totalPorTipoMarcacao);
+                assertEquals(totalPorTipoMarcacao.getTotalTipoMarcacao(), tipoMarcacao.getTempoTotalTipoIntervalo());
+                assertEquals(totalPorTipoMarcacao.getTotalHorasNoturnasTipoMarcacao(), tipoMarcacao.getTempoTotalHorasNoturnas());
+            }
+        }
+
+        assertTrue("Todo o Map precisa ser processado", totaisTiposColab.isEmpty());
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    @NotNull
+    private Map<String, TotalPorTipoMarcacao> getTotalTempoByTipoIntervaloStmt(
+            @NotNull final Connection conn,
+            @NotNull final Long codUnidade,
+            @NotNull final String codTipoIntervalo,
+            @NotNull final LocalDate dataInicial,
+            @NotNull final LocalDate dataFinal) throws Throwable {
+        PreparedStatement stmt = null;
+        ResultSet rSet = null;
+        try {
+            stmt = conn.prepareStatement(
+                    "SELECT * FROM FUNC_MARCACAO_GET_TOTAL_TEMPO_POR_TIPO_MARCACAO(?, ?, ?, ?);");
+            stmt.setLong(1, codUnidade);
+            if (codTipoIntervalo.equals("%")) {
+                stmt.setNull(2, Types.BIGINT);
+            } else {
+                stmt.setLong(2, Long.parseLong(codTipoIntervalo));
+            }
+            stmt.setObject(3, dataInicial);
+            stmt.setObject(4, dataFinal);
+            rSet = stmt.executeQuery();
+            final Map<String, TotalPorTipoMarcacao> totaisTipoColab = new HashMap<>();
+            int totalSize = 0;
+            while (rSet.next()) {
+                final String chaveMap = createMapKey(
+                        rSet.getLong("CPF_COLABORADOR"),
+                        rSet.getLong("COD_TIPO_INTERVALO"));
+                totaisTipoColab.put(chaveMap, new TotalPorTipoMarcacao(
+                        Duration.ofMillis(rSet.getLong("TEMPO_TOTAL_MILLIS")),
+                        Duration.ofMillis(rSet.getLong("TEMPO_TOTAL_HORAS_NOTURNAS_MILLIS"))));
+                totalSize++;
+            }
+
+            // A primeira vista pode parecer meio ridículo comparar isso, pois sempre iteraremos no while e iremos
+            // inserir no Map. Porém, se chaves iguais forem criadas pelo método createMapKey, o que não deveria
+            // acontecer, algum valor do Map será substituído.
+            if (totaisTipoColab.size() != totalSize) {
+                throw new IllegalStateException("Map com tamanho diferente do result set, isso pode acontecer se chaves " +
+                        "dupliacadas forem criadas, o que denota um erro na busca");
+            }
+
+            return totaisTipoColab;
+        } finally {
+            DatabaseConnection.close(stmt, rSet);
+        }
+    }
+
+    @NotNull
+    private String createMapKey(@NotNull final Long cpfColaborador, @NotNull final Long codTipoMarcacao) {
+        return String.valueOf(cpfColaborador).concat(String.valueOf(codTipoMarcacao));
+    }
+
+    private final class TotalPorTipoMarcacao {
+        @NotNull
+        private final Duration totalTipoMarcacao;
+        @NotNull
+        private final Duration totalHorasNoturnasTipoMarcacao;
+
+        TotalPorTipoMarcacao(@NotNull final Duration totalTipoMarcacao,
+                             @NotNull final Duration totalHorasNoturnasTipoMarcacao) {
+            this.totalTipoMarcacao = totalTipoMarcacao;
+            this.totalHorasNoturnasTipoMarcacao = totalHorasNoturnasTipoMarcacao;
+        }
+
+        @NotNull
+        Duration getTotalTipoMarcacao() {
+            return totalTipoMarcacao;
+        }
+
+        @NotNull
+        Duration getTotalHorasNoturnasTipoMarcacao() {
+            return totalHorasNoturnasTipoMarcacao;
+        }
+    }
+}
