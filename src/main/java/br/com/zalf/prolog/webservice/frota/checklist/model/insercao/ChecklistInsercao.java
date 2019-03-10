5bdc441287874997adba4fd6aaa694e5c9413abe
Luiz Felipe <luiz.felipe_95@hotmail.com>
2019-03-10 15:43:19 -0300

    refactor(checklist): altera nomes de atributos do objeto de inserção do check
    PL-1792

--- src/main/java/br/com/zalf/prolog/webservice/frota/checklist/model/insercao/ChecklistInsercao.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/checklist/model/insercao/ChecklistInsercao.java
@@ -41,14 +41,14 @@ public final class ChecklistInsercao {
      *
      * @see <a href="Android Docs">https://developer.android.com/reference/android/os/SystemClock.html#elapsedRealtime()</a>
      */
-    private long deviceElapsedRealtimeMillisRealizacao;
+    private long deviceUptimeRealizacaoMillis;
 
     /**
      * O tempo, em milissegundos, desde que o aparelho foi ligado até a sincronização do check.
      *
      * @see <a href="Android Docs">https://developer.android.com/reference/android/os/SystemClock.html#elapsedRealtime()</a>
      */
-    private long deviceElapsedRealtimeMillisSincronizacao;
+    private long deviceUptimeSincronizacaoMillis;
 
     public ChecklistInsercao() {
 
@@ -58,63 +58,60 @@ public final class ChecklistInsercao {
         return codModelo;
     }
 
-    public void setCodModelo(final Long codModelo) {
-        this.codModelo = codModelo;
-    }
-
     public Long getCodColaborador() {
         return codColaborador;
-    }
-
-    public void setCodColaborador(final Long codColaborador) {
-        this.codColaborador = codColaborador;
     }
 
     public Long getCodVeiculo() {
         return codVeiculo;
     }
 
-    public void setCodVeiculo(final Long codVeiculo) {
-        this.codVeiculo = codVeiculo;
-    }
-
     public String getPlacaVeiculo() {
         return placaVeiculo;
-    }
-
-    public void setPlacaVeiculo(final String placaVeiculo) {
-        this.placaVeiculo = placaVeiculo;
     }
 
     public TipoChecklist getTipo() {
         return tipo;
     }
 
-    public void setTipo(final TipoChecklist tipo) {
-        this.tipo = tipo;
-    }
-
     public long getKmColetadoVeiculo() {
         return kmColetadoVeiculo;
-    }
-
-    public void setKmColetadoVeiculo(final long kmColetadoVeiculo) {
-        this.kmColetadoVeiculo = kmColetadoVeiculo;
     }
 
     public long getTempoRealizacaoCheckInMillis() {
         return tempoRealizacaoCheckInMillis;
     }
 
-    public void setTempoRealizacaoCheckInMillis(final long tempoRealizacaoCheckInMillis) {
-        this.tempoRealizacaoCheckInMillis = tempoRealizacaoCheckInMillis;
-    }
-
     public List<ChecklistResposta> getRespostas() {
         return respostas;
     }
 
-    public void setRespostas(final List<ChecklistResposta> respostas) {
-        this.respostas = respostas;
+    public LocalDateTime getDataHoraRealizacao() {
+        return dataHoraRealizacao;
+    }
+
+    public FonteDataHora getFonteDataHoraRealizacao() {
+        return fonteDataHoraRealizacao;
+    }
+
+    public Integer getVersaoAppMomentoRealizacao() {
+        return versaoAppMomentoRealizacao;
+    }
+
+    public Integer getVersaoAppMomentoSincronizacao() {
+        return versaoAppMomentoSincronizacao;
+    }
+
+    @Nullable
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public long getDeviceUptimeRealizacaoMillis() {
+        return deviceUptimeRealizacaoMillis;
+    }
+
+    public long getDeviceUptimeSincronizacaoMillis() {
+        return deviceUptimeSincronizacaoMillis;
     }
 }
