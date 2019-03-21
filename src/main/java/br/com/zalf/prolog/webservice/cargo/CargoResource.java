094e56c58669adfeea74ddc1bfe81b0bceb41ed9
Luiz Felipe <luiz.felipe_95@hotmail.com>
2019-03-21 11:54:37 -0300

    refactor(cargos): verifica permissões nos métodos dos cargos
    PL-1656

--- src/main/java/br/com/zalf/prolog/webservice/cargo/CargoResource.java
+++ src/main/java/br/com/zalf/prolog/webservice/cargo/CargoResource.java
@@ -6,6 +6,7 @@ import br.com.zalf.prolog.webservice.cargo.model.CargoSelecao;
 import br.com.zalf.prolog.webservice.commons.util.Required;
 import br.com.zalf.prolog.webservice.errorhandling.exception.ProLogException;
 import br.com.zalf.prolog.webservice.interceptors.auth.Secured;
+import br.com.zalf.prolog.webservice.permissao.pilares.Pilares;
 import org.jetbrains.annotations.NotNull;
 
 import javax.ws.rs.*;
@@ -24,6 +25,13 @@ public final class CargoResource {
     @NotNull
     private final CargoService service = new CargoService();
 
+    /**
+     * Esse método não verifica nenhuma permissão. Isso porque ele poderá ser utilizado em diversas telas como
+     * parâmetro para filtragem e pré-requisito de seleção em algum processo, como a criação de um modelo de checklist.
+     *
+     * Controlar todos esses usos e permissões é muito complexo, e como esse método não revela nada demais, podemos
+     * deixar sem a verificação. Apenas com {@link Secured secured} verificando se tem um token válido.
+     */
     @GET
     @Secured
     @Path("/todos")
@@ -33,7 +41,12 @@ public final class CargoResource {
     }
 
     @GET
-    @Secured
+    @Secured(permissions = {
+            Pilares.Gente.Permissao.VINCULAR_CARGO,
+            Pilares.Gente.Permissao.VISUALIZAR,
+            Pilares.Gente.Colaborador.VISUALIZAR,
+            Pilares.Gente.Colaborador.CADASTRAR,
+            Pilares.Gente.Colaborador.EDITAR})
     @Path("/em-uso")
     public List<CargoEmUso> getCargosEmUsoUnidade(
             @QueryParam("codUnidade") @Required Long codUnidade) throws ProLogException {
@@ -41,7 +54,12 @@ public final class CargoResource {
     }
 
     @GET
-    @Secured
+    @Secured(permissions = {
+            Pilares.Gente.Permissao.VINCULAR_CARGO,
+            Pilares.Gente.Permissao.VISUALIZAR,
+            Pilares.Gente.Colaborador.VISUALIZAR,
+            Pilares.Gente.Colaborador.CADASTRAR,
+            Pilares.Gente.Colaborador.EDITAR})
     @Path("/nao-utilizados")
     public List<CargoNaoUtilizado> getCargosNaoUtilizadosUnidade(
             @QueryParam("codUnidade") @Required Long codUnidade) throws ProLogException {
