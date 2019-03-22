3ca00f29f5b21596816875a6038d79f88707e401
didi <dioge.vanzella@gmail.com>
2019-03-22 10:47:19 -0300

    feat(OkHttp3): insere biblioteca para setar o tempo de conexão

--- src/main/java/br/com/zalf/prolog/webservice/integracao/protheusrodalog/data/ProtheusRodalogRestClient.java
+++ src/main/java/br/com/zalf/prolog/webservice/integracao/protheusrodalog/data/ProtheusRodalogRestClient.java
@@ -1,9 +1,12 @@
 package br.com.zalf.prolog.webservice.integracao.protheusrodalog.data;
 
 import br.com.zalf.prolog.webservice.commons.gson.GsonUtils;
+import okhttp3.OkHttpClient;
 import org.jetbrains.annotations.Nullable;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created on 28/02/19.
@@ -11,12 +14,17 @@ import retrofit2.converter.gson.GsonConverterFactory;
  * @author Diogenes Vanzela (https://github.com/diogenesvanzella)
  */
 public final class ProtheusRodalogRestClient {
+    private static final long DEFAULT_TIMEOUT_MINUTES = 1;
     @Nullable
     private static Retrofit retrofit;
 
     public static Retrofit getRetrofit() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
+                    .client(new OkHttpClient.Builder()
+                            .connectTimeout(DEFAULT_TIMEOUT_MINUTES, TimeUnit.MINUTES)
+                            .readTimeout(DEFAULT_TIMEOUT_MINUTES, TimeUnit.MINUTES)
+                            .build())
                     .addConverterFactory(GsonConverterFactory.create(GsonUtils.getGson()))
                     .baseUrl("http://192.168.221.213:8086/rest/")
                     .build();
