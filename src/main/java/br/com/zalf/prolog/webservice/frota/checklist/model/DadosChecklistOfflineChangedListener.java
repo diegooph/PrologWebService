2393ea150ef1fa0a8e3f96e14b7edb24fceba919
didi <dioge.vanzella@gmail.com>
2019-03-29 08:55:37 -0300

    feat(VersaoDadosChecklistOfflineAtualizador): cria classe para tratar as atualizações
    PL-1789

---
+++ src/main/java/br/com/zalf/prolog/webservice/frota/checklist/model/DadosChecklistOfflineChangedListener.java
@@ -0,0 +1,10 @@
+package br.com.zalf.prolog.webservice.frota.checklist.model;
+
+/**
+ * Created on 29/03/19.
+ *
+ * @author Diogenes Vanzela (https://github.com/diogenesvanzella)
+ */
+public interface DadosChecklistOfflineChangedListener {
+
+}
---
+++ src/main/java/br/com/zalf/prolog/webservice/frota/checklist/model/VersaoDadosChecklistOfflineAtualizador.java
@@ -0,0 +1,9 @@
+package br.com.zalf.prolog.webservice.frota.checklist.model;
+
+/**
+ * Created on 29/03/19.
+ *
+ * @author Diogenes Vanzela (https://github.com/diogenesvanzella)
+ */
+public class VersaoDadosChecklistOfflineAtualizador implements DadosChecklistOfflineChangedListener {
+}
