cc0f5d855cb0a5a07e300609d4f157cacbbd100f
didi <dioge.vanzella@gmail.com>
2019-04-15 11:43:15 -0300

    refactor(VersaoDadosChecklist): cria método para incrementar a versão de dados
    PL-1789

--- src/main/java/br/com/zalf/prolog/webservice/frota/checklist/offline/VersaoDadosChecklistOfflineAtualizador.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/checklist/offline/VersaoDadosChecklistOfflineAtualizador.java
@@ -1,11 +1,13 @@
 package br.com.zalf.prolog.webservice.frota.checklist.offline;
 
+import br.com.zalf.prolog.webservice.database.DatabaseConnection;
 import br.com.zalf.prolog.webservice.permissao.pilares.Pilares;
 import org.jetbrains.annotations.NotNull;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * Created on 29/03/19.
@@ -21,21 +23,36 @@ public class VersaoDadosChecklistOfflineAtualizador implements DadosChecklistOff
     public void onInsertColaborador(@NotNull final Connection connection,
                                     @NotNull final Long codColaborador) throws Throwable {
         if (hasPermissionTo(connection, codColaborador, Pilares.Frota.Checklist.REALIZAR)) {
-            
+            incrementaVersaoDadosUnidadeFromColaborador(connection, codColaborador);
         }
+    }
+
+    private void incrementaVersaoDadosUnidadeFromColaborador(@NotNull final Connection connection,
+                                                             @NotNull final Long codColaborador) {
     }
 
     private boolean hasPermissionTo(@NotNull final Connection connection,
                                     @NotNull final Long codColaborador,
-                                    final int codPermissao) {
+                                    final int codPermissao) throws Throwable {
         PreparedStatement stmt = null;
         ResultSet rSet = null;
         try {
-
+            stmt = connection.prepareStatement("");
+            stmt.setLong(1, codColaborador);
+            stmt.setInt(1, codPermissao);
+            rSet = stmt.executeQuery();
+            if (rSet.next()) {
+                return rSet.getBoolean("TEM_PERMISSAO");
+            } else {
+                throw new SQLException("Erro ao verificar a existência de permissão:\n" +
+                        "codColaborador: " + codColaborador + "\n" +
+                        "codPermissao: " + codPermissao);
+            }
         } finally {
+            DatabaseConnection.close(stmt, rSet);
         }
-        return false;
     }
+
 
     @Override
     public void onUpdateColaborador(@NotNull final Connection connection,
