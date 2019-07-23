1bec476f16d09d0f8a2aebbe50dca98fd251d0a7
thaisksf <thaisksf@gmail.com>
2019-07-23 14:53:02 -0300

    feat(implantacao): monta estrutura
    PL-2186

---
+++ src/main/java/br/com/zalf/prolog/webservice/implantacao/conferencia/frota/veiculo/VeiculoConferenciaDao.java
@@ -0,0 +1,9 @@
+package br.com.zalf.prolog.webservice.implantacao.conferencia.frota.veiculo;
+
+/**
+ * Created on 23/07/19.
+ *
+ * @author Thais Francisco (https://github.com/thaisksf)
+ */
+public interface VeiculoConferenciaDao {
+}
