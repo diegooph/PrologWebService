3b90743520eaeea69ee8c82d243ad06ad658ddbc
Natan Rotta <natanrotta@gmail.com>
2019-07-25 09:01:50 -0300

    Merge branch 'feat-conferencia-planilha-import-veiculos' of /home/dev/IdeaProjects/PrologWebService with conflicts.

--- src/main/java/br/com/zalf/prolog/webservice/implantacao/conferencia/frota/veiculo/VeiculoConferenciaResource.java
+++ src/main/java/br/com/zalf/prolog/webservice/implantacao/conferencia/frota/veiculo/VeiculoConferenciaResource.java
@@ -1,43 +1,11 @@
 package br.com.zalf.prolog.webservice.implantacao.conferencia.frota.veiculo;
 
-import br.com.zalf.prolog.webservice.errorhandling.exception.ProLogException;
-import br.com.zalf.prolog.webservice.interceptors.auth.Secured;
-import br.com.zalf.prolog.webservice.interceptors.versioncodebarrier.AppVersionCodeHandler;
-import br.com.zalf.prolog.webservice.interceptors.versioncodebarrier.DefaultAppVersionCodeHandler;
-import br.com.zalf.prolog.webservice.interceptors.versioncodebarrier.VersionCodeHandlerMode;
-import br.com.zalf.prolog.webservice.interceptors.versioncodebarrier.VersionNotPresentAction;
-import org.glassfish.jersey.media.multipart.FormDataParam;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import java.io.InputStream;
-import java.util.List;
 
 /**
  * Created on 23/07/19.
  *
  * @author Thais Francisco (https://github.com/thaisksf)
  */
-
-@Path("/veiculo_conferencia")
-@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
-@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-@AppVersionCodeHandler(
-        implementation = DefaultAppVersionCodeHandler.class,
-        targetVersionCode = 64,
-        versionCodeHandlerMode = VersionCodeHandlerMode.BLOCK_THIS_VERSION_AND_BELOW,
-        actionIfVersionNotPresent = VersionNotPresentAction.BLOCK_ANYWAY)
 public class VeiculoConferenciaResource {
 
-    private VeiculoConferenciaService service = new VeiculoConferenciaService ();
-
-    @POST
-    @Secured
-    @Consumes({MediaType.MULTIPART_FORM_DATA})
-    public List<Long> insert(@FormDataParam("file") InputStream fileInputStream) throws ProLogException {
-        return service.insert(fileInputStream);
-    }
 }
