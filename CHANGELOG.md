570d476421c91e12a03205275e470cbef56d6d66
Luiz Felipe <luiz.felipe_95@hotmail.com>
2019-08-27 11:50:37 -0300

    refactor(quiz): melhora método de busca de um quiz pelo código
    Agora é utilizado apenas o código único do quiz na busca.
    
    PL-2249

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@ Change Log
 
 ### Refactors
 * Remove provisoriamente validador de placa do veículo no cadastro (foco em internacionalização)
+* Melhora método de busca de um quiz pelo código (PL-2249)
 
 <a name="v0.1.0"></a>
 ## Version [v0.1.0](https://github.com/luizfp/PrologWebService/compare/v0.0.99...v0.1.0) (2019-08-19)
--- src/main/java/br/com/zalf/prolog/webservice/gente/quiz/quiz/QuizResource.java
+++ src/main/java/br/com/zalf/prolog/webservice/gente/quiz/quiz/QuizResource.java
@@ -1,6 +1,9 @@
 package br.com.zalf.prolog.webservice.gente.quiz.quiz;
 
 import br.com.zalf.prolog.webservice.commons.network.Response;
+import br.com.zalf.prolog.webservice.commons.util.Platform;
+import br.com.zalf.prolog.webservice.commons.util.Required;
+import br.com.zalf.prolog.webservice.commons.util.UsedBy;
 import br.com.zalf.prolog.webservice.gente.quiz.quiz.model.Quiz;
 import br.com.zalf.prolog.webservice.permissao.pilares.Pilares;
 import br.com.zalf.prolog.webservice.interceptors.auth.Secured;
@@ -20,8 +23,9 @@ public class QuizResource {
     private QuizService service = new QuizService();
 
     @POST
+    @UsedBy(platforms = {Platform.ANDROID, Platform.WEBSITE})
     @Secured(permissions = Pilares.Gente.Quiz.REALIZAR)
-    public Response insert(Quiz quiz) {
+    public Response insert(@Required Quiz quiz) {
         if (service.insert(quiz)) {
             return Response.ok("Quiz inserido com sucesso");
         } else {
@@ -30,20 +34,32 @@ public class QuizResource {
     }
 
     @GET
+    @UsedBy(platforms = {Platform.ANDROID, Platform.WEBSITE})
     @Secured(permissions = {Pilares.Gente.Quiz.VISUALIZAR, Pilares.Gente.Quiz.REALIZAR})
     @Path("/{cpf}")
-    public List<Quiz> getRealizadosByColaborador(@PathParam("cpf") Long cpf,
-                                                 @QueryParam("limit") int limit,
-                                                 @QueryParam("offset") int offset) {
+    public List<Quiz> getRealizadosByColaborador(@PathParam("cpf") @Required Long cpf,
+                                                 @QueryParam("limit") @Required int limit,
+                                                 @QueryParam("offset") @Required int offset) {
         return service.getRealizadosByColaborador(cpf, limit, offset);
     }
 
+    @GET
+    @UsedBy(platforms = {Platform.ANDROID, Platform.WEBSITE})
+    @Secured(permissions = {Pilares.Gente.Quiz.VISUALIZAR, Pilares.Gente.Quiz.REALIZAR})
+    @Path("/visualizacao/{codQuiz}")
+    public Quiz getByCod(@PathParam("codQuiz") @Required Long codQuiz) {
+        return service.getByCod(codQuiz);
+    }
+
+    /**
+     * @deprecated at 2019-08-27. Use {@link #getByCod(Long)} instead.
+     */
     @GET
     @Secured(permissions = {Pilares.Gente.Quiz.VISUALIZAR, Pilares.Gente.Quiz.REALIZAR})
     @Path("/{codUnidade}/{codModeloQuiz}/{codQuiz}")
     public Quiz getByCod(@PathParam("codUnidade") Long codUnidade,
                          @PathParam("codModeloQuiz") Long codModeloQuiz,
                          @PathParam("codQuiz") Long codQuiz) {
-        return service.getByCod(codUnidade, codQuiz, codModeloQuiz);
+        return service.getByCod(codQuiz);
     }
 }
