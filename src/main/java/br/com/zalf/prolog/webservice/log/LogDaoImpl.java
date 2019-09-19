0fc9e374bb19725abc77ec0246fd5e025d65fc25
didi <dioge.vanzella@gmail.com>
2019-09-19 10:48:09 -0300

    refactor(integração): melhora sistema para salvar logs de integração
    PL-2306

--- src/main/java/br/com/zalf/prolog/webservice/log/LogDaoImpl.java
+++ src/main/java/br/com/zalf/prolog/webservice/log/LogDaoImpl.java
@@ -1,9 +1,12 @@
 package br.com.zalf.prolog.webservice.log;
 
 import br.com.zalf.prolog.webservice.commons.util.SqlType;
+import br.com.zalf.prolog.webservice.commons.util.date.Now;
 import br.com.zalf.prolog.webservice.database.DatabaseConnection;
-import br.com.zalf.prolog.webservice.integracao.logger.LogRequisicao;
+import br.com.zalf.prolog.webservice.integracao.logger.RequestLog;
+import br.com.zalf.prolog.webservice.integracao.logger.ResponseLog;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -11,19 +14,14 @@ import java.sql.SQLException;
 
 import static br.com.zalf.prolog.webservice.commons.util.StatementUtils.bindValueOrNull;
 
-public class LogDaoImpl extends DatabaseConnection implements LogDao {
-
-    public LogDaoImpl() {
-
-    }
-
+public final class LogDaoImpl extends DatabaseConnection implements LogDao {
     @Override
     public boolean insert(@NotNull final String log, @NotNull final String identificador) throws SQLException {
         Connection conn = null;
         PreparedStatement stmt = null;
         try {
             conn = getConnection();
-            stmt = conn.prepareStatement("INSERT INTO LOG_JSON(JSON, IDENTIFICADOR) VALUES (?,?)");
+            stmt = conn.prepareStatement("INSERT INTO LOG_JSON(JSON, IDENTIFICADOR) VALUES (?, ?)");
             stmt.setString(1, log);
             stmt.setString(2, identificador);
             return stmt.executeUpdate() > 0;
@@ -33,42 +31,35 @@ public class LogDaoImpl extends DatabaseConnection implements LogDao {
     }
 
     @Override
-    public void insertRequestLog(@NotNull final String tokenRequisicao,
-                                 @NotNull final LogRequisicao logRequisicao) throws Throwable {
+    public void insertRequestResponseLog(@NotNull final String tokenRequisicao,
+                                         @NotNull final RequestLog requestLog,
+                                         @Nullable final ResponseLog responseLog) throws Throwable {
         Connection conn = null;
         PreparedStatement stmt = null;
         try {
             conn = getConnection();
-            stmt = conn.prepareStatement("INSERT INTO INTEGRACAO.LOG_REQUISICAO " +
-                    "(COD_EMPRESA, " +
-                    " CLASS_RESOURCE, " +
-                    " METHOD_RESOURCE, " +
-                    " HTTP_METHOD, " +
-                    " URL_ACESSO, " +
-                    " HEADERS, " +
-                    " PATH_PARAMETERS, " +
-                    " QUERY_PARAMETERS, " +
-                    " BODY_REQUEST, " +
-                    " DATA_HORA_REQUISICAO) " +
-                    "VALUES ((SELECT TI.COD_EMPRESA " +
-                    "         FROM INTEGRACAO.TOKEN_INTEGRACAO TI " +
-                    "         WHERE TI.TOKEN_INTEGRACAO = ?), ?, ?, ?, ?, ?, ?, ?, ?, ?);");
+            stmt = conn.prepareStatement("SELECT * FROM INTEGRACAO.FUNC_GERAL_SALVA_LOG_INTEGRACAO(" +
+                    "F_TOKEN_INTEGRACAO := ?, " +
+                    "F_RESPONSE_STATUS := ?, " +
+                    "F_REQUEST_JSON := ?, " +
+                    "F_RESPONSE_JSON := ?," +
+                    "F_DATA_HORA_REQUEST := ?)");
             stmt.setString(1, tokenRequisicao);
-            bindValueOrNull(stmt, 2, logRequisicao.getClassResource(), SqlType.TEXT);
-            bindValueOrNull(stmt, 3, logRequisicao.getMethodResource(), SqlType.TEXT);
-            bindValueOrNull(stmt, 4, logRequisicao.getHttpMethod(), SqlType.TEXT);
-            bindValueOrNull(stmt, 5, logRequisicao.getUrlAcesso(), SqlType.TEXT);
-            bindValueOrNull(stmt, 6, logRequisicao.getHeaders(), SqlType.TEXT);
-            bindValueOrNull(stmt, 7, logRequisicao.getPathParamns(), SqlType.TEXT);
-            bindValueOrNull(stmt, 8, logRequisicao.getQueryParamns(), SqlType.TEXT);
-            bindValueOrNull(stmt, 9, logRequisicao.getBodyRequest(), SqlType.TEXT);
-            stmt.setObject(10, logRequisicao.getDataHoraRequisicao());
-            if (stmt.executeUpdate() <= 0) {
-                throw new SQLException("Não foi possível salvar o Log");
-            }
+            bindValueOrNull(
+                    stmt,
+                    2,
+                    responseLog == null ? null : responseLog.getStatusCode(),
+                    SqlType.INTEGER);
+            stmt.setString(3, RequestLog.toJson(requestLog));
+            bindValueOrNull(
+                    stmt,
+                    4,
+                    responseLog == null ? null : ResponseLog.toJson(responseLog),
+                    SqlType.VARCHAR);
+            stmt.setObject(5, Now.localDateTimeUtc());
+            stmt.execute();
         } finally {
             close(conn, stmt);
         }
     }
-
 }
