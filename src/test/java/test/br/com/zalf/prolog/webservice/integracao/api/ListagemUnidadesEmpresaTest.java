8ec5386077ffeb86abef56ef376b7450bc666d0d
Luiz Felipe <luiz.felipe_95@hotmail.com>
2019-09-20 12:08:35 -0300

    test: cria pacote base com nome 'test' para as classes de testes

---
+++ src/test/java/test/br/com/zalf/prolog/webservice/integracao/api/ListagemUnidadesEmpresaTest.java
@@ -0,0 +1,40 @@
+package test.br.com.zalf.prolog.webservice.integracao.api;
+
+import br.com.zalf.prolog.webservice.database.DatabaseManager;
+import br.com.zalf.prolog.webservice.integracao.api.unidade.ApiUnidade;
+import br.com.zalf.prolog.webservice.integracao.api.unidade.ApiUnidadeService;
+import com.google.common.truth.Truth;
+import org.junit.Test;
+import test.br.com.zalf.prolog.webservice.BaseTest;
+
+import java.util.List;
+
+/**
+ * Created on 19/08/19.
+ *
+ * @author Diogenes Vanzela (https://github.com/diogenesvanzella)
+ */
+public class ListagemUnidadesEmpresaTest extends BaseTest {
+    private static final String TOKEN_INTEGRACAO = "kffdm2ba5ai3lsk79kqur9rb3mq7hv59qa8pr0sho4mcr56clck";
+    private ApiUnidadeService service;
+
+    @Override
+    public void initialize() throws Throwable {
+        super.initialize();
+        DatabaseManager.init();
+        service = new ApiUnidadeService();
+    }
+
+    @Override
+    public void destroy() {
+        DatabaseManager.finish();
+        super.destroy();
+    }
+
+    @Test
+    public void listagemUnidadesEmpresaTest() {
+        final List<ApiUnidade> unidades = service.getUnidades(TOKEN_INTEGRACAO, false);
+        Truth.assertThat(unidades).isNotNull();
+        Truth.assertThat(unidades).isNotEmpty();
+    }
+}
