5fcae3972fc75cd0cf2d0570eb09d1c8becae168
Luiz Felipe <luiz.felipe_95@hotmail.com>
2019-10-07 15:08:04 -0300

    test(aferição): valida funcionamento dos testes do aferidor
    PL-2343

--- src/main/java/br/com/zalf/prolog/webservice/frota/pneu/aferidor/teste/model/ComandoExecutadoTeste.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/aferidor/teste/model/ComandoExecutadoTeste.java
@@ -3,6 +3,8 @@ package br.com.zalf.prolog.webservice.frota.pneu.aferidor.teste.model;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Objects;
+
 /**
  * Created on 2019-10-07
  *
@@ -28,5 +30,19 @@ public final class ComandoExecutadoTeste {
     @Nullable
     public String getValorRecebido() {
         return valorRecebido;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final ComandoExecutadoTeste that = (ComandoExecutadoTeste) o;
+        return comandoEnviado.equals(that.comandoEnviado) &&
+                Objects.equals(valorRecebido, that.valorRecebido);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(comandoEnviado, valorRecebido);
     }
 }
