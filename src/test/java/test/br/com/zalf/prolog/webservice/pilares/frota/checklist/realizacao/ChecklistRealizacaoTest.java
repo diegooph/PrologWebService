0ef0eab6b70e4a8c302ca2a0121bd190f34cff4f
Luiz Felipe <luiz.felipe_95@hotmail.com>
2019-10-14 11:44:56 -0300

    test(checklist): implementa teste de realização de checklist offline
    PL-2227

--- src/test/java/test/br/com/zalf/prolog/webservice/pilares/frota/checklist/realizacao/ChecklistRealizacaoTest.java
+++ src/test/java/test/br/com/zalf/prolog/webservice/pilares/frota/checklist/realizacao/ChecklistRealizacaoTest.java
@@ -20,6 +20,7 @@ import br.com.zalf.prolog.webservice.frota.checklist.modelo.model.insercao.Pergu
 import br.com.zalf.prolog.webservice.frota.checklist.modelo.model.insercao.ResultInsertModeloChecklist;
 import br.com.zalf.prolog.webservice.frota.checklist.modelo.model.visualizacao.ModeloChecklistVisualizacao;
 import br.com.zalf.prolog.webservice.frota.checklist.modelo.model.visualizacao.PerguntaModeloChecklistVisualizacao;
+import br.com.zalf.prolog.webservice.frota.checklist.offline.ChecklistOfflineService;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -49,6 +50,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public final class ChecklistRealizacaoTest extends BaseTest {
     private static final String CPF_TOKEN = "03383283194";
+    // Token da unidade 5.
+    private static final String TOKEN_CHECK_OFF = "6jylp6yo2Cx5V1tgolZo0dMX5nHWyYP6IOs9UrxX4wdaxXHnJrcKVyrbmA9mjYs2";
     private ChecklistModeloService service;
     private String token;
 
@@ -66,7 +69,7 @@ public final class ChecklistRealizacaoTest extends BaseTest {
     }
 
     @Test
-    public void insereChecklistOnline_buscaParaComparar_deveTerInfosIguais() {
+    void insereChecklistOnline_buscaParaComparar_deveTerInfosIguais() {
         final List<PerguntaModeloChecklistInsercao> perguntas = new ArrayList<>();
         {
             // P1.
@@ -278,7 +281,217 @@ public final class ChecklistRealizacaoTest extends BaseTest {
     }
 
     @Test
-    public void insereChecklistOffline_buscaParaComparar_deveTerInfosIguais() {
+    void insereChecklistOffline_buscaParaComparar_deveTerInfosIguais() {
+        final List<PerguntaModeloChecklistInsercao> perguntas = new ArrayList<>();
+        {
+            // P1.
 
+            final List<AlternativaModeloChecklistInsercao> alternativas = new ArrayList<>();
+            // A1.
+            alternativas.add(new AlternativaModeloChecklistInsercao(
+                    "A1",
+                    PrioridadeAlternativa.ALTA,
+                    false,
+                    1,
+                    false));
+            // A2.
+            alternativas.add(new AlternativaModeloChecklistInsercao(
+                    "Outros",
+                    PrioridadeAlternativa.CRITICA,
+                    true,
+                    2,
+                    true));
+
+            perguntas.add(new PerguntaModeloChecklistInsercao(
+                    "P1",
+                    1L,
+                    1,
+                    true,
+                    alternativas));
+        }
+
+        {
+            // P2.
+
+            final List<AlternativaModeloChecklistInsercao> alternativas = new ArrayList<>();
+            // B1.
+            alternativas.add(new AlternativaModeloChecklistInsercao(
+                    "B1",
+                    PrioridadeAlternativa.ALTA,
+                    false,
+                    1,
+                    true));
+            // B2.
+            alternativas.add(new AlternativaModeloChecklistInsercao(
+                    "Outros",
+                    PrioridadeAlternativa.BAIXA,
+                    true,
+                    2,
+                    false));
+
+            perguntas.add(new PerguntaModeloChecklistInsercao(
+                    "P2",
+                    null,
+                    2,
+                    false,
+                    alternativas));
+        }
+
+        final Long codUnidade = 5L;
+        final String nomeModelo = "$Teste Método$";
+        // 4 - Então inserimos o modelo.
+        final ResultInsertModeloChecklist result =
+                service.insertModeloChecklist(
+                        new ModeloChecklistInsercao(
+                                nomeModelo,
+                                codUnidade,
+                                Collections.emptyList(),
+                                Collections.emptyList(),
+                                perguntas),
+                        token);
+
+        // 5 - Agora buscamos o modelo inserido.
+        final ModeloChecklistVisualizacao modeloBuscado = service.getModeloChecklist(
+                codUnidade,
+                result.getCodModeloChecklistInserido());
+
+
+        // Responde o checklist.
+        final List<ChecklistResposta> respostas = new ArrayList<>();
+
+        {
+            // Responde a P1 - ela É single_choice.
+            final PerguntaModeloChecklistVisualizacao p1 = modeloBuscado.getPerguntas().get(0);
+            final List<ChecklistAlternativaResposta> alternativas = new ArrayList<>();
+
+            // A1.
+            final AlternativaModeloChecklist a1 = p1.getAlternativas().get(0);
+            alternativas.add(new ChecklistAlternativaResposta(
+                    a1.getCodigo(),
+                    true,
+                    false,
+                    null));
+
+            // A2.
+            final AlternativaModeloChecklist a2 = p1.getAlternativas().get(1);
+            alternativas.add(new ChecklistAlternativaResposta(
+                    a2.getCodigo(),
+                    false,
+                    true,
+                    null));
+
+            respostas.add(new ChecklistResposta(p1.getCodigo(), alternativas));
+        }
+
+        {
+            // Responde a P2 - ela NÃO É single_choice.
+            final PerguntaModeloChecklistVisualizacao p2 = modeloBuscado.getPerguntas().get(1);
+            final List<ChecklistAlternativaResposta> alternativas = new ArrayList<>();
+
+            // B1.
+            final AlternativaModeloChecklist b1 = p2.getAlternativas().get(0);
+            alternativas.add(new ChecklistAlternativaResposta(
+                    b1.getCodigo(),
+                    true,
+                    false,
+                    null));
+
+            // B2.
+            final AlternativaModeloChecklist b2 = p2.getAlternativas().get(1);
+            alternativas.add(new ChecklistAlternativaResposta(
+                    b2.getCodigo(),
+                    true,
+                    true,
+                    "Está com problema..."));
+
+            respostas.add(new ChecklistResposta(p2.getCodigo(), alternativas));
+        }
+
+        final ChecklistInsercao insercao = new ChecklistInsercao(
+                5L,
+                result.getCodModeloChecklistInserido(),
+                result.getCodVersaoModeloChecklistInserido(),
+                2272L,
+                CPF_TOKEN,
+                3195L,
+                "PRO0001",
+                TipoChecklist.SAIDA,
+                112,
+                10000,
+                respostas,
+                ProLogDateParser.toLocalDateTime("2019-10-14T09:35:10"),
+                FonteDataHora.LOCAL_CELULAR,
+                80,
+                83,
+                "device didID",
+                "deviceImei",
+                10000,
+                11000);
+
+        final ChecklistOfflineService checklistService = new ChecklistOfflineService();
+        final Long codChecklistInserido = checklistService.insertChecklistOffline(
+                TOKEN_CHECK_OFF,
+                83,
+                insercao).getCodigo();
+
+        {
+            // Compara as propriedades do checklist inserido com o buscado.
+            final Checklist checklist = new ChecklistService().getByCod(codChecklistInserido, token);
+
+            assertThat(checklist).isNotNull();
+            assertThat(checklist.getCodigo()).isEqualTo(codChecklistInserido);
+            assertThat(checklist.getCodModelo()).isEqualTo(result.getCodModeloChecklistInserido());
+            assertThat(checklist.getCodVersaoModeloChecklist()).isEqualTo(result.getCodVersaoModeloChecklistInserido());
+            assertThat(checklist.getColaborador().getCpf()).isEqualTo(Long.parseLong(CPF_TOKEN));
+            assertThat(checklist.getPlacaVeiculo()).isEqualTo("PRO0001");
+            assertThat(checklist.getTipo()).isEqualTo(TipoChecklist.SAIDA.asChar());
+            assertThat(checklist.getKmAtualVeiculo()).isEqualTo(112);
+            assertThat(checklist.getTempoRealizacaoCheckInMillis()).isEqualTo(10000);
+            assertThat(checklist.getData()).isEqualTo(ProLogDateParser.toLocalDateTime("2019-10-14T09:35:10"));
+            assertThat(checklist.getQtdItensOk()).isEqualTo(0);
+            assertThat(checklist.getQtdItensNok()).isEqualTo(2);
+            assertThat(checklist.getQtdAlternativasOk()).isEqualTo(1);
+            assertThat(checklist.getQtdAlternativasNok()).isEqualTo(3);
+            {
+                // Compara a P1.
+                final PerguntaRespostaChecklist p1 = checklist.getListRespostas().get(0);
+
+                // A1.
+                final AlternativaChecklist a1 = p1.getAlternativasResposta().get(0);
+                assertThat(a1.getAlternativa()).isEqualTo("A1");
+                assertThat(a1.getRespostaOutros()).isNull();
+                assertThat(a1.isTipoOutros()).isFalse();
+                assertThat(a1.isSelected()).isTrue();
+                assertThat(a1.getOrdemExibicao()).isEqualTo(1);
+
+                // A2.
+                final AlternativaChecklist a2 = p1.getAlternativasResposta().get(1);
+                assertThat(a2.getAlternativa()).isEqualTo("Outros");
+                assertThat(a2.getRespostaOutros()).isNull();
+                assertThat(a2.isTipoOutros()).isTrue();
+                assertThat(a2.isSelected()).isFalse();
+                assertThat(a2.getOrdemExibicao()).isEqualTo(2);
+            }
+            {
+                // Compara a P2.
+                final PerguntaRespostaChecklist p2 = checklist.getListRespostas().get(1);
+
+                // B1.
+                final AlternativaChecklist b1 = p2.getAlternativasResposta().get(0);
+                assertThat(b1.getAlternativa()).isEqualTo("B1");
+                assertThat(b1.getRespostaOutros()).isNull();
+                assertThat(b1.isTipoOutros()).isFalse();
+                assertThat(b1.isSelected()).isTrue();
+                assertThat(b1.getOrdemExibicao()).isEqualTo(1);
+
+                // B2.
+                final AlternativaChecklist b2 = p2.getAlternativasResposta().get(1);
+                assertThat(b2.getAlternativa()).isEqualTo("Outros");
+                assertThat(b2.getRespostaOutros()).isEqualTo("Está com problema...");
+                assertThat(b2.isTipoOutros()).isTrue();
+                assertThat(b2.isSelected()).isTrue();
+                assertThat(b2.getOrdemExibicao()).isEqualTo(2);
+            }
+        }
     }
 }
