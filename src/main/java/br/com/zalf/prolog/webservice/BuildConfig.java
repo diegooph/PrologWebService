356479b9d02adbcdc2887b848108ea6c9a235abf
Luiz Felipe <luiz.felipe_95@hotmail.com>
2019-11-24 20:17:40 -0300

    feat: prepara para release v1.0.7

--- src/main/java/br/com/zalf/prolog/webservice/BuildConfig.java
+++ src/main/java/br/com/zalf/prolog/webservice/BuildConfig.java
@@ -1,6 +1,6 @@
 package br.com.zalf.prolog.webservice;
 
 public class BuildConfig {
-	 public static final boolean DEBUG = true;
+	 public static final boolean DEBUG = false;
 	 public static final String VERSION_CODE = "v2_1_0_7";
 }
