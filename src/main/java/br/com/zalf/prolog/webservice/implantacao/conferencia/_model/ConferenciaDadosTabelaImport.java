bbfaf07fd5b552df7f09c4656c40e230651452b8
thaisksf <thaisksf@gmail.com>
2019-12-13 11:02:30 -0300

    refactor(pneu): cria estrutura para impedir repeticao de codigo
    PL-2320

---
+++ src/main/java/br/com/zalf/prolog/webservice/implantacao/conferencia/_model/ConferenciaDadosTabelaImport.java
@@ -0,0 +1,29 @@
+package br.com.zalf.prolog.webservice.implantacao.conferencia._model;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created on 13/12/19.
+ *
+ * @author Thais Francisco (https://github.com/thaisksf)
+ */
+public class ConferenciaDadosTabelaImport {
+    @NotNull
+    private Long codDadosAutorImport;
+    @NotNull
+    private String nomeTabelaImport;
+
+    public ConferenciaDadosTabelaImport(@NotNull final Long codDadosAutorImport,
+                                        @NotNull final String nomeTabelaImport) {
+        this.codDadosAutorImport = codDadosAutorImport;
+        this.nomeTabelaImport = nomeTabelaImport;
+    }
+
+    public final Long getCodDadosAutorImport() {
+        return codDadosAutorImport;
+    }
+
+    public final String getNomeTabelaImport() {
+        return nomeTabelaImport;
+    }
+}
