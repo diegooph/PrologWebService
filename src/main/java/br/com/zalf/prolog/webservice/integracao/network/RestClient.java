97d4a07b25f9cdfed82ec484f5a76c7fca2f3d12
didi <dioge.vanzella@gmail.com>
2020-03-13 10:16:28 -0300

    refactor(integração): cria cache para o RestClient
    PL-2562

--- src/main/java/br/com/zalf/prolog/webservice/integracao/network/RestClient.java
+++ src/main/java/br/com/zalf/prolog/webservice/integracao/network/RestClient.java
@@ -5,22 +5,40 @@ import org.jetbrains.annotations.NotNull;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public final class RestClient {
+    @NotNull
+    private static final String DEFAULT_BASE_URL = "http://localhost:8080/";
+    @NotNull
+    private static final Map<String, Object> SERVICE_CACHE = new HashMap<>();
     @NotNull
     private static final Retrofit RETROFIT;
 
     static {
         RETROFIT = new Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create(GsonUtils.getGson()))
-                .baseUrl("Constants.BASE_URL")
                 .client(OkHttp.provideNetworkDefaultClient())
+                .baseUrl(DEFAULT_BASE_URL)
                 .build();
     }
 
     private RestClient() {
     }
 
-    public static <T> T getService(final Class<T> serviceClass) {
-        return RETROFIT.create(serviceClass);
+    @NotNull
+    public static <T> T getService(@NotNull final Class<T> serviceClass) {
+        final String canonicalName = serviceClass.getCanonicalName();
+        if (!SERVICE_CACHE.containsKey(canonicalName)) {
+            synchronized (RestClient.class) {
+                if (!SERVICE_CACHE.containsKey(canonicalName)) {
+                    final T service = RETROFIT.create(serviceClass);
+                    SERVICE_CACHE.put(serviceClass.getCanonicalName(), service);
+                }
+            }
+        }
+        //noinspection unchecked
+        return (T) SERVICE_CACHE.get(canonicalName);
     }
 }
