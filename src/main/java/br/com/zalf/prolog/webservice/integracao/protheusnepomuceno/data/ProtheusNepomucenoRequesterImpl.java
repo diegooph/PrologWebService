c7f1803e14d5a0fcab470133591b89b2c1335a8f
Wellington <wvinim@msn.com>
2020-03-13 09:32:38 -0300

    feat(integração): estrutura parcial de listagem de pneus para aferição avulsa
    PL-2563

--- src/main/java/br/com/zalf/prolog/webservice/integracao/protheusnepomuceno/data/ProtheusNepomucenoRequesterImpl.java
+++ src/main/java/br/com/zalf/prolog/webservice/integracao/protheusnepomuceno/data/ProtheusNepomucenoRequesterImpl.java
@@ -3,9 +3,15 @@ package br.com.zalf.prolog.webservice.integracao.protheusnepomuceno.data;
 import br.com.zalf.prolog.webservice.integracao.network.RestClient;
 import br.com.zalf.prolog.webservice.integracao.protheusnepomuceno.model.AfericaoAvulsaProtheusNepomuceno;
 import br.com.zalf.prolog.webservice.integracao.protheusnepomuceno.model.AfericaoPlacaProtheusNepomuceno;
+import br.com.zalf.prolog.webservice.integracao.protheusnepomuceno.model.PneuEstoqueProtheusNepomuceno;
+import br.com.zalf.prolog.webservice.integracao.protheusrodalog.model.error.ProtheusRodalogException;
+import br.com.zalf.prolog.webservice.integracao.rodoparhorizonte.model.error.RodoparHorizonteException;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import retrofit2.Call;
 import retrofit2.Response;
+
+import java.util.List;
 
 /**
  * Created on 3/10/20
@@ -33,7 +39,34 @@ public final class ProtheusNepomucenoRequesterImpl implements ProtheusNepomuceno
         return handleResponse(call.execute());
     }
 
-    private ResponseAfericaoProtheusNepomuceno handleResponse(final Response<ResponseAfericaoProtheusNepomuceno> execute) {
-        return null;
+    @NotNull
+    @Override
+    public List<PneuEstoqueProtheusNepomuceno> getListagemPneusEmEstoque(@NotNull final String codFiliais) throws Throwable {
+        final ProtheusNepomucenoRest service = RestClient.getService(ProtheusNepomucenoRest.class);
+        final Call<List<PneuEstoqueProtheusNepomuceno>> call =
+                service.getListagemPneusEmEstoque(codFiliais);
+        return handleResponse(call.execute());
+    }
+
+    @NotNull
+    private <T> T handleResponse(@Nullable final Response<T> response) {
+        if (response != null) {
+            if (response.isSuccessful() && response.body() != null) {
+                return response.body();
+            } else {
+                if (response.errorBody() == null) {
+                    throw new ProtheusRodalogException(
+                            "[INTEGRACAO - NEPOMUCENO] Protheus não retornou todas as informações necessárias",
+                            "A comunicação retornou erro porém sem nenhuma informação no corpo do erro");
+                }
+                throw new ProtheusRodalogException(
+                        "[INTEGRACAO - NEPOMUCENO] Protheus não conseguiu processar as informações",
+                        "A requisição do token retornou erro, provavelmente falta mapeamento no Protheus");
+            }
+        } else {
+            throw new RodoparHorizonteException(
+                    "[INTEGRACAO - NEPOMUCENO] Nunhuma resposta obtida da integração com o sistema Protheus",
+                    "A comunicação com o Protheus retornou um response vazio");
+        }
     }
 }
