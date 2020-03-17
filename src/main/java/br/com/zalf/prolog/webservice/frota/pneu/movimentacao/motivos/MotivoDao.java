2323cae8b8873c958642dc1c53dbbab14840f650
gustavocnp95 <c98jjkdhgy>
2020-03-17 14:57:38 -0300

    feature(movimentacao): cria motivoDao - PL-2607

---
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/movimentacao/motivos/MotivoDao.java
@@ -0,0 +1,15 @@
+package br.com.zalf.prolog.webservice.frota.pneu.movimentacao.motivos;
+
+import br.com.zalf.prolog.webservice.frota.pneu.movimentacao.motivos._model.MotivoTrocaInsercao;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created on 2020-03-17
+ *
+ * @author Gustavo Navarro (https://github.com/gustavocnp95)
+ */
+public interface MotivoDao {
+
+    void insert(@NotNull MotivoTrocaInsercao motivoTrocaInsercao);
+
+}
