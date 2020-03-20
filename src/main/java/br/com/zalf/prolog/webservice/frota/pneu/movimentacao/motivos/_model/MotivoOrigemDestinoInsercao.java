08f89ee62694fb03f5c75672fe1ddba35c992591
gustavocnp95 <c98jjkdhgy>
2020-03-20 16:07:08 -0300

    feature(movimentacao): cria motivoInsercao - PL-2607

---
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/movimentacao/motivos/_model/MotivoOrigemDestinoInsercao.java
@@ -0,0 +1,27 @@
+package br.com.zalf.prolog.webservice.frota.pneu.movimentacao.motivos._model;
+
+import br.com.zalf.prolog.webservice.frota.pneu.movimentacao._model.OrigemDestinoEnum;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * Created on 2020-03-20
+ *
+ * @author Gustavo Navarro (https://github.com/gustavocnp95)
+ */
+public @Data class MotivoOrigemDestinoInsercao {
+
+    @NotNull
+    private final Long codMotivo;
+
+    @NotNull
+    private final OrigemDestinoEnum origemMovimentacao;
+
+    @NotNull
+    private final OrigemDestinoEnum destinoMovimentacao;
+
+    @NotNull
+    private final boolean obrigatorioMotivo;
+
+}
