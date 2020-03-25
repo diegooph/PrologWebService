e906fd6727f69d621d68abe2231599518d455a93
gustavocnp95 <c98jjkdhgy>
2020-03-25 08:32:44 -0300

    feature(movimentacao): cria objetos para insercao de motivo retirada em batch  - PL-2607

---
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/movimentacao/motivos/_model/MotivoRetiradaOrigemDestinoMotivoBatch.java
@@ -0,0 +1,29 @@
+package br.com.zalf.prolog.webservice.frota.pneu.movimentacao.motivos._model;
+
+import br.com.zalf.prolog.webservice.frota.pneu.movimentacao._model.OrigemDestinoEnum;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+/**
+ * Created on 2020-03-25
+ *
+ * @author Gustavo Navarro (https://github.com/gustavocnp95)
+ */
+public @Data
+class MotivoRetiradaOrigemDestinoMotivoBatch {
+
+    @NotNull
+    private final OrigemDestinoEnum origem;
+
+    @NotNull
+    private final OrigemDestinoEnum destino;
+
+    @NotNull
+    private final boolean obrigatorio;
+
+    @NotNull
+    private final List<Long> codMotivos;
+
+}
