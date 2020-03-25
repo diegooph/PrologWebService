a34933b88883f6e1e282d785bd9f7a82cd164e6d
gustavocnp95 <c98jjkdhgy>
2020-03-25 09:53:18 -0300

    feature(movimentacao): insere endpoint para insercao em batch - PL-2607

--- src/main/java/br/com/zalf/prolog/webservice/frota/pneu/movimentacao/motivos/MotivoRetiradaOrigemDestinoResource.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/movimentacao/motivos/MotivoRetiradaOrigemDestinoResource.java
@@ -1,9 +1,11 @@
 package br.com.zalf.prolog.webservice.frota.pneu.movimentacao.motivos;
 
 import br.com.zalf.prolog.webservice.commons.util.Platform;
+import br.com.zalf.prolog.webservice.commons.util.Required;
 import br.com.zalf.prolog.webservice.commons.util.UsedBy;
 import br.com.zalf.prolog.webservice.frota.pneu.movimentacao._model.OrigemDestinoEnum;
 import br.com.zalf.prolog.webservice.frota.pneu.movimentacao.motivos._model.MotivoRetiradaOrigemDestinoInsercao;
+import br.com.zalf.prolog.webservice.frota.pneu.movimentacao.motivos._model.MotivoRetiradaOrigemDestinoInsercaoBatch;
 import br.com.zalf.prolog.webservice.frota.pneu.movimentacao.motivos._model.MotivoRetiradaOrigemDestinoListagemMotivos;
 import br.com.zalf.prolog.webservice.frota.pneu.movimentacao.motivos._model.MotivoRetiradaOrigemDestinoVisualizacaoListagem;
 import org.jetbrains.annotations.NotNull;
@@ -30,6 +32,13 @@ public class MotivoRetiradaOrigemDestinoResource {
     public Long insert(@NotNull @Valid final MotivoRetiradaOrigemDestinoInsercao motivoRetiradaOrigemDestinoInsercao,
                        @NotNull @HeaderParam("Authorization") final String tokenAutenticacao) {
         return motivoRetiradaOrigemDestinoService.insert(motivoRetiradaOrigemDestinoInsercao, tokenAutenticacao);
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON + ";charset-utf-8")
+    public List<Long> insertBatch(@NotNull @Valid @Required final List<MotivoRetiradaOrigemDestinoInsercaoBatch> origensDestinosMotivos,
+                                  @NotNull @HeaderParam("Authorization") final String tokenAutenticacao) {
+        return motivoRetiradaOrigemDestinoService.insertBatch(origensDestinosMotivos, tokenAutenticacao);
     }
 
     @GET
