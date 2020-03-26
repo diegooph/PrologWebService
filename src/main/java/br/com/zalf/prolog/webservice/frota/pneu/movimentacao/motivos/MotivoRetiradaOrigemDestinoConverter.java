15c8c0e169ec7a39e142aa4aa2d7ee4fcb394f81
gustavocnp95 <c98jjkdhgy>
2020-03-26 15:07:52 -0300

    feature(movimentacao): cria novos converters - PL-2607

--- src/main/java/br/com/zalf/prolog/webservice/frota/pneu/movimentacao/motivos/MotivoRetiradaOrigemDestinoConverter.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/movimentacao/motivos/MotivoRetiradaOrigemDestinoConverter.java
@@ -2,12 +2,17 @@ package br.com.zalf.prolog.webservice.frota.pneu.movimentacao.motivos;
 
 import br.com.zalf.prolog.webservice.frota.pneu._model.StatusPneu;
 import br.com.zalf.prolog.webservice.frota.pneu.movimentacao._model.OrigemDestinoEnum;
+import br.com.zalf.prolog.webservice.frota.pneu.movimentacao.motivos._model.MotivoRetiradaListagem;
+import br.com.zalf.prolog.webservice.frota.pneu.movimentacao.motivos._model.MotivoRetiradaOrigemDestinoListagem;
+import br.com.zalf.prolog.webservice.frota.pneu.movimentacao.motivos._model.MotivoRetiradaOrigemDestinoListagemMotivos;
 import br.com.zalf.prolog.webservice.frota.pneu.movimentacao.motivos._model.MotivoRetiradaOrigemDestinoVisualizacao;
 import br.com.zalf.prolog.webservice.geral.unidade.UnidadeConverter;
 import org.jetbrains.annotations.NotNull;
 
 import java.sql.ResultSet;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created on 2020-03-23
@@ -21,7 +26,7 @@ public class MotivoRetiradaOrigemDestinoConverter {
     }
 
     @NotNull
-    static MotivoRetiradaOrigemDestinoVisualizacao createMotivoOrigemDestinoVisualizacaoListagem(
+    static MotivoRetiradaOrigemDestinoVisualizacao createMotivoRetiradaOrigemDestinoVisualizacao(
             @NotNull final ResultSet rSet) throws Throwable {
         return new MotivoRetiradaOrigemDestinoVisualizacao(rSet.getLong("CODIGO_MOTIVO_ORIGEM_DESTINO"),
                 rSet.getString("NOME_EMPRESA"),
@@ -31,6 +36,35 @@ public class MotivoRetiradaOrigemDestinoConverter {
                 rSet.getBoolean("OBRIGATORIO"),
                 rSet.getObject("DATA_HORA_ULTIMA_ALTERACAO", LocalDateTime.class),
                 rSet.getString("NOME_COLABORADOR_ULTIMA_ALTERACAO"));
+    }
+
+    @NotNull
+    static MotivoRetiradaListagem createMotivoRetiradaListagem(final ResultSet rSet) throws Throwable {
+        return new MotivoRetiradaListagem(rSet.getLong("codigo_motivo"),
+                rSet.getString("descricao_motivo"));
+    }
+
+    @NotNull
+    static MotivoRetiradaOrigemDestinoListagemMotivos createMotivoRetiradaOrigemDestinoListagemMotivos(final ResultSet rSet) throws Throwable {
+        final List<MotivoRetiradaListagem> motivosRetirada = new ArrayList();
+        motivosRetirada.add(MotivoRetiradaOrigemDestinoConverter.createMotivoRetiradaListagem(rSet));
+
+        return new MotivoRetiradaOrigemDestinoListagemMotivos(
+                OrigemDestinoEnum.getFromStatusPneu(StatusPneu.fromString(rSet.getString("origem_movimento"))),
+                OrigemDestinoEnum.getFromStatusPneu(StatusPneu.fromString(rSet.getString("destino_movimento"))),
+                motivosRetirada,
+                rSet.getBoolean("obrigatorio"));
+    }
+
+    @NotNull
+    static MotivoRetiradaOrigemDestinoListagem createMotivoRetiradaOrigemDestinoListagem(final ResultSet rSet) throws Throwable {
+        final List<MotivoRetiradaOrigemDestinoListagemMotivos> origensDestinos = new ArrayList();
+        origensDestinos.add(MotivoRetiradaOrigemDestinoConverter.createMotivoRetiradaOrigemDestinoListagemMotivos(rSet));
+
+        return new MotivoRetiradaOrigemDestinoListagem(
+                rSet.getLong("codigo_unidade"),
+                rSet.getString("nome_unidade"),
+                origensDestinos);
     }
 
 }
