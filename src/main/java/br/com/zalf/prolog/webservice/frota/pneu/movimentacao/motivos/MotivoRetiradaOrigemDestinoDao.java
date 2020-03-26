983924e494e8587bae89f77a7fb13c8b8002dede
gustavocnp95 <c98jjkdhgy>
2020-03-26 08:44:16 -0300

    feature(movimentacao): melhora documentação - PL-2607

--- src/main/java/br/com/zalf/prolog/webservice/frota/pneu/movimentacao/motivos/MotivoRetiradaOrigemDestinoDao.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/movimentacao/motivos/MotivoRetiradaOrigemDestinoDao.java
@@ -3,7 +3,7 @@ package br.com.zalf.prolog.webservice.frota.pneu.movimentacao.motivos;
 import br.com.zalf.prolog.webservice.frota.pneu.movimentacao._model.OrigemDestinoEnum;
 import br.com.zalf.prolog.webservice.frota.pneu.movimentacao.motivos._model.MotivoRetiradaOrigemDestinoInsercao;
 import br.com.zalf.prolog.webservice.frota.pneu.movimentacao.motivos._model.MotivoRetiradaOrigemDestinoListagemMotivos;
-import br.com.zalf.prolog.webservice.frota.pneu.movimentacao.motivos._model.MotivoRetiradaOrigemDestinoVisualizacaoListagem;
+import br.com.zalf.prolog.webservice.frota.pneu.movimentacao.motivos._model.MotivoRetiradaOrigemDestinoVisualizacao;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -15,43 +15,48 @@ import java.util.List;
  */
 public interface MotivoRetiradaOrigemDestinoDao {
 
+    /**
+     * Insere uma lista de {@link MotivoRetiradaOrigemDestinoInsercao origemDestinoMotivo} no banco de dados.
+     *
+     * @param unidades          uma lista de unidades com suas rotas e seus motivos por rotas.
+     * @param tokenAutenticacao o token de sessão do usuário.
+     * @throws Throwable Caso qualquer erro ocorra.
+     */
     @NotNull
     List<Long> insert(@NotNull final List<MotivoRetiradaOrigemDestinoInsercao> unidades,
                       @NotNull final String tokenAutenticacao) throws Throwable;
 
     /**
-     * Busca um {@link MotivoRetiradaOrigemDestinoVisualizacaoListagem motivoOrigemDestino} no banco de dados.
+     * Busca um {@link MotivoRetiradaOrigemDestinoVisualizacao motivoOrigemDestino} no banco de dados.
      *
      * @param codMotivoOrigemDestino um código de motivoOrigemDestino a ser buscado no banco.
      * @param tokenAutenticacao      o token de sessão do usuário.
      * @throws Throwable Caso qualquer erro ocorra.
      */
     @NotNull
-    MotivoRetiradaOrigemDestinoVisualizacaoListagem getMotivoOrigemDestino(@NotNull final Long codMotivoOrigemDestino,
-                                                                           @NotNull final String tokenAutenticacao) throws Throwable;
+    MotivoRetiradaOrigemDestinoVisualizacao getMotivoOrigemDestino(@NotNull final Long codMotivoOrigemDestino,
+                                                                   @NotNull final String tokenAutenticacao) throws Throwable;
 
     /**
-     * Busca uma lista de {@link MotivoRetiradaOrigemDestinoVisualizacaoListagem motivoOrigemDestino} no banco de dados.
+     * Busca uma lista de {@link MotivoRetiradaOrigemDestinoVisualizacao motivoOrigemDestino} no banco de dados.
      *
-     * @param codEmpresa        um código de empresa a ser usada de parâmetro na consulta ao banco de dados.
      * @param tokenAutenticacao o token de sessão do usuário;
      * @throws Throwable Caso qualquer erro ocorra.
      */
     @NotNull
-    List<MotivoRetiradaOrigemDestinoVisualizacaoListagem> getMotivosOrigemDestino(@NotNull final Long codEmpresa,
-                                                                                  @NotNull final String tokenAutenticacao) throws Throwable;
+    List<MotivoRetiradaOrigemDestinoVisualizacao> getMotivosOrigemDestino(@NotNull final String tokenAutenticacao) throws Throwable;
 
     /**
-     * Busca uma lista de {@link MotivoRetiradaOrigemDestinoVisualizacaoListagem motivoOrigemDestino} no banco de dados.
+     * Busca uma lista de {@link MotivoRetiradaOrigemDestinoListagemMotivos motivoOrigemDestino} no banco de dados.
      *
-     * @param origem     a origem a ser utilizada de filtro.
-     * @param destino    o destino a ser usado de filtro.
-     * @param codUnidade o código da unidade dos motivos que estão sendo buscados.
+     * @param origemMovimento  a origem a ser utilizada de filtro.
+     * @param destinoMovimento o destino a ser usado de filtro.
+     * @param codUnidade       o código da unidade dos motivos que estão sendo buscados.
      * @throws Throwable Caso qualquer erro ocorra.
      */
     @NotNull
-    MotivoRetiradaOrigemDestinoListagemMotivos getMotivosByOrigemAndDestinoAndUnidade(@NotNull final OrigemDestinoEnum origem,
-                                                                                      @NotNull final OrigemDestinoEnum destino,
+    MotivoRetiradaOrigemDestinoListagemMotivos getMotivosByOrigemAndDestinoAndUnidade(@NotNull final OrigemDestinoEnum origemMovimento,
+                                                                                      @NotNull final OrigemDestinoEnum destinoMovimento,
                                                                                       @NotNull final Long codUnidade) throws Throwable;
 
 }
