32deef66a27b05feeaa9fdbd989f937ae50ca987
gustavocnp95 <c98jjkdhgy>
2020-03-26 15:59:08 -0300

    feature(movimentacao): ajusta nome da function adicionando retirada - PL-2607

--- src/main/java/br/com/zalf/prolog/webservice/frota/pneu/movimentacao/MovimentacaoDaoImpl.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/movimentacao/MovimentacaoDaoImpl.java
@@ -323,7 +323,7 @@ public final class MovimentacaoDaoImpl extends DatabaseConnection implements Mov
                                        @NotNull final Long codMotivo) throws Throwable {
         PreparedStatement stmt = null;
         try {
-            stmt = conn.prepareStatement("SELECT FUNC_MOVIMENTACAO_INSERE_MOTIVO_MOVIMENTO(" +
+            stmt = conn.prepareStatement("SELECT FUNC_MOVIMENTACAO_INSERE_MOTIVO_RETIRADA_MOVIMENTO(" +
                     "F_COD_MOVIMENTO := ?," +
                     "F_COD_MOTIVO := ?);");
             stmt.setLong(1, codMovimento);
