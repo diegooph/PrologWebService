1f951a1088ffe47bd3a07499f0e7b97d2ac82843
gustavocnp95 <c98jjkdhgy>
2020-03-27 11:32:56 -0300

    feature(movimentacao): cria objeto para retorno origem destino - PL-2607

---
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/movimentacao/motivos/_model/OrigemDestinoListagem.java
@@ -0,0 +1,24 @@
+package br.com.zalf.prolog.webservice.frota.pneu.movimentacao.motivos._model;
+
+import br.com.zalf.prolog.webservice.frota.pneu.movimentacao._model.OrigemDestinoEnum;
+import lombok.Data;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created on 2020-03-27
+ *
+ * @author Gustavo Navarro (https://github.com/gustavocnp95)
+ */
+@Data
+public class OrigemDestinoListagem {
+
+    @NotNull
+    private final Long codUnidade;
+
+    @NotNull
+    private final OrigemDestinoEnum origem;
+
+    @NotNull
+    private final OrigemDestinoEnum destino;
+
+}
