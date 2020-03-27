f8b71591750a5092dc9b22e58d98b05b1c14e5ef
gustavocnp95 <c98jjkdhgy>
2020-03-27 14:58:24 -0300

    feature(movimentacao): cria objeto para retornar historico - PL-2607

---
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/movimentacao/motivos/_model/MotivoRetiradaHistoricoListagem.java
@@ -0,0 +1,37 @@
+package br.com.zalf.prolog.webservice.frota.pneu.movimentacao.motivos._model;
+
+import lombok.Data;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.LocalDateTime;
+
+/**
+ * Created on 2020-03-27
+ *
+ * @author Gustavo Navarro (https://github.com/gustavocnp95)
+ */
+@Data
+public class MotivoRetiradaHistoricoListagem {
+
+    @NotNull
+    private final Long codHistoricoRetirada;
+
+    @NotNull
+    private final Long codMotivoRetirada;
+
+    @NotNull
+    private final String descricaoMotivoRetirada;
+
+    @NotNull
+    private final boolean ativoMotivoRetirada;
+
+    @NotNull
+    private final String codAuxiliarMotivoRetirada;
+
+    @NotNull
+    private final LocalDateTime dataHoraAlteracaoMotivoRetirada;
+
+    @NotNull
+    private final String nomeColaboradorAlteracaoMotivoRetirada;
+
+}
