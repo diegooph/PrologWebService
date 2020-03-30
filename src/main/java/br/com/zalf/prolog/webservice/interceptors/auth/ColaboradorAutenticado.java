74ff20b475f2ef30ea91de06d087e9c82b2dafdd
Luiz Felipe <luiz.felipe_95@hotmail.com>
2020-03-30 09:41:23 -0300

    feat: implementa método no ColaboradorAutenticado para buscar o ZoneId

--- src/main/java/br/com/zalf/prolog/webservice/interceptors/auth/ColaboradorAutenticado.java
+++ src/main/java/br/com/zalf/prolog/webservice/interceptors/auth/ColaboradorAutenticado.java
@@ -1,10 +1,13 @@
 package br.com.zalf.prolog.webservice.interceptors.auth;
 
+import br.com.zalf.prolog.webservice.TimeZoneManager;
+import br.com.zalf.prolog.webservice.commons.util.Log;
 import br.com.zalf.prolog.webservice.interceptors.auth.authenticator.StatusSecured;
 import lombok.Data;
 import org.jetbrains.annotations.NotNull;
 
 import java.security.Principal;
+import java.time.ZoneId;
 
 /**
  * Created on 2020-03-25
@@ -13,6 +16,7 @@ import java.security.Principal;
  */
 @Data
 public final class ColaboradorAutenticado implements Principal {
+    private static final String TAG = ColaboradorAutenticado.class.getSimpleName();
     @NotNull
     private final Long codigo;
     @NotNull
@@ -23,5 +27,15 @@ public final class ColaboradorAutenticado implements Principal {
     @Override
     public String getName() {
         return String.valueOf(codigo);
+    }
+
+    @NotNull
+    public ZoneId getZoneIdUnidadeColaborador() {
+        try {
+            return TimeZoneManager.getZoneIdForCpf(cpf);
+        } catch (final Throwable throwable) {
+            Log.e(TAG, "Erro ao buscar ZoneId pelo CPF: " + cpf, throwable);
+            throw new RuntimeException(throwable);
+        }
     }
 }
