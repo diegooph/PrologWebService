aa9c9b74461f8dab9157aadc596ad448d26ee561
Luiz Felipe <luiz.felipe_95@hotmail.com>
2020-03-30 10:14:51 -0300

    fix: corrige path do BuildConfig

--- src/main/java/br/com/zalf/prolog/webservice/commons/util/Log.java
+++ src/main/java/br/com/zalf/prolog/webservice/commons/util/Log.java
@@ -1,6 +1,6 @@
 package br.com.zalf.prolog.webservice.commons.util;
 
-import br.com.zalf.prolog.webservice.BuildConfig;
+import br.com.zalf.prolog.webservice.config.BuildConfig;
 import io.sentry.Sentry;
 import org.apache.logging.log4j.LogManager;
 
