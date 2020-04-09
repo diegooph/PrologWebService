e8ace9fbcfa673234b754f9a0da31ed34364f116
gustavocnp95 <c98jjkdhgy>
2020-04-09 13:42:37 -0300

    refactor(pneu): adiciona atributo codMotivo da listagem de motivo movimento PL-2609

--- src/main/java/br/com/zalf/prolog/webservice/frota/pneu/movimentacao/motivos/_model/MotivoMovimentoListagem.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/movimentacao/motivos/_model/MotivoMovimentoListagem.java
@@ -2,6 +2,7 @@ package br.com.zalf.prolog.webservice.frota.pneu.movimentacao.motivos._model;
 
 import lombok.Data;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Created on 2020-04-01
@@ -10,9 +11,13 @@ import org.jetbrains.annotations.NotNull;
  */
 @Data
 public final class MotivoMovimentoListagem {
+
     @NotNull
     private final Long codMotivoMovimento;
     @NotNull
     private final String descricaoMotivoMovimento;
+    @Nullable
+    private final String codAuxiliar;
     private final boolean ativo;
+
 }
