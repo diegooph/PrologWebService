0c93bdb0ea07b1c8da93957d90bdb8bd0f5888f1
thaisksf <thaisksf@gmail.com>
2020-04-14 16:59:59 -0300

    refactor(interno): documenta
    PL-2034

--- src/main/java/br/com/zalf/prolog/webservice/interno/apresentacao/ApresentacaoDao.java
+++ src/main/java/br/com/zalf/prolog/webservice/interno/apresentacao/ApresentacaoDao.java
@@ -1,10 +1,23 @@
 package br.com.zalf.prolog.webservice.interno.apresentacao;
 
+import org.jetbrains.annotations.NotNull;
+
 /**
  * Created on 13/04/20.
  *
  * @author Thais Francisco (https://github.com/thaisksf)
  */
 public interface ApresentacaoDao {
-    void getResetaClonaEmpresaApresentacao(String username, Long codEmpresaBase, Long codEmpresaUsuario) throws Throwable;
+
+    /**
+     * Método para resetar uma empresa de apresentação e clonar dados da empresa base.
+     *
+     * @param username   usuário cadastrado.
+     * @param codEmpresaBase código de empresa a qual vai servir de base para realizar a clonagem.
+     * @param codEmpresaUsuario código da empresa do usuário que será resetada.
+     * @throws Throwable Se algum erro ocorrer.
+     */
+    void getResetaClonaEmpresaApresentacao(@NotNull final String username,
+                                           @NotNull final Long codEmpresaBase,
+                                           @NotNull final Long codEmpresaUsuario) throws Throwable;
 }
