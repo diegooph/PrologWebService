3277abe90278e59c9a35ffd71522ce05249448e9
thaisksf <thaisksf@gmail.com>
2020-05-12 15:34:52 -0300

    refactor(veiculo): cria novos objeto
    PL-2692

---
+++ src/main/java/br/com/zalf/prolog/webservice/frota/veiculo/model/VeiculoVisualizacaoPneu.java
@@ -0,0 +1,11 @@
+package br.com.zalf.prolog.webservice.frota.veiculo.model;
+
+/**
+ * Created on 12/05/2020.
+ *
+ * @author Thais Francisco (https://github.com/thaisksf)
+ */
+public class VeiculoVisualizacaoPneu {
+
+
+}
