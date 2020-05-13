f2a0ec6474656d8ce42234ab205043ece4d4969e
thaisksf <thaisksf@gmail.com>
2020-05-13 15:32:10 -0300

    feat(veiculo): cria converter para VeiculoVisualizacao e Pneus
    PL-2692

--- src/main/java/br/com/zalf/prolog/webservice/frota/veiculo/VeiculoConverter.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/veiculo/VeiculoConverter.java
@@ -2,6 +2,7 @@ package br.com.zalf.prolog.webservice.frota.veiculo;
 
 import br.com.zalf.prolog.webservice.frota.veiculo.model.VeiculoListagem;
 import br.com.zalf.prolog.webservice.frota.veiculo.model.VeiculoVisualizacao;
+import br.com.zalf.prolog.webservice.frota.veiculo.model.VeiculoVisualizacaoPneu;
 import org.jetbrains.annotations.NotNull;
 
 import java.sql.ResultSet;
@@ -62,5 +63,46 @@ public class VeiculoConverter {
                 rSet.getString("TIPO"),
                 rSet.getString("MARCA"),
                 rSet.getLong("COD_MARCA"));
+    }
+
+    public static VeiculoVisualizacaoPneu createVeiculoVisualizacaoPneu(ResultSet rSet) throws SQLException {
+        return new VeiculoVisualizacaoPneu(
+        rSet.getLong("CODIGO"),
+        rSet.getLong("CODIGO_CLIENTE"),
+        rSet.getString("NOME_MARCA_PNEU"),
+        rSet.getLong("COD_MARCA_PNEU"),
+        rSet.getLong("COD_UNIDADE_ALOCADO"),
+        rSet.getLong("COD_REGIONAL_ALOCADO"),
+        rSet.getDouble("PRESSAO_ATUAL"),
+        rSet.getInt("VIDA_ATUAL"),
+        rSet.getInt("VIDA_TOTAL"),
+        rSet.getBoolean("PNEU_NOVO_NUNCA_RODADO"),
+        rSet.getString("NOME_MODELO_PNEU"),
+        rSet.getLong("COD_MODELO_PNEU"),
+        rSet.getInt("QT_SULCOS_MODELO_PNEU"),
+        rSet.getDouble("ALTURA_SULCOS_MODELO_PNEU"),
+        rSet.getInt("ALTURA"),
+        rSet.getInt("LARGURA"),
+        rSet.getDouble("ARO"),
+        rSet.getLong("COD_DIMENSAO"),
+        rSet.getDouble("PRESSAO_RECOMENDADA"),
+        rSet.getDouble("ALTURA_SULCO_CENTRAL_INTERNO"),
+        rSet.getDouble("ALTURA_SULCO_CENTRAL_EXTERNO"),
+        rSet.getDouble("ALTURA_SULCO_INTERNO"),
+        rSet.getDouble("ALTURA_SULCO_EXTERNO"),
+        rSet.getBoolean("STATUS"),
+        rSet.getString("DOT"),
+        rSet.getDouble("VALOR"),
+        rSet.getLong("COD_MODELO_BANDA"),
+        rSet.getString("NOME_MODELO_BANDA"),
+        rSet.getInt("QT_SULCOS_MODELO_BANDA"),
+        rSet.getDouble("ALTURA_SULCOS_MODELO_BANDA"),
+        rSet.getLong("COD_MARCA_BANDA"),
+        rSet.getString("NOME_MARCA_BANDA"),
+        rSet.getDouble("VALOR_BANDA"),
+        rSet.getInt("POSICAO_PNEU"),
+        rSet.getInt("POSICAO_APLICADO_CLIENTE"),
+        rSet.getLong("COD_VEICULO_APLICADO"),
+        rSet.getString("PLACA_APLICADO"));
     }
 }
