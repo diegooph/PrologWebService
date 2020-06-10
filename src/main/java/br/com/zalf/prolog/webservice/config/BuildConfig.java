6b7e420d0b19e18575c44eb17e5363978be6cd1f
didi <dioge.vanzella@gmail.com>
2020-06-10 19:42:36 -0300

    feat: prepara para release v2_1_0_32

--- src/main/java/br/com/zalf/prolog/webservice/config/BuildConfig.java
+++ src/main/java/br/com/zalf/prolog/webservice/config/BuildConfig.java
@@ -1,6 +1,6 @@
 package br.com.zalf.prolog.webservice.config;
 
 public class BuildConfig {
-    public static final boolean DEBUG = true;
+    public static final boolean DEBUG = false;
     public static final String VERSION_CODE = "v2_1_0_32";
 }
