efca11c6205a3ce3bfa57496c78891fedf442a24
didi <dioge.vanzella@gmail.com>
2020-07-07 16:54:56 -0300

    refactor(integração): cria functions para as queries hardcoded
    PL-2671

--- src/main/java/br/com/zalf/prolog/webservice/gente/empresa/EmpresaDaoImpl.java
+++ src/main/java/br/com/zalf/prolog/webservice/gente/empresa/EmpresaDaoImpl.java
@@ -276,22 +276,16 @@ public class EmpresaDaoImpl extends DatabaseConnection implements EmpresaDao {
         PreparedStatement stmt = null;
         try {
             conn = getConnection();
-            stmt = conn.prepareStatement("SELECT DISTINCT PP.codigo AS COD_PILAR, PP.pilar, FP.codigo AS COD_FUNCAO, FP.funcao\n" +
-                    "FROM PILAR_PROLOG PP\n" +
-                    "JOIN FUNCAO_PROLOG_v11 FP ON FP.cod_pilar = PP.codigo\n" +
-                    "JOIN unidade_pilar_prolog upp on upp.cod_pilar = pp.codigo\n" +
-                    "WHERE upp.cod_unidade = ?\n" +
-                    "ORDER BY PP.pilar, FP.funcao");
+            stmt = conn.prepareStatement("select * from func_empresa_get_visao_unidade(f_cod_unidade => ?);");
             stmt.setLong(1, codUnidade);
             rSet = stmt.executeQuery();
             pilares = createPilares(rSet);
+            final Visao visao = new Visao();
+            visao.setPilares(pilares);
+            return visao;
         } finally {
-            closeConnection(conn, stmt, rSet);
+            close(conn, stmt, rSet);
         }
-
-        final Visao visao = new Visao();
-        visao.setPilares(pilares);
-        return visao;
     }
 
     private List<Pilar> getPilaresCargo(final Long codUnidade, final Long codCargo) throws SQLException {
@@ -301,17 +295,14 @@ public class EmpresaDaoImpl extends DatabaseConnection implements EmpresaDao {
         PreparedStatement stmt = null;
         try {
             conn = getConnection();
-            stmt = conn.prepareStatement("SELECT DISTINCT PP.codigo AS COD_PILAR, PP.pilar, FP.codigo AS COD_FUNCAO, FP.funcao FROM cargo_funcao_prolog_V11 CF\n" +
-                    "JOIN PILAR_PROLOG PP ON PP.codigo = CF.cod_pilar_prolog\n" +
-                    "JOIN FUNCAO_PROLOG_V11 FP ON FP.cod_pilar = PP.codigo AND FP.codigo = CF.cod_funcao_prolog\n" +
-                    "WHERE CF.cod_unidade = ? AND cod_funcao_colaborador = ?\n" +
-                    "ORDER BY PP.pilar, FP.funcao");
+            stmt = conn.prepareStatement("select * " +
+                    "from func_empresa_get_visao_cargo(f_cod_unidade => ?, f_cod_cargo_colaborador => ?);");
             stmt.setLong(1, codUnidade);
             stmt.setLong(2, codCargo);
             rSet = stmt.executeQuery();
             pilares = createPilares(rSet);
         } finally {
-            closeConnection(conn, stmt, rSet);
+            close(conn, stmt, rSet);
         }
 
         return pilares;
