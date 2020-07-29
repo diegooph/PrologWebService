5349d39c99c14bbf31e42b113c7fe1b1b5858116
thaisksf <thaisksf@gmail.com>
2020-07-29 17:52:42 -0300

    feat(colaborador): cria objeto automatizado colaboradores
    PL-2460

---
+++ src/main/java/br/com/zalf/prolog/webservice/interno/implantacao/conferencia/gente/colaborador/_model/ColaboradorPlanilha.java
@@ -0,0 +1,76 @@
+package br.com.zalf.prolog.webservice.interno.implantacao.conferencia.gente.colaborador._model;
+
+/**
+ * Created on 29/07/2020.
+ *
+ * @author Thais Francisco (https://github.com/thaisksf)
+ */
+public class ColaboradorPlanilha {
+    private String cpf;
+    private String pis;
+    private String nome;
+    private String dataNascimento;
+    private String dataAdmissao;
+    private String matriculaPromax;
+    private String matriculaPonto;
+    private String equipe;
+    private String setor;
+    private String funcao;
+    private String email;
+    private String telefone;
+    private String pais;
+
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setPis(String pis) {
+        this.pis = pis;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public void setDataAdmissao(String dataAdmissao) {
+        this.dataAdmissao = dataAdmissao;
+    }
+
+    public void setMatriculaPromax(String matriculaPromax) {
+        this.matriculaPromax = matriculaPromax;
+    }
+
+    public void setMatriculaPonto(String matriculaPonto) {
+        this.matriculaPonto = matriculaPonto;
+    }
+
+    public void setEquipe(String equipe) {
+        this.equipe = equipe;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+}
+
