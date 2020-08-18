5ed3ec2f444062af8385e86a693ccca2c8169994
gustavocnp95 <c98jjkdhgy>
2020-08-18 17:37:19 -0300

    feat(checklist): inicia criacao do objeto para busca das informacoes de os no banco
    PL-2850

---
+++ src/main/java/br/com/zalf/prolog/webservice/integracao/agendador/os/_model/OsIntegracao.java
@@ -0,0 +1,30 @@
+package br.com.zalf.prolog.webservice.integracao.agendador.os._model;
+
+import lombok.Data;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.LocalDateTime;
+
+/**
+ * Created on 2020-08-18
+ *
+ * @author Gustavo Navarro (https://github.com/gustavocnp95)
+ */
+@Data
+public class OsIntegracao {
+
+    @NotNull
+    private final Long codUnidade;
+    @NotNull
+    private final Long codOsProlog;
+    @NotNull
+    private final LocalDateTime dataHoraAbertura;
+    @NotNull
+    private final String placaVeiculo;
+    @NotNull
+    private final Long kmVeiculoNaAbertura;
+    @NotNull
+    private final String cpfCriadorChecklist;
+    @NotNull
+    private final
+}
