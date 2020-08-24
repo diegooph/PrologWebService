695e62a65351d0d427bd8b528e614ea666dd8e19
gustavocnp95 <c98jjkdhgy>
2020-08-24 17:32:12 -0300

    feat(checklist): adiciona constants para fixar no codigo busca da url
    PL-2903

--- src/main/java/br/com/zalf/prolog/webservice/integracao/avacorpavilan/AvaCorpAvilanConstants.java
+++ src/main/java/br/com/zalf/prolog/webservice/integracao/avacorpavilan/AvaCorpAvilanConstants.java
@@ -1,6 +1,7 @@
 package br.com.zalf.prolog.webservice.integracao.avacorpavilan;
 
 import br.com.zalf.prolog.webservice.config.BuildConfig;
+import br.com.zalf.prolog.webservice.integracao.sistema.SistemaKey;
 
 /**
  * Created on 9/28/17
@@ -10,15 +11,17 @@ import br.com.zalf.prolog.webservice.config.BuildConfig;
 @SuppressWarnings("ConstantConditions")
 public final class AvaCorpAvilanConstants {
 
+    public static final SistemaKey SISTEMA_KEY_AVILAN = SistemaKey.AVACORP_AVILAN;
+    public static final Long CODIGO_EMPRESA_AVILAN = 2L;
+    private static final String BASE_URL_TESTES = "http://prolog.avaconcloud.com/Avilan/IntegracaoPrologTestes/";
+    private static final String BASE_URL_PROD = "http://prolog.avaconcloud.com/Avilan/IntegracaoProlog/";
+    public static final String BASE_URL = BuildConfig.DEBUG ? BASE_URL_TESTES : BASE_URL_PROD;
+    private static final String NAMESPACE_TESTES = "http://www.avacorp.com.br/integracaoprologtestes";
+    private static final String NAMESPACE_PROD = "http://www.avacorp.com.br/integracaoprolog";
+    public static final String NAMESPACE = BuildConfig.DEBUG ? NAMESPACE_TESTES : NAMESPACE_PROD;
+
     private AvaCorpAvilanConstants() {
 
     }
 
-    private static final String BASE_URL_TESTES = "http://prolog.avaconcloud.com/Avilan/IntegracaoPrologTestes/";
-    private static final String BASE_URL_PROD = "http://prolog.avaconcloud.com/Avilan/IntegracaoProlog/";
-    public static final String BASE_URL = BuildConfig.DEBUG ? BASE_URL_TESTES : BASE_URL_PROD;
-
-    private static final String NAMESPACE_TESTES = "http://www.avacorp.com.br/integracaoprologtestes";
-    private static final String NAMESPACE_PROD = "http://www.avacorp.com.br/integracaoprolog";
-    public static final String NAMESPACE = BuildConfig.DEBUG ? NAMESPACE_TESTES : NAMESPACE_PROD;
 }
