85f7ec84f09bfcde2f1644e09fd113fd7fb3fd71
thaisksf <thaisksf@gmail.com>
2020-08-31 14:39:59 -0300

    refactor(pneu): adiciona tipo vinculo aos imports e formata
    PL-2771

--- src/main/java/br/com/zalf/prolog/webservice/interno/implantacao/conferencia/_model/TipoImport.java
+++ src/main/java/br/com/zalf/prolog/webservice/interno/implantacao/conferencia/_model/TipoImport.java
@@ -10,57 +10,64 @@ import org.jetbrains.annotations.NotNull;
  */
 public enum TipoImport {
 
-        VEICULO("VEICULO") {
-            @NotNull
-            @Override
-            public String getLegibleString() {
-                return "VEICULO";
-            }
-        },
-        PNEU("PNEU") {
-            @NotNull
-            @Override
-            public String getLegibleString() {
-                return "PNEU";
-            }
-        },
-        COLABORADOR("COLABORADOR") {
-            @NotNull
-            @Override
-            public String getLegibleString() {
-                return "COLABORADOR";
-            }
-        };
-
+    VEICULO("VEICULO") {
         @NotNull
-        private final String stringRepresentation;
+        @Override
+        public String getLegibleString() {
+            return "VEICULO";
+        }
+    },
+    PNEU("PNEU") {
+        @NotNull
+        @Override
+        public String getLegibleString() {
+            return "PNEU";
+        }
+    },
+    COLABORADOR("COLABORADOR") {
+        @NotNull
+        @Override
+        public String getLegibleString() {
+            return "COLABORADOR";
+        }
+    },
+    VINCULO("VINCULO") {
+        @NotNull
+        @Override
+        public String getLegibleString() {
+            return "VINCULO";
+        }
+    };
+
+    @NotNull
+    private final String stringRepresentation;
 
     TipoImport(@NotNull final String stringRepresentation) {
-            this.stringRepresentation = stringRepresentation;
-        }
+        this.stringRepresentation = stringRepresentation;
+    }
 
-        @NotNull
-        public abstract String getLegibleString();
+    @NotNull
+    public abstract String getLegibleString();
 
-        @NotNull
-        public String asString() {
-            return stringRepresentation;
-        }
+    @NotNull
+    public String asString() {
+        return stringRepresentation;
+    }
 
-        public static TipoImport fromString(@NotNull final String string) {
-            Preconditions.checkNotNull(string, "string cannot be null!");
+    public static TipoImport fromString(@NotNull final String string) {
+        Preconditions.checkNotNull(string, "string cannot be null!");
 
-            for (final TipoImport tipoImport : TipoImport.values()) {
-                if (string.equals(tipoImport.stringRepresentation)) {
-                    return tipoImport;
-                }
+        for (final TipoImport tipoImport : TipoImport.values()) {
+            if (string.equals(tipoImport.stringRepresentation)) {
+                return tipoImport;
             }
-
-            throw new IllegalArgumentException("Nenhum tipo de import encontrado para a string: " + string);
         }
 
-        @Override
-        public String toString() {
-            return asString();
-        }
+        throw new IllegalArgumentException("Nenhum tipo de import encontrado para a string: " + string);
+    }
+
+    @Override
+    public String toString() {
+        return asString();
+    }
 }
