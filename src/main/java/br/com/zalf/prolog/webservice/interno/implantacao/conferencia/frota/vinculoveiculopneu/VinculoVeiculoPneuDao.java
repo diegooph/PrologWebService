7d719a3483a4099db6ed45531f4aa6046ac7b6e7
thaisksf <thaisksf@gmail.com>
2020-08-31 17:09:07 -0300

    feat(pneu): cria ws vinculo entre veiculos e pneus
    PL-2771

---
+++ src/main/java/br/com/zalf/prolog/webservice/interno/implantacao/conferencia/frota/vinculoveiculopneu/VinculoVeiculoPneuDao.java
@@ -0,0 +1,26 @@
+package br.com.zalf.prolog.webservice.interno.implantacao.conferencia.frota.vinculoveiculopneu;
+
+import br.com.zalf.prolog.webservice.interno.implantacao.conferencia._model.TipoImport;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created on 31/08/2020.
+ *
+ * @author Thais Francisco (https://github.com/thaisksf)
+ */
+public interface VinculoVeiculoPneuDao {
+    /**
+     * Método para retornar a conferência da planilha de import de veículos.
+     *
+     * @param codEmpresa        Código da emrpesa para qual as informações serão conferidas.
+     * @param codUnidade        Código da unidade para a qual as informações serão conferidas.
+     * @param jsonPlanilha      Informações da planilha de vínculo entre veículos e pneus em formato Json.
+     * @param tipoImportVinculo Informação do tipo de import.
+     * @throws Throwable Se algum erro ocorrer.
+     */
+    void importPlanilhaVinculoVeiculoPneu(@NotNull final Long codEmpresa,
+                                          @NotNull final Long codUnidade,
+                                          @NotNull final String usuario,
+                                          @NotNull final String jsonPlanilha,
+                                          @NotNull final TipoImport tipoImportVinculo) throws Throwable;
+}
---
+++ src/main/java/br/com/zalf/prolog/webservice/interno/implantacao/conferencia/frota/vinculoveiculopneu/_model/VinculoVeiculoPneu.java
@@ -0,0 +1,23 @@
+package br.com.zalf.prolog.webservice.interno.implantacao.conferencia.frota.vinculoveiculopneu._model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Created on 31/08/2020.
+ *
+ * @author Thais Francisco (https://github.com/thaisksf)
+ */
+@Getter
+@Setter
+public class VinculoVeiculoPneu {
+
+        @Nullable
+        private String placa;
+        @Nullable
+        private String numeroFogo;
+        @Nullable
+        private String nomenclatura;
+}
+
