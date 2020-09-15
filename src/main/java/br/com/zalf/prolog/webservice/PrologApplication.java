99928dab1aab43263cef1d6a7499c844488ca3de
didi <dioge.vanzella@gmail.com>
2020-09-15 08:13:15 -0300

    refactor(spring): centraliza configurações
    PL-2936

--- src/main/java/br/com/zalf/prolog/webservice/PrologApplication.java
+++ src/main/java/br/com/zalf/prolog/webservice/PrologApplication.java
@@ -1,10 +1,13 @@
 package br.com.zalf.prolog.webservice;
 
 import br.com.zalf.prolog.webservice.database.DataSourceLifecycleManager;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import javax.servlet.ServletContextListener;
 
@@ -25,5 +28,14 @@ public class PrologApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PrologApplication.class, args);
+    }
+
+    @Configuration
+    public static class PrologConfig extends ResourceConfig {
+        public PrologConfig() {
+            packages("jersey.config.server.provider.packages",
+                    "br.com.zalf.prolog.webservice");
+            register(MultiPartFeature.class);
+        }
     }
 }
