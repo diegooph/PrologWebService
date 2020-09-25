c87976ae1adb5657585f7358bcddaa1e9ad77456
didi <dioge.vanzella@gmail.com>
2020-09-25 10:07:12 -0300

    refactor(): adiciona inicialização do jersey no Spring
    PL-3159

--- src/main/java/br/com/zalf/prolog/webservice/config/PrologApplication.java
+++ src/main/java/br/com/zalf/prolog/webservice/config/PrologApplication.java
@@ -5,9 +5,12 @@ import br.com.zalf.prolog.webservice.messaging.push.FirebaseLifecycleManager;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.ServerProperties;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,17 +22,35 @@ import javax.servlet.ServletContextListener;
  * @author Diogenes Vanzela (https://github.com/diogenesvanzella)
  */
 @SpringBootApplication
-public class PrologApplication {
+public class PrologApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PrologApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(@NotNull final SpringApplicationBuilder builder) {
+        builder.sources(PrologApplication.class);
+        return builder;
+    }
+
     @Bean
-    public ServletListenerRegistrationBean<ServletContextListener> listenerRegistrationBean() {
+    public ServletListenerRegistrationBean<ServletContextListener> dataSourceLifecycleManagerRegistrationBean() {
         final ServletListenerRegistrationBean<ServletContextListener> bean = new ServletListenerRegistrationBean<>();
         bean.setListener(new DataSourceLifecycleManager());
+        return bean;
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean<ServletContextListener> firebaseLifecycleManagerRegistrationBean() {
+        final ServletListenerRegistrationBean<ServletContextListener> bean = new ServletListenerRegistrationBean<>();
         bean.setListener(new FirebaseLifecycleManager());
+        return bean;
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean<ServletContextListener> prologConsoleTextMakerRegistrationBean() {
+        final ServletListenerRegistrationBean<ServletContextListener> bean = new ServletListenerRegistrationBean<>();
         bean.setListener(new ProLogConsoleTextMaker());
         return bean;
     }
