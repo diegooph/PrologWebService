80e6414f8d3b6dea7d007b84250967db6b421150
gustavocnp95 <c98jjkdhgy>
2020-10-08 10:53:08 -0300

    feat(afericao): cria converter
    PL-3182

---
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/afericao/relatorios/AfericaoRelatorioConverter.java
@@ -0,0 +1,49 @@
+package br.com.zalf.prolog.webservice.frota.pneu.afericao.relatorios;
+
+import br.com.zalf.prolog.webservice.frota.pneu.afericao.relatorios._model.AfericaoExportacaoProtheus;
+import br.com.zalf.prolog.webservice.frota.pneu.afericao.relatorios._model.AfericaoExportacaoProtheusInfosPneu;
+import br.com.zalf.prolog.webservice.frota.pneu.afericao.relatorios._model.AfericaoExportacaoProtheusInfosVeiculo;
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.ResultSet;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+/**
+ * Created on 2020-10-08
+ *
+ * @author Gustavo Navarro (https://github.com/gustavocnp95)
+ */
+public final class AfericaoRelatorioConverter {
+    public static AfericaoExportacaoProtheus createAfericaoExportacaoProtheus(
+            @NotNull final ResultSet rSet) throws Throwable {
+        return new AfericaoExportacaoProtheus(
+                createAfericaoExportacaoProtheusInfosVeiculo(rSet),
+                createAfericaoExportacaoProtheusInfosPneu(rSet)
+        );
+    }
+
+    private static AfericaoExportacaoProtheusInfosVeiculo createAfericaoExportacaoProtheusInfosVeiculo(
+            @NotNull final ResultSet rSet) throws Throwable {
+        return new AfericaoExportacaoProtheusInfosVeiculo(
+                rSet.getString("cabecalho_linha_um"),
+                rSet.getString("placa"),
+                rSet.getObject("data", LocalDate.class),
+                rSet.getObject("hora", LocalTime.class)
+        );
+    }
+
+    private static AfericaoExportacaoProtheusInfosPneu createAfericaoExportacaoProtheusInfosPneu(
+            @NotNull final ResultSet rSet) throws Throwable {
+        return new AfericaoExportacaoProtheusInfosPneu(
+                rSet.getString("cabecalho_linha_dois"),
+                rSet.getString("cod_cliente_pneu"),
+                rSet.getString("nomenclatura_posicao"),
+                rSet.getDouble("calibragem_aferida"),
+                rSet.getDouble("calibragem_realizada"),
+                rSet.getDouble("sulco_interno"),
+                rSet.getDouble("sulco_central_interno"),
+                rSet.getDouble("sulco_externo")
+        );
+    }
+}
