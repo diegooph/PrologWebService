ecaa80f3ecadc5e6853230028c182dc3c0477ee4
gustavocnp95 <c98jjkdhgy>
2020-10-08 14:04:04 -0300

    feat(afericao): modifica estrutura dos models
    PL-3182

--- src/main/java/br/com/zalf/prolog/webservice/frota/pneu/afericao/relatorios/_model/AfericaoExportacaoProtheusInfos.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/afericao/relatorios/_model/AfericaoExportacaoProtheusInfos.java
@@ -3,17 +3,15 @@ package br.com.zalf.prolog.webservice.frota.pneu.afericao.relatorios._model;
 import lombok.Data;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
-
 /**
  * Created on 2020-10-08
  *
  * @author Gustavo Navarro (https://github.com/gustavocnp95)
  */
 @Data
-public final class AfericaoExportacaoProtheus {
+public final class AfericaoExportacaoProtheusInfos {
     @NotNull
-    private final Long codAfericao;
+    private final AfericaoExportacaoProtheusInfosVeiculo infosVeiculo;
     @NotNull
-    private final List<AfericaoExportacaoProtheusInfos> infosAfericao;
+    private final AfericaoExportacaoProtheusInfosPneu infosPneu;
 }
