6d928238cf6126f50ce44785c670e614c0e91b73
gustavocnp95 <c98jjkdhgy>
2020-10-08 10:05:39 -0300

    feat(afericao): cria assinatura de metodo para buscar afericoes do protheus
    PL-3182

--- src/main/java/br/com/zalf/prolog/webservice/frota/pneu/afericao/relatorios/AfericaoRelatorioDao.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/afericao/relatorios/AfericaoRelatorioDao.java
@@ -1,7 +1,9 @@
 package br.com.zalf.prolog.webservice.frota.pneu.afericao.relatorios;
 
 import br.com.zalf.prolog.webservice.commons.report.Report;
+import br.com.zalf.prolog.webservice.frota.pneu.afericao._model.AfericaoExportacaoProtheus;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.OutputStream;
 import java.time.LocalDate;
@@ -13,7 +15,6 @@ import java.util.List;
  * @author Thais Francisco (https://github.com/thaisksf)
  */
 public interface AfericaoRelatorioDao {
-
     /**
      * Método para buscar o relatório de cronograma das aferições de placas em CSV.
      *
@@ -63,4 +64,22 @@ public interface AfericaoRelatorioDao {
     Report getDadosGeraisAfericoesReport(@NotNull final List<Long> codUnidades,
                                          @NotNull final LocalDate dataInicial,
                                          @NotNull final LocalDate dataFinal) throws Throwable;
+
+    /**
+     * Método utilizado para buscar um objeto no padrão do Protheus para possibilitar a importação das aferições dentro
+     * do sistema Protheus. Nesta busca, permitimos o filtro de data. Contudo, se qualquer uma das datas forem nulas,
+     * o filtro de data será desconsiderado e buscaremos as aferições para todas as datas.
+     *
+     * @param codUnidades uma lista de unidades a serem usadas no filtro da busca.
+     * @param codVeiculos uma lista de veículos a serem usados no filtro da busca.
+     * @param dataInicial a data inicial a ser usada no filtro, podendo ser nula.
+     * @param dataFinal   a data final a ser usada no filtro, podendo ser nula.
+     * @return um objeto no padrão do Protheus com as informações das aferições.
+     * @throws Throwable se qualquer erro ocorrer.
+     */
+    @NotNull
+    AfericaoExportacaoProtheus getExportacaoAfericoesProtheus(@NotNull final List<Long> codUnidades,
+                                                              @NotNull final List<Long> codVeiculos,
+                                                              @Nullable final LocalDate dataInicial,
+                                                              @Nullable final LocalDate dataFinal) throws Throwable;
 }
