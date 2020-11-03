d9e003a26aaa1cc5f775a73829f346888dd1dd43
thaisksf <thaisfrancisco@prologapp.com>
2020-11-03 14:36:37 -0300

    feat(veiculo): cria branch intermediário
    PL-3124

---
+++ src/main/java/br/com/zalf/prolog/webservice/frota/veiculo/acoplamento/VeiculoAcoplamentoDao.java
@@ -0,0 +1,9 @@
+package br.com.zalf.prolog.webservice.frota.veiculo.acoplamento;
+
+/**
+ * Created on 2020-11-03
+ *
+ * @author Thais Francisco (https://github.com/thaisksf)
+ */
+public interface VeiculoAcoplamentoDao {
+}
