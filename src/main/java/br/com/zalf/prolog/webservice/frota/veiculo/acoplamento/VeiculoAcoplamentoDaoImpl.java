56e8014bee1e71863ad25920d5f96a21d854c0d2
Luiz Felipe <luiz.felipe_95@hotmail.com>
2020-11-10 15:24:58 -0300

    feat(core): cria estrutura inicial do método de inserção de processo de acoplamento
    PL-3210

--- src/main/java/br/com/zalf/prolog/webservice/frota/veiculo/acoplamento/VeiculoAcoplamentoDaoImpl.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/veiculo/acoplamento/VeiculoAcoplamentoDaoImpl.java
@@ -1,9 +1,133 @@
 package br.com.zalf.prolog.webservice.frota.veiculo.acoplamento;
 
+import br.com.zalf.prolog.webservice.commons.util.StatementUtils;
+import br.com.zalf.prolog.webservice.commons.util.StringUtils;
+import br.com.zalf.prolog.webservice.database.DatabaseConnection;
+import br.com.zalf.prolog.webservice.database.DatabaseUtils;
+import br.com.zalf.prolog.webservice.errorhandling.Exceptions;
+import br.com.zalf.prolog.webservice.frota.veiculo.acoplamento._model.realizacao.VeiculoAcoplamento;
+import com.google.common.collect.Lists;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.OffsetDateTime;
+import java.util.List;
+
 /**
  * Created on 2020-11-03
  *
  * @author Thais Francisco (https://github.com/thaisksf)
  */
 public final class VeiculoAcoplamentoDaoImpl {
+    private static final int EXECUTE_BATCH_SUCCESS = 0;
+
+    public void removeAcoplamentoAtual(@NotNull final Connection conn,
+                                       @NotNull final Long codProcessoAcoplamento) {
+        PreparedStatement stmt = null;
+        try {
+            stmt = conn.prepareStatement("delete from veiculo_acoplamento_atual where cod_processo = ?;");
+            DatabaseUtils.bind(stmt, Lists.newArrayList(codProcessoAcoplamento));
+            stmt.execute();
+        } catch (final SQLException e) {
+            throw Exceptions.rethrow(e);
+        } finally {
+            DatabaseConnection.close(stmt);
+        }
+    }
+
+    @NotNull
+    public Long insertProcessoAcoplamento(@NotNull final Connection conn,
+                                          @NotNull final Long codUnidadeAcoplamento,
+                                          @NotNull final Long codColaboradorRealizacao,
+                                          @NotNull final OffsetDateTime dataHoraAtual,
+                                          @Nullable final String observacao) {
+        PreparedStatement stmt = null;
+        ResultSet rSet = null;
+        try {
+            stmt = conn.prepareStatement("insert into public.veiculo_acoplamento_processo " +
+                    "(cod_unidade, cod_colaborador, data_hora, observacao) " +
+                    "values (?, ?, ?, ?) returning codigo as codigo;");
+            DatabaseUtils.bind(stmt, Lists.newArrayList(
+                    codUnidadeAcoplamento,
+                    codColaboradorRealizacao,
+                    dataHoraAtual,
+                    StringUtils.trimToNull(observacao)));
+            rSet = stmt.executeQuery();
+            if (rSet.next() && rSet.getLong("codigo") > 0) {
+                return rSet.getLong("codigo");
+            } else {
+                throw new IllegalStateException("Erro ao inserir processo de acoplamento");
+            }
+        } catch (final SQLException e) {
+            throw Exceptions.rethrow(e);
+        } finally {
+            DatabaseConnection.close(stmt, rSet);
+        }
+    }
+
+    public void insertHistoricoAcoplamentos(@NotNull final Connection conn,
+                                            @NotNull final Long codProcessoAcoplamento,
+                                            @NotNull final List<VeiculoAcoplamento> acoplamentos) {
+        PreparedStatement stmt = null;
+        try {
+            stmt = conn.prepareStatement("insert into public.veiculo_acoplamento_historico " +
+                    "(cod_processo, cod_posicao, cod_diagrama, motorizado, cod_veiculo, km, acao) " +
+                    "values (?, ?, ?, ?, ?, ?, ?);");
+            for (final VeiculoAcoplamento acoplamento : acoplamentos) {
+                DatabaseUtils.bind(stmt, Lists.newArrayList(
+                        codProcessoAcoplamento,
+                        acoplamento.getPosicaoAcaoRealizada(),
+                        acoplamento.getCodDiagramaVeiculo(),
+                        acoplamento.getMotorizado(),
+                        acoplamento.getCodVeiculo(),
+                        acoplamento.getKmColetado(),
+                        acoplamento.getAcaoRealizada()));
+                stmt.addBatch();
+            }
+            StatementUtils.executeBatchAndValidate(
+                    stmt,
+                    acoplamentos.size(),
+                    EXECUTE_BATCH_SUCCESS,
+                    "Erro ao inserir histórico de acoplamentos");
+        } catch (final SQLException e) {
+            throw Exceptions.rethrow(e);
+        } finally {
+            DatabaseConnection.close(stmt);
+        }
+    }
+
+    public void insertEstadoAtualAcoplamentos(@NotNull final Connection conn,
+                                              @NotNull final Long codProcessoAcoplamento,
+                                              @NotNull final Long codUnidadeAcoplamento,
+                                              @NotNull final List<VeiculoAcoplamento> acoplamentos) {
+        PreparedStatement stmt = null;
+        try {
+            stmt = conn.prepareStatement("insert into public.veiculo_acoplamento_atual " +
+                    "(cod_processo, cod_unidade, cod_posicao, cod_diagrama, motorizado, cod_veiculo) " +
+                    "values (?, ?, ?, ?, ?, ?);");
+            for (final VeiculoAcoplamento acoplamento : acoplamentos) {
+                DatabaseUtils.bind(stmt, Lists.newArrayList(
+                        codProcessoAcoplamento,
+                        codUnidadeAcoplamento,
+                        acoplamento.getPosicaoAcaoRealizada(),
+                        acoplamento.getCodDiagramaVeiculo(),
+                        acoplamento.getMotorizado(),
+                        acoplamento.getCodVeiculo()));
+                stmt.addBatch();
+            }
+            StatementUtils.executeBatchAndValidate(
+                    stmt,
+                    acoplamentos.size(),
+                    EXECUTE_BATCH_SUCCESS,
+                    "Erro ao inserir histórico de acoplamentos");
+        } catch (final SQLException e) {
+            throw Exceptions.rethrow(e);
+        } finally {
+            DatabaseConnection.close(stmt);
+        }
+    }
 }
--- src/main/java/br/com/zalf/prolog/webservice/frota/veiculo/acoplamento/VeiculoAcoplamentoService.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/veiculo/acoplamento/VeiculoAcoplamentoService.java
@@ -1,9 +1,78 @@
 package br.com.zalf.prolog.webservice.frota.veiculo.acoplamento;
 
+import br.com.zalf.prolog.webservice.Injection;
+import br.com.zalf.prolog.webservice.commons.util.Log;
+import br.com.zalf.prolog.webservice.commons.util.date.Now;
+import br.com.zalf.prolog.webservice.database.DatabaseConnection;
+import br.com.zalf.prolog.webservice.database.DatabaseUtils;
+import br.com.zalf.prolog.webservice.frota.veiculo.VeiculoService;
+import br.com.zalf.prolog.webservice.frota.veiculo.acoplamento._model.realizacao.VeiculoAcoplamento;
+import br.com.zalf.prolog.webservice.frota.veiculo.acoplamento._model.realizacao.VeiculoAcoplamentoProcessoRealizacao;
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.Connection;
+
 /**
  * Created on 2020-11-03
  *
  * @author Thais Francisco (https://github.com/thaisksf)
  */
 public final class VeiculoAcoplamentoService {
+    private static final String TAG = VeiculoAcoplamentoService.class.getSimpleName();
+
+    public void realizaProcessoAcoplamento(@NotNull final Long codColaborador,
+                                           @NotNull final VeiculoAcoplamentoProcessoRealizacao processoRealizacao) {
+        final VeiculoAcoplamentoDaoImpl dao = new VeiculoAcoplamentoDaoImpl();
+        // 0 - Validações?
+
+        final Connection connection = DatabaseConnection.getConnection();
+        try {
+            connection.setAutoCommit(false);
+
+            // 1 - Atualiza KMs - Deve acontecer antes de remover o acoplamento atual, pois se baseará nele para a
+            // propagação dos KMs.
+            final VeiculoService service = new VeiculoService();
+            processoRealizacao
+                    .getAcoplamentos()
+                    .stream()
+                    .filter(VeiculoAcoplamento::coletouKm)
+                    .forEach(veiculoAcoplamento -> {
+                        // TODO: Utilizar VeiculoService para atualizar os KMs.
+                    });
+
+            // 2 - Remove os veículos do processo editado atual.
+            processoRealizacao
+                    .estaEditandoProcessoAcoplamento()
+                    .ifPresent(codProcessoEditado -> dao.removeAcoplamentoAtual(connection, codProcessoEditado));
+
+            // 3 - Inserir processo acoplamento.
+            final Long codProcessoInserido = dao.insertProcessoAcoplamento(
+                    connection,
+                    processoRealizacao.getCodUnidade(),
+                    codColaborador,
+                    Now.offsetDateTimeUtc(),
+                    processoRealizacao.getObservacao());
+
+            // 4 - Inserir histórico acoplamentos.
+            dao.insertHistoricoAcoplamentos(connection, codProcessoInserido, processoRealizacao.getAcoplamentos());
+
+            // 5 - Inserir processo atual.
+            dao.insertEstadoAtualAcoplamentos(
+                    connection,
+                    processoRealizacao.getCodUnidade(),
+                    codProcessoInserido,
+                    processoRealizacao.getAcoplamentos());
+
+            // 6 - Commita e seja feliz.
+            connection.commit();
+        } catch (final Throwable throwable) {
+            Log.e(TAG, "Erro ao realizar processo de acoplamento", throwable);
+            throw Injection
+                    .provideProLogExceptionHandler()
+                    .map(throwable, "Erro ao realizar engate/desengate, tente novamente.");
+        } finally {
+            DatabaseUtils.safeRollback(connection);
+            DatabaseConnection.close(connection);
+        }
+    }
 }
