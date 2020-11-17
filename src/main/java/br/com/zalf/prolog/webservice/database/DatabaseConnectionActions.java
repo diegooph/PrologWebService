e5bdbbf0d19930d0770c7914c724769ccf951221
Steinert999 <steinert999@outlook.com>
2020-11-17 16:35:51 -0300

    docs(core): adiciona comentários na interface
    PL-3265

--- src/main/java/br/com/zalf/prolog/webservice/database/DatabaseConnectionActions.java
+++ src/main/java/br/com/zalf/prolog/webservice/database/DatabaseConnectionActions.java
@@ -12,9 +12,47 @@ import java.sql.ResultSet;
 public interface DatabaseConnectionActions {
 
     Connection getConnection();
+
+    /**
+     * Método responsável por fechar a conexão com o banco com uma lista de AutoCloseable
+     *
+     * @see AutoCloseable
+     * @since 17 de nov de 2020
+     */
     void close(AutoCloseable... closeables);
+
+    /**
+     * Método responsável por fechar a conexão com o banco com AutoCloseable
+     *
+     * @see AutoCloseable
+     * @since 17 de nov de 2020
+     */
     void close(AutoCloseable closeable);
+
+    /**
+     * Método responsável por fechar a conexão com o banco.
+     *
+     * @see Connection
+     * @since 17 de nov de 2020
+     */
     void close(Connection connection);
+
+    /**
+     * Método responsável por fechar a conexão com o banco e o ResultSet.
+     *
+     * @see Connection
+     * @see ResultSet
+     * @since 17 de nov de 2020
+     */
     void close(Connection connection, ResultSet rs);
+
+    /**
+     * Método responsável por fechar a conexão com o banco, ResultSet e PreparedStatement
+     *
+     * @see Connection
+     * @see ResultSet
+     * @see PreparedStatement
+     * @since 17 de nov de 2020
+     */
     void close(Connection connection, ResultSet rs, PreparedStatement stmt);
 }
