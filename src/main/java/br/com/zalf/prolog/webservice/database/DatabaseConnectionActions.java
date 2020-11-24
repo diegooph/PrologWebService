9a0bb10bde1a5e5e6a7dfed783d663e5d7ca2c50
Steinert999 <steinert999@outlook.com>
2020-11-24 09:21:39 -0300

    docs(core): melhora comentários
    PL-3265

--- src/main/java/br/com/zalf/prolog/webservice/database/DatabaseConnectionActions.java
+++ src/main/java/br/com/zalf/prolog/webservice/database/DatabaseConnectionActions.java
@@ -14,16 +14,18 @@ public interface DatabaseConnectionActions {
     Connection getConnection();
 
     /**
-     * Método responsável por fechar a conexão com o banco com uma lista de AutoCloseable
+     * Método responsável por fechar a conexão com o banco com uma lista de AutoCloseable.
      *
+     * @param closeables conjunto de objetos para fechar a conexão.
      * @see AutoCloseable
      * @since 17 de nov de 2020
      */
     void close(AutoCloseable... closeables);
 
     /**
-     * Método responsável por fechar a conexão com o banco com AutoCloseable
+     * Método responsável por fechar a conexão com o banco com AutoCloseable.
      *
+     * @param closeable objeto para fechar a conexão.
      * @see AutoCloseable
      * @since 17 de nov de 2020
      */
@@ -32,6 +34,7 @@ public interface DatabaseConnectionActions {
     /**
      * Método responsável por fechar a conexão com o banco.
      *
+     * @param connection objeto para conexão com banco de dados.
      * @see Connection
      * @since 17 de nov de 2020
      */
@@ -40,6 +43,8 @@ public interface DatabaseConnectionActions {
     /**
      * Método responsável por fechar a conexão com o banco e o ResultSet.
      *
+     * @param connection objeto para conexão com banco de dados.
+     * @param rs         objeto para adquirir o resultado da query.
      * @see Connection
      * @see ResultSet
      * @since 17 de nov de 2020
@@ -49,6 +54,9 @@ public interface DatabaseConnectionActions {
     /**
      * Método responsável por fechar a conexão com o banco, ResultSet e PreparedStatement
      *
+     * @param connection objeto para conexão com banco de dados.
+     * @param rs objeto para adquirir o resultado da query.
+     * @param stmt objeto para gerenciamento e criação da query.
      * @see Connection
      * @see ResultSet
      * @see PreparedStatement
