371e2af7aa1023bfdbf57f326628daffd433d115
Steinert999 <steinert999@outlook.com>
2020-11-25 15:50:02 -0300

    fix(core): adiciona anotações para configuração das propriedades do Datasource
    PL-3266

--- src/main/java/br/com/zalf/prolog/webservice/database/SpringDatabaseManager.java
+++ src/main/java/br/com/zalf/prolog/webservice/database/SpringDatabaseManager.java
@@ -2,9 +2,11 @@ package br.com.zalf.prolog.webservice.database;
 
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 
@@ -27,6 +29,8 @@ public class SpringDatabaseManager {
     private String driver;
 
     @Bean
+    @ConfigurationProperties("spring.datasource.hikari")
+    @Primary
     public DataSource getDataSource() {
         return DataSourceBuilder.create()
                 .type(HikariDataSource.class)
