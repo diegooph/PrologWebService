286a11e57a20bed4bd646f12348494b514f93229
Steinert999 <steinert999@outlook.com>
2020-11-26 16:33:52 -0300

    feat(core): adiciona entidade Unidade
    PL-3309

---
+++ src/main/java/br/com/zalf/prolog/webservice/geral/unidade/_model/UnidadeEntity.java
@@ -0,0 +1,72 @@
+package br.com.zalf.prolog.webservice.geral.unidade._model;
+
+import br.com.zalf.prolog.webservice.commons.util.date.Now;
+import br.com.zalf.prolog.webservice.gente.empresa._model.EmpresaEntity;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * Created on 2020-11-24
+ *
+ * @author Guilherme Steinert (https://github.com/steinert999)
+ */
+@Entity
+@Table(name = "unidade",
+       indexes = {@Index(name = "idx_unidade_cod_empresa", columnList = "codigo")})
+@Data
+public class UnidadeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo",
+            nullable = false)
+    private Long codigo;
+
+    @Column(name = "nome",
+            length = 40,
+            nullable = false)
+    private String nome;
+
+    @Column(name = "total_colaboradores")
+    private Integer totalColaboradores;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cod_regional",
+                foreignKey = @ForeignKey(name = "fk_unidade_regional",
+                                         value = ConstraintMode.CONSTRAINT),
+                referencedColumnName = "codigo",
+                nullable = false)
+    private RegionalEntity regional;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cod_empresa",
+                foreignKey = @ForeignKey(name = "fk_unidade_empresa",
+                                         value = ConstraintMode.CONSTRAINT),
+                referencedColumnName = "codigo",
+                nullable = false)
+    private EmpresaEntity empresa;
+
+    @Column(name = "timezone",
+            nullable = false)
+    private String timezone;
+
+    @Column(name = "data_hora_cadastro",
+            nullable = false)
+    private LocalDateTime dataHoraCadastro = Now.localDateTimeUtc();
+
+    @Column(name = "status_ativo",
+            nullable = false)
+    private boolean ativo = true;
+
+    @Column(name = "cod_auxiliar")
+    private String codAuxiliar;
+
+    @Column(name = "latitude_unidade")
+    private String latitudeUnidade;
+
+    @Column(name = "longitude_unidade")
+    private String longitudeUnidade;
+
+}
