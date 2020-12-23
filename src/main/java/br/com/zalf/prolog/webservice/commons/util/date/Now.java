66391c5ffc02ec8b6067accb1d68836e93eb1f28
gustavocnp95 <c98jjkdhgy>
2020-12-23 16:36:29 -0300

    refact(core): refatora classe now

--- src/main/java/br/com/zalf/prolog/webservice/commons/util/date/Now.java
+++ src/main/java/br/com/zalf/prolog/webservice/commons/util/date/Now.java
@@ -11,37 +11,36 @@ import java.time.*;
  * @author Luiz Felipe (https://github.com/luizfp)
  */
 public final class Now {
-
     private Now() {
         throw new IllegalStateException(Now.class.getSimpleName() + " cannot be instantiated!");
     }
 
-    public static long utcMillis() {
+    public static long getUtcMillis() {
         return System.currentTimeMillis();
     }
 
     @NotNull
-    public static Timestamp timestampUtc() {
-        return new Timestamp(utcMillis());
+    public static Timestamp getTimestampUtc() {
+        return new Timestamp(getUtcMillis());
     }
 
     @NotNull
-    public static LocalDate localDateUtc() {
+    public static LocalDate getLocalDateUtc() {
         return LocalDate.now(Clock.systemUTC());
     }
 
     @NotNull
-    public static LocalDateTime localDateTimeUtc() {
+    public static LocalDateTime getLocalDateTimeUtc() {
         return LocalDateTime.now(Clock.systemUTC());
     }
 
     @NotNull
-    public static OffsetDateTime offsetDateTimeUtc() {
+    public static OffsetDateTime getOffsetDateTimeUtc() {
         return OffsetDateTime.now(Clock.systemUTC());
     }
 
     @NotNull
-    public static ZonedDateTime zonedDateTimeTzAware(@NotNull final ZoneId zoneId) {
+    public static ZonedDateTime getZonedDateTimeTzAware(@NotNull final ZoneId zoneId) {
         return ZonedDateTime.now(zoneId);
     }
 }
