1e4d6c6ea934a77af7297ccf58eb7f3beaec1645
gustavocnp95 <c98jjkdhgy>
2020-12-23 16:17:46 -0300

    refact(core): refatora date utils

--- src/main/java/br/com/zalf/prolog/webservice/commons/util/date/DateUtils.java
+++ src/main/java/br/com/zalf/prolog/webservice/commons/util/date/DateUtils.java
@@ -1,6 +1,5 @@
 package br.com.zalf.prolog.webservice.commons.util.date;
 
-import com.google.common.base.Preconditions;
 import org.jetbrains.annotations.NotNull;
 
 import java.sql.Timestamp;
@@ -16,140 +15,89 @@ import java.util.concurrent.TimeUnit;
 
 public class DateUtils {
 
-    /**
-     * Converte uma data no formato java.util.Date para java.sql.Timestamp
-     *
-     * @param utilDate
-     * @return java.sql.Timestamp
-     */
-    public static Timestamp toTimestamp(Date utilDate) {
-        return new Timestamp(utilDate.getTime());
+    private DateUtils() {
+        throw new UnsupportedOperationException("An util class cannot be instatied!");
     }
 
-    /**
-     * Converte uma data no formato java.util.Date para java.sql.Date
-     *
-     * @param utilDate
-     * @return java.sql.Date
-     */
-    public static java.sql.Date toSqlDate(Date utilDate) {
-        return new java.sql.Date(utilDate.getTime());
+    @NotNull
+    public static Timestamp toTimestamp(@NotNull final Date dateToConvert) {
+        return new Timestamp(dateToConvert.getTime());
     }
 
-    /**
-     * Converte um java.time.LocalDate para java.sql.Date
-     *
-     * @param localDate
-     * @return java.sql.Date
-     */
-    public static java.sql.Date toSqlDate(LocalDate localDate) {
-        return java.sql.Date.valueOf(localDate);
+    @NotNull
+    public static java.sql.Date toSqlDate(@NotNull final Date dateToConvert) {
+        return new java.sql.Date(dateToConvert.getTime());
     }
 
-    /**
-     * Converte um java.sql.Date para java.time.LocalDate
-     *
-     * @param sqlDate
-     * @return java.time.LocalDate
-     */
-    public static LocalDate toLocalDate(java.sql.Date sqlDate) {
-        return sqlDate.toLocalDate();
+    @NotNull
+    public static java.sql.Date toSqlDate(@NotNull final LocalDate localDateToConvert) {
+        return java.sql.Date.valueOf(localDateToConvert);
     }
 
-    /**
-     * Converte um java.util.date para LocalDateTime
-     *
-     * @param date
-     * @return
-     */
-    public static LocalDateTime toLocalDateTime(Date date) {
-        Instant instant = Instant.ofEpochMilli(date.getTime());
-        LocalDateTime res = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
-        return res;
+    @NotNull
+    public static LocalDate toLocalDate(@NotNull final java.sql.Date sqlDateToConvert) {
+        return sqlDateToConvert.toLocalDate();
     }
 
-    /**
-     * Converte um java.util.Date para java.time.LocalDate
-     *
-     * @param utilDate
-     * @return
-     */
-    public static LocalDate toLocalDate(Date utilDate) {
-        return toSqlDate(utilDate).toLocalDate();
+    @NotNull
+    public static LocalDate toLocalDate(@NotNull final Date dateToConvert) {
+        return toSqlDate(dateToConvert).toLocalDate();
     }
 
-    public static LocalDate toLocalDate(final long dateMillis, @NotNull final ZoneId zoneId) {
+    @NotNull
+    public static LocalDate toLocalDate(final long dateInMillisToConvert, @NotNull final ZoneId zoneId) {
         return Instant
-                .ofEpochMilli(dateMillis)
+                .ofEpochMilli(dateInMillisToConvert)
                 .atZone(zoneId)
                 .toLocalDate();
     }
 
     @NotNull
-    public static LocalDate validateAndParse(@NotNull final String date, @NotNull final DateTimeFormatter formatter) {
-        Preconditions.checkNotNull(date);
-        return LocalDate.parse(date, formatter);
+    public static LocalDateTime toLocalDateTime(@NotNull final Date dateToConvert) {
+        final Instant instant = Instant.ofEpochMilli(dateToConvert.getTime());
+        return LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
     }
 
-    /**
-     * Retorna um date com o primerio dia do mes inserido
-     *
-     * @param date
-     * @return
-     */
-    public static Date getPrimeiroDiaMes(Date date) {
-
-        Calendar first = Calendar.getInstance();
-        first.setTime(DateUtils.toSqlDate(date));
-        first.set(Calendar.DAY_OF_MONTH, 1);
-        return new java.sql.Date(first.getTimeInMillis());
+    @NotNull
+    public static LocalDate validateAndParse(@NotNull final String dateToParse,
+                                             @NotNull final DateTimeFormatter formatter) {
+        return LocalDate.parse(dateToParse, formatter);
     }
 
-    /**
-     * Retorna um date com o ultimo dia do mes inserido
-     *
-     * @param date
-     * @return
-     */
-    public static Date getUltimoDiaMes(Date date) {
-
-        Calendar last = Calendar.getInstance();
-        last.setTime(DateUtils.toSqlDate(date));
-        last.set(Calendar.DAY_OF_MONTH, 1);
-        last.add(Calendar.MONTH, 1);
-        last.add(Calendar.DAY_OF_MONTH, -1);
-
-        return new java.sql.Date(last.getTimeInMillis());
+    @NotNull
+    public static Date getPrimeiroDiaMes(@NotNull final Date date) {
+        final Calendar firstDay = Calendar.getInstance();
+        firstDay.setTime(DateUtils.toSqlDate(date));
+        firstDay.set(Calendar.DAY_OF_MONTH, 1);
+        return new java.sql.Date(firstDay.getTimeInMillis());
     }
 
-    public static long secondsBetween(final long startDateMillis, final long endDateMillis) {
-        return TimeUnit.MILLISECONDS.toSeconds(endDateMillis - startDateMillis);
+    @NotNull
+    public static Date getUltimoDiaMes(@NotNull final Date date) {
+        final Calendar lastDay = Calendar.getInstance();
+        lastDay.setTime(DateUtils.toSqlDate(date));
+        lastDay.set(Calendar.DAY_OF_MONTH, 1);
+        lastDay.add(Calendar.MONTH, 1);
+        lastDay.add(Calendar.DAY_OF_MONTH, -1);
+        return new java.sql.Date(lastDay.getTimeInMillis());
     }
 
-    /**
-     * Método que verifica se um ano está dentro do limite estabelecido.
-     *
-     * @param data               - data a ser analisada.
-     * @param anoMaximoPermitido - ano máximo permitido.
-     * @param anoMinimoPermitido - ano mínimo permitido.
-     * @return true caso o ano esteja fora do limite estabelecido.
-     */
-    public static boolean verificaAno(Date data, int anoMaximoPermitido, int anoMinimoPermitido) {
-        SimpleDateFormat ano = new SimpleDateFormat("yyyy");
-        final int anoData = Integer.parseInt(ano.format(data));
-        return anoData >= anoMaximoPermitido || anoData <= anoMinimoPermitido;
+    public static long secondsBetween(final long startDateInMillis, final long endDateInMillis) {
+        return TimeUnit.MILLISECONDS.toSeconds(endDateInMillis - startDateInMillis);
     }
 
-    /**
-     * Método que verifica se um ano está dentro do limite estabelecido.
-     *
-     * @param data               - data a ser analisada.
-     * @param anoMaximoPermitido - ano máximo permitido.
-     * @param anoMinimoPermitido - ano mínimo permitido.
-     * @return true caso o ano esteja fora do limite estabelecido.
-     */
-    public static boolean verificaAno(LocalDate data, int anoMaximoPermitido, int anoMinimoPermitido) {
-        final int anoData = data.getYear();
-        return anoData >= anoMaximoPermitido || anoData <= anoMinimoPermitido;
+    public static boolean verificaAno(@NotNull final Date dataASerVerificada,
+                                      final int anoMaximoPermitido,
+                                      final int anoMinimoPermitido) {
+        final SimpleDateFormat anoFormat = new SimpleDateFormat("yyyy");
+        final int anoExtraido = Integer.parseInt(anoFormat.format(dataASerVerificada));
+        return anoExtraido >= anoMaximoPermitido || anoExtraido <= anoMinimoPermitido;
+    }
+
+    public static boolean verificaAno(final LocalDate dataASerVerificada,
+                                      final int anoMaximoPermitido,
+                                      final int anoMinimoPermitido) {
+        final int anoExtraido = dataASerVerificada.getYear();
+        return anoExtraido >= anoMaximoPermitido || anoExtraido <= anoMinimoPermitido;
     }
 }
