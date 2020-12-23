286622a645c45f2002605203398d81649b8a29ee
Steinert999 <steinert999@outlook.com>
2020-12-23 15:13:57 -0300

    refactor(produtividade): reajusta metodo para try-with-resources
    PL-3137

--- src/main/java/br/com/zalf/prolog/webservice/entrega/produtividade/relatorio/ProdutividadeRelatorioDaoImpl.java
+++ src/main/java/br/com/zalf/prolog/webservice/entrega/produtividade/relatorio/ProdutividadeRelatorioDaoImpl.java
@@ -147,19 +147,20 @@ public class ProdutividadeRelatorioDaoImpl extends DatabaseConnection implements
             @NotNull final String cpfColaborador,
             @NotNull final LocalDate dataInicial,
             @NotNull final LocalDate dataFinal) throws SQLException {
+
         final List<ProdutividadeColaboradorRelatorio> relatorioColaboradores = new ArrayList<>();
         List<ProdutividadeColaboradorDia> relatorioDias = new ArrayList<>();
-        Connection conn = null;
-        PreparedStatement stmt = null;
-        ResultSet rSet = null;
-        try {
-            conn = getConnection();
-            stmt = conn.prepareStatement(
-                    "select * from func_relatorio_produtividade_remuneracao_acumulada_colaborador(" +
-                            "f_cod_unidade => ?, " +
-                            "f_cpf_colaborador => ?, " +
-                            "f_data_inicial => ?, " +
-                            "f_data_final => ?);");
+
+        final String sql = "select * " +
+                "from func_relatorio_produtividade_remuneracao_acumulada_colaborador(" +
+                "f_cod_unidade => ?, " +
+                "f_cpf_colaborador => ?, " +
+                "f_data_inicial => ?, " +
+                "f_data_final => ?);";
+
+        try (final Connection conn = getConnection();
+             final PreparedStatement stmt = conn.prepareStatement(sql)) {
+
             stmt.setLong(1, codUnidade);
             if (Filtros.isFiltroTodos(cpfColaborador)) {
                 stmt.setNull(2, Types.BIGINT);
@@ -168,31 +169,36 @@ public class ProdutividadeRelatorioDaoImpl extends DatabaseConnection implements
             }
             stmt.setObject(3, dataInicial);
             stmt.setObject(4, dataFinal);
-            rSet = stmt.executeQuery();
-            String ultimoNomeColaborador = null, nomeColaboradorAtual;
-            Long ultimoCpf = null, cpfAtual;
-            while (rSet.next()) {
-                cpfAtual = rSet.getLong("CPF_COLABORADOR");
-                nomeColaboradorAtual = rSet.getString("NOME_COLABORADOR");
-                if (ultimoCpf == null) {
-                    ultimoCpf = cpfAtual;
-                    ultimoNomeColaborador = nomeColaboradorAtual;
-                } else if (!cpfAtual.equals(ultimoCpf)) {
-                    // trocou de usuário
+
+            try (final ResultSet rSet = stmt.executeQuery()) {
+                String ultimoNomeColaborador = null;
+                String nomeColaboradorAtual;
+                Long ultimoCpf = null;
+                Long cpfAtual;
+
+                while (rSet.next()) {
+                    cpfAtual = rSet.getLong("CPF_COLABORADOR");
+                    nomeColaboradorAtual = rSet.getString("NOME_COLABORADOR");
+                    if (ultimoCpf == null) {
+                        ultimoCpf = cpfAtual;
+                        ultimoNomeColaborador = nomeColaboradorAtual;
+                    } else if (!cpfAtual.equals(ultimoCpf)) {
+                        // trocou de usuário
+                        relatorioColaboradores.add(
+                                createProdutividadeColaboradorRelatorio(ultimoCpf,
+                                                                        ultimoNomeColaborador,
+                                                                        relatorioDias));
+                        relatorioDias = new ArrayList<>();
+                        ultimoCpf = cpfAtual;
+                        ultimoNomeColaborador = nomeColaboradorAtual;
+                    }
+                    relatorioDias.add(createProdutividadeColaboradorDia(rSet));
+                }
+                if (ultimoCpf != null) {
                     relatorioColaboradores.add(
                             createProdutividadeColaboradorRelatorio(ultimoCpf, ultimoNomeColaborador, relatorioDias));
-                    relatorioDias = new ArrayList<>();
-                    ultimoCpf = cpfAtual;
-                    ultimoNomeColaborador = nomeColaboradorAtual;
                 }
-                relatorioDias.add(createProdutividadeColaboradorDia(rSet));
             }
-            if (ultimoCpf != null) {
-                relatorioColaboradores.add(
-                        createProdutividadeColaboradorRelatorio(ultimoCpf, ultimoNomeColaborador, relatorioDias));
-            }
-        } finally {
-            close(conn, stmt, rSet);
         }
         return relatorioColaboradores.stream()
                 .sorted((produtividade1, produtividade2) -> {
