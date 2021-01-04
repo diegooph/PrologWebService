b3081186b3392605aa68a8792a688b867b7b424c
gustavocnp95 <c98jjkdhgy>
2021-01-04 12:06:12 -0300

    refact(core): refatora nome de método

--- src/main/java/br/com/zalf/prolog/webservice/commons/util/MathUtils.java
+++ src/main/java/br/com/zalf/prolog/webservice/commons/util/MathUtils.java
@@ -6,19 +6,11 @@ package br.com.zalf.prolog.webservice.commons.util;
  * @author Luiz Felipe (https://github.com/luizfp)
  */
 public final class MathUtils {
-
     private MathUtils() {
         throw new IllegalStateException(MathUtils.class.getSimpleName() + " cannot be instantiated!");
     }
 
-    /**
-     * This method is one (1) based. A call like that getNthDigit(123, 2) would return 2 and not 3.
-     *
-     * @param number a number.
-     * @param n the nth digit to recovery.
-     * @return a specific digit of a {@code int} value.
-     */
-    public static int getNthDigit(final int number, final int n) {
-        return Character.digit(String.valueOf(number).charAt(n - 1), 10);
+    public static int getNumberInPosition(final int number, final int position) {
+        return Character.digit(String.valueOf(number).charAt(position - 1), 10);
     }
 }
