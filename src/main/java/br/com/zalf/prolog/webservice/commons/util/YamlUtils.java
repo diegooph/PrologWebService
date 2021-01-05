f4e761ff3bcd879434c14dd5335ddfe2faa327bd
gustavocnp95 <c98jjkdhgy>
2021-01-05 16:23:22 -0300

    refact(core): refatora yaml

--- src/main/java/br/com/zalf/prolog/webservice/commons/util/YamlUtils.java
+++ src/main/java/br/com/zalf/prolog/webservice/commons/util/YamlUtils.java
@@ -2,6 +2,7 @@ package br.com.zalf.prolog.webservice.commons.util;
 
 import org.apache.commons.io.IOUtils;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 
@@ -13,22 +14,28 @@ import java.io.InputStream;
  * @author Luiz Felipe (https://github.com/luizfp)
  */
 public final class YamlUtils {
-
     private YamlUtils() {
         throw new IllegalStateException(YamlUtils.class.getSimpleName() + " cannot be instantiated!");
     }
 
+    @NotNull
     public static <T> T parseFromResource(@NotNull final String resourceName,
                                           @NotNull final Class<T> classLoader) {
         InputStream inputStream = null;
         try {
-            inputStream = classLoader
-                    .getClassLoader()
-                    .getResourceAsStream(resourceName);
+            inputStream = getInputStream(resourceName, classLoader);
             final Yaml yaml = new Yaml(new Constructor(classLoader));
             return yaml.load(inputStream);
         } finally {
             IOUtils.closeQuietly(inputStream);
         }
+    }
+
+    @Nullable
+    private static <T> InputStream getInputStream(final @NotNull String resourceName,
+                                                  final @NotNull Class<T> classLoader) {
+        return classLoader
+                .getClassLoader()
+                .getResourceAsStream(resourceName);
     }
 }
