23b1ae7b74ad0a45cb7174a2e3298ca21ab5f65f
gustavocnp95 <c98jjkdhgy>
2021-01-05 08:50:54 -0300

    refact(core): apaga linha a mais

--- src/main/java/br/com/zalf/prolog/webservice/commons/util/ProLogCustomHeaders.java
+++ src/main/java/br/com/zalf/prolog/webservice/commons/util/ProLogCustomHeaders.java
@@ -9,17 +9,16 @@ import org.jetbrains.annotations.NotNull;
  * @author Luiz Felipe (https://github.com/luizfp)
  */
 public final class ProLogCustomHeaders {
-
-    public ProLogCustomHeaders() {
-        throw new IllegalStateException(ProLogCustomHeaders.class.getSimpleName() + " cannot be instantiated!");
-    }
-
     @NotNull
     public static final String VERSAO_DADOS_INTERVALO = IntervaloOfflineSupport.HEADER_NAME_VERSAO_DADOS_INTERVALO;
     @NotNull
     public static final String HEADER_TOKEN_INTEGRACAO = "ProLog-Token-Integracao";
     @NotNull
     public static final String HEADER_TOKEN_AGENDADOR = "ProLog-Token-Agendador";
+
+    public ProLogCustomHeaders() {
+        throw new IllegalStateException(ProLogCustomHeaders.class.getSimpleName() + " cannot be instantiated!");
+    }
 
     public static final class AppVersionAndroid {
         @NotNull
