d760fd987692276e47fb4a24db2cc805acb4b9e1
gustavocnp95 <c98jjkdhgy>
2021-01-05 11:32:53 -0300

    refact(core): apaga coentario desnecessario

--- src/main/java/br/com/zalf/prolog/webservice/commons/util/S3FileSender.java
+++ src/main/java/br/com/zalf/prolog/webservice/commons/util/S3FileSender.java
@@ -13,19 +13,10 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 
-/**
- * Classe responsável por gerenciar a comunicação com a API da Amazon.
- */
 public class S3FileSender {
     private final String accessKeyId;
     private final String secretKey;
     private AmazonS3 amazonS3Client;
-
-    public static class S3FileSenderException extends Exception {
-        S3FileSenderException(@NotNull final Exception e) {
-            super("Erro ao enviar arquivo para o S3", e);
-        }
-    }
 
     public S3FileSender(final String accessKeyId, final String secretKey) {
         Preconditions.checkNotNull(accessKeyId);
@@ -40,10 +31,10 @@ public class S3FileSender {
         Preconditions.checkNotNull(fileName);
         Preconditions.checkNotNull(file);
 
-        PutObjectRequest put = new PutObjectRequest(bucketName, fileName, file);
+        final PutObjectRequest put = new PutObjectRequest(bucketName, fileName, file);
         try {
             putObject(put);
-        } catch (Exception e) {
+        } catch (final Exception e) {
             // Erro ao enviar arquivo, subimos o erro.
             throw new S3FileSenderException(e);
         }
@@ -56,14 +47,14 @@ public class S3FileSender {
         return getS3Client().getUrl(bucketName, fileName).toString();
     }
 
-    private void putObject(PutObjectRequest putObjectRequest) throws Exception {
+    private void putObject(final PutObjectRequest putObjectRequest) throws Exception {
         getS3Client().putObject(putObjectRequest);
     }
 
     private AmazonS3 getS3Client() {
         if (amazonS3Client == null) {
-            ClientConfiguration clientConfiguration = getClientConfiguration();
-            AWSCredentials awsCreds = getAwsCredentials();
+            final ClientConfiguration clientConfiguration = getClientConfiguration();
+            final AWSCredentials awsCreds = getAwsCredentials();
             return amazonS3Client = AmazonS3ClientBuilder
                     .standard()
                     .withRegion(Regions.SA_EAST_1)
@@ -75,12 +66,18 @@ public class S3FileSender {
     }
 
     private ClientConfiguration getClientConfiguration() {
-        ClientConfiguration clientConfiguration = new ClientConfiguration();
+        final ClientConfiguration clientConfiguration = new ClientConfiguration();
         clientConfiguration.setSocketTimeout(60 * 1000); // 60 segundos
         return clientConfiguration;
     }
 
     private AWSCredentials getAwsCredentials() {
         return new BasicAWSCredentials(accessKeyId, secretKey);
+    }
+
+    public static class S3FileSenderException extends Exception {
+        S3FileSenderException(@NotNull final Exception e) {
+            super("Erro ao enviar arquivo para o S3", e);
+        }
     }
 }
