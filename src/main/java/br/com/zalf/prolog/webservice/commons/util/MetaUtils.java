884fd2e354ec78d3a132b4949f47ea318ca49f9d
gustavocnp95 <c98jjkdhgy>
2021-01-05 17:34:43 -0300

    refact(core): refatora e simplifica classe

--- src/main/java/br/com/zalf/prolog/webservice/commons/util/MetaUtils.java
+++ src/main/java/br/com/zalf/prolog/webservice/commons/util/MetaUtils.java
@@ -14,67 +14,33 @@ import java.time.temporal.ChronoUnit;
  * @author Jean Zart (https://github.com/jeanzart)
  */
 public class MetaUtils {
-
-    /**
-     * Verifica se bateu a meta, para indicadores em que o resultado tem que ficar abaixo da meta (Ex Devolução)
-     * @param resultado
-     * @param meta
-     * @return
-     */
-    public static boolean bateuMeta(final double resultado, final double meta){
-        if(resultado <= meta) {
-            return true;
-        }
-        return false;
+    private MetaUtils() {
+        throw new IllegalStateException(MetaUtils.class.getSimpleName() + " cannot be instantiated!");
     }
 
-    // verifica indicadores com tempo (jornada, tempo interno, tempo em rota, largada...)
+    public static boolean bateuMeta(final double resultado, final double meta) {
+        return resultado <= meta;
+    }
 
-    /**
-     * Verifica se bateu a meta, utilizado para indicadores de tempo (Ex Jornada)
-     * @param resultado
-     * @param meta
-     * @return
-     */
     public static boolean bateuMeta(final Time resultado, final Time meta) {
         final LocalTime r = TimeUtils.toLocalTime(resultado);
         final LocalTime m = TimeUtils.toLocalTime(meta);
-        if(r.isBefore(m) || r.equals(m)) {
-            return true;
-        }
-        return false;
+        return r.isBefore(m) || r.equals(m);
     }
 
-    /**
-     * Verifica se bateu a meta, para indicadores em que o resultado tem que ser superior a meta (Ex Tracking)
-     * @param resultado
-     * @param meta
-     * @return
-     */
-    public static boolean bateuMetaMapas(final double resultado, final double meta){
-        if(resultado >= meta) {
-            return true;
-        }
-        return false;
+    public static boolean bateuMetaMapas(final double resultado, final double meta) {
+        return resultado >= meta;
     }
 
-    /**
-     * Método utilitário para calcular o tempo de largada a partir do horário de saída e de matinal.
-     * @param hrSaida
-     * @param hrMatinal
-     * @return
-     */
     public static Time calculaTempoLargada(final Time hrSaida, final Time hrMatinal) {
         final LocalTime horaSaida = TimeUtils.toLocalTime(hrSaida);
         final LocalTime horaMatinal = TimeUtils.toLocalTime(hrMatinal);
-        /**
-         * Tempo fixo de matinal, 30 minutos.
-         */
         final LocalTime matinal = LocalTime.of(00, 30, 00);
-        if(horaMatinal.isAfter(horaSaida)){
+        if (horaMatinal.isAfter(horaSaida)) {
             return TimeUtils.toSqlTime(matinal);
         } else {
-            return TimeUtils.toSqlTime(horaSaida.minus(horaMatinal.getLong(ChronoField.MILLI_OF_DAY), ChronoUnit.MILLIS));
+            return TimeUtils.toSqlTime(horaSaida.minus(horaMatinal.getLong(ChronoField.MILLI_OF_DAY),
+                                                       ChronoUnit.MILLIS));
         }
     }
 }
