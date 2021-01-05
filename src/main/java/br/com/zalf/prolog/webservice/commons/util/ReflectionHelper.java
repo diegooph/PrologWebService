3212d1fefed89f32189a75863fe3d34d08d5f220
gustavocnp95 <c98jjkdhgy>
2021-01-05 11:15:17 -0300

    refact(core): adiciona anotações

--- src/main/java/br/com/zalf/prolog/webservice/commons/util/ReflectionHelper.java
+++ src/main/java/br/com/zalf/prolog/webservice/commons/util/ReflectionHelper.java
@@ -1,20 +1,22 @@
 package br.com.zalf.prolog.webservice.commons.util;
 
+import org.jetbrains.annotations.NotNull;
+
 /**
  * Created on 14/05/2018
  *
  * @author Luiz Felipe (https://github.com/luizfp)
  */
 public final class ReflectionHelper {
-
     private ReflectionHelper() {
         throw new IllegalStateException(ReflectionHelper.class.getSimpleName() + " cannot be instantiated!");
     }
 
-    public static <T> T instance(Class<? extends T> clazz) {
+    @NotNull
+    public static <T> T instance(@NotNull final Class<? extends T> clazz) {
         try {
             return clazz.newInstance();
-        } catch (Exception e) {
+        } catch (final Exception e) {
             throw new RuntimeException(e);
         }
     }
