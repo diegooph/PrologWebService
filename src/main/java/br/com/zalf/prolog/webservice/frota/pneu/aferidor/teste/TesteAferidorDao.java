105b2587aaffb9a797409ce3c2ff1ad6f32088b6
Steinert999 <steinert999@outlook.com>
2021-01-05 16:46:55 -0300

    docs(): remove docs TesteAferidorDao

--- src/main/java/br/com/zalf/prolog/webservice/frota/pneu/aferidor/teste/TesteAferidorDao.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/aferidor/teste/TesteAferidorDao.java
@@ -10,22 +10,10 @@ import org.jetbrains.annotations.NotNull;
  * @author Luiz Felipe (https://github.com/luizfp)
  */
 public interface TesteAferidorDao {
-    /**
-     * Busca o procedimento de teste que será executado no equipamento de aferição através do aplicativo.
-     *
-     * @return O procedimento de teste que deverá ser executado.
-     * @throws Throwable Caso algum erro ocorrer.
-     */
+
     @NotNull
     ProcedimentoTesteAferidor getProcedimentoTeste() throws Throwable;
 
-    /**
-     * Salva o teste do equipamento de aferição que foi executado no aplicativo.
-     *
-     * @param teste O teste executado.
-     * @return O código do teste executado inserido.
-     * @throws Throwable Caso algum erro ocorrer.
-     */
     @NotNull
     Long insereTeste(@NotNull final TesteAferidorExecutado teste) throws Throwable;
 }
