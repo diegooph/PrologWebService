eae207a357b2b9167bd650af0bbaa395f42bfb7c
Steinert999 <steinert999@outlook.com>
2021-01-05 14:35:13 -0300

    docs(): remove docs EntreEmContatoDao

--- src/main/java/br/com/zalf/prolog/webservice/contato/EntreEmContatoDao.java
+++ src/main/java/br/com/zalf/prolog/webservice/contato/EntreEmContatoDao.java
@@ -8,15 +8,6 @@ import org.jetbrains.annotations.NotNull;
  * @author Luiz Felipe (https://github.com/luizfp)
  */
 public interface EntreEmContatoDao {
-
-    /**
-     * Método utilizado para salvar as mensagens de futuros clientes que entraram em contato a partir do formulário
-     * de contato presente no aplicativo.
-     *
-     * @param contato - Um objeto {@link MensagemContato} contendo as informações do cliente.
-     * @return - O código da mensagem inserida no Banco de dados.
-     * @throws Throwable - Se algum erro ocorrer no processo de inserção de informções no banco de dados.
-     */
     @NotNull
     Long insertNovaMensagemContato(@NotNull final MensagemContato contato) throws Throwable;
 }
