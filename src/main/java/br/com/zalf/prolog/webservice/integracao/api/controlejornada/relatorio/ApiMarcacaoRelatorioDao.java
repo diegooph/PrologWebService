51aea253680d58ed961c198e1524bfad7b506adc
Steinert999 <steinert999@outlook.com>
2021-01-06 14:44:10 -0300

    docs: remove docs ApiMarcacaoRelatorioDao

--- src/main/java/br/com/zalf/prolog/webservice/integracao/api/controlejornada/relatorio/ApiMarcacaoRelatorioDao.java
+++ src/main/java/br/com/zalf/prolog/webservice/integracao/api/controlejornada/relatorio/ApiMarcacaoRelatorioDao.java
@@ -14,18 +14,6 @@ import java.util.List;
  */
 public interface ApiMarcacaoRelatorioDao {
 
-    /**
-     * Busca os dados das marcações dos colaboradores respeitando os parâmetros de filtros e no padrão da portaria 1510.
-     *
-     * @param tokenIntegracao O token de integração que está requisitando as informações.
-     * @param dataInicial Data inicial do período de busca.
-     * @param dataFinal Data final do período de busca.
-     * @param codUnidadeProLog Código da unidade do ProLog para busca dos dados, opcional.
-     * @param codTipoMarcacao Código do tipo de marcação para busca dos dados, opcional.
-     * @param cpfColaborador CPF do colaborador para busca dos dados, opcional.
-     * @return Uma lista contendo todas as marcações no padrão da portaria 1510.
-     * @throws Throwable caso ocorra algum erro.
-     */
     @NotNull
     List<ApiMarcacaoRelatorio1510> getRelatorioPortaria1510(
             @NotNull final String tokenIntegracao,
