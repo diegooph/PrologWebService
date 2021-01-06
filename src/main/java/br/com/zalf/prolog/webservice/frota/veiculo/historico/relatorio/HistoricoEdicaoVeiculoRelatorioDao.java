b407b38a1a8d120f66184d5cdf7b65bd3ed7aef4
Steinert999 <steinert999@outlook.com>
2021-01-06 11:01:08 -0300

    docs: remove docs HistoricoEdicaoVeiculoRelatorioDao

--- src/main/java/br/com/zalf/prolog/webservice/frota/veiculo/historico/relatorio/HistoricoEdicaoVeiculoRelatorioDao.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/veiculo/historico/relatorio/HistoricoEdicaoVeiculoRelatorioDao.java
@@ -10,15 +10,7 @@ import java.io.OutputStream;
  * @author Gustavo Navarro (https://github.com/gustavocnp95)
  */
 public interface HistoricoEdicaoVeiculoRelatorioDao {
-    /**
-     * Busca o histórico de edições de um veiculo baseado em um código de veiculo e um código de empresa, retornando-o
-     * em formato csv.
-     *
-     * @param outputStream arquivo binário onde o csv será escrito.
-     * @param codVeiculo   um código de um veiculo para buscar o histórico.
-     * @param codEmpresa   o código da empresa do histórico desejado.
-     * @throws Throwable caso qualquer erro ocorrer.
-     */
+
     void getHistoricoEdicaoVeiculoCsv(@NotNull OutputStream outputStream,
                                       @NotNull final Long codEmpresa,
                                       @NotNull final Long codVeiculo) throws Throwable;
