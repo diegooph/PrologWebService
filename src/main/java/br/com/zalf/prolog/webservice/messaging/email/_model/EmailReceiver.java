e92b77c8d4885d9f79003e66b2fc9bd4918a3dc0
Steinert999 <steinert999@outlook.com>
2021-02-01 15:18:31 -0300

    feat(core): adiciona DTO's para SendContact do Mailjet
    PL-3414

---
+++ src/main/java/br/com/zalf/prolog/webservice/messaging/email/_model/EmailReceiver.java
@@ -0,0 +1,29 @@
+package br.com.zalf.prolog.webservice.messaging.email._model;
+
+import com.mailjet.client.transactional.SendContact;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created on 2021-01-29
+ *
+ * @author Guilherme Steinert (https://github.com/steinert999)
+ */
+
+
+public class EmailReceiver extends SendContact {
+
+    private EmailReceiver(final String email, final String name) {
+        super(email, name);
+    }
+    private EmailReceiver(final String email) {
+        super(email);
+    }
+
+    public static EmailReceiver of(@NotNull final String email, @NotNull final String name) {
+        return new EmailReceiver(email, name);
+    }
+
+    public static EmailReceiver of(@NotNull final String email) {
+        return new EmailReceiver(email);
+    }
+}
--- src/main/java/br/com/zalf/prolog/webservice/messaging/email/_model/EmailSender.java
+++ src/main/java/br/com/zalf/prolog/webservice/messaging/email/_model/EmailSender.java
@@ -1,5 +1,6 @@
 package br.com.zalf.prolog.webservice.messaging.email._model;
 
+import com.mailjet.client.transactional.SendContact;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -7,31 +8,22 @@ import org.jetbrains.annotations.NotNull;
  *
  * @author Luiz Felipe (https://github.com/luizfp)
  */
-public final class EmailSender {
-    @NotNull
-    private final String email;
-    @NotNull
-    private final String name;
 
-    private EmailSender(@NotNull final String email,
-                        @NotNull final String name) {
-        this.email = email;
-        this.name = name;
+public final class EmailSender extends SendContact {
+
+    private EmailSender(@NotNull final String email, @NotNull final String name) {
+        super(email, name);
+    }
+    private EmailSender(@NotNull final String email) {
+        super(email);
     }
 
-    @NotNull
     public static EmailSender of(@NotNull final String email,
                                  @NotNull final String name) {
         return new EmailSender(email, name);
     }
 
-    @NotNull
-    public String getEmail() {
-        return email;
-    }
-
-    @NotNull
-    public String getName() {
-        return name;
+    public static EmailSender of(@NotNull final String email) {
+        return new EmailSender(email);
     }
 }
