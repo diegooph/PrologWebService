dc6ab6d4fce8ceb984c33af58c13e900542966cf
Steinert999 <steinert999@outlook.com>
2021-02-01 15:27:05 -0300

    refactor(core): adiciona lombok para Getter e construtor
    PL-3414

--- src/main/java/br/com/zalf/prolog/webservice/messaging/email/_model/EmailTemplate.java
+++ src/main/java/br/com/zalf/prolog/webservice/messaging/email/_model/EmailTemplate.java
@@ -1,20 +1,17 @@
 package br.com.zalf.prolog.webservice.messaging.email._model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
  * Created on 2020-02-25
  *
  * @author Luiz Felipe (https://github.com/luizfp)
  */
+@AllArgsConstructor
+@Getter
 public enum EmailTemplate {
-    ABERTURA_SOCORRO_ROTA(1255647);
+    ABERTURA_SOCORRO_ROTA(1255647L);
 
-    private final int templateId;
-
-    EmailTemplate(final int templateId) {
-        this.templateId = templateId;
-    }
-
-    public int getTemplateId() {
-        return templateId;
-    }
+    private final Long templateId;
 }
