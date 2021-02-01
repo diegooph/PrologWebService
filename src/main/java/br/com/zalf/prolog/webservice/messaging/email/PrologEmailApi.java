b54b6d437077ce9262788f4af3bbfc68bd69d1f0
Steinert999 <steinert999@outlook.com>
2021-02-01 15:41:25 -0300

    refactor(core): altera implementação para salvar historico dos emails e enviar emails
    PL-3414

--- src/main/java/br/com/zalf/prolog/webservice/messaging/email/PrologEmailApi.java
+++ src/main/java/br/com/zalf/prolog/webservice/messaging/email/PrologEmailApi.java
@@ -2,6 +2,8 @@ package br.com.zalf.prolog.webservice.messaging.email;
 
 import br.com.zalf.prolog.webservice.commons.util.Log;
 import br.com.zalf.prolog.webservice.messaging.MessageScope;
+import br.com.zalf.prolog.webservice.messaging.email._model.EmailReceiver;
+import br.com.zalf.prolog.webservice.messaging.email._model.EmailRequestResponseHolder;
 import br.com.zalf.prolog.webservice.messaging.email._model.EmailTemplateMessage;
 import br.com.zalf.prolog.webservice.messaging.email.task.PrologEmailSaveLogTask;
 import org.jetbrains.annotations.NotNull;
@@ -29,10 +31,18 @@ public final class PrologEmailApi {
         this.logTask = logTask;
     }
 
-    public void deliverTemplate(@NotNull final List<String> emailsToSend,
+    public void deliverTemplate(@NotNull final Set<EmailReceiver> receivers,
                                 @NotNull final MessageScope messageScope,
-                                @NotNull final EmailTemplateMessage templateMessage) {
-        Log.d(TAG, String.format("Enviando e-mail para %d destinatário(s): %s", emailsToSend.size(), templateMessage));
-        new PrologEmailWorker(emailsToSend, messageScope, templateMessage).execute();
+                                @NotNull final EmailTemplateMessage template) {
+        Throwable emailApiException = null;
+        EmailRequestResponseHolder holder = null;
+        try {
+            Log.d(TAG, String.format("Enviando e-mail para %d destinatário(s): %s", receivers.size(), template));
+            holder = this.emailApi.sendMessage(receivers, template);
+        } catch (final Throwable t) {
+            emailApiException = t;
+            Log.e(TAG, "Erro fatal ao enviar e-mails via API! Nada foi enviado.", t);
+        }
+        logTask.saveToDatabase(messageScope, holder, emailApiException);
     }
 }
