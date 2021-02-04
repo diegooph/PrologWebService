c0458bbe93f96deaf66c80ca50f8458f362fa225
didi <dioge.vanzella@gmail.com>
2021-02-04 10:36:40 -0300

    refactor(testes): adiciona flyway para migrar o DB
    PL-3307

--- src/test/java/test/br/com/zalf/prolog/webservice/pilares/geral/unidade/PostgresTest.java
+++ src/test/java/test/br/com/zalf/prolog/webservice/pilares/geral/unidade/PostgresTest.java
@@ -1,5 +1,7 @@
 package test.br.com.zalf.prolog.webservice.pilares.geral.unidade;
 
+import org.flywaydb.core.Flyway;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -8,6 +10,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import static org.junit.Assert.assertEquals;
 
@@ -18,20 +21,44 @@ public class PostgresTest {
             .withDatabaseName("integration-tests-db")
             .withUsername("sa")
             .withPassword("sa");
+    private final Connection conn = getConnection();
 
     @Test
-    public void testPostgresContainerRunning()
-            throws Exception {
-        final String jdbcUrl = postgresContainer.getJdbcUrl();
-        final String username = postgresContainer.getUsername();
-        final String password = postgresContainer.getPassword();
-        final Connection conn = DriverManager
-                .getConnection(jdbcUrl, username, password);
-        final ResultSet resultSet =
-                conn.createStatement().executeQuery("SELECT 1");
+    public void testPostgresContainerRunning() throws Exception {
+        final ResultSet resultSet = conn.createStatement().executeQuery("SELECT 1");
         resultSet.next();
         final int result = resultSet.getInt(1);
 
         assertEquals(1, result);
+    }
+
+    @Test
+    public void testFlywayMigration() throws Exception {
+        Flyway.configure()
+                .dataSource(postgresContainer.getJdbcUrl(),
+                            postgresContainer.getUsername(),
+                            postgresContainer.getPassword())
+                .load()
+                .migrate();
+
+        final ResultSet resultSet = conn.createStatement().executeQuery("SELECT * " +
+                                                                                "FROM CUSTOMERS " +
+                                                                                "WHERE ID = 1");
+        resultSet.next();
+        final int result = resultSet.getInt(1);
+
+        assertEquals(1, result);
+    }
+
+    @NotNull
+    private Connection getConnection() {
+        try {
+            return DriverManager.getConnection(postgresContainer.getJdbcUrl(),
+                                               postgresContainer.getUsername(),
+                                               postgresContainer.getPassword());
+        } catch (final SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
     }
 }
