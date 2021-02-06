dc52555d704e9d11d6a537cd88b56ee00ea665d1
Luiz Felipe <luiz.felipe_95@hotmail.com>
2021-02-06 16:25:44 -0300

    refactor(core): melhora FirebaseFactory
    PL-3414

--- src/main/java/br/com/zalf/prolog/webservice/messaging/push/FirebaseFactory.java
+++ src/main/java/br/com/zalf/prolog/webservice/messaging/push/FirebaseFactory.java
@@ -5,17 +5,15 @@ import br.com.zalf.prolog.webservice.commons.util.StringUtils;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import lombok.var;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Optional;
 
@@ -26,22 +24,22 @@ import java.util.Optional;
  */
 @Configuration
 public class FirebaseFactory {
-
     private static final String TAG = FirebaseFactory.class.getSimpleName();
+    @NotNull
     private final String databaseUrl;
+    @NotNull
     private final String credentials;
 
     @Autowired
-    FirebaseFactory(@Value("${firebase.database.url:https://prolog-debug.firebaseio.com}") final String databaseUrl,
-                    @Value("${firebase.credentials:}") final String credentials) {
+    FirebaseFactory(
+            @Value("${firebase.database.url:https://prolog-debug.firebaseio.com}") @NotNull final String databaseUrl,
+            @Value("${firebase.credentials:}") @NotNull final String credentials) {
         this.databaseUrl = databaseUrl;
         this.credentials = credentials;
     }
 
-
     @PostConstruct
     public void init() {
-
         if (isNotInitialized()) {
             Log.i(TAG, "Firebase ainda não foi inicializado, iremos tentar inicializar.");
             getCredentials().ifPresent(credentials -> {
@@ -50,29 +48,27 @@ public class FirebaseFactory {
                                                   .setCredentials(credentials)
                                                   .setDatabaseUrl(databaseUrl)
                                                   .build());
-                Log.d(TAG, "Firebase inicializado com sucesso!!");
+                Log.i(TAG, "Firebase inicializado com sucesso!!");
             });
         }
-
     }
 
     @PreDestroy
     public void finish() {
         if (isInitialized()) {
-           FirebaseApp.getInstance().delete();
+            FirebaseApp.getInstance().delete();
         }
-        final String message = "FirebaseApp deletado com sucesso!!";
-        Log.d(TAG, message);
+        Log.i(TAG, "FirebaseApp deletado com sucesso!!");
     }
 
+    @NotNull
     private Optional<GoogleCredentials> getCredentials() {
-        if (containsCredentials()) {
+        if (doWeHaveCredentials()) {
             try (final InputStream io = new FileInputStream(credentials)) {
                 return Optional.of(GoogleCredentials.fromStream(io));
-            } catch (FileNotFoundException e) {
-                final String message = String.format("Arquivo com as credenciais: %s não foi encontrado.", credentials);
-                Log.e(TAG, message, e);
-            } catch (Throwable e) {
+            } catch (final FileNotFoundException e) {
+                Log.e(TAG, String.format("Arquivo com as credenciais: %s não foi encontrado.", credentials), e);
+            } catch (final Throwable e) {
                 Log.e(TAG, "Erro ao buscar credenciais", e);
             }
         }
@@ -81,18 +77,14 @@ public class FirebaseFactory {
     }
 
     private boolean isInitialized() {
-        return !FirebaseApp.getApps().isEmpty() && FirebaseApp.getApps().size() != 0;
+        return !FirebaseApp.getApps().isEmpty();
     }
 
     private boolean isNotInitialized() {
         return !isInitialized();
     }
-    private boolean containsCredentials() {
-        return !notContainsCredentials();
-    }
 
-    private boolean notContainsCredentials() {
-        return StringUtils.isNullOrEmpty(credentials);
+    private boolean doWeHaveCredentials() {
+        return !StringUtils.isNullOrEmpty(credentials);
     }
-
 }
