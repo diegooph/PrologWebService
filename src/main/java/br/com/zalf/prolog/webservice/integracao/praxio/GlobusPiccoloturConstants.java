7672bb749862b03f8c9c687233dce367676be225
Luiz Felipe <luiz.felipe_95@hotmail.com>
2021-02-11 15:03:00 -0300

    revert: refactor(integração): altera forma de utilizar constantes na integração da Piccolotur

--- src/main/java/br/com/zalf/prolog/webservice/integracao/praxio/GlobusPiccoloturConstants.java
+++ src/main/java/br/com/zalf/prolog/webservice/integracao/praxio/GlobusPiccoloturConstants.java
@@ -14,7 +14,19 @@ public final class GlobusPiccoloturConstants {
     // #######                       CONSTANTES UTILIZADAS PARA A INTEGRAÇÃO COM O GLOBUS                        #######
     // #################################################################################################################
     // #################################################################################################################
+    /**
+     * Utilizamos a <code>URL_TESTES</code> em ambiente de testes para evitar com o ambiente de testes da Praxio em
+     * qualquer situação. Para eventos específicos onde queremos testar no ambiente da Praxio, modificaremos essa
+     * validação.
+     */
+    @NotNull
+    // URL_TESTES_LOCAL = "http://localhost:8184/bruno.maia/ManutencaoWsTerceiros.asmx"
+    private static final String URL_TESTES = "http://sp.bgmrodotec.com.br:8184/bruno.maia/ManutencaoWsTerceiros.asmx";
+    @NotNull
+    public static final String WSDL_LOCATION =
+            "http://erp.piccolotur.com.br:55582/GlobusMais/ManutencaoWsTerceiros.asmx";
     public static final String NAMESPACE = "http://bgmrodotec.com.br/globus5/ManutencaoWsTerceiros";
+
     public static final String TOKEN_AUTENTICACAO_OS =
             BuildConfig.DEBUG ? "MTc0Nzs0OTk7ODEzNA==" : "MTEzNzI7NDk5OzgxMzQ=";
     public static final int SHORT_CODE_AUTENTICACAO_OS = 1032;
@@ -22,19 +34,6 @@ public final class GlobusPiccoloturConstants {
     public static final String USUARIO_PROLOG_INTEGRACAO = "MANAGER";
     public static final String CPF_COLABORADOR_LOCAIS_MOVIMENTO = "35118345898";
     public static final String COD_UNIDADE_NOME_LOCAL_MOVIMENTO_SEPARATOR = " - ";
-    /**
-     * Utilizamos a <code>URL_TESTES</code> em ambiente de testes para evitar com o ambiente de testes da Praxio em
-     * qualquer situação. Para eventos específicos onde queremos testar no ambiente da Praxio, modificaremos essa
-     * validação.
-     */
-    @NotNull
-    private static final String URL_TESTES = BuildConfig.DEBUG
-            ? "http://localhost:8184/bruno.maia/ManutencaoWsTerceiros.asmx"
-            : "http://sp.bgmrodotec.com.br:8184/bruno.maia/ManutencaoWsTerceiros.asmx";
-    @NotNull
-    public static final String WSDL_LOCATION = BuildConfig.DEBUG
-            ? URL_TESTES
-            : "http://erp.piccolotur.com.br:55582/GlobusMais/ManutencaoWsTerceiros.asmx";
 
     private GlobusPiccoloturConstants() {
         throw new IllegalStateException(GlobusPiccoloturConstants.class.getSimpleName() + " can not be instantiated");
