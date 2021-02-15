2eb1a0939fb87c8adac8107be03c05e1487cb998
Steinert999 <steinert999@outlook.com>
2021-02-15 09:09:52 -0300

    feat(afericao): adiciona estrutura de Service
    PL-3458

---
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/afericao/v3/service/AfericaoV3Service.java
@@ -0,0 +1,24 @@
+package br.com.zalf.prolog.webservice.frota.pneu.afericao.v3.service;
+
+import br.com.zalf.prolog.webservice.frota.pneu.afericao.v3._model.dto.busca.FiltroAfericaoAvulsa;
+import br.com.zalf.prolog.webservice.frota.pneu.afericao.v3._model.dto.busca.FiltroAfericaoPlaca;
+import br.com.zalf.prolog.webservice.frota.pneu.afericao.v3._model.projections.AfericaoAvulsaProjection;
+import br.com.zalf.prolog.webservice.frota.pneu.afericao.v3._model.projections.AfericaoPlacaProjection;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+/**
+ * Created on 2021-02-09
+ *
+ * @author Guilherme Steinert (https://github.com/steinert999)
+ */
+public interface AfericaoV3Service {
+
+
+    @NotNull
+    List<AfericaoPlacaProjection> getAfericoesPlacas(FiltroAfericaoPlaca filtro);
+
+    @NotNull
+    List<AfericaoAvulsaProjection> getAfericoesAvulsas(FiltroAfericaoAvulsa filtro);
+}
