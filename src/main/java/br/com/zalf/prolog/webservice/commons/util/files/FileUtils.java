199b1d14bc2116ea2783bba7b29d474b0e90c0f9
Steinert999 <steinert999@outlook.com>
2021-02-16 17:42:53 -0300

    refactor(core): ajusta criação do diretório temporário conforme padrão novo personalizado
    PL-3436

--- src/main/java/br/com/zalf/prolog/webservice/commons/util/files/FileUtils.java
+++ src/main/java/br/com/zalf/prolog/webservice/commons/util/files/FileUtils.java
@@ -1,5 +1,6 @@
 package br.com.zalf.prolog.webservice.commons.util.files;
 
+import br.com.zalf.prolog.webservice.commons.util.Log;
 import org.apache.commons.io.FilenameUtils;
 import org.jetbrains.annotations.NotNull;
 
@@ -13,6 +14,8 @@ import java.time.format.DateTimeFormatter;
  * @author Luiz Felipe (https://github.com/luizfp)
  */
 public final class FileUtils {
+
+    private static final String TAG = FileUtils.class.getSimpleName();
 
     private static final String TEMP_DIR_REF = System.getProperty("java.io.tmpdir");
 
@@ -32,7 +35,16 @@ public final class FileUtils {
 
     @NotNull
     public static File createTempDir() {
-        return com.google.common.io.Files.createTempDir();
+        final String baseName = getBaseName();
+        final File baseDir = new File(TEMP_DIR_REF);
+        final File tmpDir = new File(baseDir, baseName);
+        if (!tmpDir.exists()) {
+            Log.i(TAG, "Realizando criação do diretório:" + tmpDir.getName());
+            if (!tmpDir.mkdir()) {
+                throw new IllegalStateException("Failed to create temp directory: " + baseName);
+            }
+        }
+        return tmpDir;
     }
 
     public static File getTempDir() {
