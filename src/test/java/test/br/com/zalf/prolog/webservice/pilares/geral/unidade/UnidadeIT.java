9da4d39f2e7ea2a25e5011391455453d081234a6
Steinert999 <steinert999@outlook.com>
2021-02-24 17:02:34 -0300

    refactor(core): ajusta teste para busca de UnidadeVisualizacaoDTO por cod_unidade
    PL-3307

--- src/test/java/test/br/com/zalf/prolog/webservice/pilares/geral/unidade/UnidadeIT.java
+++ src/test/java/test/br/com/zalf/prolog/webservice/pilares/geral/unidade/UnidadeIT.java
@@ -1,29 +1,38 @@
 package test.br.com.zalf.prolog.webservice.pilares.geral.unidade;
 
 import br.com.zalf.prolog.webservice.geral.unidade._model.UnidadeVisualizacaoListagemDto;
-import com.google.common.truth.Truth;
+import lombok.var;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
 import test.br.com.zalf.prolog.webservice.IntegrationTest;
+
+import java.net.URI;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class UnidadeIT extends IntegrationTest {
 
     @Test
     @DisplayName("Busca uma unidade através de um código.")
-    void getUnidadeByCodigo() {
-        final long codUnidade = 5;
-        final ResponseEntity<UnidadeVisualizacaoListagemDto> unidadeByCodigo =
-                restTemplate.exchange("/unidades/{codUnidade}",
-                                      HttpMethod.GET,
-                                      null,
-                                      UnidadeVisualizacaoListagemDto.class,
-                                      codUnidade);
-        Truth.assertThat(unidadeByCodigo).isNotNull();
-        Truth.assertThat(unidadeByCodigo.getStatusCode()).isEqualTo(HttpStatus.OK);
-        Truth.assertThat(unidadeByCodigo.getBody()).isNotNull();
-        Truth.assertThat(unidadeByCodigo.getBody().getCodUnidade()).isEqualTo(codUnidade);
+    @Disabled
+    void givenCodUnidadeToRequest_ThenReturnUnidadeVisualizacaoListagemAndStatusOK() {
+        final Long codUnidade = 5L;
+        final String path = createPathWithPort("unidades/" + codUnidade);
+        final var requestEntity = RequestEntity
+                .get(URI.create(path))
+                .accept(MediaType.APPLICATION_JSON)
+                .build();
+        final ResponseEntity<UnidadeVisualizacaoListagemDto> response =
+                getTestRestTemplate().exchange(path,
+                                               HttpMethod.GET,
+                                               requestEntity,
+                                               ParameterizedTypeReference.forType(UnidadeVisualizacaoListagemDto.class));
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody().getCodUnidade()).isEqualTo(codUnidade);
     }
 }
