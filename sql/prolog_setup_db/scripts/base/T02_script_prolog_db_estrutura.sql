453529678c89c073552ba4be5603674c2f7e66f7
Steinert999 <steinert999@outlook.com>
2021-02-24 16:47:21 -0300

    feat(core): adiciona functions necessárias para teste
    PL-3307

--- sql/prolog_setup_db/scripts/base/T02_script_prolog_db_estrutura.sql
+++ sql/prolog_setup_db/scripts/base/T02_script_prolog_db_estrutura.sql
@@ -39209,4 +39209,76 @@ BEGIN
 END ;
 $$;
 
+CREATE OR REPLACE FUNCTION FUNC_LIBERA_TODAS_PERMISSOES(F_CPF BIGINT, OUT F_AVISO_PERMISSOES_INSERIDAS TEXT)
+    RETURNS TEXT
+    LANGUAGE PLPGSQL
+    SECURITY DEFINER
+AS
+$$
+DECLARE
+    F_COD_UNIDADE       BIGINT;
+    F_COD_FUNCAO        BIGINT;
+    F_COD_FUNCAO_PROLOG BIGINT;
+BEGIN
+    -- VERIFICA SE COLABORADOR EXISTE
+    PERFORM FUNC_GARANTE_COLABORADOR_EXISTE(F_CPF);
+
+    SELECT CO.COD_UNIDADE, CO.COD_FUNCAO FROM COLABORADOR CO WHERE CO.CPF = F_CPF INTO F_COD_UNIDADE, F_COD_FUNCAO;
+
+    DELETE FROM CARGO_FUNCAO_PROLOG_V11 WHERE COD_FUNCAO_COLABORADOR = F_COD_FUNCAO AND COD_UNIDADE = F_COD_UNIDADE;
+
+    FOREACH F_COD_FUNCAO_PROLOG IN ARRAY (SELECT ARRAY_AGG(FPV11.CODIGO) FROM FUNCAO_PROLOG_V11 FPV11)
+        LOOP
+            INSERT INTO CARGO_FUNCAO_PROLOG_V11 (COD_UNIDADE, COD_FUNCAO_COLABORADOR, COD_FUNCAO_PROLOG,
+                                                 COD_PILAR_PROLOG)
+            SELECT F_COD_UNIDADE,
+                   F_COD_FUNCAO,
+                   F_COD_FUNCAO_PROLOG,
+                   (SELECT FPV11.COD_PILAR FROM FUNCAO_PROLOG_V11 FPV11 WHERE FPV11.CODIGO = F_COD_FUNCAO_PROLOG);
+
+        END LOOP;
+
+    --MENSAGEM DE SUCESSO.
+    SELECT 'Todas as permissões do Prolog foram atribuídas ao cargo de código ' || F_COD_FUNCAO ||
+           ', unidade: ' || F_COD_UNIDADE || '.'
+    INTO F_AVISO_PERMISSOES_INSERIDAS;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION FUNC_UNIDADE_VISUALIZACAO(
+    F_COD_UNIDADE BIGINT
+)
+    RETURNS TABLE
+            (
+                CODIGO_UNIDADE               BIGINT,
+                NOME_UNIDADE                 TEXT,
+                TOTAL_COLABORADORES_UNIDADE  BIGINT,
+                TIMEZONE_UNIDADE             TEXT,
+                DATA_HORA_CADASTRO_UNIDADE   TIMESTAMP WITHOUT TIME ZONE,
+                STATUS_ATIVO_UNIDADE         BOOLEAN,
+                CODIGO_AUXILIAR_UNIDADE      TEXT,
+                LATITUDE_UNIDADE             TEXT,
+                LONGITUDE_UNIDADE            TEXT,
+                CODIGO_REGIONAL_UNIDADE      BIGINT,
+                NOME_REGIAO_REGIONAL_UNIDADE TEXT
+            )
+    LANGUAGE SQL
+AS
+$$
+SELECT UN.CODIGO                                                            AS CODIGO_UNIDADE,
+       UN.NOME                                                              AS NOME_UNIDADE,
+       (SELECT COUNT(*) FROM COLABORADOR C WHERE C.cod_unidade = UN.CODIGO) AS TOTAL_COLABORADORES_UNIDADE,
+       UN.TIMEZONE                                                          AS TIMEZONE_UNIDADE,
+       UN.DATA_HORA_CADASTRO AT TIME ZONE UN.TIMEZONE                       AS DATA_HORA_CADASTRO_UNIDADE,
+       UN.STATUS_ATIVO                                                      AS STATUS_ATIVO_UNIDADE,
+       UN.COD_AUXILIAR                                                      AS CODIGO_AUXILIAR_UNIDADE,
+       UN.LATITUDE_UNIDADE                                                  AS LATITUDE_UNIDADE,
+       UN.LONGITUDE_UNIDADE                                                 AS LONGITUDE_UNIDADE,
+       R.CODIGO                                                             AS CODIGO_REGIONAL_UNIDADE,
+       R.REGIAO                                                             AS NOME_REGIAO_REGIONAL_UNIDADE
+FROM UNIDADE UN
+         INNER JOIN REGIONAL R ON UN.COD_REGIONAL = R.CODIGO
+WHERE UN.CODIGO = F_COD_UNIDADE;
+$$;
+
 
