a8c5706e8e9be7f1ad721532b98127bf521e55fc
Steinert999 <steinert999@outlook.com>
2021-02-24 16:56:45 -0300

    refactor(core): altera instancia da configuração do testcontainers para classe especifica
    PL-3307

--- src/test/java/test/br/com/zalf/prolog/webservice/IntegrationTest.java
+++ src/test/java/test/br/com/zalf/prolog/webservice/IntegrationTest.java
@@ -4,44 +4,34 @@ import br.com.zalf.prolog.webservice.config.PrologApplication;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.images.builder.ImageFromDockerfile;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.containers.JdbcDatabaseContainer;
 
-import java.io.File;
+import java.util.Collections;
+import java.util.List;
 
-@ExtendWith({SpringExtension.class})
-@SpringBootTest(classes = PrologApplication.class,
-                webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Testcontainers
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = {PrologApplication.class})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ContextConfiguration(initializers = {TestContainer.DockerPostgresDatasourceInitializer.class})
+
 public class IntegrationTest {
-    @NotNull
-    private static final String POSTGRES_IMAGE_WITH_POSTGIS = "postgis/postgis:12-2.5-alpine";
-    @Container
-    public static GenericContainer<?> genericContainer =
-            new GenericContainer<>(
-                    new ImageFromDockerfile()
-                            .withDockerfile(new File("Dockerfile").toPath()));
-    @Container
-    protected static PostgreSQLContainer<?> postgresContainer =
-            new PostgreSQLContainer<>(DockerImageName
-                                              .parse(POSTGRES_IMAGE_WITH_POSTGIS)
-                                              .asCompatibleSubstituteFor("postgres:12.2"));
-    @Autowired
-    protected TestRestTemplate restTemplate;
 
+    private static final JdbcDatabaseContainer<?> container = TestContainer.getContainer();
+    
     @BeforeAll
     static void beforeAll() {
         final FlywayConfiguration configuration = new FlywayConfiguration(container.getJdbcUrl(),
                                                                           container.getUsername(),
                                                                           container.getPassword());
         configuration.migrate();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        container.stop();
     }
 }
