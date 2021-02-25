b047622212fc95885f388a6cd89aa20cdb953d9c
Steinert999 <steinert999@outlook.com>
2021-02-25 11:39:31 -0300

    refactor(core): adiciona anotação informando que é para scheduling com spring
    PL-3436

--- src/main/java/br/com/zalf/prolog/webservice/schedules/Scheduler.java
+++ src/main/java/br/com/zalf/prolog/webservice/schedules/Scheduler.java
@@ -1,10 +1,13 @@
 package br.com.zalf.prolog.webservice.schedules;
 
+import org.springframework.scheduling.annotation.Scheduled;
+
 /**
  * Created on 2020-11-30
  *
  * @author Guilherme Steinert (https://github.com/steinert999)
  */
 public interface Scheduler {
+    @Scheduled
     void doWork();
 }
