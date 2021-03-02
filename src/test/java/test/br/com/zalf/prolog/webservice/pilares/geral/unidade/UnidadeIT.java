ea5e4bda5125c89462221c69cd4861b86967eab1
Steinert999 <steinert999@outlook.com>
2021-03-02 13:59:16 -0300

    feat(core): adiciona teste para resource de List<UnidadeVisualizacaoListagemDto>
    PL-3307

--- src/test/java/test/br/com/zalf/prolog/webservice/pilares/geral/unidade/UnidadeIT.java
+++ src/test/java/test/br/com/zalf/prolog/webservice/pilares/geral/unidade/UnidadeIT.java
@@ -2,6 +2,9 @@ package test.br.com.zalf.prolog.webservice.pilares.geral.unidade;
 
 import br.com.zalf.prolog.webservice.geral.unidade.UnidadeDao;
 import br.com.zalf.prolog.webservice.geral.unidade._model.UnidadeVisualizacaoListagemDto;
+import org.assertj.core.api.AbstractAssert;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +13,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.UriComponentsBuilder;
 import test.br.com.zalf.prolog.webservice.IntegrationTest;
 
 import java.net.URI;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -41,5 +48,30 @@ public class UnidadeIT extends IntegrationTest {
         assertBaseValidations(response);
         assertThat(response.getBody().getCodUnidade())
                 .isEqualTo(TEST_UNIDADE_ID);
+    }
+
+    @Test
+    @DisplayName("Dado código da empresa e códigos de regionais, retorne uma lista de UnidadeVisualizacaoListagem.")
+    void givenCodUnidadeAndCodRegionais_ThenReturnListUnidadeVisualizacaoListagemAndStatusOk() {
+        final MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("codEmpresa", "3");
+        params.add("codsRegionais", "1");
+
+        final UriComponentsBuilder builder = UriComponentsBuilder
+                .fromHttpUrl(createPathWithPort(RESOURCE))
+                .queryParams(params);
+
+        final RequestEntity<Void> requestEntity = RequestEntity
+                .get(URI.create(builder.toUriString()))
+                .accept(MediaType.APPLICATION_JSON)
+                .build();
+
+        final ResponseEntity<List<UnidadeVisualizacaoListagemDto>> response = getTestRestTemplate()
+                .exchange(requestEntity, new ParameterizedTypeReference<List<UnidadeVisualizacaoListagemDto>>() {});
+
+        assertBaseValidations(response);
+        response.getBody().stream()
+                .map(Assertions::assertThat)
+                .forEach(AbstractAssert::isNotNull);
     }
 }
