42c0fb783a40473001edf556158edbf76ae7952b
didi <dioge.vanzella@gmail.com>
2021-03-01 22:05:51 -0300

    refactor(integração): descomenta método de listagem de veículo
    PL-2874

--- src/main/java/br/com/zalf/prolog/webservice/integracao/webfinatto/SistemaWebFinatto.java
+++ src/main/java/br/com/zalf/prolog/webservice/integracao/webfinatto/SistemaWebFinatto.java
@@ -234,27 +234,26 @@ public class SistemaWebFinatto extends Sistema {
     public List<VeiculoListagem> getVeiculosByUnidades(@NotNull final List<Long> codUnidades,
                                                        final boolean apenasAtivos,
                                                        @Nullable final Long codTipoVeiculo) throws Throwable {
-        return super.getVeiculosByUnidades(codUnidades, apenasAtivos, codTipoVeiculo);
-        //        Connection conn = null;
-        //        final DatabaseConnectionProvider connectionProvider = new DatabaseConnectionProvider();
-        //        try {
-        //            conn = connectionProvider.provideDatabaseConnection();
-        //            final UnidadeDeParaHolder unidadeDeParaHolder =
-        //                    integracaoDao.getCodAuxiliarByCodUnidadeProlog(conn, codUnidades);
-        //            final TipoVeiculoConfigAfericaoHolder tipoVeiculoConfigAfericaoHolder =
-        //                    integracaoDao.getTipoVeiculoConfigAfericaoHolder(conn,
-        //                                                                     unidadeDeParaHolder
-        //                                                                     .getCodUnidadesMapeadas());
-        //            final List<VeiculoWebFinatto> veiculosByFiliais =
-        //                    internalGetVeiculosByFiliais(conn,
-        //                                                 unidadeDeParaHolder.getCodEmpresaProlog(),
-        //                                                 unidadeDeParaHolder.getCodFiliais());
-        //            return SistemaWebFinattoConverter.createVeiculosListagem(unidadeDeParaHolder,
-        //                                                                     tipoVeiculoConfigAfericaoHolder,
-        //                                                                     veiculosByFiliais);
-        //        } finally {
-        //            connectionProvider.closeResources(conn);
-        //        }
+        Connection conn = null;
+        final DatabaseConnectionProvider connectionProvider = new DatabaseConnectionProvider();
+        try {
+            conn = connectionProvider.provideDatabaseConnection();
+            final UnidadeDeParaHolder unidadeDeParaHolder =
+                    integracaoDao.getCodAuxiliarByCodUnidadeProlog(conn, codUnidades);
+            final TipoVeiculoConfigAfericaoHolder tipoVeiculoConfigAfericaoHolder =
+                    integracaoDao.getTipoVeiculoConfigAfericaoHolder(conn,
+                                                                     unidadeDeParaHolder
+                                                                             .getCodUnidadesMapeadas());
+            final List<VeiculoWebFinatto> veiculosByFiliais =
+                    internalGetVeiculosByFiliais(conn,
+                                                 unidadeDeParaHolder.getCodEmpresaProlog(),
+                                                 unidadeDeParaHolder.getCodFiliais());
+            return SistemaWebFinattoConverter.createVeiculosListagem(unidadeDeParaHolder,
+                                                                     tipoVeiculoConfigAfericaoHolder,
+                                                                     veiculosByFiliais);
+        } finally {
+            connectionProvider.closeResources(conn);
+        }
     }
 
     @Override
