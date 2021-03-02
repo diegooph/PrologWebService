4e1b13e378260173e947a7964b1386c948e83082
Steinert999 <steinert999@outlook.com>
2021-03-02 13:53:29 -0300

    refactor(core): ajusta busca através de RequestEntity
    PL-3307

--- src/test/java/test/br/com/zalf/prolog/webservice/pilares/geral/unidade/UnidadeIT.java
+++ src/test/java/test/br/com/zalf/prolog/webservice/pilares/geral/unidade/UnidadeIT.java
@@ -1,11 +1,15 @@
 package test.br.com.zalf.prolog.webservice.pilares.geral.unidade;
 
+import br.com.zalf.prolog.webservice.geral.unidade.UnidadeDao;
 import br.com.zalf.prolog.webservice.geral.unidade._model.UnidadeVisualizacaoListagemDto;
-import lombok.var;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import test.br.com.zalf.prolog.webservice.IntegrationTest;
 
 import java.net.URI;
@@ -17,20 +21,17 @@ public class UnidadeIT extends IntegrationTest {
     @Test
     @DisplayName("Busca uma unidade através de um código.")
     void givenCodUnidadeToRequest_ThenReturnUnidadeVisualizacaoListagemAndStatusOK() {
-        final Long codUnidade = 5L;
-        final String path = createPathWithPort("unidades/" + codUnidade);
-        final var requestEntity = RequestEntity
-                .get(URI.create(path))
+
+        final RequestEntity<Void> requestEntity = RequestEntity
+                .get(URI.create(createPathWithPort(RESOURCE + TEST_UNIDADE_ID)))
                 .accept(MediaType.APPLICATION_JSON)
                 .build();
-        final ResponseEntity<UnidadeVisualizacaoListagemDto> response =
-                getTestRestTemplate().exchange(path,
-                                               HttpMethod.GET,
-                                               requestEntity,
-                                               ParameterizedTypeReference.forType(UnidadeVisualizacaoListagemDto.class));
 
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getBody()).isNotNull();
-        assertThat(response.getBody().getCodUnidade()).isEqualTo(codUnidade);
+        final ResponseEntity<UnidadeVisualizacaoListagemDto> response = getTestRestTemplate()
+                .exchange(requestEntity, ParameterizedTypeReference.forType(UnidadeVisualizacaoListagemDto.class));
+
+        assertBaseValidations(response);
+        assertThat(response.getBody().getCodUnidade())
+                .isEqualTo(TEST_UNIDADE_ID);
     }
 }
