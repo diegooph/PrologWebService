664a231ce658f1bb91ad099fc266e7df9eafcfb0
Steinert999 <steinert999@outlook.com>
2021-03-03 10:41:06 -0300

    refactor(core): altera instancia de JdbcContainer de estática para injeção de dependencia como bean
    PL-3307

--- src/test/java/test/br/com/zalf/prolog/webservice/TestContainer.java
+++ src/test/java/test/br/com/zalf/prolog/webservice/TestContainer.java
@@ -2,8 +2,10 @@ package test.br.com.zalf.prolog.webservice;
 
 import org.jetbrains.annotations.NotNull;
 import org.junit.ClassRule;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.support.TestPropertySourceUtils;
 import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -16,6 +18,7 @@ import org.testcontainers.utility.DockerImageName;
  * @author Guilherme Steinert (https://github.com/steinert999)
  */
 @Testcontainers
+@TestConfiguration
 public class TestContainer {
 
     private static final String TEST_DB_NAME = "prolog_test";
@@ -36,14 +39,15 @@ public class TestContainer {
     }
 
     @NotNull
-    public static JdbcDatabaseContainer<?> getContainer() {
-        return JDBC_BASE_CONTAINER;
-    }
-
-    @NotNull
     private static DockerImageName getDockerImageName() {
         return DockerImageName.parse(DOCKER_IMAGE_REPO)
                 .asCompatibleSubstituteFor("postgres");
+    }
+
+    @Bean
+    @NotNull
+    public JdbcDatabaseContainer<?> getContainer() {
+        return JDBC_BASE_CONTAINER;
     }
 
     static class DockerPostgresDatasourceInitializer
