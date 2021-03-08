34b387743bc7702d5f2d5a8bf18b6f035c82fed8
Steinert999 <steinert999@outlook.com>
2021-03-08 08:34:53 -0300

    feat(afericao): adiciona configurações de simulação de restTemplate para Aferição
    PL-3458

---
+++ src/test/java/test/br/com/zalf/prolog/webservice/pilares/frota/pneu/afericao/AfericaoApiClient.java
@@ -0,0 +1,77 @@
+package test.br.com.zalf.prolog.webservice.pilares.frota.pneu.afericao;
+
+import br.com.zalf.prolog.webservice.frota.pneu.afericao.v3._model.dto.AfericaoAvulsaDto;
+import br.com.zalf.prolog.webservice.frota.pneu.afericao.v3._model.dto.AfericaoPlacaDto;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestComponent;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+
+/**
+ * Created on 2021-03-05
+ *
+ * @author Guilherme Steinert (https://github.com/steinert999)
+ */
+@TestComponent
+public class AfericaoApiClient {
+
+    private static final String RESOURCE = "/v3/afericoes";
+
+    @Autowired
+    @NotNull
+    private TestRestTemplate restTemplate;
+
+    public ResponseEntity<List<AfericaoPlacaDto>> getAfericoesPlacas(final List<Long> codUnidades,
+                                                                    final String placaVeiculo,
+                                                                    final Long codTipoVeiculo,
+                                                                    final String dataInicial,
+                                                                    final String dataFinal,
+                                                                    final int limit,
+                                                                    final int offset) {
+
+        final UriComponents components = UriComponentsBuilder
+                .fromPath(RESOURCE)
+                .path("/placa")
+                .queryParam("unidades", codUnidades)
+                .queryParam("placa", placaVeiculo)
+                .queryParam("codTipoVeiculo", codTipoVeiculo)
+                .queryParam("dataInicial", dataInicial)
+                .queryParam("dataFinal", dataFinal)
+                .queryParam("limit", limit)
+                .queryParam("offset", offset)
+                .build();
+        final RequestEntity<Void> reqEntity = RequestEntity
+                .get(components.toUri())
+                .build();
+
+        return restTemplate.exchange(reqEntity, new ParameterizedTypeReference<List<AfericaoPlacaDto>>() {});
+    }
+
+    public ResponseEntity<List<AfericaoAvulsaDto>> getAfericoesAvulsas(final List<Long> codUnidades,
+                                                       final String dataInicial,
+                                                       final String dataFinal,
+                                                       final int limit,
+                                                       final int offset) {
+        final UriComponents components = UriComponentsBuilder
+                .fromPath(RESOURCE)
+                .path("/avulsa")
+                .queryParam("unidades", codUnidades)
+                .queryParam("dataInicial", dataInicial)
+                .queryParam("dataFinal", dataFinal)
+                .queryParam("limit", limit)
+                .queryParam("offset", offset)
+                .build();
+
+        final RequestEntity<Void> reqEntity = RequestEntity
+                .get(components.toUri())
+                .build();
+        return restTemplate.exchange(reqEntity, new ParameterizedTypeReference<List<AfericaoAvulsaDto>>() {});
+    }
+}
