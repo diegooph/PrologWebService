aaa2d122b46291126046978828edfc700faef167
Steinert999 <steinert999@outlook.com>
2021-03-15 14:59:34 -0300

    feat(pneu): adiciona implementação do metodo de inserção para PneuFotoService
    PL-3459

--- src/main/java/br/com/zalf/prolog/webservice/frota/pneu/v3/service/PneuFotoV3ServiceImpl.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/v3/service/PneuFotoV3ServiceImpl.java
@@ -3,10 +3,12 @@ package br.com.zalf.prolog.webservice.frota.pneu.v3.service;
 import br.com.zalf.prolog.webservice.frota.pneu.v3._model.PneuEntity;
 import br.com.zalf.prolog.webservice.frota.pneu.v3._model.PneuFotoEntity;
 import br.com.zalf.prolog.webservice.frota.pneu.v3.dao.PneuFotoV3Dao;
-import org.apache.commons.lang3.NotImplementedException;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created on 2021-03-12
@@ -28,6 +30,11 @@ public class PneuFotoV3ServiceImpl implements PneuFotoV3Service {
     @Override
     @NotNull
     public List<PneuFotoEntity> createPneuFotos(@NotNull final PneuEntity pneu, @NotNull final List<String> urls) {
-        throw new NotImplementedException("metodo não implementado até o momento.");
+        return urls.stream()
+                .map(url -> PneuFotoEntity.createPneuFotoEntity(pneu, url))
+                .filter(this::notCreated)
+                .map(dao::save)
+                .peek(foto -> Log.d(TAG, "Foi inserido a imagem: " + foto + " para o pneu de id: " + pneu.getId()))
+                .collect(Collectors.toList());
     }
 }
