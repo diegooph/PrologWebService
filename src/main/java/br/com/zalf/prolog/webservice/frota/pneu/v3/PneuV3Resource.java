c502f8e5b69fce351a79349da10227246e7853f2
Steinert999 <steinert999@outlook.com>
2021-03-15 14:54:12 -0300

    feat(pneu): adiciona dependencias para PneuFotoResource
    PL-3459

--- src/main/java/br/com/zalf/prolog/webservice/frota/pneu/v3/PneuV3Resource.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/v3/PneuV3Resource.java
@@ -1,8 +1,12 @@
 package br.com.zalf.prolog.webservice.frota.pneu.v3;
 
 import br.com.zalf.prolog.webservice.frota.pneu.v3._model.dto.PneuCadastro;
+import br.com.zalf.prolog.webservice.frota.pneu.v3.mapper.PneuMapper;
+import br.com.zalf.prolog.webservice.frota.pneu.v3.service.PneuFotoV3Service;
+import br.com.zalf.prolog.webservice.frota.pneu.v3.service.PneuV3Service;
 import br.com.zalf.prolog.webservice.interceptors.debug.ConsoleDebugLog;
-import org.apache.commons.lang3.NotImplementedException;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import javax.ws.rs.Consumes;
@@ -23,6 +27,21 @@ import javax.ws.rs.core.Response;
 @Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
 @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
 public class PneuV3Resource implements PneuV3ApiDoc {
+
+    private static final String TAG = PneuV3Resource.class.getSimpleName();
+
+    private final PneuV3Service service;
+    private final PneuFotoV3Service pneuFotoService;
+    private final PneuMapper<PneuEntity, PneuCadastro> pneuCadastroMapper;
+
+    @Autowired
+    public PneuV3Resource(@NotNull final PneuV3Service service,
+                          @NotNull final PneuMapper<PneuEntity, PneuCadastro> pneuCadastroMapper,
+                          @NotNull final PneuFotoV3Service pneuFotoService) {
+        this.service = service;
+        this.pneuCadastroMapper = pneuCadastroMapper;
+        this.pneuFotoService = pneuFotoService;
+    }
 
     @POST
     @Override
