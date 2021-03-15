ab5d16cc12fd90354955eff42aaf23905dd49cfe
Steinert999 <steinert999@outlook.com>
2021-03-15 14:31:35 -0300

    feat(pneu): adiciona mapper para PneuCadastro
    PL-3459

---
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/v3/mapper/PneuCadastroMapper.java
@@ -0,0 +1,63 @@
+package br.com.zalf.prolog.webservice.frota.pneu.v3.mapper;
+
+import br.com.zalf.prolog.webservice.frota.pneu.relatorios._model.SulcoPressao;
+import br.com.zalf.prolog.webservice.frota.pneu.v3._model.PneuEntity;
+import br.com.zalf.prolog.webservice.frota.pneu.v3._model.dto.PneuCadastro;
+import org.apache.commons.lang3.NotImplementedException;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.LocalDateTime;
+
+/**
+ * Created on 2021-03-15
+ *
+ * @author Guilherme Steinert (https://github.com/steinert999)
+ */
+@Component
+public class PneuCadastroMapper implements PneuMapper<PneuEntity, PneuCadastro> {
+
+    @Override
+    @NotNull
+    public PneuCadastro toDto(@NotNull final PneuEntity entity) {
+        throw new NotImplementedException("Não há necessidade de implementação deste metodo.");
+    }
+
+    @Override
+    @NotNull
+    public PneuEntity toEntity(@NotNull final PneuCadastro dto) {
+        return PneuEntity.builder()
+                .dadosCadastro(getDadosCadastro(dto))
+                .pressao(getPressao(dto))
+                .codCliente(dto.getCodCliente())
+                .codUnidade(dto.getCodUnidade())
+                .codEmpresa(dto.getCodEmpresa())
+                .codEmpresa(dto.getCodEmpresa())
+                .codModelo(dto.getCodModeloBanda())
+                .vidaAtual(BigInteger.valueOf(dto.getVidaAtual()))
+                .vidaTotal(BigInteger.valueOf(dto.getVidaTotal()))
+                .valor(BigDecimal.valueOf(dto.getCustoAquisicao()))
+                .dot(dto.getDot())
+                .usado(dto.isPneuUsado())
+                .build();
+
+    }
+
+    @NotNull
+    private PneuEntity.Pressao getPressao(@NotNull final PneuCadastro dto) {
+        return PneuEntity.Pressao.builder()
+                .recomendada(BigDecimal.valueOf(dto.getPressaoRecomendada()))
+                .build();
+    }
+
+    @NotNull
+    private PneuEntity.DadosCadastro getDadosCadastro(@NotNull final PneuCadastro dto) {
+        return PneuEntity.DadosCadastro.builder()
+                .codUnidade(dto.getCodUnidade())
+                .dataInclusao(LocalDateTime.now())
+                .build();
+    }
+
+}
