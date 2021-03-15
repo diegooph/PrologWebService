7d8ea201b062865861f056909935aa9b04d049b3
Steinert999 <steinert999@outlook.com>
2021-03-15 14:25:24 -0300

    style(pneu): adiciona @NotNull na assinatura do método
    PL-3459

--- src/main/java/br/com/zalf/prolog/webservice/frota/pneu/v3/service/PneuV3Service.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/v3/service/PneuV3Service.java
@@ -1,6 +1,7 @@
 package br.com.zalf.prolog.webservice.frota.pneu.v3.service;
 
 import br.com.zalf.prolog.webservice.frota.pneu.v3._model.PneuEntity;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Created on 2021-03-12
@@ -8,5 +9,7 @@ import br.com.zalf.prolog.webservice.frota.pneu.v3._model.PneuEntity;
  * @author Guilherme Steinert (https://github.com/steinert999)
  */
 public interface PneuV3Service {
-    PneuEntity create(PneuEntity pneu);
+
+    @NotNull
+    PneuEntity create(@NotNull final PneuEntity pneu);
 }
