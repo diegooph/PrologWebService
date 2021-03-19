c6f2110a88a34ebd06b3e818f2656ad4efe0bdeb
Steinert999 <steinert999@outlook.com>
2021-03-19 17:48:58 -0300

    feat(pneu): adiciona base implementação testes
    PL-3459

---
+++ src/test/java/test/br/com/zalf/prolog/webservice/pilares/frota/pneu/v3/PneuApiClient.java
@@ -0,0 +1,32 @@
+package test.br.com.zalf.prolog.webservice.pilares.frota.pneu.v3;
+
+import br.com.zalf.prolog.webservice.frota.pneu.v3._model.dto.PneuCadastroDto;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestComponent;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import java.net.URI;
+
+/**
+ * Created on 2021-03-16
+ *
+ * @author Guilherme Steinert (https://github.com/steinert999)
+ */
+@TestComponent
+public class PneuApiClient {
+    private static final String RESOURCE = "/v3/pneus";
+
+    @Autowired
+    @NotNull
+    private TestRestTemplate restTemplate;
+
+    public ResponseEntity<String> insert(final PneuCadastroDto dto) {
+        return insert(dto, String.class);
+    }
+
+    public <T> ResponseEntity<T> insert(final PneuCadastroDto dto, final Class<T> responseType) {
+        return restTemplate.postForEntity(URI.create(RESOURCE), dto, responseType);
+    }
+}
