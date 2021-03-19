3f6600b646de011fe7522161a7270fbb4f2231e6
Steinert999 <steinert999@outlook.com>
2021-03-19 16:11:01 -0300

    feat(pneu): adiciona dependencias do service de PneuServico
    PL-3459

--- src/main/java/br/com/zalf/prolog/webservice/frota/pneu/v3/service/servico/PneuServicoV3ServiceImpl.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/v3/service/servico/PneuServicoV3ServiceImpl.java
@@ -2,9 +2,15 @@ package br.com.zalf.prolog.webservice.frota.pneu.v3.service.servico;
 
 import br.com.zalf.prolog.webservice.frota.pneu.v3._model.PneuEntity;
 import br.com.zalf.prolog.webservice.frota.pneu.v3._model.servico.PneuServicoEntity;
-import org.apache.commons.lang3.NotImplementedException;
+import br.com.zalf.prolog.webservice.frota.pneu.v3._model.servico.PneuTipoServicoEntity;
+import br.com.zalf.prolog.webservice.frota.pneu.v3.dao.servico.PneuServicoV3Dao;
+import lombok.var;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.math.BigDecimal;
 
 /**
  * Created on 2021-03-18
@@ -13,6 +19,19 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class PneuServicoV3ServiceImpl implements PneuServicoV3Service {
+
+    private final PneuServicoV3Dao dao;
+    private final PneuTipoServicoV3Service tipoServicoService;
+    private final PneuServicoHistoricoV3Service pneuServicoHistoricoService;
+
+    @Autowired
+    public PneuServicoV3ServiceImpl(@NotNull final PneuServicoV3Dao dao,
+                                    @NotNull final PneuTipoServicoV3Service tipoServicoService,
+                                    @NotNull final PneuServicoHistoricoV3Service pneuServicoHistoricoService) {
+        this.dao = dao;
+        this.tipoServicoService = tipoServicoService;
+        this.pneuServicoHistoricoService = pneuServicoHistoricoService;
+    }
 
     @Override
     @NotNull
