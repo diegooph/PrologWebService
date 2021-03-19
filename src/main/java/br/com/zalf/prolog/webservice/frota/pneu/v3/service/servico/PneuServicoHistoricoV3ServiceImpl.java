b094516f14c820a6a3cb4deffd36ef0d1034dc1a
Steinert999 <steinert999@outlook.com>
2021-03-19 16:27:50 -0300

    feat(pneu): adiciona metodo createHistoricoCadastro
    PL-3459

--- src/main/java/br/com/zalf/prolog/webservice/frota/pneu/v3/service/servico/PneuServicoHistoricoV3ServiceImpl.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/v3/service/servico/PneuServicoHistoricoV3ServiceImpl.java
@@ -1,6 +1,8 @@
 package br.com.zalf.prolog.webservice.frota.pneu.v3.service.servico;
 
+import br.com.zalf.prolog.webservice.database._model.DadosDelecao;
 import br.com.zalf.prolog.webservice.frota.pneu.v3._model.servico.PneuServicoEntity;
+import br.com.zalf.prolog.webservice.frota.pneu.v3._model.servico.PneuServicoHistoricoCadastroEntity;
 import br.com.zalf.prolog.webservice.frota.pneu.v3.dao.servico.ServicoHistoricoCadastroV3Dao;
 import br.com.zalf.prolog.webservice.frota.pneu.v3.dao.servico.ServicoHistoricoVidaV3Dao;
 import org.jetbrains.annotations.NotNull;
@@ -39,5 +41,15 @@ public class PneuServicoHistoricoV3ServiceImpl implements PneuServicoHistoricoV3
     private boolean isCadastro(final PneuServicoEntity.FonteServico fonteServico) {
         return fonteServico.equals(PneuServicoEntity.FonteServico.CADASTRO);
     }
-}
 
+    private PneuServicoHistoricoCadastroEntity createHistoricoCadastro(final PneuServicoEntity pneuServico) {
+        final PneuServicoHistoricoCadastroEntity.Id id = PneuServicoHistoricoCadastroEntity.Id.builder()
+                .pneu(pneuServico.getPneu())
+                .servico(pneuServico)
+                .build();
+        return PneuServicoHistoricoCadastroEntity.builder()
+                .id(id)
+                .dadosDelecao(DadosDelecao.createDefaultDadosDelecao())
+                .build();
+    }
+}
