3c45533bcc85c3e7fe5fef33fc0347fcae7eaa96
didi <dioge.vanzella@gmail.com>
2021-03-19 16:58:49 -0300

    refactor(veículo): adiciona chamada para outros serives dentro do insert do veículo
    PL-3460

--- src/main/java/br/com/zalf/prolog/webservice/frota/veiculo/v3/VeiculoV3Service.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/veiculo/v3/VeiculoV3Service.java
@@ -3,10 +3,16 @@ package br.com.zalf.prolog.webservice.frota.veiculo.v3;
 import br.com.zalf.prolog.webservice.Injection;
 import br.com.zalf.prolog.webservice.commons.network.SuccessResponse;
 import br.com.zalf.prolog.webservice.commons.util.Log;
+import br.com.zalf.prolog.webservice.frota.veiculo.tipoveiculo.v3.TipoVeiculoV3Service;
+import br.com.zalf.prolog.webservice.frota.veiculo.tipoveiculo.v3._model.TipoVeiculoEntity;
 import br.com.zalf.prolog.webservice.frota.veiculo.v3._model.VeiculoEntity;
+import br.com.zalf.prolog.webservice.frota.veiculo.v3.diagrama.DiagramaService;
+import br.com.zalf.prolog.webservice.frota.veiculo.v3.diagrama._model.DiagramaEntity;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 public class VeiculoV3Service {
@@ -14,16 +20,32 @@ public class VeiculoV3Service {
     private static final String TAG = VeiculoV3Service.class.getSimpleName();
     @NotNull
     private final VeiculoV3Dao veiculoDao;
+    @NotNull
+    private final TipoVeiculoV3Service tipoVeiculoService;
+    @NotNull
+    private final DiagramaService diagramaService;
 
     @Autowired
-    public VeiculoV3Service(@NotNull final VeiculoV3Dao veiculoDao) {
+    public VeiculoV3Service(@NotNull final VeiculoV3Dao veiculoDao,
+                            @NotNull final TipoVeiculoV3Service tipoVeiculoService,
+                            @NotNull final DiagramaService diagramaService) {
         this.veiculoDao = veiculoDao;
+        this.tipoVeiculoService = tipoVeiculoService;
+        this.diagramaService = diagramaService;
     }
 
     @NotNull
+    @Transactional
     public SuccessResponse insert(@NotNull final VeiculoEntity veiculoEntity) {
         try {
-            final VeiculoEntity saved = veiculoDao.save(veiculoEntity);
+            final TipoVeiculoEntity tipoVeiculoEntity = tipoVeiculoService.getByCod(veiculoEntity.getCodTipo());
+            final DiagramaEntity diagramaEntity = diagramaService.getByCod(tipoVeiculoEntity.getCodDiagrama());
+            final VeiculoEntity veiculoInsert = veiculoEntity.toBuilder()
+                    .withMotorizado(diagramaEntity.isMotorizado())
+                    .withCodDiagrama(tipoVeiculoEntity.getCodDiagrama().longValue())
+                    .build();
+
+            final VeiculoEntity saved = veiculoDao.save(veiculoInsert);
             return new SuccessResponse(saved.getCodigo(), "Veículo inserido com sucesso.");
         } catch (final Throwable t) {
             Log.e(TAG, "Erro ao inserir veículo.", t);
