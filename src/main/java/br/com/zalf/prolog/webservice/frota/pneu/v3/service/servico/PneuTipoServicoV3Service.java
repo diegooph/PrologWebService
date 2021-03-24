a104281d7a1bf5fa466b1d3d8abe17feadd338a6
Steinert999 <steinert999@outlook.com>
2021-03-24 16:08:45 -0300

    refactor(pneu): adiciona implementação direta do PneuTipoServicoService no PneuServicoService
    PL-3459

--- src/main/java/br/com/zalf/prolog/webservice/frota/pneu/v3/service/servico/PneuTipoServicoV3Service.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/v3/service/servico/PneuTipoServicoV3Service.java
@@ -1,15 +1,31 @@
 package br.com.zalf.prolog.webservice.frota.pneu.v3.service.servico;
 
 import br.com.zalf.prolog.webservice.frota.pneu.v3._model.servico.PneuTipoServicoEntity;
+import br.com.zalf.prolog.webservice.frota.pneu.v3.dao.servico.PneuTipoServicoV3Dao;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 /**
  * Created on 2021-03-18
  *
  * @author Guilherme Steinert (https://github.com/steinert999)
  */
-public interface PneuTipoServicoV3Service {
+@Service
+public class PneuTipoServicoV3Service {
+
+    private final PneuTipoServicoV3Dao dao;
+
+    @Autowired
+    public PneuTipoServicoV3Service(@NotNull final PneuTipoServicoV3Dao dao) {
+        this.dao = dao;
+    }
 
     @NotNull
-    PneuTipoServicoEntity getInitialTipoServicoForVidaIncrementada();
+    @Transactional
+    public PneuTipoServicoEntity getInitialTipoServicoForVidaIncrementada() {
+        return this.dao.getInitialTipoServicoForVidaIncrementada();
+    }
 }
