303f148a5ff22b4cb06bef9accc2d2c63638b2b8
Steinert999 <steinert999@outlook.com>
2021-03-25 16:46:04 -0300

    refactor(pneu): adiciona composite key de forma embedded
    PL-3459

--- src/main/java/br/com/zalf/prolog/webservice/frota/pneu/v3/_model/servico/PneuServicoRealizadoEntity.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/pneu/v3/_model/servico/PneuServicoRealizadoEntity.java
@@ -1,28 +1,30 @@
 package br.com.zalf.prolog.webservice.frota.pneu.v3._model.servico;
 
-import br.com.zalf.prolog.webservice.database._model.BaseEntity;
 import br.com.zalf.prolog.webservice.frota.pneu.v3._model.PneuEntity;
 import br.com.zalf.prolog.webservice.frota.pneu.v3._model.servico.converters.FonteServicoPneuConverter;
 import lombok.*;
-import org.jetbrains.annotations.NotNull;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.math.BigDecimal;
-import java.util.stream.Stream;
+import java.util.Objects;
 
 /**
  * Created on 2021-03-16
  *
  * @author Guilherme Steinert (https://github.com/steinert999)
  */
-@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "pneu_servico_realizado", schema = "public")
 @Builder
-@Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class PneuServicoRealizadoEntity extends BaseEntity {
+@Getter
+@EqualsAndHashCode
+public class PneuServicoRealizadoEntity {
+
+    @EmbeddedId
+    private Key key;
 
     @ManyToOne(fetch = FetchType.LAZY,
                targetEntity = PneuTipoServicoEntity.class)
@@ -75,7 +77,27 @@ public class PneuServicoRealizadoEntity extends BaseEntity {
     }
 
     public boolean isCadastro() {
-        return this.fonteServico.equals(FonteServico.CADASTRO);
+        return Objects.equals(key.fonteServicoRealizado, FonteServico.CADASTRO);
     }
 
+    @Embeddable
+    @Builder
+    @AllArgsConstructor
+    @Getter
+    @NoArgsConstructor
+    @EqualsAndHashCode
+    public static class Key implements Serializable {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.SEQUENCE,
+                        generator = "pneu_servico_realizado_generator")
+        @SequenceGenerator(name = "pneu_servico_realizado_generator",
+                           sequenceName = "pneu_servico_realizado_data_codigo_seq")
+        @Column(name = "codigo", nullable = false, unique = true)
+        private Long id;
+
+        @Convert(converter = FonteServicoPneuConverter.class)
+        @Column(name = "fonte_servico_realizado", nullable = false, length = 20)
+        private FonteServico fonteServicoRealizado;
+    }
 }
