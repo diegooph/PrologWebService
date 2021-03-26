bd9422b16cf23ce0619986de817ebbe32a7698b6
Luiz Felipe <luiz.felipe_95@hotmail.com>
2021-03-26 09:11:11 -0300

    feat(veículo): cria entities parciais para atualizar KM
    PL-3557

---
+++ src/main/java/br/com/zalf/prolog/webservice/frota/kmprocessos/entities/ChecklistOrdemServicoItemEntity.java
@@ -0,0 +1,30 @@
+package br.com.zalf.prolog.webservice.frota.kmprocessos.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ * Created on 2021-03-25
+ *
+ * @author Luiz Felipe (https://github.com/luizfp)
+ */
+@Builder(toBuilder = true, setterPrefix = "with")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+@Table(schema = "public", name = "checklist_ordem_servico_itens")
+public final class ChecklistOrdemServicoItemEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo", nullable = false)
+    private Long codigo;
+    @Column(name = "cod_unidade", nullable = false)
+    private Long codUnidade;
+    @Column(name = "km_veiculo", nullable = false)
+    private long kmColetadoVeiculo;
+}
---
+++ src/main/java/br/com/zalf/prolog/webservice/frota/kmprocessos/entities/ChecklistV3Dao.java
@@ -0,0 +1,12 @@
+package br.com.zalf.prolog.webservice.frota.kmprocessos.entities;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created on 2021-03-25
+ *
+ * @author Luiz Felipe (https://github.com/luizfp)
+ */
+public interface ChecklistV3Dao extends JpaRepository<ChecklistEntity, Long> {
+
+}
