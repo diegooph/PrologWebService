263133dc1835d18dc8e36457aa06afac8f6c7a3d
Luiz Felipe <luiz.felipe_95@hotmail.com>
2021-03-26 09:49:26 -0300

    feat(aferição): cria estrutura de entity/service/dao
    PL-3557

---
+++ src/main/java/br/com/zalf/prolog/webservice/v3/frota/afericao/AfericaoService.java
@@ -0,0 +1,27 @@
+package br.com.zalf.prolog.webservice.v3.frota.afericao;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created on 2021-03-26
+ *
+ * @author Luiz Felipe (https://github.com/luizfp)
+ */
+@Service
+public class AfericaoService {
+
+    @NotNull
+    private final AfericaoDao afericaoDao;
+
+    @Autowired
+    public AfericaoService(@NotNull final AfericaoDao afericaoDao) {
+        this.afericaoDao = afericaoDao;
+    }
+
+    @NotNull
+    public AfericaoEntity getByCodigo(@NotNull final Long codigo) {
+        return afericaoDao.getOne(codigo);
+    }
+}
