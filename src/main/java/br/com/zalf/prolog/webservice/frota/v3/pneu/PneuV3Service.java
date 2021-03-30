07eb267d3d95589fa540581c655c5954a7b53bd2
didi <dioge.vanzella@gmail.com>
2021-03-30 11:37:57 -0300

    refactor(pneu): bloqueia empresas e unidades que tem integração
    PL-3459

--- src/main/java/br/com/zalf/prolog/webservice/frota/v3/pneu/PneuV3Service.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/v3/pneu/PneuV3Service.java
@@ -7,7 +7,7 @@ import br.com.zalf.prolog.webservice.frota.v3.pneu._model.PneuCadastroDto;
 import br.com.zalf.prolog.webservice.frota.v3.pneu._model.PneuEntity;
 import br.com.zalf.prolog.webservice.frota.v3.pneu.pneuservico.PneuServicoV3Service;
 import br.com.zalf.prolog.webservice.frota.veiculo.historico._model.OrigemAcaoEnum;
-import br.com.zalf.prolog.webservice.interceptors.v3.OperacoesBloqueadasYaml;
+import br.com.zalf.prolog.webservice.integracao.OperacoesBloqueadasYaml;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -49,11 +49,13 @@ public class PneuV3Service {
     public SuccessResponse insert(@Nullable final String tokenIntegracao,
                                   @NotNull final PneuCadastroDto pneuCadastroDto) {
         try {
-            final PneuEntity pneuEntity = this.pneuCadastroMapper.toEntity(pneuCadastroDto);
+            operacoesBloqueadas.validateEmpresaUnidadeBloqueada(pneuCadastroDto.getCodEmpresaAlocado(),
+                                                                pneuCadastroDto.getCodUnidadeAlocado());
+            final PneuEntity pneuEntity = pneuCadastroMapper.toEntity(pneuCadastroDto);
             final PneuEntity pneuInsert = pneuEntity.toBuilder()
                     .origemCadastro(getOrigemCadastro(tokenIntegracao))
                     .build();
-            final PneuEntity savedPneu = this.pneuDao.save(pneuInsert);
+            final PneuEntity savedPneu = pneuDao.save(pneuInsert);
             if (savedPneu.isRecapado()) {
                 //noinspection ConstantConditions
                 this.pneuServicoService.insertServicoCadastroPneu(savedPneu, pneuCadastroDto.getValorBandaPneu());
