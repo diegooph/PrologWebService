562f45b43b5dff4acd9aded326914fc1fcad8a21
didi <dioge.vanzella@gmail.com>
2021-03-30 14:36:49 -0300

    refactor(pneu): adiciona javax validations no DTO de pneu
    PL-3459

--- src/main/java/br/com/zalf/prolog/webservice/frota/v3/pneu/_model/PneuCadastroDto.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/v3/pneu/_model/PneuCadastroDto.java
@@ -3,8 +3,7 @@ package br.com.zalf.prolog.webservice.frota.v3.pneu._model;
 import lombok.Value;
 
 import javax.annotation.Nullable;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 
 /**
@@ -24,20 +23,26 @@ public class PneuCadastroDto {
     Long codModeloPneu;
     @NotNull(message = "O código de dimensão não pode ser nulo.")
     Long codDimensaoPneu;
-    @NotNull
+    @Min(value = 1, message = "A vida atual deve ser um valor entre 1 e 11.")
+    @Max(value = 11, message = "A vida atual deve ser um valor entre 1 e 11.")
     Integer vidaAtualPneu;
-    @NotNull
+    @Min(value = 1, message = "A vida total deve ser um valor entre 1 e 11.")
+    @Max(value = 11, message = "A vida total deve ser um valor entre 1 e 11.")
     Integer vidaTotalPneu;
+    @PositiveOrZero(message = "A pressão recomendada não pode ser um valor negativo.")
     @NotNull(message = "A pressão recomendada não pode ser nula.")
     Double pressaoRecomendada;
-    @Size(max = 20, message = "O DOT pode conter no máximo 20 caracteres")
+    @Size(max = 20, message = "O DOT pode conter no máximo 20 caracteres.")
+    @Nullable
     String dotPneu;
-    @NotNull(message = "O custo de aquisição não pode ser nulo.")
+    @PositiveOrZero(message = "O valor do pneu não pode ser um valor negativo.")
+    @NotNull(message = "O valor do pneu não pode ser nulo.")
     BigDecimal valorPneu;
-    @NotNull
+    @NotNull(message = "A flag 'pneuNovoNuncaRodado' é obrigatória.")
     Boolean pneuNovoNuncaRodado;
     @Nullable
     Long codModeloBanda;
+    @PositiveOrZero(message = "O valor da banda do pneu não pode ser um valor negativo.")
     @Nullable
     BigDecimal valorBandaPneu;
 }
