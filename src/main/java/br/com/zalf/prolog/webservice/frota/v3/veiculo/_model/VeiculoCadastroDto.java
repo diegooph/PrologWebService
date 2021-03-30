cbe81db2a190e61b0c6dde38a6d7169f4b530b17
didi <dioge.vanzella@gmail.com>
2021-03-30 18:47:04 -0300

    refactor(veiculo): melhora descrição dos javax validations
    PL-3460

--- src/main/java/br/com/zalf/prolog/webservice/frota/v3/veiculo/_model/VeiculoCadastroDto.java
+++ src/main/java/br/com/zalf/prolog/webservice/frota/v3/veiculo/_model/VeiculoCadastroDto.java
@@ -14,20 +14,21 @@ import javax.validation.constraints.Size;
  */
 @Data
 public final class VeiculoCadastroDto {
-    @NotNull(message = "O código da empresa é obrigatório.")
+    @NotNull(message = "O código da empresa não pode ser nulo.")
     private final Long codEmpresaAlocado;
-    @NotNull(message = "O código da unidade é obrigatório.")
+    @NotNull(message = "O código da unidade não pode ser nulo.")
     private final Long codUnidadeAlocado;
     @Size(min = 1, max = 7, message = "A placa deve conter entre 1 e 7 caracteres.")
+    @NotNull(message = "A placa do veículo não pode ser nulo.")
     private final String placaVeiculo;
     @Nullable
     private final String identificadorFrota;
-    @NotNull(message = "O código do modelo do veículo é obrigatório.")
+    @NotNull(message = "O código do modelo do veículo não pode ser nulo.")
     private final Long codModeloVeiculo;
-    @NotNull(message = "O código do tipo do veículo é obrigatório.")
+    @NotNull(message = "O código do tipo do veículo não pode ser nulo.")
     private final Long codTipoVeiculo;
     @PositiveOrZero(message = "O KM fornecido não pode ser um número negativo.")
     private final long kmAtualVeiculo;
-    @NotNull(message = "A flag 'possui hubodômetro' é obrigatória.")
+    @NotNull(message = "A flag 'possui hubodômetro' não pode ser nulo.")
     private final Boolean possuiHubodometro;
 }
