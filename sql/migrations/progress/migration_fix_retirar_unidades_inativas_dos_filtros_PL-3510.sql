5fb2c77d0aa0c1ae81b97fbbf8c8d7dd7ec5f314
gustavocnp95 <c98jjkdhgy>
2021-04-07 10:19:07 -0300

    fix(core): adiciona trecho de codigo para atualizar as unidaeds
    PL-3510

--- sql/migrations/progress/migration_fix_retirar_unidades_inativas_dos_filtros_PL-3510.sql
+++ sql/migrations/progress/migration_fix_retirar_unidades_inativas_dos_filtros_PL-3510.sql
@@ -34,3 +34,15 @@ create
     on unidade
     for each row
 execute procedure tg_valida_inativacao_unidade();
+
+-- Realiza update de unidades que estão inativas mas tem os dados que não pode deixaar inativar
+update unidade
+set status_ativo = true
+where codigo in (select codigo
+                 from unidade u
+                 where u.status_ativo = false
+                   and (((select count(*) from colaborador_data cd where cd.cod_unidade = u.codigo) > 0)
+                     or ((select count(*) from veiculo_data v where v.cod_unidade = u.codigo and v.deletado = false) >
+                         0)
+                     or ((select count(*) from pneu_data p where p.cod_unidade = u.codigo and status <> 'DESCARTE') >
+                         0)));
