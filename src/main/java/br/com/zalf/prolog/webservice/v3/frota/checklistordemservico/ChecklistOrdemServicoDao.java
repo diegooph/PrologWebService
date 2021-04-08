43a41b16cb0e91b642acf37e5031713f1573e8ce
gustavocnp95 <gustavocnavarro@gmail.com>
2021-04-08 18:15:28 -0300

    refactor(servico): cria primeira chamada de function
    PL-3462

--- src/main/java/br/com/zalf/prolog/webservice/v3/frota/checklistordemservico/ChecklistOrdemServicoDao.java
+++ src/main/java/br/com/zalf/prolog/webservice/v3/frota/checklistordemservico/ChecklistOrdemServicoDao.java
@@ -2,7 +2,11 @@ package br.com.zalf.prolog.webservice.v3.frota.checklistordemservico;
 
 import br.com.zalf.prolog.webservice.v3.frota.checklistordemservico._model.ChecklistOrdemServicoEntity;
 import br.com.zalf.prolog.webservice.v3.frota.checklistordemservico._model.ChecklistOrdemServicoPk;
+import br.com.zalf.prolog.webservice.v3.frota.checklistordemservico._model.ChecklistOrdemServicoProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /**
  * Created on 2021-03-26
@@ -10,5 +14,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author Luiz Felipe (https://github.com/luizfp)
  */
 public interface ChecklistOrdemServicoDao extends JpaRepository<ChecklistOrdemServicoEntity, ChecklistOrdemServicoPk> {
-
+    @Query(value = "select * from func_checklist_ordem_servico_listagem(:codUnidade);", nativeQuery = true)
+    List<ChecklistOrdemServicoProjection> getOrdensServico(final long codUnidade);
 }
