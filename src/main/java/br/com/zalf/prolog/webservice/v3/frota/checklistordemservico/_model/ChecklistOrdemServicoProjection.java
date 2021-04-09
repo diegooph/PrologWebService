9c294f8f1b964800dc4a6ffd10a3a7ee4eb634ac
gustavocnp95 <gustavocnavarro@gmail.com>
2021-04-09 10:34:17 -0300

    refactor(servico): adiciona mais informaçoes na projection
    PL-3462

--- src/main/java/br/com/zalf/prolog/webservice/v3/frota/checklistordemservico/_model/ChecklistOrdemServicoProjection.java
+++ src/main/java/br/com/zalf/prolog/webservice/v3/frota/checklistordemservico/_model/ChecklistOrdemServicoProjection.java
@@ -1,5 +1,6 @@
 package br.com.zalf.prolog.webservice.v3.frota.checklistordemservico._model;
 
+import br.com.zalf.prolog.webservice.frota.checklist.ordemservico.model.StatusItemOrdemServico;
 import org.springframework.beans.factory.annotation.Value;
 
 import java.time.LocalDateTime;
@@ -22,4 +23,49 @@ public interface ChecklistOrdemServicoProjection {
 
     @Value("#{target.data_hora_fechamento}")
     LocalDateTime getDataHoraFechamento();
+
+    @Value("#{target.codigo_item_os}")
+    long getCodigoItemOs();
+
+    @Value("#{target.cpf_mecanico}")
+    Long getCodigoCpfMecanico();
+
+    @Value("#{target.codigo_pergunta_primeiro_apontamento}")
+    long getCodigoPerguntaPrimeiroApontamento();
+
+    @Value("#{target.codigo_contexto_pergunta}")
+    long getCodigoContextoPergunta();
+
+    @Value("#{target.codigo_alternativa_primeiro_apontamento}")
+    long getCodigoAlternativaPrimeiroApontamento();
+
+    @Value("#{target.codigo_contexto_alternativa}")
+    long getCodigoContextoAlternativa();
+
+    @Value("#{target.status_resolucao}")
+    StatusItemOrdemServico getStatusItemOs();
+
+    @Value("#{target.quantidade_apontamentos}")
+    int getQuantidadeApontamentos();
+
+    @Value("#{target.km}")
+    Long getKm();
+
+    @Value("#{target.codigo_agrupamento_resolucao_em_lote}")
+    Long getCodigoAgrupamentoResolucaoEmLote();
+
+    @Value("#{target.data_hora_conserto}")
+    LocalDateTime getDataHoraConserto();
+
+    @Value("#{target.data_hora_inicio_resolucao}")
+    LocalDateTime getDataHoraInicioResolucao();
+
+    @Value("#{target.data_hora_fim_resolucao}")
+    LocalDateTime getDataHoraFimResolucao();
+
+    @Value("#{target.tempo_realizacao}")
+    Long getTempoRealizacao();
+
+    @Value("#{target.feedback_conserto}")
+    String getFeedbackConserto();
 }
