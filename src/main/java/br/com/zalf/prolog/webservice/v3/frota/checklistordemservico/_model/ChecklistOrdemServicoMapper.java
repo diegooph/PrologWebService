5962c8e09e9397e74529c4d8f15aee7c338d5da1
gustavocnp95 <gustavocnavarro@gmail.com>
2021-04-09 12:11:42 -0300

    refactor(servico): cria mapper
    PL-3462

--- src/main/java/br/com/zalf/prolog/webservice/v3/frota/checklistordemservico/_model/ChecklistOrdemServicoMapper.java
+++ src/main/java/br/com/zalf/prolog/webservice/v3/frota/checklistordemservico/_model/ChecklistOrdemServicoMapper.java
@@ -1,29 +1,53 @@
 package br.com.zalf.prolog.webservice.v3.frota.checklistordemservico._model;
 
+import br.com.zalf.prolog.webservice.frota.checklist.ordemservico.model.StatusItemOrdemServico;
 import br.com.zalf.prolog.webservice.frota.checklist.ordemservico.model.StatusOrdemServico;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public final class ChecklistOrdemServicoMapper {
     @NotNull
     public List<ChecklistOrdemServicoListagemDto> toDto(
             @NotNull final List<ChecklistOrdemServicoProjection> ordensServico) {
-        return ordensServico
-                .stream()
-                .map(this::toDto)
-                .collect(Collectors.toList());
-    }
+        if (ordensServico.isEmpty()) {
+            return Collections.emptyList();
+        }
 
-    @NotNull
-    private ChecklistOrdemServicoListagemDto toDto(@NotNull final ChecklistOrdemServicoProjection projection) {
-        return new ChecklistOrdemServicoListagemDto(projection.getCodigoOs(),
-                projection.getCodigoUnidade(),
-                projection.getCodigoChecklist(),
-                StatusOrdemServico.fromString(projection.getStatusOs()),
-                projection.getDataHoraFechamento());
+        final List<ChecklistOrdemServicoListagemDto> ordensDto = new ArrayList<>();
+        long osAntiga = 0;
+        for (final ChecklistOrdemServicoProjection osProjection : ordensServico) {
+            if (osAntiga != osProjection.getCodigoOs()) {
+                ordensDto.add(new ChecklistOrdemServicoListagemDto(osProjection.getCodigoOs(),
+                        osProjection.getCodigoUnidade(),
+                        osProjection.getCodigoChecklist(),
+                        StatusOrdemServico.fromString(osProjection.getStatusOs()),
+                        new ArrayList<>(),
+                        osProjection.getDataHoraFechamento()));
+            }
+            ordensDto.get(ordensDto.size() - 1).getItensOs().add(new ChecklistOrdemServicoItemDto(osProjection.getCodigoItemOs(),
+                    osProjection.getCodigoOs(),
+                    osProjection.getCodigoUnidade(),
+                    osProjection.getCpfMecanico(),
+                    osProjection.getCodigoPerguntaPrimeiroApontamento(),
+                    osProjection.getCodigoContextoPergunta(),
+                    osProjection.getCodigoAlternativaPrimeiroApontamento(),
+                    osProjection.getCodigoContextoAlternativa(),
+                    StatusItemOrdemServico.fromString(osProjection.getStatusItemOs()),
+                    osProjection.getQuantidadeApontamentos(),
+                    osProjection.getKm(),
+                    osProjection.getCodigoAgrupamentoResolucaoEmLote(),
+                    osProjection.getDataHoraConserto(),
+                    osProjection.getDataHoraInicioResolucao(),
+                    osProjection.getDataHoraFimResolucao(),
+                    osProjection.getTempoRealizacao(),
+                    osProjection.getFeedbackConserto()));
+            osAntiga = osProjection.getCodigoOs();
+        }
+        return ordensDto;
     }
 }
