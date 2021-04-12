9035e0ca5e7f1556de644d5e58786d3872c5c7b7
thaisksf <thaisfrancisco@prologapp.com>
2021-04-12 10:38:18 -0300

    feat(checklist): cria mapper
    PL-3461

---
+++ src/main/java/br/com/zalf/prolog/webservice/v3/frota/checklist/ChecklistListagemMapper.java
@@ -0,0 +1,30 @@
+package br.com.zalf.prolog.webservice.v3.frota.checklist;
+
+import br.com.zalf.prolog.webservice.v3.frota.checklist._model.ChecklistListagemDto;
+import br.com.zalf.prolog.webservice.v3.frota.checklist._model.ChecklistProjection;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created on 2021-04-07
+ *
+ * @author Thais Francisco (https://github.com/thaisksf)
+ */
+@Component
+public class ChecklistListagemMapper {
+
+    public List<ChecklistListagemDto> toDto(final List<ChecklistProjection> checklistsGet) {
+        return checklistsGet
+                .stream()
+                .map(this::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @NotNull
+    public ChecklistListagemDto toDto(@NotNull final ChecklistProjection checklistProjection) {
+        return null;
+    }
+}
