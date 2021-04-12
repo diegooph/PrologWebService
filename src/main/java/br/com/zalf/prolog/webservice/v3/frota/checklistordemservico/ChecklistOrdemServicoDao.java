b3ad9a549b4069ae576d2a9e50436445237b826b
gustavocnp95 <c98jjkdhgy>
2021-04-12 14:51:01 -0300

    refactor(servico): ajusta nome do parametro
    PL-3462

--- src/main/java/br/com/zalf/prolog/webservice/v3/frota/checklistordemservico/ChecklistOrdemServicoDao.java
+++ src/main/java/br/com/zalf/prolog/webservice/v3/frota/checklistordemservico/ChecklistOrdemServicoDao.java
@@ -3,6 +3,7 @@ package br.com.zalf.prolog.webservice.v3.frota.checklistordemservico;
 import br.com.zalf.prolog.webservice.v3.frota.checklistordemservico._model.ChecklistOrdemServicoEntity;
 import br.com.zalf.prolog.webservice.v3.frota.checklistordemservico._model.ChecklistOrdemServicoPk;
 import br.com.zalf.prolog.webservice.v3.frota.checklistordemservico._model.ChecklistOrdemServicoProjection;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -15,6 +16,6 @@ import java.util.List;
  */
 public interface ChecklistOrdemServicoDao extends JpaRepository<ChecklistOrdemServicoEntity, ChecklistOrdemServicoPk> {
     @Query(value = "select * from func_checklist_ordem_servico_listagem(" +
-            "f_cod_unidade => to_bigint_array(:codUnidade));", nativeQuery = true)
-    List<ChecklistOrdemServicoProjection> getOrdensServico(final List<Long> codUnidade);
+            "f_cod_unidades => to_bigint_array(:codUnidade));", nativeQuery = true)
+    List<ChecklistOrdemServicoProjection> getOrdensServico(@NotNull final List<Long> codUnidade);
 }
