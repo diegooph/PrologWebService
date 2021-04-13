365ad99dd4a258f4026bb1f5a05a9f7137039153
gustavocnp95 <c98jjkdhgy>
2021-04-13 14:22:18 -0300

    refactor(servico): adiciona passagem de parametro de incluir itens
    PL-3510

--- src/main/java/br/com/zalf/prolog/webservice/v3/frota/checklistordemservico/ChecklistOrdemServicoResource.java
+++ src/main/java/br/com/zalf/prolog/webservice/v3/frota/checklistordemservico/ChecklistOrdemServicoResource.java
@@ -55,6 +55,7 @@ public final class ChecklistOrdemServicoResource {
                                          statusOrdemServico,
                                          incluirItensOrdemServico,
                                          limit,
-                                         offset));
+                                         offset),
+                incluirItensOrdemServico);
     }
 }
