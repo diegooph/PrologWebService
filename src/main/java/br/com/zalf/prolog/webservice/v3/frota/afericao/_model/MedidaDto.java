787f83fce9676d84d0294f6c844242cada0a93a6
Steinert999 <steinert999@outlook.com>
2021-04-14 11:34:59 -0300

    style(aferição): adiciona anotações de validação no DTO
    PL-3570

--- src/main/java/br/com/zalf/prolog/webservice/v3/frota/afericao/_model/MedidaDto.java
+++ src/main/java/br/com/zalf/prolog/webservice/v3/frota/afericao/_model/MedidaDto.java
@@ -1,6 +1,8 @@
 package br.com.zalf.prolog.webservice.v3.frota.afericao._model;
 
 import lombok.Value;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.math.BigDecimal;
 
@@ -11,12 +13,20 @@ import java.math.BigDecimal;
  */
 @Value(staticConstructor = "of")
 public class MedidaDto {
+    @NotNull
     Long codPneu;
+    @NotNull
     Integer posicao;
+    @Nullable
     BigDecimal psi;
+    @NotNull
     Integer vidaMomentoAfericao;
+    @Nullable
     BigDecimal alturaSulcoInterno;
+    @Nullable
     BigDecimal alturaSulcoCentralInterno;
+    @Nullable
     BigDecimal alturaSulcoCentralExterno;
+    @Nullable
     BigDecimal alturaSulcoExterno;
 }
