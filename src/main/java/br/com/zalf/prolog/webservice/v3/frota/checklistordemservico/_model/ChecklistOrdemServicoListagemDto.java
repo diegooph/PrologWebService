e6eb191c95478b3a0427e36cbc4573eb2df59501
gustavocnp95 <c98jjkdhgy>
2021-04-15 15:14:35 -0300

    refactor(servico): documenta os dois models de retorno
    PL-3462

--- src/main/java/br/com/zalf/prolog/webservice/v3/frota/checklistordemservico/_model/ChecklistOrdemServicoListagemDto.java
+++ src/main/java/br/com/zalf/prolog/webservice/v3/frota/checklistordemservico/_model/ChecklistOrdemServicoListagemDto.java
@@ -1,6 +1,8 @@
 package br.com.zalf.prolog.webservice.v3.frota.checklistordemservico._model;
 
 import br.com.zalf.prolog.webservice.frota.checklist.ordemservico.model.StatusOrdemServico;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -8,15 +10,33 @@ import org.jetbrains.annotations.Nullable;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@ApiModel(description = "Objeto com as informações de uma unidade.")
 @Data
 public class ChecklistOrdemServicoListagemDto {
+    @ApiModelProperty(
+            value = "Código da ordem de serviço.",
+            example = "23")
     private final long codigoOs;
+    @ApiModelProperty(
+            value = "Código único da unidade.",
+            example = "215")
     private final long codUnidade;
+    @ApiModelProperty(
+            value = "Código do checklist que abriu essa ordem de serviço.",
+            example = "10")
     private final long codChecklistAberturaOs;
+    @ApiModelProperty(
+            value = "O status da ordem de serviço, aberta ou fechada.",
+            example = "A")
     @NotNull
     private final StatusOrdemServico statusOs;
+    @ApiModelProperty(
+            value = "Os itens da ordem de serviço.")
     @Nullable
     private final List<ChecklistOrdemServicoItemDto> itensOs;
+    @ApiModelProperty(
+            value = "A data em que a O.S foi totalmente fechada.",
+            example = "2019-08-18T10:47:00")
     @Nullable
     private final LocalDateTime dataHoraFechamentoOs;
 }
