00c454db04a9f761c820ab42085c551e25c0f318
gustavocnp95 <c98jjkdhgy>
2021-04-20 17:55:52 -0300

    feat(movimentação): cria service
    PL-3602

---
+++ src/main/java/br/com/zalf/prolog/webservice/v3/frota/movimentacao/MovimentacaoService.java
@@ -0,0 +1,15 @@
+package br.com.zalf.prolog.webservice.v3.frota.movimentacao;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created on 2021-04-20
+ *
+ * @author Gustavo Navarro (https://github.com/gustavocnp95)
+ */
+@Service
+public final class MovimentacaoService {
+    public String getMovimentacoes() {
+        return null;
+    }
+}
