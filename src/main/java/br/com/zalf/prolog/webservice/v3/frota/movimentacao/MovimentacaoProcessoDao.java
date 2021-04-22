bfddf9540ee7df2fc580c4cbeef2ff565d0ed1a0
gustavocnp95 <c98jjkdhgy>
2021-04-22 18:48:58 -0300

    feat(movimentação): adiciona novos joins
    PL-3602

--- src/main/java/br/com/zalf/prolog/webservice/v3/frota/movimentacao/MovimentacaoProcessoDao.java
+++ src/main/java/br/com/zalf/prolog/webservice/v3/frota/movimentacao/MovimentacaoProcessoDao.java
@@ -1,7 +1,11 @@
 package br.com.zalf.prolog.webservice.v3.frota.movimentacao;
 
 import br.com.zalf.prolog.webservice.v3.frota.movimentacao._model.MovimentacaoProcessoEntity;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /**
  * Created on 2021-03-26
@@ -9,5 +13,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author Luiz Felipe (https://github.com/luizfp)
  */
 public interface MovimentacaoProcessoDao extends JpaRepository<MovimentacaoProcessoEntity, Long> {
-
+    @Query("select distinct mpe from MovimentacaoProcessoEntity mpe "
+                   + "inner join fetch mpe.colaboradorRealizacaoProcesso "
+                   + "inner join fetch mpe.movimentacoes m "
+                   + "inner join fetch m.movimentacaoOrigem "
+                   + "inner join fetch m.movimentacaoDestino "
+                   + "inner join fetch m.pneu "
+                   + "where mpe.codUnidade = :codUnidade "
+                   + "order by mpe.codigo")
+    List<MovimentacaoProcessoEntity> getAll(long codUnidade, Pageable pageable);
 }
