5ec5d590ee369054a240955f173e742f61989419
gustavocnp95 <c98jjkdhgy>
2021-04-23 13:45:38 -0300

    feat(movimentação): adiciona chamada para metodo do service
    PL-3602

--- src/main/java/br/com/zalf/prolog/webservice/v3/frota/movimentacao/MovimentacaoProcessoResource.java
+++ src/main/java/br/com/zalf/prolog/webservice/v3/frota/movimentacao/MovimentacaoProcessoResource.java
@@ -3,12 +3,14 @@ package br.com.zalf.prolog.webservice.v3.frota.movimentacao;
 import br.com.zalf.prolog.webservice.interceptors.auth.Secured;
 import br.com.zalf.prolog.webservice.interceptors.debug.ConsoleDebugLog;
 import br.com.zalf.prolog.webservice.permissao.pilares.Pilares;
+import br.com.zalf.prolog.webservice.v3.frota.movimentacao._model.MovimentacaoProcessoEntity;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import java.util.List;
 
 /**
  * Created on 2021-04-20
@@ -32,7 +34,7 @@ public final class MovimentacaoProcessoResource {
             Pilares.Frota.Pneu.Movimentacao.MOVIMENTAR_VEICULO_ESTOQUE,
             Pilares.Frota.Pneu.Movimentacao.MOVIMENTAR_ANALISE,
             Pilares.Frota.Pneu.Movimentacao.MOVIMENTAR_DESCARTE})
-    public String getMovimentacoes() {
-        return null;
+    public List<MovimentacaoProcessoEntity> getMovimentacoes() {
+        return service.getAll();
     }
 }
