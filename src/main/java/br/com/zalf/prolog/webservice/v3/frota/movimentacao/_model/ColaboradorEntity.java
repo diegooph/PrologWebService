975edcbb16ab4bb4367867da793aaeb544352583
gustavocnp95 <c98jjkdhgy>
2021-04-26 14:36:34 -0300

    feat(movimentação): mapeia unidade no colaborador
    PL-3602

--- src/main/java/br/com/zalf/prolog/webservice/v3/frota/movimentacao/_model/ColaboradorEntity.java
+++ src/main/java/br/com/zalf/prolog/webservice/v3/frota/movimentacao/_model/ColaboradorEntity.java
@@ -1,14 +1,12 @@
 package br.com.zalf.prolog.webservice.v3.frota.movimentacao._model;
 
+import br.com.zalf.prolog.webservice.v3.geral.unidade._model.UnidadeEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 /**
  * Created on 2021-04-22
@@ -29,4 +27,7 @@ public final class ColaboradorEntity {
     private Long codigo;
     @Column(name = "nome")
     private String nome;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cod_unidade")
+    private UnidadeEntity unidade;
 }
