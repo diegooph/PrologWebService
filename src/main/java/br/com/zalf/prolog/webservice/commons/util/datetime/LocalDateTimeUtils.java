20779210634730a22136de164debd10b98505fa9
gustavocnp95 <c98jjkdhgy>
2021-04-26 15:52:51 -0300

    feat(movimentação): cria localdatetime utils
    PL-3602

---
+++ src/main/java/br/com/zalf/prolog/webservice/commons/util/datetime/LocalDateTimeUtils.java
@@ -0,0 +1,19 @@
+package br.com.zalf.prolog.webservice.commons.util.datetime;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+
+/**
+ * Created on 2021-04-26
+ *
+ * @author Gustavo Navarro (https://github.com/gustavocnp95)
+ */
+public final class LocalDateTimeUtils {
+    public static LocalDateTime applyTimezone(@NotNull final LocalDateTime localDateTimeToApplyTimezone,
+                                              @NotNull final ZoneId timezoneId) {
+        return localDateTimeToApplyTimezone.atOffset(ZoneOffset.UTC).atZoneSameInstant(timezoneId).toLocalDateTime();
+    }
+}
