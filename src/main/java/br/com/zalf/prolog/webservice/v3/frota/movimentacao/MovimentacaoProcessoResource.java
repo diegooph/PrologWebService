9bd949a75df960ce881ee237d74763995c5accc5
gustavocnp95 <c98jjkdhgy>
2021-04-26 16:49:16 -0300

    feat(movimentação): adiciona recebimento de parametros
    PL-3602

--- src/main/java/br/com/zalf/prolog/webservice/v3/frota/movimentacao/MovimentacaoProcessoResource.java
+++ src/main/java/br/com/zalf/prolog/webservice/v3/frota/movimentacao/MovimentacaoProcessoResource.java
@@ -1,5 +1,6 @@
 package br.com.zalf.prolog.webservice.v3.frota.movimentacao;
 
+import br.com.zalf.prolog.webservice.commons.network.metadata.Optional;
 import br.com.zalf.prolog.webservice.interceptors.auth.Secured;
 import br.com.zalf.prolog.webservice.interceptors.debug.ConsoleDebugLog;
 import br.com.zalf.prolog.webservice.permissao.pilares.Pilares;
@@ -8,8 +9,10 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.Max;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 import java.util.List;
 
 /**
@@ -39,7 +42,15 @@ public final class MovimentacaoProcessoResource {
             Pilares.Frota.Pneu.Movimentacao.MOVIMENTAR_VEICULO_ESTOQUE,
             Pilares.Frota.Pneu.Movimentacao.MOVIMENTAR_ANALISE,
             Pilares.Frota.Pneu.Movimentacao.MOVIMENTAR_DESCARTE})
-    public List<MovimentacaoProcessoListagemDto> getMovimentacoes() {
+    public List<MovimentacaoProcessoListagemDto> getMovimentacoes(
+            @QueryParam("codUnidades") @NotNull final List<Long> codUnidades,
+            @QueryParam("codColaborador") @Optional final Long codColaborador,
+            @QueryParam("codVeiculo") @Optional final Long codVeiculo,
+            @QueryParam("codPneu") @Optional final Long codPneu,
+            @QueryParam("dataInicial") @NotNull final String dataInicial,
+            @QueryParam("dataFinal") @NotNull final String dataFinal,
+            @QueryParam("limit") @Max(value = 1000, message = "O limite pode ser no máximo 1000.") final int limit,
+            @QueryParam("offset") final int offset) {
         return mapper.toDto(service.getAll());
     }
 }
