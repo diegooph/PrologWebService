bd260951b1a8b899744e8618f628e8f6036f7a2b
thaisksf <thaisfrancisco@prologapp.com>
2021-04-27 13:49:41 -0300

    feat(checklist): cria estrutura de perguntas e alternativas
    PL-3461

--- src/main/java/br/com/zalf/prolog/webservice/v3/frota/checklist/ChecklistListagemMapper.java
+++ src/main/java/br/com/zalf/prolog/webservice/v3/frota/checklist/ChecklistListagemMapper.java
@@ -1,5 +1,6 @@
 package br.com.zalf.prolog.webservice.v3.frota.checklist;
 
+import br.com.zalf.prolog.webservice.v3.frota.checklist._model.ChecklistAlternativaDto;
 import br.com.zalf.prolog.webservice.v3.frota.checklist._model.ChecklistListagemDto;
 import br.com.zalf.prolog.webservice.v3.frota.checklist._model.ChecklistPerguntasDto;
 import br.com.zalf.prolog.webservice.v3.frota.checklist._model.ChecklistProjection;
@@ -25,7 +26,6 @@ public class ChecklistListagemMapper {
                 .collect(Collectors.groupingBy(ChecklistProjection::getCodChecklist))
                 .forEach((codChecklist, checklistProjections) ->
                                  listagemDtos.add(toChecklistListagemDto(checklistProjections, incluirRespostas)));
-
         return listagemDtos;
     }
 
@@ -55,7 +55,6 @@ public class ChecklistListagemMapper {
                                        projection.getTotalPerguntasNok(),
                                        projection.getTotalAlternativasOk(),
                                        projection.getTotalAlternativasNok(),
-                                       projection.getTotalImagensPerguntasOk(),
                                        projection.getTotalMidiasPerguntasOk(),
                                        projection.getTotalMidiasAlternativasNok(),
                                        projection.getTotalNokBaixa(),
@@ -73,11 +72,54 @@ public class ChecklistListagemMapper {
                                        projection.getDeviceUptimeSincronizacaoMillis(),
                                        projection.temMidiaPerguntaOk(),
                                        projection.temMidiaAlternativaNok(),
-                                       incluirRespostas ? toChecklistPerguntaDto(projection) : null);
+                                       incluirRespostas ? toChecklistPerguntaDto(checklistProjections) : null);
     }
 
     @NotNull
-    private List<ChecklistPerguntasDto> toChecklistPerguntaDto(@NotNull final ChecklistProjection projection) {
-        return null;
+    private List<ChecklistPerguntasDto> toChecklistPerguntaDto(
+            @NotNull final List<ChecklistProjection> checklistProjection) {
+        final List<ChecklistPerguntasDto> perguntasDto = new ArrayList<>();
+        checklistProjection.stream()
+                .collect(Collectors.groupingBy(ChecklistProjection::getCodPergunta))
+                .forEach((codPergunta, perguntasProjections) ->
+                                 perguntasDto.add(createChecklistPerguntasDto(perguntasProjections)));
+        return perguntasDto;
+    }
+
+    @NotNull
+    private ChecklistPerguntasDto createChecklistPerguntasDto(
+            @NotNull final List<ChecklistProjection> perguntasProjection) {
+        final ChecklistProjection perguntaProjection = perguntasProjection.get(0);
+        return ChecklistPerguntasDto.of(perguntaProjection.getCodPergunta(),
+                                        perguntaProjection.getCodContextoPergunta(),
+                                        perguntaProjection.getDescricaoPergunta(),
+                                        perguntaProjection.getOrdemPergunta(),
+                                        perguntaProjection.isPerguntaSingleChoice(),
+                                        perguntaProjection.getAnexoMidiaPerguntaOk(),
+                                        toChecklistAlternativaDto(perguntasProjection));
+    }
+
+    @NotNull
+    private List<ChecklistAlternativaDto> toChecklistAlternativaDto(
+            @NotNull final List<ChecklistProjection> alternativasProjection) {
+        final List<ChecklistAlternativaDto> alternativasDto = new ArrayList<>();
+        alternativasProjection.forEach(alternativa -> alternativasDto.add(createChecklistAlternativasDto(alternativa)));
+        return alternativasDto;
+    }
+
+    @NotNull
+    private ChecklistAlternativaDto createChecklistAlternativasDto(@NotNull final ChecklistProjection alternativa) {
+        return ChecklistAlternativaDto.of(alternativa.getCodAlternativa(),
+                                          alternativa.getCodContextoAlternativa(),
+                                          alternativa.getDescricaoAlternativa(),
+                                          alternativa.getOrdemAlternativa(),
+                                          alternativa.getPrioridadeAlternativa(),
+                                          alternativa.getAlternativaTipoOutros(),
+                                          alternativa.deveAbrirOrdemServico(),
+                                          alternativa.getAnexoMidiaAlternativaNok(),
+                                          alternativa.getCodAuxiliarAlternativa(),
+                                          alternativa.isAlternativaSelecionada(),
+                                          alternativa.getRespostaTipoOutros());
     }
 }
+
