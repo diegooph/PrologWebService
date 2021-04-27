545e1174d7f4e8c683fd38e5bf2f7d76bacc1bb7
thaisksf <thaisfrancisco@prologapp.com>
2021-04-27 17:30:26 -0300

    feat(checklist): cria dados para teste de Checklist
    Insere um veículo, retorna o código do mesmo e realiza uma quantidade X de checks nesse veículo de maneira dinâmica.
    
    PL-3461

---
+++ src/test/java/test/br/com/zalf/prolog/webservice/pilares/frota/checklist/InsertChecklistInitialDataForTestsCallback.java
@@ -0,0 +1,89 @@
+package test.br.com.zalf.prolog.webservice.pilares.frota.checklist;
+
+import br.com.zalf.prolog.webservice.commons.util.Log;
+import org.flywaydb.core.api.callback.BaseCallback;
+import org.flywaydb.core.api.callback.Context;
+import org.flywaydb.core.api.callback.Event;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Created on 2021-04-27
+ *
+ * @author Thais Francisco (https://github.com/thaisksf)
+ */
+public class InsertChecklistInitialDataForTestsCallback extends BaseCallback {
+
+    private static final String TAG = InsertChecklistInitialDataForTestsCallback.class.getSimpleName();
+    private static final int QTD_CHECKLISTS = 10;
+
+    @Override
+    public void handle(final Event event, final Context context) {
+        if (event.equals(Event.AFTER_MIGRATE)) {
+            Log.i(TAG, "Iniciando execução da inclusão do veículo para testes de checklist...");
+            try (final Statement stmt = context.getConnection().createStatement()) {
+                final ResultSet rSetCodVeiculo = insertVeiculoChecklist(stmt);
+                if (rSetCodVeiculo.next()) {
+                    int count = 0;
+                    do {
+                        Log.i(TAG, "Iniciando execução da inclusão de dados de checklist de teste...");
+                        final ResultSet rSetCodChecklist = insertChecklist(stmt, rSetCodVeiculo);
+                        if (rSetCodChecklist.next()) {
+                            Log.i(TAG, "Iniciando execução da inclusão das respostas de checklist de teste...");
+                            insertRespostasChecklist(stmt, rSetCodChecklist);
+                        }
+                        count++;
+                    } while (count < QTD_CHECKLISTS);
+                }
+            } catch (final SQLException e) {
+                Log.e(TAG, "Erro ao inserir dados de checklist", e);
+            }
+            Log.i(TAG, "Finalizando execução das inserções");
+        }
+    }
+
+    private void insertRespostasChecklist(final Statement stmt, final ResultSet rSetCodChecklist) throws SQLException {
+        final Long codChecklist = rSetCodChecklist.getLong("codigo");
+        int codPergunta = 1;
+        do {
+            final String sqlChecklistAlternativa =
+                    "insert into public.checklist_respostas_nok(cod_unidade," +
+                            " cod_checklist_modelo, cod_versao_checklist_modelo, cod_checklist, " +
+                            "cod_pergunta, cod_alternativa, resposta_outros) " +
+                            "values (215, 1, 1, " + codChecklist + "," + codPergunta + ", 1, 'teste');";
+            stmt.execute(sqlChecklistAlternativa);
+            codPergunta++;
+        } while (codPergunta < 4);
+    }
+
+    private ResultSet insertVeiculoChecklist(final Statement stmt) throws SQLException {
+        return stmt.executeQuery(
+                "INSERT INTO public.veiculo_data (placa, cod_unidade, km, status_ativo, cod_tipo, cod_modelo," +
+                        " cod_eixos, data_hora_cadastro, cod_unidade_cadastro, deletado, data_hora_deletado, " +
+                        "pg_username_delecao, cod_empresa, cod_diagrama, motorizado, origem_cadastro) " +
+                        "values ('CHE0001', 215, 1000, true, 64, 1207, 1, '2020-01-21 19:15:31.971416', 5, false, " +
+                        "null, null, 3, 2, true, 'INTERNO') returning codigo;");
+    }
+
+    private ResultSet insertChecklist(final Statement stmt, final ResultSet rSetCodVeiculo) throws SQLException {
+        final Long codVeiculo = rSetCodVeiculo.getLong("codigo");
+        final String sqlChecklist = "insert into public.checklist (cod_unidade, cod_checklist_modelo, " +
+                "cod_versao_checklist_modelo, data_hora, data_hora_realizacao_tz_aplicado, " +
+                "data_hora_importado_prolog, cpf_colaborador, cod_veiculo, tipo, tempo_realizacao, " +
+                "km_veiculo, data_hora_sincronizacao, fonte_data_hora_realizacao, " +
+                "versao_app_momento_realizacao, versao_app_momento_sincronizacao, device_id, " +
+                "device_imei, device_uptime_realizacao_millis, device_uptime_sincronizacao_millis, " +
+                "foi_offline, total_perguntas_ok, total_perguntas_nok, total_alternativas_ok, " +
+                "total_alternativas_nok) " +
+                "values (215, 1, " +
+                "1, '2021-04-08 14:32:59.733039', '2021-04-08 11:32:59.733039', " +
+                "null, 3383283194, " + codVeiculo + ", 'S', 27275, " +
+                "11331, '2021-04-08 14:32:59.733166', 'SERVIDOR', " +
+                "121, 121, '7d6867d2498bd65e', " +
+                "null, 0, 211810, false, 0, 3, 1, 7) returning codigo;";
+        final ResultSet rSetCodChecklist = stmt.executeQuery(sqlChecklist);
+        return rSetCodChecklist;
+    }
+}
