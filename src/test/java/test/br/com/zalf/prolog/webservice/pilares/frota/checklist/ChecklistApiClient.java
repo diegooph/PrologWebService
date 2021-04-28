714c2e3146ee528e0d0b54ee0248cb91f8d83cfc
thaisksf <thaisfrancisco@prologapp.com>
2021-04-28 15:24:28 -0300

    refactor(checklist): cria testes para listagem de Checklist
    Modifica o método utilizado para testes com parâmetros corretos e cria método para teste com parâmetros fora do padrão.
    
    PL-3461

--- src/test/java/test/br/com/zalf/prolog/webservice/pilares/frota/checklist/ChecklistApiClient.java
+++ src/test/java/test/br/com/zalf/prolog/webservice/pilares/frota/checklist/ChecklistApiClient.java
@@ -1,7 +1,9 @@
 package test.br.com.zalf.prolog.webservice.pilares.frota.checklist;
 
+import br.com.zalf.prolog.webservice.errorhandling.sql.ClientSideErrorException;
 import br.com.zalf.prolog.webservice.v3.frota.checklist._model.ChecklistListagemDto;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestComponent;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -31,7 +33,13 @@ public class ChecklistApiClient {
     @NotNull
     public ResponseEntity<List<ChecklistListagemDto>> getChecklistsByFitlro(@NotNull final List<Long> codUnidades,
                                                                             @NotNull final String dataInicial,
-                                                                            @NotNull final String dataFinal) {
+                                                                            @NotNull final String dataFinal,
+                                                                            @Nullable final Long codColaborador,
+                                                                            @Nullable final Long codVeiculo,
+                                                                            @Nullable final Long codTipoVeiculo,
+                                                                            final boolean incluirRespostas,
+                                                                            final int limit,
+                                                                            final int offset) {
         final UriComponents components = UriComponentsBuilder
                 .fromPath(RESOURCE)
                 .queryParam("codUnidades", codUnidades.stream()
@@ -39,14 +47,39 @@ public class ChecklistApiClient {
                         .collect(Collectors.joining(",")))
                 .queryParam("dataInicial", dataInicial)
                 .queryParam("dataFinal", dataFinal)
-                .queryParam("incluirRespostas", false)
-                .queryParam("limit", 1)
-                .queryParam("offset", 0)
+                .queryParam("codColaborador", codColaborador)
+                .queryParam("codVeiculo", codVeiculo)
+                .queryParam("codTipoVeiculo", codTipoVeiculo)
+                .queryParam("incluirRespostas", incluirRespostas)
+                .queryParam("limit", limit)
+                .queryParam("offset", offset)
                 .build();
         final RequestEntity<Void> requestEntity = RequestEntity
                 .get(components.toUri())
                 .accept(MediaType.APPLICATION_JSON)
                 .build();
-        return restTemplate.exchange(requestEntity, new ParameterizedTypeReference<List<ChecklistListagemDto>>() {});
+        return restTemplate.exchange(requestEntity, new ParameterizedTypeReference<>() {});
+    }
+
+    public ResponseEntity<ClientSideErrorException> getChecklistsWithWrongTypeUnidades(
+            @NotNull final List<String> wrongTypeCodUnidades,
+            final String dataInicial,
+            final String dataFinal,
+            final int limit,
+            final int offset) {
+        final UriComponents components = UriComponentsBuilder
+                .fromPath(RESOURCE)
+                .queryParam("codUnidades", String.join(",", wrongTypeCodUnidades))
+                .queryParam("dataInicial", dataInicial)
+                .queryParam("dataFinal", dataFinal)
+                .queryParam("incluirRespostas", false)
+                .queryParam("limit", limit)
+                .queryParam("offset", offset)
+                .build();
+        final RequestEntity<Void> reqEntity = RequestEntity
+                .get(components.toUri())
+                .accept(MediaType.APPLICATION_JSON)
+                .build();
+        return restTemplate.exchange(reqEntity, new ParameterizedTypeReference<>() {});
     }
 }
