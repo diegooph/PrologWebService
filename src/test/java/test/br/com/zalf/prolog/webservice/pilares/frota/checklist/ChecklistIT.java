8da08aadd7f7d37c9f0f0dd817515f05141280c6
thaisksf <thaisfrancisco@prologapp.com>
2021-04-28 15:25:07 -0300

    feat(checklist): cria testes para listagem de checklis
    PL-3461

--- src/test/java/test/br/com/zalf/prolog/webservice/pilares/frota/checklist/ChecklistIT.java
+++ src/test/java/test/br/com/zalf/prolog/webservice/pilares/frota/checklist/ChecklistIT.java
@@ -1,5 +1,6 @@
 package test.br.com.zalf.prolog.webservice.pilares.frota.checklist;
 
+import br.com.zalf.prolog.webservice.errorhandling.sql.ClientSideErrorException;
 import br.com.zalf.prolog.webservice.v3.frota.checklist._model.ChecklistListagemDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,14 +23,91 @@ public class ChecklistIT extends IntegrationTest {
     private ChecklistApiClient client;
 
     @Test
-    @DisplayName("Dado código da unidade, retorne a UnidadeVisualizacaoListagem correspondente.")
-    void givenCodUnidadeToRequest_ThenReturnUnidadeVisualizacaoListagemAndStatusOK() {
+    @DisplayName("Dado os parâmetros corretos, retorne a Listagem de Checklist.")
+    void givenCorrectParameters_ThenReturnChecksSize() {
         final ResponseEntity<List<ChecklistListagemDto>> responseEntity =
                 client.getChecklistsByFitlro(List.of(215L),
-                                             "2020-01-01",
-                                             "2021-03-12");
+                                             "2021-03-01",
+                                             "2021-03-30",
+                                             null,
+                                             null,
+                                             null,
+                                             false,
+                                             10,
+                                             0);
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(responseEntity.getBody()).isNotNull();
-        assertThat(responseEntity.getBody().size()).isEqualTo(0);
+        assertThat(responseEntity.getBody().size()).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("Dado os parâmetros corretos que não incluem resposta, retorne a Listagem de Checklist sem respostas.")
+    void givenCorrectParametersWithoutIncluirRespostas_ThenReturnListChecklistDtoAndStatusOk() {
+        final ResponseEntity<List<ChecklistListagemDto>> responseEntity =
+                client.getChecklistsByFitlro(List.of(215L),
+                                             "2021-03-01",
+                                             "2021-03-30",
+                                             null,
+                                             null,
+                                             null,
+                                             false,
+                                             10,
+                                             0);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isNotNull();
+        assertThat(responseEntity.getBody().get(0).getChecklistPerguntasDtos()).isNull();
+    }
+
+    @Test
+    @DisplayName("Dado os parâmetros corretos que incluem resposta, retorne a Listagem de Checklist com respostas.")
+    void givenCorrectParametersWithIncluirRespostas_ThenReturnListChecklistDtoAndStatusOk() {
+        final ResponseEntity<List<ChecklistListagemDto>> responseEntity =
+                client.getChecklistsByFitlro(List.of(215L),
+                                             "2021-03-01",
+                                             "2021-03-30",
+                                             null,
+                                             null,
+                                             null,
+                                             true,
+                                             10,
+                                             0);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isNotNull();
+        assertThat(responseEntity.getBody().get(0).getChecklistPerguntasDtos()).isNotNull();
+        assertThat(responseEntity.getBody()
+                           .get(0)
+                           .getChecklistPerguntasDtos()
+                           .get(0)
+                           .getChecklistAlternativaDtos()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Dado parâmetros que n possuem Data, retorne a Listagem de Checklist vazia.")
+    void givenParametersWithoutData_ThenReturnEmptyListChecklistDto() {
+        final ResponseEntity<List<ChecklistListagemDto>> responseEntity =
+                client.getChecklistsByFitlro(List.of(5L),
+                                             "2021-02-01",
+                                             "2021-02-27",
+                                             2145L,
+                                             3986L,
+                                             64L,
+                                             false,
+                                             10,
+                                             0);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isNotNull();
+        assertThat(responseEntity.getBody()).isEmpty();
+    }
+
+    @Test
+    @DisplayName("Dado o tipo de dado codUnidades incorreto, retorne ClientSideErrorException.")
+    void givenWrongTypeParameter_ThenReturnClientSideErrorExceptionBadRequest() {
+        final ResponseEntity<ClientSideErrorException> response = client.getChecklistsWithWrongTypeUnidades(
+                List.of("a"),
+                "2021-03-01",
+                "2021-03-30",
+                2,
+                0);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
