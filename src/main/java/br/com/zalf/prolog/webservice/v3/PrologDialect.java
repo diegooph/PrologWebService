735c085c56c9be3ff7336691bd13dab26bae9624
gustavocnp95 <c98jjkdhgy>
2021-04-29 14:20:08 -0300

    feat(movimentação): cria dialeto customizado para registrar function de conversão
    PL-3602

---
+++ src/main/java/br/com/zalf/prolog/webservice/v3/PrologDialect.java
@@ -0,0 +1,17 @@
+package br.com.zalf.prolog.webservice.v3;
+
+import org.hibernate.dialect.PostgreSQL10Dialect;
+import org.hibernate.dialect.function.StandardSQLFunction;
+import org.hibernate.type.LocalDateType;
+
+/**
+ * Created on 2021-04-29
+ *
+ * @author Gustavo Navarro (https://github.com/gustavocnp95)
+ */
+public class PrologDialect extends PostgreSQL10Dialect {
+
+    public PrologDialect() {
+        registerFunction("to_date", new StandardSQLFunction("to_date", LocalDateType.INSTANCE));
+    }
+}
