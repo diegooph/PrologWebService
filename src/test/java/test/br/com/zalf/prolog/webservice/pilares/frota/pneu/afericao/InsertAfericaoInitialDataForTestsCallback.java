3e198a8a246cbb357524ccb38f53ec29d693ebb8
Steinert999 <steinert999@outlook.com>
2021-04-30 17:31:56 -0300

    fix(): corrige dados inserção de afericao_data
    PL-3570

--- src/test/java/test/br/com/zalf/prolog/webservice/pilares/frota/pneu/afericao/InsertAfericaoInitialDataForTestsCallback.java
+++ src/test/java/test/br/com/zalf/prolog/webservice/pilares/frota/pneu/afericao/InsertAfericaoInitialDataForTestsCallback.java
@@ -5,7 +5,6 @@ import org.flywaydb.core.api.callback.BaseCallback;
 import org.flywaydb.core.api.callback.Context;
 import org.flywaydb.core.api.callback.Event;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -23,9 +22,20 @@ public class InsertAfericaoInitialDataForTestsCallback extends BaseCallback {
         if (event.equals(Event.AFTER_MIGRATE)) {
             Log.i(TAG, "Iniciando execução da inclusão de dados de aferição de teste...");
             try (final Statement statement = context.getConnection().createStatement()) {
-              statement.execute("INSERT INTO public.afericao_data (codigo, data_hora, placa_veiculo, cpf_aferidor, km_veiculo, tempo_realizacao, tipo_medicao_coletada, cod_unidade, tipo_processo_coleta, deletado, data_hora_deletado, pg_username_delecao, cod_diagrama, forma_coleta_dados, motivo_delecao, cod_veiculo) VALUES (2196, '2019-11-06 23:20:33.701000', 'PRO0001', 3383283194, 21167, 179461, 'SULCO_PRESSAO', 5, 'PLACA', false, null, null, 1, 'EQUIPAMENTO', null, 3195);");
-              statement.execute("INSERT INTO public.afericao_data (codigo, data_hora, placa_veiculo, cpf_aferidor, km_veiculo, tempo_realizacao, tipo_medicao_coletada, cod_unidade, tipo_processo_coleta, deletado, data_hora_deletado, pg_username_delecao, cod_diagrama, forma_coleta_dados, motivo_delecao, cod_veiculo) VALUES (10614,'2019-09-19 12:33:06.021000', null, 3383283194, null, 18810, 'SULCO', 5, 'PNEU_AVULSO', false, null, null, null, 'EQUIPAMENTO', null, null);");
-          } catch (final SQLException e ) {
+                statement.execute(
+                        "INSERT INTO public.afericao_data (codigo, data_hora, cpf_aferidor, km_veiculo, " +
+                                "tempo_realizacao, tipo_medicao_coletada, cod_unidade, tipo_processo_coleta, " +
+                                "deletado, data_hora_deletado, pg_username_delecao, cod_diagrama, forma_coleta_dados," +
+                                " motivo_delecao, cod_veiculo) VALUES (2196, '2019-11-06 23:20:33.701000', " +
+                                "3383283194, 21167, 179461, 'SULCO_PRESSAO', 5, 'PLACA', false, null, null, 1, " +
+                                "'EQUIPAMENTO', null, 3195);");
+                statement.execute(
+                        "INSERT INTO public.afericao_data (codigo, data_hora, cpf_aferidor, km_veiculo, " +
+                                "tempo_realizacao, tipo_medicao_coletada, cod_unidade, tipo_processo_coleta, " +
+                                "deletado, data_hora_deletado, pg_username_delecao, cod_diagrama, forma_coleta_dados," +
+                                " motivo_delecao, cod_veiculo) VALUES (10614,'2019-09-19 12:33:06.021000', " +
+                                "3383283194, null, 18810, 'SULCO', 5, 'PNEU_AVULSO', false, null, null, null, 'EQUIPAMENTO', null, null);");
+            } catch (final SQLException e ) {
               Log.e(TAG, "Erro ao inserir dados de aferição", e);
           }
           Log.i(TAG, "Finalizando execução das inserções");
