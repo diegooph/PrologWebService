e5ed0245ee4efd3ec7cfff9347a54755e5a35207
gustavocnp95 <c98jjkdhgy>
2021-04-30 18:31:16 -0300

    feat(movimentação): deleta functionn nao utilizada
    PL-3602

--- sql/prolog_queries/utils/apply_timezone.sql
+++
@@ -1,8 +0,0 @@
-create
-or replace function apply_timezone(datetime timestamp with time zone, tz text)
-    returns timestamp without time zone
-    language sql
-as
-$$
-select datetime at time zone tz;
-$$;
