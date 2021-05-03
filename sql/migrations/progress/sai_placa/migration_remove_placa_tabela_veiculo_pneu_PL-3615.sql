1851b811829bd5c0a8000416ea87b828b7172470
Steinert999 <steinert999@outlook.com>
2021-05-03 16:59:28 -0300

    feat(suporte): adiciona modificações à migration
    PL-3615

--- sql/migrations/progress/sai_placa/migration_remove_placa_tabela_veiculo_pneu_PL-3615.sql
+++ sql/migrations/progress/sai_placa/migration_remove_placa_tabela_veiculo_pneu_PL-3615.sql
@@ -688,3 +688,22 @@ begin
     into aviso_pneus_desvinculados;
 end
 $$;
+
+create or replace function suporte.func_veiculo_transfere_veiculo_entre_empresas(f_placa_veiculo varchar(7),
+                                                                                 f_cod_empresa_origem bigint,
+                                                                                 f_cod_unidade_origem bigint,
+                                                                                 f_cod_empresa_destino bigint,
+                                                                                 f_cod_unidade_destino bigint,
+                                                                                 f_cod_modelo_veiculo_destino bigint,
+                                                                                 f_cod_tipo_veiculo_destino bigint,
+                                                                                 out veiculo_transferido text)
+    returns text
+    language plpgsql
+    security definer
+as
+$$
+begin
+    perform suporte.func_historico_salva_execucao();
+    raise exception 'O veiculo pode ser cadastrado com a mesma placa na nova empresa. Não é necessário utilizar esse procedimento.';
+end
+$$;
