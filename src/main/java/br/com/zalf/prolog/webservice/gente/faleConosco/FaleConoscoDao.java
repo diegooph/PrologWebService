7923719a34d837d55568fc93115a0f488bf3f615
gustavocnp95 <c98jjkdhgy>
2021-05-05 13:43:59 -0300

    refactor(movimentação): refatora get by colaborador para receber mais dados
    PL-3602

--- src/main/java/br/com/zalf/prolog/webservice/gente/faleConosco/FaleConoscoDao.java
+++ src/main/java/br/com/zalf/prolog/webservice/gente/faleConosco/FaleConoscoDao.java
@@ -1,22 +1,26 @@
 package br.com.zalf.prolog.webservice.gente.faleConosco;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.sql.SQLException;
 import java.util.List;
-
 
 /**
  * Contém os métodos para manipular os fale conosco
  */
 public interface FaleConoscoDao {
 
-	Long insert(FaleConosco faleConosco, Long codUnidade) throws SQLException;
+    Long insert(FaleConosco faleConosco, Long codUnidade) throws SQLException;
 
-	FaleConosco getByCod(Long codigo, Long codUnidade) throws Exception;
+    FaleConosco getByCod(Long codigo, Long codUnidade) throws Exception;
 
-	List<FaleConosco> getAll(long dataInicial, long dataFinal, int limit, int offset, String cpf,
-							 String equipe, Long codUnidade, String status, String categoria) throws Exception;
+    List<FaleConosco> getAll(long dataInicial, long dataFinal, int limit, int offset, String cpf,
+                             String equipe, Long codUnidade, String status, String categoria) throws Exception;
 
-	List<FaleConosco> getByColaborador(Long cpf, String status) throws Exception;
+    @NotNull
+    List<FaleConosco> getByColaborador(@NotNull final Long codEmpresa,
+                                       @NotNull final Long cpf,
+                                       @NotNull final String status) throws Exception;
 
-	boolean insertFeedback(FaleConosco faleConosco, Long codUnidade) throws SQLException;
+    boolean insertFeedback(FaleConosco faleConosco, Long codUnidade) throws SQLException;
 }
