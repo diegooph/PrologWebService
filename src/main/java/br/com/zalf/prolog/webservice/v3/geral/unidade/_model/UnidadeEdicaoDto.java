5a2934a4f052a2291736f3662b94a7fc7c4d654f
didi <dioge.vanzella@gmail.com>
2021-05-07 14:49:09 -0300

    docs: padroniza documentação das Unidades
    PL-3621

--- src/main/java/br/com/zalf/prolog/webservice/v3/geral/unidade/_model/UnidadeEdicaoDto.java
+++ src/main/java/br/com/zalf/prolog/webservice/v3/geral/unidade/_model/UnidadeEdicaoDto.java
@@ -1,7 +1,6 @@
 package br.com.zalf.prolog.webservice.v3.geral.unidade._model;
 
 import br.com.zalf.prolog.webservice.v3.validation.CodUnidade;
-import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,39 +15,26 @@ import javax.validation.constraints.Size;
  *
  * @author Gustavo Navarro (https://github.com/gustavocnp95)
  */
-@ApiModel(description = "Objeto utilizado para sobrescrever informações da Unidade. Se tratando de sobrescrita, caso " +
-        "alguma propriedade não for fornecida, assumiremos null.\nPara sobrescrever apenas uma propriedade envie as " +
-        "demais contendo o valor original.\nPara remover uma propriedade, envie null e as demais contendo o valor " +
-        "original.")
 @Builder
 @Getter
 public final class UnidadeEdicaoDto {
-    @ApiModelProperty(
-            value = "Código da unidade",
-            example = "215")
+    @ApiModelProperty(value = "Código da unidade.", required = true, example = "215")
     @NotNull(message = "O código da unidade é obrigatório.")
     @CodUnidade
     private final Long codUnidade;
-    @ApiModelProperty(
-            value = "Nome da unidade.",
-            example = "Unidade de testes")
+    @ApiModelProperty(value = "Nome da unidade.", required = true, example = "Unidade de testes")
     @NotBlank(message = "O nome da unidade não pode estar vazio.")
     @Size(max = 40, message = "O nome da unidade pode conter no máximo 40 caracteres.")
     private final String nomeUnidade;
-    @ApiModelProperty(
-            value = "Um código auxiliar para a unidade.",
-            example = "01:01",
-            notes = "O código auxiliar é normalmente utilizado para integrações com sistemas externos.")
+    @ApiModelProperty(value = "Um código auxiliar para a unidade.",
+                      notes = "O código auxiliar é normalmente utilizado para integrações com sistemas externos.",
+                      example = "01:01")
     @Nullable
     private final String codAuxiliarUnidade;
-    @ApiModelProperty(
-            value = "A latitude da localização da unidade.",
-            example = "-27.641369")
+    @ApiModelProperty(value = "A latitude da localização da unidade.", example = "-27.641369")
     @Nullable
     private final String latitudeUnidade;
-    @ApiModelProperty(
-            value = "A latitude da localização da unidade.",
-            example = "-48.679233")
+    @ApiModelProperty(value = "A latitude da localização da unidade.", example = "-48.679233")
     @Nullable
     private final String longitudeUnidade;
 }
