cc61b22f54afe87087616b735e668af28de24acc
didi <dioge.vanzella@gmail.com>
2021-05-07 10:27:08 -0300

    refactor(veículo): motherfucker documentation
    PL-3621

--- src/main/java/br/com/zalf/prolog/webservice/v3/frota/veiculo/VeiculoV3ResourceApiDoc.java
+++ src/main/java/br/com/zalf/prolog/webservice/v3/frota/veiculo/VeiculoV3ResourceApiDoc.java
@@ -1,5 +1,6 @@
 package br.com.zalf.prolog.webservice.v3.frota.veiculo;
 
+import br.com.zalf.prolog.webservice.commons.network.Response;
 import br.com.zalf.prolog.webservice.commons.network.SuccessResponse;
 import br.com.zalf.prolog.webservice.v3.frota.veiculo._model.VeiculoCadastroDto;
 import io.swagger.annotations.*;
@@ -8,16 +9,15 @@ import javax.validation.Valid;
 
 @Api(value = "Gestão de Veículos")
 public interface VeiculoV3ResourceApiDoc {
-    @ApiOperation(value = "Método utilizado para inserir um veículo.", response = SuccessResponse.class)
+    @ApiOperation(value = "Insere um veículo.", response = SuccessResponse.class)
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Operação efetuada com sucesso."),
-            @ApiResponse(code = 401, message = "Operação não autorizada"),
-            @ApiResponse(code = 404, message = "Operação não encontrada"),
-            @ApiResponse(code = 500, message = "Erro ao executar operação")
+            @ApiResponse(code = 200, message = "Operação efetuada com sucesso.", response = SuccessResponse.class),
+            @ApiResponse(code = 401, message = "Operação não autorizada", response = Response.class),
+            @ApiResponse(code = 404, message = "Operação não encontrada", response = Response.class),
+            @ApiResponse(code = 500, message = "Erro ao executar operação", response = Response.class)
     })
     SuccessResponse insert(
             @ApiParam(hidden = true) final String tokenIntegracao,
             @ApiParam(value = "Dados do veículo para inserir.",
-                      required = true)
-            @Valid final VeiculoCadastroDto veiculoCadastroDto);
+                      required = true) @Valid final VeiculoCadastroDto veiculoCadastroDto);
 }
