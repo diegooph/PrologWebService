5fd908c7f1573e0609bd228c7159a09fa4af9744
gustavocnp95 <c98jjkdhgy>
2021-05-10 17:50:55 -0300

    refactor(movimentação): documenta
    PL-3602

--- sql/migrations/progress/migration_corrige_kms_errados_PL-3620.sql
+++ sql/migrations/progress/migration_corrige_kms_errados_PL-3620.sql
@@ -1,9 +1,12 @@
+-- Aqui corrigimos o km no item da O.S.
 update checklist_ordem_servico_itens_data
 set km = vpkh.km_final
 from veiculo_processo_km_historico vpkh
 where checklist_ordem_servico_itens_data.cod_agrupamento_resolucao_em_lote = vpkh.cod_processo_veiculo
   and vpkh.tipo_processo_veiculo = 'FECHAMENTO_ITEM_CHECKLIST';
 
+-- Aqui corrigimos a info de se é ou não fonte do processo, pois como enviava outra placa todos reboques acoplados
+-- ficavam como se não fossem fonte do processo.
 update veiculo_processo_km_historico
 set veiculo_fonte_processo = (cd.cod_veiculo = veiculo_processo_km_historico.cod_veiculo)
 from checklist_ordem_servico_itens_data cosid
