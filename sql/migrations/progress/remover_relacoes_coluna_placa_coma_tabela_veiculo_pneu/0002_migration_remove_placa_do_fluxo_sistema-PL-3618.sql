2bd9fb3c7ed9cae3b175072ff88d2756c2155359
Allan Barbosa <allanbarbosa@prologapp.com>
2021-05-10 16:46:53 -0300

    refactor(pneu): altera busca do pneu por placa para que retorne a coluna placa da tabela de veiculo e não da veiculo_placa
    PL-3618

--- sql/migrations/progress/remover_relacoes_coluna_placa_coma_tabela_veiculo_pneu/0002_migration_remove_placa_do_fluxo_sistema-PL-3618.sql
+++ sql/migrations/progress/remover_relacoes_coluna_placa_coma_tabela_veiculo_pneu/0002_migration_remove_placa_do_fluxo_sistema-PL-3618.sql
@@ -312,3 +312,107 @@ from pneu p
 where p.codigo = f_cod_pneu
 order by p.codigo_cliente asc;
 $$;
+
+create or replace function func_pneu_get_pneu_by_placa(f_placa varchar(7))
+  returns table (
+    NOME_MARCA_PNEU              varchar(255),
+    COD_MARCA_PNEU               bigint,
+    CODIGO                       bigint,
+    CODIGO_CLIENTE               varchar(255),
+    COD_UNIDADE_ALOCADO          bigint,
+    COD_REGIONAL_ALOCADO         bigint,
+    PRESSAO_ATUAL                real,
+    VIDA_ATUAL                   integer,
+    VIDA_TOTAL                   integer,
+    PNEU_NOVO_NUNCA_RODADO       boolean,
+    NOME_MODELO_PNEU             varchar(255),
+    COD_MODELO_PNEU              bigint,
+    QT_SULCOS_MODELO_PNEU        smallint,
+    ALTURA_SULCOS_MODELO_PNEU    real,
+    ALTURA                       integer,
+    LARGURA                      integer,
+    ARO                          real,
+    COD_DIMENSAO                 bigint,
+    PRESSAO_RECOMENDADA          real,
+    ALTURA_SULCO_CENTRAL_INTERNO real,
+    ALTURA_SULCO_CENTRAL_EXTERNO real,
+    ALTURA_SULCO_INTERNO         real,
+    ALTURA_SULCO_EXTERNO         real,
+    STATUS                       varchar(255),
+    DOT                          varchar(20),
+    VALOR                        real,
+    COD_MODELO_BANDA             bigint,
+    NOME_MODELO_BANDA            varchar(255),
+    QT_SULCOS_MODELO_BANDA       smallint,
+    ALTURA_SULCOS_MODELO_BANDA   real,
+    COD_MARCA_BANDA              bigint,
+    NOME_MARCA_BANDA             varchar(255),
+    VALOR_BANDA                  real,
+    POSICAO_PNEU                 integer,
+    POSICAO_APLICADO_CLIENTE     varchar(255),
+    COD_VEICULO_APLICADO         bigint,
+    PLACA_APLICADO               varchar(7),
+    IDENTIFICADOR_FROTA          text
+  )
+language sql
+as $$
+select
+    mp.nome                                  as nome_marca_pneu,
+    mp.codigo                                as cod_marca_pneu,
+    p.codigo,
+    p.codigo_cliente,
+    u.codigo                                 as cod_unidade_alocado,
+    r.codigo                                 as cod_regional_alocado,
+    p.pressao_atual,
+    p.vida_atual,
+    p.vida_total,
+    p.pneu_novo_nunca_rodado,
+    mop.nome                                 as nome_modelo_pneu,
+    mop.codigo                               as cod_modelo_pneu,
+    mop.qt_sulcos                            as qt_sulcos_modelo_pneu,
+    mop.altura_sulcos                        as altura_sulcos_modelo_pneu,
+    pd.altura,
+    pd.largura,
+    pd.aro,
+    pd.codigo                                as cod_dimensao,
+    p.pressao_recomendada,
+    p.altura_sulco_central_interno,
+    p.altura_sulco_central_externo,
+    p.altura_sulco_interno,
+    p.altura_sulco_externo,
+    p.status,
+    p.dot,
+    p.valor,
+    mob.codigo                               as cod_modelo_banda,
+    mob.nome                                 as nome_modelo_banda,
+    mob.qt_sulcos                            as qt_sulcos_modelo_banda,
+    mob.altura_sulcos                        as altura_sulcos_modelo_banda,
+    mab.codigo                               as cod_marca_banda,
+    mab.nome                                 as nome_marca_banda,
+    pvv.valor                                as valor_banda,
+    po.posicao_prolog                        as posicao_pneu,
+    coalesce(ppne.nomenclatura :: text, '-') as posicao_aplicado_cliente,
+    vei.codigo                               as cod_veiculo_aplicado,
+    vei.placa                                as placa_aplicado,
+    vei.identificador_frota                  as identificador_frota
+from pneu p
+         join modelo_pneu mop on mop.codigo = p.cod_modelo
+         join marca_pneu mp on mp.codigo = mop.cod_marca
+         join dimensao_pneu pd on pd.codigo = p.cod_dimensao
+         join unidade u on u.codigo = p.cod_unidade
+         join regional r on u.cod_regional = r.codigo
+         left join veiculo_pneu vp on p.codigo = vp.cod_pneu
+         left join veiculo vei on vei.codigo = vp.cod_veiculo
+         left join veiculo_tipo vt on vt.codigo = vei.cod_tipo and vt.cod_empresa = p.cod_empresa
+         left join veiculo_diagrama vd on vt.cod_diagrama = vd.codigo
+         left join pneu_ordem po on vp.posicao = po.posicao_prolog
+         left join modelo_banda mob on mob.codigo = p.cod_modelo_banda and mob.cod_empresa = u.cod_empresa
+         left join marca_banda mab on mab.codigo = mob.cod_marca and mab.cod_empresa = mob.cod_empresa
+         left join pneu_valor_vida pvv on pvv.cod_pneu = p.codigo and pvv.vida = p.vida_atual
+         left join pneu_posicao_nomenclatura_empresa ppne on
+            ppne.cod_empresa = p.cod_empresa and
+            ppne.cod_diagrama = vd.codigo and
+            ppne.posicao_prolog = vp.posicao
+where vei.placa = f_placa
+order by po.ordem_exibicao asc;
+$$;
