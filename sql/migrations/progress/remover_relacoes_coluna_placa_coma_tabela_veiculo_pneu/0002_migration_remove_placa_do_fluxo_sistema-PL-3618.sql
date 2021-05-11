d3158326387f6c5b976068bc5120645e1272538c
Allan Barbosa <allanbarbosa@prologapp.com>
2021-05-11 11:43:05 -0300

    refactor(veículo): remove placa ao inserir uma movimentação de pneus para veículo
    PL-3618

--- sql/migrations/progress/remover_relacoes_coluna_placa_coma_tabela_veiculo_pneu/0002_migration_remove_placa_do_fluxo_sistema-PL-3618.sql
+++ sql/migrations/progress/remover_relacoes_coluna_placa_coma_tabela_veiculo_pneu/0002_migration_remove_placa_do_fluxo_sistema-PL-3618.sql
@@ -608,3 +608,55 @@ select v_cod_edicao_historico_antigo,
        v_antigo_possui_hubodometro;
 end;
 $$;
+
+create or replace function func_veiculo_insere_veiculo_pneu(f_cod_unidade bigint,
+                                                            f_placa text,
+                                                            f_cod_veiculo bigint,
+                                                            f_cod_pneu bigint,
+                                                            f_posicao bigint)
+    returns boolean
+    language plpgsql
+as
+$$
+declare
+v_cod_empresa  bigint;
+    v_cod_tipo     bigint;
+    v_cod_diagrama bigint;
+begin
+    v_cod_empresa := (select u.cod_empresa
+                      from unidade u
+                      where u.codigo = f_cod_unidade);
+
+    v_cod_tipo := (select vd.cod_tipo
+                   from veiculo_data vd
+                   where vd.placa = f_placa);
+
+    v_cod_diagrama := (select vt.cod_diagrama
+                       from public.veiculo_tipo vt
+                       where vt.codigo = v_cod_tipo
+                         and vt.cod_empresa = v_cod_empresa);
+
+    if v_cod_diagrama is null or v_cod_diagrama <= 0
+    then
+        perform throw_generic_error('Não foi possível realizar o vínculo entre veículo e pneu.');
+end if;
+
+insert into veiculo_pneu(cod_unidade,
+                         cod_pneu,
+                         posicao,
+                         cod_diagrama,
+                         cod_veiculo)
+values (f_cod_unidade,
+        f_cod_pneu,
+        f_posicao,
+        v_cod_diagrama,
+        f_cod_veiculo);
+
+if not found
+    then
+        perform throw_generic_error('Não foi possível realizar o vínculo entre veículo e pneu.');
+end if;
+
+    return found;
+end;
+$$;
