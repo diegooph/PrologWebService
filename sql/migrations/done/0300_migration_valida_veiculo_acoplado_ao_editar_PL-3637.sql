cd89bb164c6cf45e9c048ac552947354dd52d4d2
didi <dioge.vanzella@gmail.com>
2021-05-17 17:35:16 -0300

    fix: corrige function para não lançar versão errada

--- sql/migrations/done/0300_migration_valida_veiculo_acoplado_ao_editar_PL-3637.sql
+++ sql/migrations/done/0300_migration_valida_veiculo_acoplado_ao_editar_PL-3637.sql
@@ -28,14 +28,14 @@ create or replace function func_veiculo_atualiza_veiculo(f_cod_veiculo bigint,
 as
 $$
 declare
-v_cod_empresa       constant  bigint not null  := (select v.cod_empresa
+    v_cod_empresa     constant    bigint not null  := (select v.cod_empresa
                                                        from veiculo v
                                                        where v.codigo = f_cod_veiculo);
     v_novo_cod_diagrama constant  bigint not null  := (select vt.cod_diagrama
                                                        from veiculo_tipo vt
                                                        where vt.codigo = f_novo_cod_tipo
                                                          and vt.cod_empresa = v_cod_empresa);
-    v_novo_cod_marca    constant  bigint not null  := (select mv.cod_marca
+    v_novo_cod_marca  constant    bigint not null  := (select mv.cod_marca
                                                        from modelo_veiculo mv
                                                        where mv.codigo = f_novo_cod_modelo);
     v_cod_edicao_historico_antigo bigint;
@@ -50,25 +50,25 @@ v_cod_empresa       constant  bigint not null  := (select v.cod_empresa
     v_antigo_cod_marca            bigint;
     v_antigo_cod_modelo           bigint;
     v_antigo_status               boolean;
-    v_novo_motorizado   constant  boolean not null := (select vd.motorizado
+    v_novo_motorizado constant    boolean not null := (select vd.motorizado
                                                        from veiculo_diagrama vd
                                                        where vd.codigo = v_novo_cod_diagrama);
     v_antigo_possui_hubodometro   boolean;
     v_acoplado                    boolean;
 begin
-select v.cod_unidade,
-       v.placa,
-       v.identificador_frota,
-       v.km,
-       v.cod_diagrama,
-       v.cod_tipo,
-       mv.cod_marca,
-       v.cod_modelo,
-       v.status_ativo,
-       v.possui_hubodometro,
-       v.acoplado
-into strict
-    v_cod_unidade,
+    select v.cod_unidade,
+           v.placa,
+           v.identificador_frota,
+           v.km,
+           v.cod_diagrama,
+           v.cod_tipo,
+           mv.cod_marca,
+           v.cod_modelo,
+           v.status_ativo,
+           v.possui_hubodometro,
+           v.acoplado
+    into strict
+        v_cod_unidade,
         v_antiga_placa,
         v_antigo_identificador_frota,
         v_antigo_km,
@@ -79,31 +79,40 @@ into strict
         v_antigo_status,
         v_antigo_possui_hubodometro,
         v_acoplado
-from veiculo v
-    join modelo_veiculo mv on v.cod_modelo = mv.codigo
-where v.codigo = f_cod_veiculo;
+    from veiculo v
+             join modelo_veiculo mv on v.cod_modelo = mv.codigo
+    where v.codigo = f_cod_veiculo;
 
-if (f_novo_km < 0)
+    -- Validamos se o km foi inputado corretamente.
+    if (f_novo_km < 0)
     then
         perform throw_generic_error(
                 'A quilometragem do veículo não pode ser um número negativo.');
-end if;
+    end if;
 
+    -- Validamos se o tipo foi alterado mesmo com o veículo contendo pneus aplicados.
     if ((v_antigo_cod_tipo <> f_novo_cod_tipo)
         and (select count(vp.*)
              from veiculo_pneu vp
-             where vp.placa = (select v.placa from veiculo v where v.codigo = f_cod_veiculo)) > 0)
+             where vp.cod_veiculo = f_cod_veiculo) > 0)
     then
         perform throw_generic_error(
                 'O tipo do veículo não pode ser alterado se a placa contém pneus aplicados.');
-end if;
+    end if;
 
+    -- Validamos se o tipo foi alterado mesmo com o veículo fazendo parte de um acoplamento.
     if ((v_antigo_cod_tipo <> f_novo_cod_tipo) and v_acoplado)
     then
         perform throw_generic_error(
                 'O tipo do veículo não pode ser alterado se a placa faz parte de um acoplamento.');
     end if;
 
+    -- Agora que passou nas verificações, calcula quantas alterações foram feitas:
+    -- hstore é uma estrutura que salva os dados como chave => valor. Fazendo hstore(novo) - hstore(antigo) irá
+    -- sobrar apenas as entradas (chave => valor) que mudaram. Depois, aplicamos um akeys(hstore), que retorna um
+    -- array das chaves (apenas as que mudaram) (poderia ser um avalues(hstore) também). Por fim, fazemos um
+    -- f_size_array para saber o tamanho desse array: isso nos dá o número de edições realizadas.
+    -- IMPORTANTE: como a placa não é atualiza no update abaixo, também ignoramos ela na contagem de total de edições.
     v_total_edicoes := f_size_array(akeys(hstore((f_novo_identificador_frota,
                                                   f_novo_km,
                                                   v_novo_cod_diagrama,
@@ -120,56 +129,58 @@ end if;
                                                                                         v_antigo_status,
                                                                                         v_antigo_possui_hubodometro))));
 
+    -- O update no veículo só será feito se algo de fato mudou. E algo só mudou se o total de edições for maior que 0.
     if (v_total_edicoes is not null and v_total_edicoes > 0)
     then
-select codigo_historico_estado_antigo, codigo_historico_estado_novo
-into strict v_cod_edicao_historico_antigo, v_cod_edicao_historico_novo
-from func_veiculo_gera_historico_atualizacao(v_cod_empresa,
-    f_cod_veiculo,
-    f_cod_colaborador_edicao,
-    f_origem_edicao,
-    f_data_hora_edicao,
-    f_informacoes_extras_edicao,
-    f_nova_placa,
-    f_novo_identificador_frota,
-    f_novo_km,
-    v_novo_cod_diagrama,
-    f_novo_cod_tipo,
-    f_novo_cod_modelo,
-    f_novo_status,
-    f_novo_possui_hubodometro,
-    v_total_edicoes);
+        select codigo_historico_estado_antigo, codigo_historico_estado_novo
+        into strict v_cod_edicao_historico_antigo, v_cod_edicao_historico_novo
+        from func_veiculo_gera_historico_atualizacao(v_cod_empresa,
+                                                     f_cod_veiculo,
+                                                     f_cod_colaborador_edicao,
+                                                     f_origem_edicao,
+                                                     f_data_hora_edicao,
+                                                     f_informacoes_extras_edicao,
+                                                     f_nova_placa,
+                                                     f_novo_identificador_frota,
+                                                     f_novo_km,
+                                                     v_novo_cod_diagrama,
+                                                     f_novo_cod_tipo,
+                                                     f_novo_cod_modelo,
+                                                     f_novo_status,
+                                                     f_novo_possui_hubodometro,
+                                                     v_total_edicoes);
 
-update veiculo
-set identificador_frota = f_novo_identificador_frota,
-    km                  = f_novo_km,
-    cod_modelo          = f_novo_cod_modelo,
-    cod_tipo            = f_novo_cod_tipo,
-    cod_diagrama        = v_novo_cod_diagrama,
-    status_ativo        = f_novo_status,
-    motorizado          = v_novo_motorizado,
-    possui_hubodometro  = f_novo_possui_hubodometro,
-    foi_editado         = true
-where codigo = f_cod_veiculo
-  and cod_empresa = v_cod_empresa;
+        update veiculo
+        set identificador_frota = f_novo_identificador_frota,
+            km                  = f_novo_km,
+            cod_modelo          = f_novo_cod_modelo,
+            cod_tipo            = f_novo_cod_tipo,
+            cod_diagrama        = v_novo_cod_diagrama,
+            status_ativo        = f_novo_status,
+            motorizado          = v_novo_motorizado,
+            possui_hubodometro  = f_novo_possui_hubodometro,
+            foi_editado         = true
+        where codigo = f_cod_veiculo
+          and cod_empresa = v_cod_empresa;
 
-if (not found)
+        -- Verificamos se o update na tabela de veículos ocorreu com êxito.
+        if (not found)
         then
             perform throw_generic_error('Não foi possível atualizar o veículo, tente novamente.');
-end if;
-end if;
+        end if;
+    end if;
 
-return query
-select v_cod_edicao_historico_antigo,
-       v_cod_edicao_historico_novo,
-       v_total_edicoes,
-       v_antiga_placa,
-       v_antigo_identificador_frota,
-       v_antigo_km,
-       v_antigo_cod_diagrama,
-       v_antigo_cod_tipo,
-       v_antigo_cod_modelo,
-       v_antigo_status,
-       v_antigo_possui_hubodometro;
+    return query
+        select v_cod_edicao_historico_antigo,
+               v_cod_edicao_historico_novo,
+               v_total_edicoes,
+               v_antiga_placa,
+               v_antigo_identificador_frota,
+               v_antigo_km,
+               v_antigo_cod_diagrama,
+               v_antigo_cod_tipo,
+               v_antigo_cod_modelo,
+               v_antigo_status,
+               v_antigo_possui_hubodometro;
 end;
 $$;
