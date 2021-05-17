1ce2c5abe00173ea35485350ade5677af4ddb96e
didi <dioge.vanzella@gmai.com>
2021-05-16 21:05:14 -0300

    refactor(pneu): altera function que busca alterações de pneus
    PL-3490

--- sql/migrations/progress/logar_bd_quem_altera_pneu/0001_migration_logar_bd_quem_altera_pneu_PL-3490.sql
+++ sql/migrations/progress/logar_bd_quem_altera_pneu/0001_migration_logar_bd_quem_altera_pneu_PL-3490.sql
@@ -48,7 +48,7 @@ create or replace function func_pneu_atualiza(f_cod_cliente text,
 as
 $$
 declare
-v_cod_cliente         text;
+    v_cod_cliente         text;
     v_cod_modelo          bigint;
     v_cod_dimensao        bigint;
     v_cod_modelo_banda    bigint;
@@ -58,16 +58,16 @@ v_cod_cliente         text;
     v_pressao_recomendada double precision;
     v_cod_unidade         bigint;
 begin
-select codigo_cliente,
-       cod_modelo,
-       cod_dimensao,
-       cod_modelo_banda,
-       coalesce(dot, ''),
-       valor,
-       vida_total,
-       pressao_recomendada,
-       cod_unidade
-into strict v_cod_cliente,
+    select codigo_cliente,
+           cod_modelo,
+           cod_dimensao,
+           cod_modelo_banda,
+           coalesce(dot, ''),
+           valor,
+           vida_total,
+           pressao_recomendada,
+           cod_unidade
+    into strict v_cod_cliente,
         v_cod_modelo,
         v_cod_dimensao,
         v_cod_modelo_banda,
@@ -76,10 +76,10 @@ into strict v_cod_cliente,
         v_vida_total,
         v_pressao_recomendada,
         v_cod_unidade
-from pneu
-where codigo = f_cod_original_pneu;
+    from pneu
+    where codigo = f_cod_original_pneu;
 
-if v_cod_cliente != f_cod_cliente
+    if v_cod_cliente != f_cod_cliente
         or v_cod_modelo != f_cod_modelo
         or v_cod_dimensao != f_cod_dimensao
         or v_cod_modelo_banda != f_cod_modelo_banda
@@ -89,66 +89,74 @@ if v_cod_cliente != f_cod_cliente
         or v_pressao_recomendada != f_pressao_recomendada
         or v_cod_unidade != f_cod_unidade
     then
-update pneu
-set codigo_cliente                     = f_cod_cliente,
-    cod_modelo                         = f_cod_modelo,
-    cod_dimensao                       = f_cod_dimensao,
-    cod_modelo_banda                   = f_cod_modelo_banda,
-    dot                                = f_dot,
-    valor                              = f_valor,
-    vida_total                         = f_vida_total,
-    pressao_recomendada                = f_pressao_recomendada,
-    cod_colaborador_ultima_atualizacao = f_cod_colaborador_responsavel_edicao
-where codigo = f_cod_original_pneu
-  and cod_unidade = f_cod_unidade;
-end if;
+        update pneu
+        set codigo_cliente                     = f_cod_cliente,
+            cod_modelo                         = f_cod_modelo,
+            cod_dimensao                       = f_cod_dimensao,
+            cod_modelo_banda                   = f_cod_modelo_banda,
+            dot                                = f_dot,
+            valor                              = f_valor,
+            vida_total                         = f_vida_total,
+            pressao_recomendada                = f_pressao_recomendada,
+            cod_colaborador_ultima_atualizacao = f_cod_colaborador_responsavel_edicao
+        where codigo = f_cod_original_pneu
+          and cod_unidade = f_cod_unidade;
+    end if;
 end
 $$;
 
-create or replace function suporte.func_pneu_busca_historico_edicoes(f_cod_pneu varchar(255))
+create or replace function suporte.func_pneu_busca_historico_edicoes(f_cod_pneu bigint)
     returns table
             (
-                data_hora_utc                      timestamp with time zone,
-                operacao                           varchar,
-                codigo_pneu                        varchar(255),
-                cod_modelo                         bigint,
-                cod_dimensao                       bigint,
-                pressao_recomendada                real,
-                pressao_atual                      real,
-                altura_sulco_interno               real,
-                altura_sulco_central_interno       real,
-                altura_sulco_externo               real,
-                cod_unidade                        bigint,
-                status                             varchar(255),
-                vida_atual                         integer,
-                vida_total                         integer,
-                cod_modelo_banda                   bigint,
-                altura_sulco_central_externo       real,
-                dot                                varchar(20),
-                valor                              real,
-                data_hora_cadastro                 timestamp with time zone,
-                pneu_novo_nunca_rodado             boolean,
-                codigo                             bigint,
-                cod_empresa                        bigint,
-                cod_unidade_cadastro               integer,
-                deletado                           boolean,
-                data_hora_deletado                 timestamp with time zone,
-                pg_username_delecao                text,
-                motivo_delecao                     text,
-                origem_cadastro                    text,
-                cod_colaborador_cadastro           bigint,
-                cod_colaborador_ultima_atualizacao bigint
+                cod_log                               bigint,
+                data_hora_ultima_atualizacao          timestamp without time zone,
+                cpf_colaborador_ultima_atualizacao    bigint,
+                nome_colaborador_ultima_atualizacao   text,
+                status_colaborador_ultima_atualizacao boolean,
+                codigo_pneu                           varchar(255),
+                cod_modelo                            bigint,
+                cod_dimensao                          bigint,
+                pressao_recomendada                   real,
+                pressao_atual                         real,
+                altura_sulco_interno                  real,
+                altura_sulco_central_interno          real,
+                altura_sulco_externo                  real,
+                cod_unidade                           bigint,
+                status                                varchar(255),
+                vida_atual                            integer,
+                vida_total                            integer,
+                cod_modelo_banda                      bigint,
+                altura_sulco_central_externo          real,
+                dot                                   varchar(20),
+                valor                                 real,
+                data_hora_cadastro                    timestamp with time zone,
+                pneu_novo_nunca_rodado                boolean,
+                codigo                                bigint,
+                cod_empresa                           bigint,
+                cod_unidade_cadastro                  integer,
+                deletado                              boolean,
+                data_hora_deletado                    timestamp with time zone,
+                pg_username_delecao                   text,
+                motivo_delecao                        text,
+                origem_cadastro                       text,
+                cod_colaborador_cadastro              bigint,
+                cod_colaborador_ultima_atualizacao    bigint
             )
     language plpgsql
 as
 $$
 begin
-return query
-select pd.data_hora_utc       as data_hora_log,
-       pd.operacao            as operacao,
-       (jsonb_populate_record(NULL::pneu_data, pd.row_log)).*
-from audit.pneu_data_audit pd
-where pd.row_log ->> 'codigo_cliente' = f_cod_pneu
-order by pd.data_hora_utc desc;
+    return query
+        select pd.codigo::bigint                                       as cod_log,
+               pd.data_hora_utc at time zone tz_unidade(c.cod_unidade) as data_hora_ultima_atualizacao,
+               c.cpf                                                   as cpf_colaborador_ultima_atualizacao,
+               c.nome::text                                            as nome_colaborador_ultima_atualizacao,
+               c.status_ativo                                          as status_colaborador_ultima_atualizacao,
+               (jsonb_populate_record(NULL::pneu_data, pd.row_log)).*
+        from audit.pneu_data_audit pd
+                 left join colaborador_data c
+                           on c.codigo = (pd.row_log ->> 'cod_colaborador_ultima_atualizacao')::bigint
+        where (pd.row_log ->> 'codigo')::bigint = f_cod_pneu
+        order by pd.codigo desc;
 end
 $$;
--- sql/prolog_queries/suporte/frota/pneu/func_pneu_busca_historico_edicoes.sql
+++ sql/prolog_queries/suporte/frota/pneu/func_pneu_busca_historico_edicoes.sql
@@ -1,47 +1,55 @@
-create or replace function suporte.func_pneu_busca_historico_edicoes(f_cod_pneu varchar(255))
+create or replace function suporte.func_pneu_busca_historico_edicoes(f_cod_pneu bigint)
     returns table
             (
-                data_hora_utc                      timestamp with time zone,
-                operacao                           varchar,
-                codigo_pneu                        varchar(255),
-                cod_modelo                         bigint,
-                cod_dimensao                       bigint,
-                pressao_recomendada                real,
-                pressao_atual                      real,
-                altura_sulco_interno               real,
-                altura_sulco_central_interno       real,
-                altura_sulco_externo               real,
-                cod_unidade                        bigint,
-                status                             varchar(255),
-                vida_atual                         integer,
-                vida_total                         integer,
-                cod_modelo_banda                   bigint,
-                altura_sulco_central_externo       real,
-                dot                                varchar(20),
-                valor                              real,
-                data_hora_cadastro                 timestamp with time zone,
-                pneu_novo_nunca_rodado             boolean,
-                codigo                             bigint,
-                cod_empresa                        bigint,
-                cod_unidade_cadastro               integer,
-                deletado                           boolean,
-                data_hora_deletado                 timestamp with time zone,
-                pg_username_delecao                text,
-                motivo_delecao                     text,
-                origem_cadastro                    text,
-                cod_colaborador_cadastro           bigint,
-                cod_colaborador_ultima_atualizacao bigint
+                cod_log                               bigint,
+                data_hora_ultima_atualizacao          timestamp without time zone,
+                cpf_colaborador_ultima_atualizacao    bigint,
+                nome_colaborador_ultima_atualizacao   text,
+                status_colaborador_ultima_atualizacao boolean,
+                codigo_pneu                           varchar(255),
+                cod_modelo                            bigint,
+                cod_dimensao                          bigint,
+                pressao_recomendada                   real,
+                pressao_atual                         real,
+                altura_sulco_interno                  real,
+                altura_sulco_central_interno          real,
+                altura_sulco_externo                  real,
+                cod_unidade                           bigint,
+                status                                varchar(255),
+                vida_atual                            integer,
+                vida_total                            integer,
+                cod_modelo_banda                      bigint,
+                altura_sulco_central_externo          real,
+                dot                                   varchar(20),
+                valor                                 real,
+                data_hora_cadastro                    timestamp with time zone,
+                pneu_novo_nunca_rodado                boolean,
+                codigo                                bigint,
+                cod_empresa                           bigint,
+                cod_unidade_cadastro                  integer,
+                deletado                              boolean,
+                data_hora_deletado                    timestamp with time zone,
+                pg_username_delecao                   text,
+                motivo_delecao                        text,
+                origem_cadastro                       text,
+                cod_colaborador_cadastro              bigint,
+                cod_colaborador_ultima_atualizacao    bigint
             )
     language plpgsql
 as
 $$
 begin
 return query
-select pd.data_hora_utc       as data_hora_log,
-       pd.operacao            as operacao,
+select pd.codigo::bigint                                       as cod_log,
+        pd.data_hora_utc at time zone tz_unidade(c.cod_unidade) as data_hora_ultima_atualizacao,
+       c.cpf                                                   as cpf_colaborador_ultima_atualizacao,
+       c.nome::text                                            as nome_colaborador_ultima_atualizacao,
+        c.status_ativo                                          as status_colaborador_ultima_atualizacao,
        (jsonb_populate_record(NULL::pneu_data, pd.row_log)).*
 from audit.pneu_data_audit pd
-where pd.row_log ->> 'codigo_cliente' = f_cod_pneu
-order by pd.data_hora_utc desc;
+    left join colaborador_data c
+on c.codigo = (pd.row_log ->> 'cod_colaborador_ultima_atualizacao')::bigint
+where (pd.row_log ->> 'codigo')::bigint = f_cod_pneu
+order by pd.codigo desc;
 end
 $$;
