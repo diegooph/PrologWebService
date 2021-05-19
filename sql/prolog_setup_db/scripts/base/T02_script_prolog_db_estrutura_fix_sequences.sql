971659e467650f3e32c0b70dc1fa129d0d86adf1
didi <dioge.vanzella@gmail.com>
2021-05-19 09:58:46 -0300

    refactor: corrige sequences com nomes diferentes de Prod
    PL-3643

---
+++ sql/prolog_setup_db/scripts/base/T02_script_prolog_db_estrutura_fix_sequences.sql
@@ -0,0 +1,7 @@
+alter sequence checklist_modelo_data_codigo_seq rename to checklist_modelo_codigo_seq;
+
+alter sequence pneu_data_codigo_seq rename to pneu_codigo_seq;
+
+alter sequence checklist_perguntas_data_codigo_seq rename to checklist_perguntas_codigo_seq1;
+
+alter sequence checklist_alternativa_pergunta_data_codigo_seq rename to checklist_alternativa_pergunta_codigo_seq1;
