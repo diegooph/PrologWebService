75d39a704e1717ab85a4149a3f7e9031e63cc590
Steinert999 <steinert999@outlook.com>
2021-05-19 15:43:37 -0300

    feat(pneu): cria entidade MarcaPneu
    PL-3646

---
+++ src/main/java/br/com/zalf/prolog/webservice/v3/frota/pneu/_model/MarcaPneuEntity.java
@@ -0,0 +1,21 @@
+package br.com.zalf.prolog.webservice.v3.frota.pneu._model;
+
+import javax.persistence.*;
+
+/**
+ * Created on 2021-05-18
+ *
+ * @author Guilherme Steinert (https://github.com/steinert999)
+ */
+@Entity
+@Table(name = "marca_pneu", schema = "public")
+public class MarcaPneuEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo", nullable = false)
+    private Long codigo;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+}
