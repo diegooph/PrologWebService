cfe21d511ad022a786d306ada0cbf59f7c339514
Steinert999 <steinert999@outlook.com>
2021-05-21 16:08:59 -0300

    feat(servico): cria ServicoPneuStatus
    PL-3645

---
+++ src/main/java/br/com/zalf/prolog/webservice/v3/frota/servicopneu/_model/ServicoPneuStatus.java
@@ -0,0 +1,10 @@
+package br.com.zalf.prolog.webservice.v3.frota.servicopneu._model;
+
+/**
+ * Created on 2021-05-21
+ *
+ * @author Guilherme Steinert (https://github.com/steinert999)
+ */
+public enum ServicoPneuStatus {
+    ABERTO, FECHADO
+}
