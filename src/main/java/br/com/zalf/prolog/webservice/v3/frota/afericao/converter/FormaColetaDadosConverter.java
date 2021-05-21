2ac723175efe802941cf3e159364f1b33b78c9f9
Steinert999 <steinert999@outlook.com>
2021-05-21 16:11:21 -0300

    feat(servico): cria converter para FormaColetaDadosAfericaoEnum
    PL-3645

---
+++ src/main/java/br/com/zalf/prolog/webservice/v3/frota/afericao/converter/FormaColetaDadosConverter.java
@@ -0,0 +1,31 @@
+package br.com.zalf.prolog.webservice.v3.frota.afericao.converter;
+
+import br.com.zalf.prolog.webservice.frota.pneu.afericao.configuracao._model.FormaColetaDadosAfericaoEnum;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+/**
+ * Created on 2021-05-21
+ *
+ * @author Guilherme Steinert (https://github.com/steinert999)
+ */
+@Converter(autoApply = true)
+public class FormaColetaDadosConverter implements AttributeConverter<FormaColetaDadosAfericaoEnum, String> {
+
+    @Override
+    public String convertToDatabaseColumn(final FormaColetaDadosAfericaoEnum formaColetaDados) {
+        if (formaColetaDados == null) {
+            return null;
+        }
+        return formaColetaDados.toString();
+    }
+
+    @Override
+    public FormaColetaDadosAfericaoEnum convertToEntityAttribute(final String s) {
+        if (s == null || s.equals("")) {
+            return null;
+        }
+        return FormaColetaDadosAfericaoEnum.fromString(s);
+    }
+}
