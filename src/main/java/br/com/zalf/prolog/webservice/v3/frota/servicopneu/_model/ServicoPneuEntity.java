87091483f2894a164d7c63d37bf4ad553337fd18
Steinert999 <steinert999@outlook.com>
2021-05-21 16:12:18 -0300

    feat(servico): adiciona relacionamentos entre entidades em ServicoPneuEntity
    PL-3645

--- src/main/java/br/com/zalf/prolog/webservice/v3/frota/servicopneu/_model/ServicoPneuEntity.java
+++ src/main/java/br/com/zalf/prolog/webservice/v3/frota/servicopneu/_model/ServicoPneuEntity.java
@@ -1,8 +1,13 @@
 package br.com.zalf.prolog.webservice.v3.frota.servicopneu._model;
 
+import br.com.zalf.prolog.webservice.frota.pneu.afericao.configuracao._model.FormaColetaDadosAfericaoEnum;
+import br.com.zalf.prolog.webservice.frota.pneu.servico._model.TipoServico;
 import br.com.zalf.prolog.webservice.v3.frota.afericao._model.AfericaoEntity;
 import br.com.zalf.prolog.webservice.v3.frota.kmprocessos._model.EntityKmColetado;
 import br.com.zalf.prolog.webservice.v3.frota.kmprocessos._model.VeiculoKmColetado;
+import br.com.zalf.prolog.webservice.v3.frota.movimentacao._model.ColaboradorEntity;
+import br.com.zalf.prolog.webservice.v3.frota.movimentacao._model.MovimentacaoProcessoEntity;
+import br.com.zalf.prolog.webservice.v3.frota.pneu._model.PneuEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,6 +15,7 @@ import lombok.NoArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 /**
  * Created on 2021-03-25
@@ -34,6 +40,43 @@ public final class ServicoPneuEntity implements EntityKmColetado {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "cod_afericao", nullable = false)
     private AfericaoEntity afericao;
+    @Column(name = "tipo_servico", nullable = false)
+    private TipoServico tipoServico;
+    @ManyToOne
+    @JoinColumn(name = "cod_pneu", referencedColumnName = "codigo")
+    private PneuEntity pneu;
+    @ManyToOne
+    @JoinColumn(name = "cpf_mecanico", referencedColumnName = "cpf")
+    private ColaboradorEntity mecanico;
+    @Column(name = "data_hora_resolucao")
+    private LocalDateTime dataHoraResolucao;
+    @Column(name = "qt_apontamentos", nullable = false, columnDefinition = "default 1")
+    private Integer quantidadeApontamentos;
+    @Column(name = "psi_apos_conserto")
+    private Double psiAposConserto;
+    @Column(name = "km_momento_concerto")
+    private Long kmMomentoConserto;
+    @Column(name = "cod_alternativa")
+    private Long codAlternativa;
+    @ManyToOne
+    @JoinColumn(name = "cod_pneu_inserido", referencedColumnName = "codigo")
+    private PneuEntity pneuInserido;
+    @ManyToOne
+    @JoinColumn(name = "cod_processo_movimentacao", referencedColumnName = "codigo")
+    private MovimentacaoProcessoEntity movimentacaoProcesso;
+    @Column(name = "tempo_realizacao_millis")
+    private Long tempoRealizacaoMillis;
+    @Column(name = "fechado_automaticamente_movimentacao", nullable = false, columnDefinition = "default false")
+    private boolean fechadoMovimentacao;
+    @Column(name = "fechado_automaticamente_integracao", nullable = false, columnDefinition = "default false")
+    private boolean fechadoIntegracao;
+    @Column(name = "fechado_automaticamente_afericao", nullable = false, columnDefinition = "default false")
+    private boolean fechadoAfericao;
+    @ManyToOne
+    @JoinColumn(name = "cod_afericao_fechamento_automatico", referencedColumnName = "codigo")
+    private AfericaoEntity afericaoFechamentoAutomatico;
+    @Column(name = "forma_coleta_dados_fechamento")
+    private FormaColetaDadosAfericaoEnum formaColetaDadosFechamento;
 
     @NotNull
     @Override
