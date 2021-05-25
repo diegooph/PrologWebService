d74d29de2189ad812e268f209dca2f04594047d8
Steinert999 <steinert999@outlook.com>
2021-05-25 11:37:01 -0300

    feat(afericao): adiciona parâmetro de filtroFechado no enum
    PL-3645

--- src/main/java/br/com/zalf/prolog/webservice/v3/frota/servicopneu/_model/ServicoPneuStatus.java
+++ src/main/java/br/com/zalf/prolog/webservice/v3/frota/servicopneu/_model/ServicoPneuStatus.java
@@ -1,10 +1,17 @@
 package br.com.zalf.prolog.webservice.v3.frota.servicopneu._model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
  * Created on 2021-05-21
  *
  * @author Guilherme Steinert (https://github.com/steinert999)
  */
+@AllArgsConstructor
+@Getter
 public enum ServicoPneuStatus {
-    ABERTO, FECHADO
+    ABERTO(false), FECHADO(true), DEFAULT(null);
+
+    private final Boolean filtroFechado;
 }
