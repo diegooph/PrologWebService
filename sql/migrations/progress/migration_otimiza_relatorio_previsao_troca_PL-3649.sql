2da34b33b85ae9f03418401812a3b41694866914
Luiz Felipe <luiz.felipe_95@hotmail.com>
2021-05-26 15:52:09 -0300

    refactor(pneu): melhora assinatura do relatório func_pneu_relatorio_previsao_troca_consolidado
    PL-3649

--- sql/migrations/progress/migration_otimiza_relatorio_previsao_troca_PL-3649.sql
+++ sql/migrations/progress/migration_otimiza_relatorio_previsao_troca_PL-3649.sql
@@ -82,6 +82,40 @@ where vap.cod_unidade = any (f_cod_unidades)
 order by vap.nome_unidade_alocado;
 $$;
 
+drop function func_relatorio_pneu_previsao_troca_consolidado(f_cod_unidade text[],
+    f_status_pneu text,
+    f_data_inicial date,
+    f_data_final date);
+create function func_pneu_relatorio_previsao_troca_consolidado(f_cod_unidades bigint[],
+                                                               f_status_pneu text,
+                                                               f_data_inicial date,
+                                                               f_data_final date)
+    returns table
+            (
+                "UNIDADE"    text,
+                data         text,
+                marca        text,
+                modelo       text,
+                medidas      text,
+                "QUANTIDADE" bigint
+            )
+    language sql
+as
+$$
+select vap."UNIDADE ALOCADO",
+       to_char(vap."PREVISÃO DE TROCA", 'DD/MM/YYYY') as data,
+       vap."MARCA",
+       vap."MODELO",
+       vap."MEDIDAS",
+       count(vap."MODELO")                            as quantidade
+from view_analise_pneus vap
+where vap.cod_unidade = any (f_cod_unidades)
+  and vap."PREVISÃO DE TROCA" between f_data_inicial and f_data_final
+  and vap."STATUS PNEU" = f_status_pneu
+group by vap."UNIDADE ALOCADO", vap."PREVISÃO DE TROCA", vap."MARCA", vap."MODELO", vap."MEDIDAS"
+order by vap."UNIDADE ALOCADO", vap."PREVISÃO DE TROCA", quantidade desc;
+$$;
+
 drop view view_pneu_analise_vida_atual;
 create or replace view view_pneu_analise_vida_atual as
 with dados as (
--- sql/prolog_queries/frota/pneu/relatorios/func_pneu_relatorio_previsao_troca_consolidado.sql
+++ sql/prolog_queries/frota/pneu/relatorios/func_pneu_relatorio_previsao_troca_consolidado.sql
@@ -1,10 +1,7 @@
 -- Sobre:
 --
--- A function trás a previsão de quantos pneus você deve comprar em qual data, agrupados por marca, modelo e medida.
---
--- Histórico:
--- 2020-12-01 -> Criado arquivo específico. (gustavocnp95 PL-3332)
-create function func_relatorio_pneu_previsao_troca_consolidado(f_cod_unidade text[],
+-- A function traz a previsão de quantos pneus você deve comprar e em qual data, agrupados por marca, modelo e medida.
+create function func_pneu_relatorio_previsao_troca_consolidado(f_cod_unidades bigint[],
                                                                f_status_pneu text,
                                                                f_data_inicial date,
                                                                f_data_final date)
@@ -27,9 +24,9 @@ select vap."UNIDADE ALOCADO",
        vap."MEDIDAS",
        count(vap."MODELO")                            as quantidade
 from view_analise_pneus vap
-where vap.cod_unidade::text like any (f_cod_unidade)
+where vap.cod_unidade = any (f_cod_unidades)
   and vap."PREVISÃO DE TROCA" between f_data_inicial and f_data_final
   and vap."STATUS PNEU" = f_status_pneu
 group by vap."UNIDADE ALOCADO", vap."PREVISÃO DE TROCA", vap."MARCA", vap."MODELO", vap."MEDIDAS"
-order by vap."UNIDADE ALOCADO", vap."PREVISÃO DE TROCA" asc, quantidade desc;
+order by vap."UNIDADE ALOCADO", vap."PREVISÃO DE TROCA", quantidade desc;
 $$;
