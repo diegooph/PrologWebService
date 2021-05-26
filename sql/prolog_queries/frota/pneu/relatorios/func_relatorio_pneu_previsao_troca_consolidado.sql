6d6c34596116313c4c40636b6d11137eb93d5858
Luiz Felipe <luiz.felipe_95@hotmail.com>
2021-05-26 14:53:41 -0300

    refactor(pneu): indenta functions de previsão de troca

--- sql/prolog_queries/frota/pneu/relatorios/func_relatorio_pneu_previsao_troca_consolidado.sql
+++ sql/prolog_queries/frota/pneu/relatorios/func_relatorio_pneu_previsao_troca_consolidado.sql
@@ -4,22 +4,32 @@
 --
 -- Histórico:
 -- 2020-12-01 -> Criado arquivo específico. (gustavocnp95 PL-3332)
-create function func_relatorio_pneu_previsao_troca_consolidado(f_cod_unidade text[], f_status_pneu text, f_data_inicial date, f_data_final date)
-    returns TABLE("UNIDADE" text, data text, marca text, modelo text, medidas text, "QUANTIDADE" bigint)
+create function func_relatorio_pneu_previsao_troca_consolidado(f_cod_unidade text[],
+                                                               f_status_pneu text,
+                                                               f_data_inicial date,
+                                                               f_data_final date)
+    returns table
+            (
+                "UNIDADE"    text,
+                data         text,
+                marca        text,
+                modelo       text,
+                medidas      text,
+                "QUANTIDADE" bigint
+            )
     language sql
 as
 $$
-SELECT
-  VAP."UNIDADE ALOCADO",
-  TO_CHAR(VAP."PREVISÃO DE TROCA", 'DD/MM/YYYY') AS DATA,
-  VAP."MARCA",
-  VAP."MODELO",
-  VAP."MEDIDAS",
-  COUNT(VAP."MODELO") AS QUANTIDADE
-FROM VIEW_ANALISE_PNEUS VAP
-WHERE VAP.COD_UNIDADE::TEXT LIKE ANY (f_cod_unidade)
-      AND VAP."PREVISÃO DE TROCA" BETWEEN f_data_inicial AND f_data_final
-      AND VAP."STATUS PNEU" = f_status_pneu
-GROUP BY VAP."UNIDADE ALOCADO", VAP."PREVISÃO DE TROCA", VAP."MARCA",  VAP."MODELO",  VAP."MEDIDAS"
-ORDER BY VAP."UNIDADE ALOCADO", VAP."PREVISÃO DE TROCA" ASC, QUANTIDADE DESC;
+select vap."UNIDADE ALOCADO",
+       to_char(vap."PREVISÃO DE TROCA", 'DD/MM/YYYY') as data,
+       vap."MARCA",
+       vap."MODELO",
+       vap."MEDIDAS",
+       count(vap."MODELO")                            as quantidade
+from view_analise_pneus vap
+where vap.cod_unidade::text like any (f_cod_unidade)
+  and vap."PREVISÃO DE TROCA" between f_data_inicial and f_data_final
+  and vap."STATUS PNEU" = f_status_pneu
+group by vap."UNIDADE ALOCADO", vap."PREVISÃO DE TROCA", vap."MARCA", vap."MODELO", vap."MEDIDAS"
+order by vap."UNIDADE ALOCADO", vap."PREVISÃO DE TROCA" asc, quantidade desc;
 $$;
