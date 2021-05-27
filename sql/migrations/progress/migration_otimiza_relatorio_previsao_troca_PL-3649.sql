fecb9eccff49ea9fc74ab0d8399f04caa2f49cd8
Luiz Felipe <luiz.felipe_95@hotmail.com>
2021-05-27 08:51:56 -0300

    refactor(pneu): altera nomes colunas func_pneu_relatorio_previsao_troca_consolidado
    PL-3649

--- sql/migrations/progress/migration_otimiza_relatorio_previsao_troca_PL-3649.sql
+++ sql/migrations/progress/migration_otimiza_relatorio_previsao_troca_PL-3649.sql
@@ -102,12 +102,12 @@ create function func_pneu_relatorio_previsao_troca_consolidado(f_cod_unidades bi
     language sql
 as
 $$
-select vap.nome_unidade_alocado                       as nome_unidade_alocado,
-       to_char(vap."PREVISÃO DE TROCA", 'DD/MM/YYYY') as data,
-       vap.nome_marca                                 as nome_marca,
-       vap.nome_modelo                                as nome_modelo,
-       vap.medidas                                    as medidas,
-       count(vap.nome_modelo)                         as quantidade
+select vap.nome_unidade_alocado                  as nome_unidade_alocado,
+       to_char(vap.previsao_troca, 'DD/MM/YYYY') as data,
+       vap.nome_marca                            as nome_marca,
+       vap.nome_modelo                           as nome_modelo,
+       vap.medidas                               as medidas,
+       count(vap.nome_modelo)                    as quantidade
 from view_analise_pneus vap
 where vap.cod_unidade = any (f_cod_unidades)
   and vap.previsao_troca between f_data_inicial and f_data_final
