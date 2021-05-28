540af0b3c3e109e7499c208c744bd13014520326
Steinert999 <steinert999@outlook.com>
2021-05-28 10:27:52 -0300

    docs(afericao): adiciona documentação via swagger para o endpoint de ServicoPneu
    PL-3645

--- src/main/java/br/com/zalf/prolog/webservice/v3/frota/servicopneu/ServicoPneuApiDoc.java
+++ src/main/java/br/com/zalf/prolog/webservice/v3/frota/servicopneu/ServicoPneuApiDoc.java
@@ -2,9 +2,11 @@ package br.com.zalf.prolog.webservice.v3.frota.servicopneu;
 
 import br.com.zalf.prolog.webservice.v3.frota.servicopneu._model.ServicoPneuListagemDto;
 import br.com.zalf.prolog.webservice.v3.frota.servicopneu._model.ServicoPneuStatus;
+import io.swagger.annotations.*;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import javax.ws.rs.core.Response;
 import java.util.List;
 
 /**
@@ -12,12 +14,38 @@ import java.util.List;
  *
  * @author Guilherme Steinert (https://github.com/steinert999)
  */
+@Api(value = "Serviços de pneus")
 public interface ServicoPneuApiDoc {
 
-    List<ServicoPneuListagemDto> getServicosByUnidadeAndStatus(final @NotNull List<Long> codUnidades,
-                                                               final @NotNull ServicoPneuStatus status,
-                                                               final @Nullable Long codVeiculo,
-                                                               final @Nullable Long codPneu,
-                                                               final int limit,
-                                                               final int offset);
+    @ApiOperation(
+            value = "Lista serviços de pneus abertos e fechados.",
+            response = ServicoPneuListagemDto.class,
+            responseContainer = "List")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    code = 200, message = "Operação efetuada com sucesso.",
+                    response = ServicoPneuListagemDto.class,
+                    responseContainer = "List"),
+            @ApiResponse(code = 401, message = "Operação não autorizada", response = Response.class),
+            @ApiResponse(code = 404, message = "Operação não encontrada", response = Response.class),
+            @ApiResponse(code = 500, message = "Erro ao executar operação", response = Response.class)
+    })
+    List<ServicoPneuListagemDto> getServicosByUnidadeAndStatus(
+            @ApiParam(value = "Lista de códigos de unidade.",
+                      example = "215",
+                      required = true) final @NotNull List<Long> codUnidades,
+            @ApiParam(value = "Status do servico.",
+                      allowEmptyValue = true,
+                      example = "ABERTO",
+                      allowableValues = "ABERTO, FECHADO") final @NotNull ServicoPneuStatus status,
+            @ApiParam(value = "Código do veiculo.",
+                      allowEmptyValue = true) final @Nullable Long codVeiculo,
+            @ApiParam(value = "Código do pneu.",
+                      allowEmptyValue = true) final @Nullable Long codPneu,
+            @ApiParam(value = "limite de ordens de serviço retornados pela busca. O valor máximo é 1000.",
+                      example = "1000",
+                      required = true) final int limit,
+            @ApiParam(value = "Offset de ordens de serviço. A partir de qual checklist deve-se começar a busca.",
+                      example = "0",
+                      required = true) final int offset);
 }
--- src/main/java/br/com/zalf/prolog/webservice/v3/frota/servicopneu/ServicoPneuResource.java
+++ src/main/java/br/com/zalf/prolog/webservice/v3/frota/servicopneu/ServicoPneuResource.java
@@ -1,5 +1,6 @@
 package br.com.zalf.prolog.webservice.v3.frota.servicopneu;
 
+import br.com.zalf.prolog.webservice.interceptors.ApiExposed;
 import br.com.zalf.prolog.webservice.interceptors.auth.Secured;
 import br.com.zalf.prolog.webservice.interceptors.debug.ConsoleDebugLog;
 import br.com.zalf.prolog.webservice.permissao.pilares.Pilares;
@@ -34,6 +35,7 @@ public class ServicoPneuResource implements ServicoPneuApiDoc {
     private final ServicoPneuListagemMapper mapper;
 
     @GET
+    @ApiExposed
     @Secured(permissions = Pilares.Frota.OrdemServico.Pneu.VISUALIZAR)
     @Override
     @NotNull
