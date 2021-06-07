06040b62e4b80a67c6a681f36798941dd3160df7
thaisksf <thaisfrancisco@prologapp.com>
2021-06-07 10:10:00 -0300

    feat(veiculo): cria DTO para lista de veiculos acoplados
    PL-3672

---
+++ src/main/java/br/com/zalf/prolog/webservice/v3/frota/veiculo/_model/VeiculosAcopladosListagemDto.java
@@ -0,0 +1,23 @@
+package br.com.zalf.prolog.webservice.v3.frota.veiculo._model;
+
+import br.com.zalf.prolog.webservice.frota.veiculo.model.listagem.VeiculoAcopladoListagem;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+/**
+ * Created on 2021-06-07
+ *
+ * @author Thais Francisco (https://github.com/thaisksf)
+ */
+@Data
+public class VeiculosAcopladosListagemDto {
+    @ApiModelProperty(value = "Código do processo de acoplamento", required = true, example = "1")
+    @NotNull
+    Long codProcessoAcoplamento;
+    @ApiModelProperty(value = "Caso hajam veículos acoplados, lista quais são separados pelo código de acoplamento.")
+    @NotNull
+    List<VeiculoAcopladoListagem> veiculosAcoplados;
+}
