fb5e33fadc71e1c9bf0cbc22a37b847c61795e32
Luiz Felipe <luiz.felipe_95@hotmail.com>
2021-06-07 23:59:52 -0300

    refactor: organiza migrations

--- sql/migrations/progress/migration_listagem_pneus_api_PL-3646.sql
+++
@@ -1,5 +0,0 @@
-begin transaction;
-alter table pneu_posicao_nomenclatura_empresa
-    add constraint pk_diagrama_empresa_posicao_prolog
-        primary key (cod_empresa, cod_diagrama, posicao_prolog);
-end transaction;
