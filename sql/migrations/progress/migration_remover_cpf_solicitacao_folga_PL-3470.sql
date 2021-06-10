3be634b5d362592fd17e9dc4b0c830c9260b7589
Steinert999 <steinert999@outlook.com>
2021-06-10 17:19:59 -0300

    feat(colaborador): adiciona atualização das colunas de código
    PL-3470

--- sql/migrations/progress/migration_remover_cpf_solicitacao_folga_PL-3470.sql
+++ sql/migrations/progress/migration_remover_cpf_solicitacao_folga_PL-3470.sql
@@ -8,6 +8,10 @@ alter table solicitacao_folga
     add constraint fk_solicitacao_folga_cod_colaborador_feedback foreign key (cod_colaborador_feedback)
         references colaborador_data (codigo);
 
+update solicitacao_folga sf
+set cod_colaborador          = (select codigo from colaborador_data cd where cd.cpf = sf.cpf_colaborador),
+    cod_colaborador_feedback = (select codigo from colaborador_data cd where cd.cpf = sf.cpf_feedback);
+
 alter table solicitacao_folga
     drop column if exists cpf_colaborador,
     drop column if exists cpf_feedback;
