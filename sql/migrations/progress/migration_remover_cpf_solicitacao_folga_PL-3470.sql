49634b6450743c2c6c7ebdce955ec5f66c8f2b06
Steinert999 <steinert999@outlook.com>
2021-06-10 17:17:52 -0300

    feat(colaborador): adiciona cod_colaborador e cod_colaborador_feedback e suas constraints
    PL-3470

--- sql/migrations/progress/migration_remover_cpf_solicitacao_folga_PL-3470.sql
+++ sql/migrations/progress/migration_remover_cpf_solicitacao_folga_PL-3470.sql
@@ -1,2 +1,11 @@
 begin transaction;
+alter table solicitacao_folga
+    add column if not exists cod_colaborador          bigint,
+    add constraint fk_solicitacao_folga_cod_colaborador foreign key (cod_colaborador)
+        references colaborador_data (codigo),
+
+    add column if not exists cod_colaborador_feedback bigint,
+    add constraint fk_solicitacao_folga_cod_colaborador_feedback foreign key (cod_colaborador_feedback)
+        references colaborador_data (codigo);
+
 end transaction;
