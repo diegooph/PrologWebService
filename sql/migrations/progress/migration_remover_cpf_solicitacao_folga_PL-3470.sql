0c3a5d6f12ca4b34e4b6c2ec6ac2b5e5ae8b8ec2
Steinert999 <steinert999@outlook.com>
2021-06-10 17:19:00 -0300

    feat(colaborador): adiciona alteração para remoção das colunas de cpf_colaborador e cpf_colaborador_feedback
    PL-3470

--- sql/migrations/progress/migration_remover_cpf_solicitacao_folga_PL-3470.sql
+++ sql/migrations/progress/migration_remover_cpf_solicitacao_folga_PL-3470.sql
@@ -8,4 +8,8 @@ alter table solicitacao_folga
     add constraint fk_solicitacao_folga_cod_colaborador_feedback foreign key (cod_colaborador_feedback)
         references colaborador_data (codigo);
 
+alter table solicitacao_folga
+    drop column if exists cpf_colaborador,
+    drop column if exists cpf_feedback;
+
 end transaction;
