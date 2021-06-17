e121fdb7f1c5581f778fdb99614099f30b911679
thaisksf <thaisfrancisco@prologapp.com>
2021-06-17 13:38:46 -0300

    refactor(veiculo): modifica método de cadastro passando dto
    PL-3670

--- src/main/java/br/com/zalf/prolog/webservice/v3/frota/veiculo/VeiculoResource.java
+++ src/main/java/br/com/zalf/prolog/webservice/v3/frota/veiculo/VeiculoResource.java
@@ -8,7 +8,6 @@ import br.com.zalf.prolog.webservice.interceptors.auth.Secured;
 import br.com.zalf.prolog.webservice.interceptors.debug.ConsoleDebugLog;
 import br.com.zalf.prolog.webservice.permissao.pilares.Pilares;
 import br.com.zalf.prolog.webservice.v3.frota.veiculo._model.VeiculoCadastroDto;
-import br.com.zalf.prolog.webservice.v3.frota.veiculo._model.VeiculoEntity;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -42,7 +41,6 @@ public class VeiculoResource implements VeiculoResourceApiDoc {
     public SuccessResponse insert(
             @HeaderParam(PrologCustomHeaders.HEADER_TOKEN_INTEGRACAO) @Optional final String tokenIntegracao,
             @Valid final VeiculoCadastroDto veiculoCadastroDto) {
-        final VeiculoEntity veiculoEntity = veiculoMapper.toEntity(veiculoCadastroDto);
-        return veiculoService.insert(tokenIntegracao, veiculoEntity);
+        return veiculoService.insert(tokenIntegracao, veiculoCadastroDto);
     }
 }
