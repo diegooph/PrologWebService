9ddf33a5886a66f99797d4fecb3cef4d826eee35
thaisksf <thaisfrancisco@prologapp.com>
2021-06-17 11:14:24 -0300

    refactor(veiculo): deleta classes não mais utilizadas :eyes:
    PL-3670

--- src/main/java/br/com/zalf/prolog/webservice/frota/veiculo/tipoveiculo/v3/TipoVeiculoV3Dao.java
+++
@@ -1,9 +0,0 @@
-package br.com.zalf.prolog.webservice.frota.veiculo.tipoveiculo.v3;
-
-import br.com.zalf.prolog.webservice.frota.veiculo.tipoveiculo.v3._model.TipoVeiculoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TipoVeiculoV3Dao extends JpaRepository<TipoVeiculoEntity, Long> {
-}
