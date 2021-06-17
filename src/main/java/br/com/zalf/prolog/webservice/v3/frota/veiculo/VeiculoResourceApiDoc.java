000573c28114535b1a5ceb32296cb463010b94e3
thaisksf <thaisfrancisco@prologapp.com>
2021-06-17 13:35:56 -0300

    refactor(veiculo): adiciona listagem
    PL-3670

--- src/main/java/br/com/zalf/prolog/webservice/v3/frota/veiculo/VeiculoResourceApiDoc.java
+++ src/main/java/br/com/zalf/prolog/webservice/v3/frota/veiculo/VeiculoResourceApiDoc.java
@@ -2,10 +2,17 @@ package br.com.zalf.prolog.webservice.v3.frota.veiculo;
 
 import br.com.zalf.prolog.webservice.commons.network.Response;
 import br.com.zalf.prolog.webservice.commons.network.SuccessResponse;
+import br.com.zalf.prolog.webservice.commons.network.metadata.Required;
+import br.com.zalf.prolog.webservice.interceptors.auth.Secured;
+import br.com.zalf.prolog.webservice.permissao.pilares.Pilares;
 import br.com.zalf.prolog.webservice.v3.frota.veiculo._model.VeiculoCadastroDto;
+import br.com.zalf.prolog.webservice.v3.frota.veiculo._model.VeiculoListagemDto;
 import io.swagger.annotations.*;
 
 import javax.validation.Valid;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.QueryParam;
+import java.util.List;
 
 @Api(value = "Gestão de Veículos")
 public interface VeiculoResourceApiDoc {
@@ -20,4 +27,15 @@ public interface VeiculoResourceApiDoc {
             @ApiParam(hidden = true) final String tokenIntegracao,
             @ApiParam(value = "Dados do veículo para inserir.",
                       required = true) @Valid final VeiculoCadastroDto veiculoCadastroDto);
+
+    @Secured(permissions = {
+            Pilares.Frota.Veiculo.VISUALIZAR,
+            Pilares.Frota.Veiculo.ALTERAR,
+            Pilares.Frota.Veiculo.CADASTRAR,
+            Pilares.Frota.Checklist.VISUALIZAR_TODOS})
+    List<VeiculoListagemDto> getListagemVeiculos(
+            @QueryParam("codUnidades") @Required List<Long> codUnidades,
+            @QueryParam("incluirInativos") @DefaultValue("true") boolean incluirInativos,
+            @QueryParam("limit") int limit,
+            @QueryParam("offset") int offset);
 }
