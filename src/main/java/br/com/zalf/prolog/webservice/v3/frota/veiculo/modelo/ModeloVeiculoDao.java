5dbfb4d7b122edefbd447cb70339ecfa66fc4a0e
thaisksf <thaisfrancisco@prologapp.com>
2021-06-17 11:19:45 -0300

    feat(veiculo): cria dao para modelo de veiculo
    PL-3670

---
+++ src/main/java/br/com/zalf/prolog/webservice/v3/frota/veiculo/modelo/ModeloVeiculoDao.java
@@ -0,0 +1,19 @@
+package br.com.zalf.prolog.webservice.v3.frota.veiculo.modelo;
+
+import br.com.zalf.prolog.webservice.v3.frota.veiculo.modelo._model.ModeloVeiculoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created on 2021-06-16
+ *
+ * @author Thais Francisco (https://github.com/thaisksf)
+ */
+@Repository
+public interface ModeloVeiculoDao extends JpaRepository<ModeloVeiculoEntity, Long> {
+    @Query("select mve from ModeloVeiculoEntity mve" +
+                   " join fetch mve.marcaVeiculoEntity ma" +
+                   " where mve.codigo = :codModelo")
+    ModeloVeiculoEntity getByCod(Long codModelo);
+}
