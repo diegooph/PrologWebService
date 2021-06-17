0a18ef658c53db8eba0f2c0b7b2d073bcb67b8a7
gustavocnp95 <c98jjkdhgy>
2021-06-17 15:26:04 -0300

    refactor(checklist): atualiza arquivo especifico
    PL-3424

--- sql/prolog_queries/frota/checklist/offline/func_checklist_offline_get_colaboradores_disponiveis.sql
+++ sql/prolog_queries/frota/checklist/offline/func_checklist_offline_get_colaboradores_disponiveis.sql
@@ -12,6 +12,14 @@ create or replace function func_checklist_offline_get_colaboradores_disponiveis(
     language plpgsql
 as
 $$
+declare
+    v_cod_empresa_unidade_filtro                   constant bigint  := (select cod_empresa
+                                                                        from unidade
+                                                                        where codigo = f_cod_unidade);
+    v_is_empresa_bloqueada_checklist_entre_unidade constant boolean := (
+            v_cod_empresa_unidade_filtro in (select cod_empresa
+                                             from checklist_diferentes_unidades_empresa_bloqueada
+                                             where cod_empresa = v_cod_empresa_unidade_filtro));
 begin
     return query
         with funcoes_modelos as (
@@ -19,24 +27,24 @@ begin
             from checklist_modelo cm
                      join checklist_modelo_funcao cmf
                           on cm.codigo = cmf.cod_checklist_modelo
-            where cm.cod_unidade = f_cod_unidade
+            where f_if(v_is_empresa_bloqueada_checklist_entre_unidade,
+                       cm.cod_unidade = f_cod_unidade,
+                       cm.cod_unidade in (select codigo from unidade where cod_empresa = v_cod_empresa_unidade_filtro))
               and cm.status_ativo = true
         )
 
-        select c.cod_unidade :: bigint      as cod_unidade_colaborador,
-               c.codigo                     as cod_colaborador,
-               c.nome :: text               as nome_colaborador,
-               lpad(c.cpf :: text, 11, '0') as cpf_colaborador,
-               c.data_nascimento :: date    as data_nascimento,
-               c.cod_funcao                 as cod_cargo_colaborador,
-               c.cod_permissao :: integer   as cod_permissao_colaborador
+        select distinct c.cod_unidade :: bigint      as cod_unidade_colaborador,
+                        c.codigo                     as cod_colaborador,
+                        c.nome :: text               as nome_colaborador,
+                        lpad(c.cpf :: text, 11, '0') as cpf_colaborador,
+                        c.data_nascimento :: date    as data_nascimento,
+                        c.cod_funcao                 as cod_cargo_colaborador,
+                        c.cod_permissao :: integer   as cod_permissao_colaborador
         from colaborador c
-                 inner join unidade unidade_filtro on unidade_filtro.codigo = f_cod_unidade
-        where (c.cod_unidade = unidade_filtro.codigo
-            and c.status_ativo
-            and c.cod_funcao in (select cod_funcao from funcoes_modelos))
-           or (c.cod_empresa not in (select cod_empresa from checklist_diferentes_unidades_empresa_bloqueada)
-            and c.cod_empresa = unidade_filtro.cod_empresa
-            and c.cod_funcao in (select cod_funcao from funcoes_modelos));
+        where c.status_ativo
+          and c.cod_funcao in (select cod_funcao from funcoes_modelos)
+          and f_if(v_is_empresa_bloqueada_checklist_entre_unidade,
+                   c.cod_unidade = f_cod_unidade,
+                   c.cod_empresa = v_cod_empresa_unidade_filtro);
 end;
 $$;
