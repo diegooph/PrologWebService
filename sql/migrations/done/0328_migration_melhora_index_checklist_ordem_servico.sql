453c5e0137c9df6039b6b71f6fe1c84543816490
Luiz Felipe <luiz.felipe_95@hotmail.com>
2021-06-21 17:05:58 -0300

    fix: remove concurrently para parar de ofender o Flyway
    nested exception is org.flywaydb.core.api.FlywayException: Detected both transactional and non-transactional statements within the same migration (even though mixed is false). Offending statement found at line 3: create index concurrently idx_checklist_ordem_servico_cod_unidade on checklist_ordem_servico_data (cod_unidade) include (codigo) [non-transactional]

--- sql/migrations/done/0328_migration_melhora_index_checklist_ordem_servico.sql
+++ sql/migrations/done/0328_migration_melhora_index_checklist_ordem_servico.sql
@@ -1,4 +1,4 @@
 -- O index antigo não incluia o código da ordem de serviço.
 drop index idx_checklist_ordem_servico_cod_unidade;
-create index concurrently idx_checklist_ordem_servico_cod_unidade
+create index idx_checklist_ordem_servico_cod_unidade
     on checklist_ordem_servico_data (cod_unidade) include (codigo);
