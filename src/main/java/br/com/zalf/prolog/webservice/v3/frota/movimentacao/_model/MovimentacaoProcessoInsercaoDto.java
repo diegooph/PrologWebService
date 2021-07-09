4b8752500292a28227e9fc1a94810372b2e1e1ea
thaistks <thaisksf@gmail.com>
2021-07-09 09:14:57 -0300

    feat(movimentacao): cria dto

---
+++ src/main/java/br/com/zalf/prolog/webservice/v3/frota/movimentacao/_model/MovimentacaoProcessoInsercaoDto.java
@@ -0,0 +1,9 @@
+package br.com.zalf.prolog.webservice.v3.frota.movimentacao._model;
+
+/**
+ * Created on 2021-07-08
+ *
+ * @author Thais Francisco (https://github.com/thaisksf)
+ */
+public class MovimentacaoProcessoInsercaoDto {
+}
