012fa869e3efb49d8ed9bf8592ebb9b3385b1e7f
Luiz Felipe <luiz.felipe_95@hotmail.com>
2021-08-06 16:17:36 -0300

    refactor(login): remove método antigo de login

--- src/main/java/br/com/zalf/prolog/webservice/autenticacao/AutenticacaoResource.java
+++ src/main/java/br/com/zalf/prolog/webservice/autenticacao/AutenticacaoResource.java
@@ -3,26 +3,23 @@ package br.com.zalf.prolog.webservice.autenticacao;
 import br.com.zalf.prolog.webservice.commons.network.Response;
 import br.com.zalf.prolog.webservice.commons.util.Log;
 import br.com.zalf.prolog.webservice.interceptors.auth.Secured;
+import org.jetbrains.annotations.NotNull;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 @Path("/v2/autenticacao")
 @Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
 @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
 public class AutenticacaoResource {
     private static final String TAG = AutenticacaoResource.class.getSimpleName();
-    @Deprecated
-    private static final SimpleDateFormat FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+    @NotNull
     private final AutenticacaoService service = new AutenticacaoService();
 
     @POST
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     public Autenticacao authenticate(@FormParam("cpf") final Long cpf,
                                      @FormParam("dataNascimento") final String dataNascimento) {
-        Log.d(TAG, "CPF: " + cpf + "\nData: " + dataNascimento);
         if (service.verifyIfUserExists(cpf, dataNascimento, true).isPresent()) {
             final Autenticacao autenticacao = service.createTokenByCpf(cpf);
             Log.d(TAG, autenticacao.getToken());
@@ -48,15 +45,5 @@ public class AutenticacaoResource {
     public boolean verifyTokenValidity() {
         // Verifica se um token é valido, retornando true, caso contrario retorna 401.
         return true;
-    }
-
-    @POST
-    @Path("/verifyLogin")
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    @Deprecated
-    public Autenticacao verifyLogin(@FormParam("cpf") final Long cpf,
-                                    @FormParam("dataNascimento") final long dataNascimento) {
-        Log.d(TAG, String.valueOf(cpf) + "data: " + String.valueOf(dataNascimento));
-        return authenticate(cpf, FORMAT.format(new Date(dataNascimento)));
     }
 }
