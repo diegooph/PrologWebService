fb56ff4461bee262f99607e825eff248b70e4632
Luiz Felipe <luiz.felipe_95@hotmail.com>
2021-08-09 22:06:28 -0300

    feat(core): adiciona mais dados à tabela unidade

---
+++ sql/migrations/done/0347_migration_add_dados_enderedo_unidade.sql
@@ -0,0 +1,6 @@
+alter table unidade
+    add column pais             text,
+    add column estado_provincia text,
+    add column cidade           text,
+    add column cep              text,
+    add column endereco         text;
