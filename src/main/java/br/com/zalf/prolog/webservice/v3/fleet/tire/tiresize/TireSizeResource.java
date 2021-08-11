b2671aca9a930414f5bccb408396d92dc95c96c5
gustavocnp95 <gustavocnavarro@gmail.com>
2021-08-11 12:11:51 -0300

    feat(pneu): implementa no resource chamada para o servic
    PL-3892

--- src/main/java/br/com/zalf/prolog/webservice/v3/fleet/tire/tiresize/TireSizeResource.java
+++ src/main/java/br/com/zalf/prolog/webservice/v3/fleet/tire/tiresize/TireSizeResource.java
@@ -2,15 +2,19 @@ package br.com.zalf.prolog.webservice.v3.fleet.tire.tiresize;
 
 import br.com.zalf.prolog.webservice.commons.network.SuccessResponse;
 import br.com.zalf.prolog.webservice.interceptors.ApiExposed;
+import br.com.zalf.prolog.webservice.interceptors.auth.ColaboradorAutenticado;
 import br.com.zalf.prolog.webservice.interceptors.auth.Secured;
 import br.com.zalf.prolog.webservice.interceptors.debug.ConsoleDebugLog;
 import br.com.zalf.prolog.webservice.permissao.pilares.Pilares;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.SecurityContext;
 
 @RestController
 @ConsoleDebugLog
@@ -18,12 +22,23 @@ import javax.ws.rs.Path;
 public class TireSizeResource {
     @NotNull
     public static final String RESOURCE_PATH = "/api/v3/tires/sizes";
+    @NotNull
+    private final TireSizeService service;
+
+    @Autowired
+    public TireSizeResource(@NotNull final TireSizeService service) {
+        this.service = service;
+    }
 
     @POST
     @ApiExposed
     @Secured(permissions = Pilares.Frota.Pneu.CADASTRAR)
-    public SuccessResponse insert(
-            @Valid final TireSizeCreation tireSizeCreation) throws Throwable {
-        return new SuccessResponse(1L, "Deu bom!");
+    public SuccessResponse insert(@Context final SecurityContext securityContext,
+                                  @Valid final TireSizeCreation tireSizeCreation) throws Throwable {
+
+        return new SuccessResponse(
+                service.insert(tireSizeCreation, (ColaboradorAutenticado) securityContext.getUserPrincipal()).getId(),
+                "Registro inserido com sucesso!"
+        );
     }
 }
