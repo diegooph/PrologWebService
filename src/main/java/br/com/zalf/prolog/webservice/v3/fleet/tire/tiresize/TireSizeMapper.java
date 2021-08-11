3124a33ff50f462102ff465e7a1a94c897881df2
gustavocnp95 <gustavocnavarro@gmail.com>
2021-08-11 12:10:04 -0300

    feat(pneu): implementa metodo toentity no mapper
    PL-3892

---
+++ src/main/java/br/com/zalf/prolog/webservice/v3/fleet/tire/tiresize/TireSizeMapper.java
@@ -0,0 +1,27 @@
+package br.com.zalf.prolog.webservice.v3.fleet.tire.tiresize;
+
+import br.com.zalf.prolog.webservice.interceptors.auth.ColaboradorAutenticado;
+import br.com.zalf.prolog.webservice.v3.fleet.tire._model.TireSizeEntity;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class TireSizeMapper {
+    public TireSizeEntity toEntity(@NotNull final TireSizeCreation tireSizeCreation,
+                                   @NotNull final ColaboradorAutenticado colaboradorAutenticado) {
+
+        return TireSizeEntity.builder()
+                .withHeight(tireSizeCreation.getHeight())
+                .withWidth(tireSizeCreation.getWidth())
+                .withRim(tireSizeCreation.getRim())
+                .withAdditionalId(tireSizeCreation.getAdditionalId())
+                .withIsActive(true)
+                .withCreatedAt(LocalDateTime.now())
+                .withCreatedByUserId(colaboradorAutenticado.getCodigo())
+                .withLastedUpdateAt(LocalDateTime.now())
+                .withLastedUpdateUserId(colaboradorAutenticado.getCodigo())
+                .build();
+    }
+}
