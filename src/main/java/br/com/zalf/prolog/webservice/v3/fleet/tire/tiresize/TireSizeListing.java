36def817314aa18138bc33e06d80d4a243dcb219
gustavocnp95 <gustavocnavarro@gmail.com>
2021-08-11 16:47:22 -0300

    feat(pneu): cria dto de listagem
    PL-3892

---
+++ src/main/java/br/com/zalf/prolog/webservice/v3/fleet/tire/tiresize/TireSizeListing.java
@@ -0,0 +1,18 @@
+package br.com.zalf.prolog.webservice.v3.fleet.tire.tiresize;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder(setterPrefix = "with")
+public class TireSizeListing {
+    private final Long id;
+    private Long companyId;
+    private Double height;
+    private Double width;
+    private Double rim;
+    private String additionalId;
+    private boolean isActive;
+}
