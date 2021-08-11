bc05c2ced71611bfc6c86a61b9472c91e4bb4c7e
gustavocnp95 <gustavocnavarro@gmail.com>
2021-08-11 17:03:20 -0300

    feat(pneu): implementa chamada pro service
    PL-3892

--- src/main/java/br/com/zalf/prolog/webservice/v3/fleet/tire/tiresize/TireSizeResource.java
+++ src/main/java/br/com/zalf/prolog/webservice/v3/fleet/tire/tiresize/TireSizeResource.java
@@ -1,20 +1,25 @@
 package br.com.zalf.prolog.webservice.v3.fleet.tire.tiresize;
 
 import br.com.zalf.prolog.webservice.commons.network.SuccessResponse;
+import br.com.zalf.prolog.webservice.commons.network.metadata.Required;
 import br.com.zalf.prolog.webservice.interceptors.ApiExposed;
 import br.com.zalf.prolog.webservice.interceptors.auth.ColaboradorAutenticado;
 import br.com.zalf.prolog.webservice.interceptors.auth.Secured;
 import br.com.zalf.prolog.webservice.interceptors.debug.ConsoleDebugLog;
 import br.com.zalf.prolog.webservice.permissao.pilares.Pilares;
+import br.com.zalf.prolog.webservice.v3.validation.CompanyId;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.SecurityContext;
+import java.util.List;
 
 @RestController
 @ConsoleDebugLog
@@ -24,10 +29,13 @@ public class TireSizeResource {
     public static final String RESOURCE_PATH = "/api/v3/tires/sizes";
     @NotNull
     private final TireSizeService service;
+    @NotNull
+    private final TireSizeMapper mapper;
 
     @Autowired
-    public TireSizeResource(@NotNull final TireSizeService service) {
+    public TireSizeResource(@NotNull final TireSizeService service, @NotNull final TireSizeMapper mapper) {
         this.service = service;
+        this.mapper = mapper;
     }
 
     @POST
@@ -40,5 +48,12 @@ public class TireSizeResource {
                 service.insert(tireSizeCreation, (ColaboradorAutenticado) securityContext.getUserPrincipal()).getId(),
                 "Registro inserido com sucesso!"
         );
+    }
+
+    @GET
+    @ApiExposed
+    @Secured(permissions = {Pilares.Frota.Pneu.CADASTRAR, Pilares.Frota.Pneu.ALTERAR})
+    public List<TireSizeListing> getAll(@NotNull @CompanyId @QueryParam("companyId") @Required final Long companyId) throws Throwable {
+        return mapper.toDto(service.getAll(companyId));
     }
 }
