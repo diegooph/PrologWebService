4e67e3969adae6ea45f07180fe5bacc6edf1ecbc
gustavocnp95 <gustavocnavarro@gmail.com>
2021-08-11 16:49:04 -0300

    feat(pneu): implementa mapeamento
    PL-3892

--- src/main/java/br/com/zalf/prolog/webservice/v3/fleet/tire/tiresize/TireSizeMapper.java
+++ src/main/java/br/com/zalf/prolog/webservice/v3/fleet/tire/tiresize/TireSizeMapper.java
@@ -6,6 +6,8 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class TireSizeMapper {
@@ -25,5 +27,21 @@ public class TireSizeMapper {
                 .withLastedUpdateUserId(colaboradorAutenticado.getCodigo())
                 .withRegisterOrigin("WS")
                 .build();
+    }
+
+    public List<TireSizeListing> toDto(@NotNull final List<TireSizeEntity> tireSizesEntities) {
+        return tireSizesEntities.stream().map(this::toDto).collect(Collectors.toList());
+    }
+
+    @NotNull
+    public TireSizeListing toDto(@NotNull final TireSizeEntity tireSizeEntity) {
+        return TireSizeListing.builder()
+                .withId(tireSizeEntity.getId())
+                .withCompanyId(tireSizeEntity.getCompanyId())
+                .withHeight(tireSizeEntity.getHeight())
+                .withWidth(tireSizeEntity.getWidth())
+                .withAdditionalId(tireSizeEntity.getAdditionalId())
+                .withRim(tireSizeEntity.getRim())
+                .withIsActive(tireSizeEntity.isActive())
     }
 }
