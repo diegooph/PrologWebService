d36229cca93fae6a02d45b191a924a0b85ea0d5a
gustavocnp95 <gustavocnavarro@gmail.com>
2021-08-11 17:30:57 -0300

    feat(pneu): implementa custom query para considerar inativos tbm
    PL-3892

--- src/main/java/br/com/zalf/prolog/webservice/v3/fleet/tire/tiresize/TireSizeDao.java
+++ src/main/java/br/com/zalf/prolog/webservice/v3/fleet/tire/tiresize/TireSizeDao.java
@@ -3,11 +3,15 @@ package br.com.zalf.prolog.webservice.v3.fleet.tire.tiresize;
 import br.com.zalf.prolog.webservice.v3.fleet.tire._model.TireSizeEntity;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface TireSizeDao extends JpaRepository<TireSizeEntity, Long> {
-    List<TireSizeEntity> findAllByCompanyIdAndActiveIsTrue(@NotNull final Long companyId);
+    @Query("select tse from TireSizeEntity tse " +
+            "where tse.companyId = :companyId " +
+            "and (:includeInactive = true or tse.active = true)")
+    List<TireSizeEntity> findAll(@NotNull final Long companyId, @NotNull final Boolean includeInactive);
 }
