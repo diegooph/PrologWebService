eadd7f22a5b00452ba4a55fa40cfa4e101c6d597
gustavocnp95 <gustavocnavarro@gmail.com>
2021-08-11 16:47:09 -0300

    feat(pneu): cria metodo na dao
    PL-3892

--- src/main/java/br/com/zalf/prolog/webservice/v3/fleet/tire/tiresize/TireSizeDao.java
+++ src/main/java/br/com/zalf/prolog/webservice/v3/fleet/tire/tiresize/TireSizeDao.java
@@ -1,9 +1,13 @@
 package br.com.zalf.prolog.webservice.v3.fleet.tire.tiresize;
 
 import br.com.zalf.prolog.webservice.v3.fleet.tire._model.TireSizeEntity;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TireSizeDao extends JpaRepository<TireSizeEntity, Long> {
+    List<TireSizeEntity> findAllByCompanyIdAndActiveIsTrue(@NotNull final Long companyId);
 }
