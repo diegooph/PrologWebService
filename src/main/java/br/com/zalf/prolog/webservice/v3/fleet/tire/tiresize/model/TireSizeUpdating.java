8e4b2d35049a1efe5a32bfc1c94010924f034b74
gustavocnp95 <gustavocnavarro@gmail.com>
2021-08-13 09:13:15 -0300

    feat(pneu): cria model para receber infos para update de dimensão
    PL-3892

---
+++ src/main/java/br/com/zalf/prolog/webservice/v3/fleet/tire/tiresize/model/TireSizeUpdating.java
@@ -0,0 +1,24 @@
+package br.com.zalf.prolog.webservice.v3.fleet.tire.tiresize.model;
+
+import br.com.zalf.prolog.webservice.v3.validation.CompanyId;
+import lombok.Data;
+import org.jetbrains.annotations.Nullable;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+public class TireSizeUpdating {
+    @CompanyId
+    @NotNull(message = "O código da empresa é obrigatório!")
+    private final Long companyId;
+    @NotNull(message = "O código da dimensão é obrigatório!")
+    private final Long tireSizeId;
+    @NotNull(message = "A altura da dimensão é obrigatória!")
+    private final Double tireSizeHeight;
+    @NotNull(message = "A largura da dimensão é obrigatória!")
+    private final Double tireSizeWidth;
+    @NotNull(message = "O aro da dimensão é obrigatório!")
+    private final Double tireSizeRim;
+    @Nullable
+    private final String additionalId;
+}
