aa047117efccab624687c8898dc4f2dcbebb7e3c
gustavocnp95 <gustavocnavarro@gmail.com>
2021-08-13 11:47:55 -0300

    feat(pneu): cria metodo com companyid no where
    PL-3892

--- src/main/java/br/com/zalf/prolog/webservice/v3/fleet/tire/tiresize/TireSizeDao.java
+++ src/main/java/br/com/zalf/prolog/webservice/v3/fleet/tire/tiresize/TireSizeDao.java
@@ -9,7 +9,9 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TireSizeDao extends JpaRepository<TireSizeEntity, Long> {
@@ -18,13 +20,17 @@ public interface TireSizeDao extends JpaRepository<TireSizeEntity, Long> {
             "and (:statusActive is null or tse.active = :statusActive)")
     List<TireSizeEntity> findAll(@NotNull final Long companyId, @Nullable final Boolean statusActive);
 
+    Optional<TireSizeEntity> findByCompanyIdAndId(@NotNull final Long companyId, @NotNull final Long id);
+
     @Modifying
     @Query("update TireSizeEntity tse set tse.active = :isActive, " +
-            "tse.lastedUpdateUser = :userUpdating " +
+            "tse.lastedUpdateUser = :userUpdating, " +
+            "tse.lastedUpdateAt = :dateTimeUpdated " +
             "where tse.companyId = :companyId " +
             "and tse.id = :tireSizeId")
     int updateStatus(@NotNull final Long companyId,
                      @NotNull final Long tireSizeId,
                      @NotNull final Boolean isActive,
-                     @NotNull final UserEntity userUpdating);
+                     @NotNull final UserEntity userUpdating,
+                     @NotNull final LocalDateTime dateTimeUpdated);
 }
