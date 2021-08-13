5cdad966976fd2560fe88f259c22d65d4563cd1f
gustavocnp95 <gustavocnavarro@gmail.com>
2021-08-13 09:18:13 -0300

    feat(pneu): cria model para retorno do update
    PL-3892

---
+++ src/main/java/br/com/zalf/prolog/webservice/v3/fleet/tire/tiresize/model/TireSizeUpdated.java
@@ -0,0 +1,23 @@
+package br.com.zalf.prolog.webservice.v3.fleet.tire.tiresize.model;
+
+import lombok.Data;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@Data
+public class TireSizeUpdated {
+    @NotNull
+    private final Long companyId;
+    @NotNull
+    private final Long tireSizeId;
+    @NotNull
+    private final Double tireSizeHeight;
+    @NotNull
+    private final Double tireSizeWidth;
+    @NotNull
+    private final Double tireSizeRim;
+    @Nullable
+    private final String additionalId;
+    @NotNull
+    private final Boolean active;
+}
