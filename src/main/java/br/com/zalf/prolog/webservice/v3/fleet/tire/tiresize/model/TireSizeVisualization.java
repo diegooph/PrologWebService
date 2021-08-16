cabb43c9b9568b2ea6c916115453fbf4133b4d34
gustavocnp95 <gustavocnavarro@gmail.com>
2021-08-16 12:14:33 -0300

    feat(pneu): cria model para get de dimensão por id
    PL-3892

---
+++ src/main/java/br/com/zalf/prolog/webservice/v3/fleet/tire/tiresize/model/TireSizeVisualization.java
@@ -0,0 +1,25 @@
+package br.com.zalf.prolog.webservice.v3.fleet.tire.tiresize.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder(setterPrefix = "with")
+public class TireSizeVisualization {
+    private final Long id;
+    private final Long companyId;
+    private final Double height;
+    private final Double width;
+    private final Double rim;
+    private final String additionalId;
+    private final boolean isActive;
+    private final LocalDateTime createdAt;
+    private final Long createdAtUserId;
+    private final String createdAtUserName;
+    private final LocalDateTime lastedUpdateAt;
+    private final Long lastedUpdateUserId;
+    private final String lastedUpdateUserName;
+    private final String registerOrigin;
+}
