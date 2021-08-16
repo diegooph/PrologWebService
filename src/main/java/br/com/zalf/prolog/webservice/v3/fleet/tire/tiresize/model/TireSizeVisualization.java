e04d7b2a3c3e8a77a06c8e0ac8e3fea30f386328
gustavocnp95 <gustavocnavarro@gmail.com>
2021-08-16 14:12:03 -0300

    feat(pneu): adiciona annotations no dto
    PL-3892

--- src/main/java/br/com/zalf/prolog/webservice/v3/fleet/tire/tiresize/model/TireSizeVisualization.java
+++ src/main/java/br/com/zalf/prolog/webservice/v3/fleet/tire/tiresize/model/TireSizeVisualization.java
@@ -2,23 +2,37 @@ package br.com.zalf.prolog.webservice.v3.fleet.tire.tiresize.model;
 
 import lombok.Builder;
 import lombok.Data;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder(setterPrefix = "with")
 public class TireSizeVisualization {
+    @NotNull
     private final Long id;
+    @NotNull
     private final Double height;
+    @NotNull
     private final Double width;
+    @NotNull
     private final Double rim;
+    @NotNull
     private final String additionalId;
     private final boolean active;
+    @NotNull
     private final LocalDateTime createdAt;
+    @Nullable
     private final Long createdAtUserId;
+    @Nullable
     private final String createdAtUserName;
+    @Nullable
     private final LocalDateTime lastedUpdateAt;
+    @Nullable
     private final Long lastedUpdateUserId;
+    @Nullable
     private final String lastedUpdateUserName;
+    @Nullable
     private final String registerOrigin;
 }
