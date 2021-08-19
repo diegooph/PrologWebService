b6a862a13a05abe5b59b87baa406eabb0509d3e8
gustavocnp95 <gustavocnavarro@gmail.com>
2021-08-19 12:09:17 -0300

    feat(pneu): cria teste de erro para listagem de dimensoes
    PL-3892

--- src/test/java/test/br/com/zalf/prolog/webservice/v3/fleet/tire/tiresize/TireSizeApiClient.java
+++ src/test/java/test/br/com/zalf/prolog/webservice/v3/fleet/tire/tiresize/TireSizeApiClient.java
@@ -1,15 +1,24 @@
 package test.br.com.zalf.prolog.webservice.v3.fleet.tire.tiresize;
 
 import br.com.zalf.prolog.webservice.commons.network.SuccessResponse;
+import br.com.zalf.prolog.webservice.errorhandling.exception.ProLogException;
+import br.com.zalf.prolog.webservice.errorhandling.sql.ClientSideErrorException;
 import br.com.zalf.prolog.webservice.v3.fleet.tire.tiresize.TireSizeResource;
 import br.com.zalf.prolog.webservice.v3.fleet.tire.tiresize.model.TireSizeCreateDto;
+import br.com.zalf.prolog.webservice.v3.fleet.tire.tiresize.model.TireSizeDto;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestComponent;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 @TestComponent
 public class TireSizeApiClient {
@@ -20,5 +29,37 @@ public class TireSizeApiClient {
     @NotNull
     public ResponseEntity<SuccessResponse> insert(@NotNull final TireSizeCreateDto dto) {
         return restTemplate.postForEntity(URI.create(TireSizeResource.RESOURCE_PATH), dto, SuccessResponse.class);
+    }
+
+    @NotNull
+    public ResponseEntity<List<TireSizeDto>> getAllTireSizes(final long companyId,
+                                                             final boolean tireSizeStatus) {
+        final UriComponents components = UriComponentsBuilder
+                .fromPath(TireSizeResource.RESOURCE_PATH)
+                .queryParam("companyId", companyId)
+                .queryParam("status", tireSizeStatus)
+                .build();
+        final RequestEntity<Void> reqEntity = RequestEntity
+                .get(components.toUri())
+                .accept(MediaType.APPLICATION_JSON)
+                .build();
+        return restTemplate.exchange(reqEntity, new ParameterizedTypeReference<List<TireSizeDto>>() {
+        });
+    }
+
+    @NotNull
+    public ResponseEntity<ClientSideErrorException> getAllTireSizesWithWrongCompanyId(final long companyId,
+                                                                                      final boolean tireSizeStatus) {
+        final UriComponents components = UriComponentsBuilder
+                .fromPath(TireSizeResource.RESOURCE_PATH)
+                .queryParam("companyId", companyId)
+                .queryParam("status", tireSizeStatus)
+                .build();
+        final RequestEntity<Void> reqEntity = RequestEntity
+                .get(components.toUri())
+                .accept(MediaType.APPLICATION_JSON)
+                .build();
+        return restTemplate.exchange(reqEntity, new ParameterizedTypeReference<ClientSideErrorException>() {
+        });
     }
 }
--- src/test/java/test/br/com/zalf/prolog/webservice/v3/fleet/tire/tiresize/TireSizeIt.java
+++ src/test/java/test/br/com/zalf/prolog/webservice/v3/fleet/tire/tiresize/TireSizeIt.java
@@ -1,13 +1,18 @@
 package test.br.com.zalf.prolog.webservice.v3.fleet.tire.tiresize;
 
 import br.com.zalf.prolog.webservice.commons.network.SuccessResponse;
+import br.com.zalf.prolog.webservice.errorhandling.exception.ProLogException;
+import br.com.zalf.prolog.webservice.errorhandling.sql.ClientSideErrorException;
 import br.com.zalf.prolog.webservice.v3.fleet.tire.tiresize.model.TireSizeCreateDto;
+import br.com.zalf.prolog.webservice.v3.fleet.tire.tiresize.model.TireSizeDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import test.br.com.zalf.prolog.webservice.IntegrationTest;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -41,6 +46,22 @@ public class TireSizeIt extends IntegrationTest {
                                 .withWidth(2.0)
                                 .withAdditionalId("Teste cod auxiliar")
                                 .build());
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    @DisplayName("given correct query params, return a list of tire sizes")
+    void givenCorrectTireToList_ThenReturnTireSizeList() {
+        final ResponseEntity<List<TireSizeDto>> response =
+                client.getAllTireSizes(3, true);
+        assertThat(response.getBody()).isNotEmpty();
+    }
+
+    @Test
+    @DisplayName("given wrong company id, return forbidden")
+    void givenCorrectTireToList_ThenForbbiden() {
+        final ResponseEntity<ClientSideErrorException> response =
+                client.getAllTireSizesWithWrongCompanyId(10, true);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
     }
 }
