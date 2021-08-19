d61eb1a2d022fae682e303cae8560c72054792c6
gustavocnp95 <gustavocnavarro@gmail.com>
2021-08-19 10:13:17 -0300

    feat(pneu): modifica dto para usar build
    PL-3892

--- src/main/java/br/com/zalf/prolog/webservice/v3/fleet/tire/tiresize/model/TireSizeCreateDto.java
+++ src/main/java/br/com/zalf/prolog/webservice/v3/fleet/tire/tiresize/model/TireSizeCreateDto.java
@@ -1,11 +1,14 @@
 package br.com.zalf.prolog.webservice.v3.fleet.tire.tiresize.model;
 
 import br.com.zalf.prolog.webservice.v3.validation.CompanyId;
+import lombok.Builder;
 import lombok.Data;
+import lombok.Value;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 @Data
+@Builder(setterPrefix = "with")
 public class TireSizeCreateDto {
     @CompanyId
     @NotNull
