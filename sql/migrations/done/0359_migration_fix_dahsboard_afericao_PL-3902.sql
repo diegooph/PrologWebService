8022cd19ac63a94d19b55c7d23b6413d9cda4d30
Diego Luiz <diegooph@gmail.com>
2021-08-23 15:54:05 -0300

    removendo teste e adicionando migration

--- sql/migrations/done/0359_migration_fix_dahsboard_afericao_PL-3902.sql
+++ sql/migrations/done/0359_migration_fix_dahsboard_afericao_PL-3902.sql
@@ -1,3 +1,5 @@
+drop function if exists func_pneu_relatorio_status_placas_afericao(bigint[], timestamp with time zone);
+
 create or replace function func_pneu_relatorio_status_placas_afericao(f_cod_unidades bigint[],
                                                                       f_data_hora_atual_utc timestamp with time zone)
     returns table
@@ -10,13 +12,13 @@ create or replace function func_pneu_relatorio_status_placas_afericao(f_cod_unid
 as
 $$
 declare
-    qtd_placas_ativas bigint := (select count(v.placa)
+qtd_placas_ativas bigint := (select count(v.placa)
                                  from veiculo v
                                  where v.cod_unidade = any (f_cod_unidades)
                                    and v.status_ativo = true);
 begin
-    return query
-        with qtd_placas_vencidas as (
+return query
+    with qtd_placas_vencidas as (
             select (select count(placa)
                     from func_afericao_relatorio_qtd_dias_placas_vencidas(f_cod_unidades,
                                                                           f_data_hora_atual_utc))
@@ -28,9 +30,9 @@ begin
         )
 
 
-        select qpv.qtd_vencidas                     as qtd_vencidas,
-               qtd_placas_ativas - qpv.qtd_vencidas - nunca_aferidas as qtd_prazo,
-               nunca_aferidas      as qtd_nunca_aferidas
-        from qtd_placas_vencidas qpv;
+select qpv.qtd_vencidas                     as qtd_vencidas,
+       qtd_placas_ativas - qpv.qtd_vencidas - nunca_aferidas as qtd_prazo,
+       nunca_aferidas      as qtd_nunca_aferidas
+from qtd_placas_vencidas qpv;
 end;
 $$;
